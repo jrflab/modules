b2f68959df8be2b77d1edddce49da25df67ae085
raylim <raylim@mm.st>
2013-10-15 16:38:18 -0400

    split sorting

--- processBamMD5.mk
+++ processBamMD5.mk
@@ -1,18 +1,52 @@
 # various bam processing steps
 ##### MAKE INCLUDES #####
+include ~/share/modules/Makefile.inc
 include ~/share/modules/gatk.inc
 
-SAMPLE_FILE ?= samples.txt
-SAMPLES ?= $(shell cat $(SAMPLE_FILE))
 
+LOGDIR ?= log/process_bam.$(NOW)
+
+DUP_TYPE ?= rmdup
+NO_RECAL ?= false
+NO_REALN ?= false
 SPLIT_CHR ?= true
+SPLIT_FASTQ ?= false
+SPLIT_SORT ?= false
+NUM_SORT_SPLITS ?= 50
+SORT_SPLIT_SEQ = $(shell seq 0 $$(($(NUM_SORT_SPLITS) - 1)))
 
 # not primary alignment
 # read fails platform/vendor quality checks
 BAM_FILTER_FLAGS ?= 768
 
+
 .DELETE_ON_ERROR:
 .SECONDARY: 
+
+NO_ALIGN ?= false
+ifeq ($(NO_ALIGN),true)
+BAM_SUFFIX := sorted.filtered
+ifeq ($(NO_REALN),false)
+BAM_SUFFIX := $(BAM_SUFFIX).realn
+endif
+
+ifeq ($(DUP_TYPE),rmdup)
+BAM_SUFFIX := $(BAM_SUFFIX).rmdup
+else ifeq ($(DUP_TYPE),markdup) 
+BAM_SUFFIX := $(BAM_SUFFIX).markdup
+endif
+
+ifeq ($(NO_RECAL),false)
+BAM_SUFFIX := $(BAM_SUFFIX).recal
+endif
+
+BAM_SUFFIX := $(BAM_SUFFIX).bam
+BAMS = $(foreach sample,$(SAMPLES),bam/$(sample).bam)
+processed_bams : $(addsuffix .md5,$(BAMS)) $(addsuffix .bai,$(BAMS))
+
+bam/%.bam.md5 : unprocessed_bam/%.$(BAM_SUFFIX).md5
+	$(INIT) cp $< $@ && ln -f $(<:.md5=) $(@:.md5=)
+endif
 
 # indices
 # if bam file is a symlink, need to create a symlink to index
@@ -21,6 +55,9 @@ BAM_FILTER_FLAGS ?= 768
 
 %.bai : %.bam.md5
 	$(call LSCRIPT_MEM,4G,8G,"$(CHECK_MD5) $(SAMTOOLS) index $(<M) $@")
+
+%.bam.md5 : %.bam
+	$(INIT) $(MD5)
 
 # sam to bam
 #%.bam : %.sam
@@ -41,12 +78,29 @@ BAM_FILTER_FLAGS ?= 768
 %.recal.bam.md5 : %.bam.md5 %.recal_report.grp
 	$(call LSCRIPT_MEM,11G,15G,"$(CHECK_MD5) $(call GATK_MEM,10G) -T PrintReads -R $(REF_FASTA) -I $(<M) -BQSR $(word 2,$^) -o $(@:.md5=) &> $(LOG) && $(MD5) && $(RM) $< $(<:.md5=)")
 
-# sort only if necessary
-#%.sorted.bam : %.bam
-#	$(call INIT_MEM,20G,25G) if ! $(SAMTOOLS) view -H $< | grep -q 'SO:coordinate' -; then $(call SORT_SAM_MEM,19G) I=$< O=$@ SO=coordinate &> $(LOG); else ln -v $< $@; fi && $(RM) $<
+#sort only if necessary
+#%.sorted.bam.md5 : %.bam.md5
+#	 if ! $(SAMTOOLS) view -H $(<M) | grep -q 'SO:coordinate' -; then $(call LSCRIPT_MEM,20G,25G,"$(CHECK_MD5) $(call SORT_SAM_MEM,19G) I=$(<M) O=$(@M) SO=coordinate"); else cp $< $@ && ln -v $(<M) $(@M); fi && $(RM) $<
 
+#define split-sort
+#%.sorted_split_$1.bam.md5 : %.bam.md5
+#$$(call LSCRIPT_MEM,17G,19G,"$$(CHECK_MD5) cat <($$(SAMTOOLS) view -H $$(<M)) <($$(SAMTOOLS) view $$(<M) | awk 'NR % $$(NUM_SORT_SPLITS) == $1') | $$(call SORT_SAM_MEM,15G,3000000) I=/dev/stdin O=$$(@M) SO=coordinate && $$(MD5)")
+#endef
+
+ifeq ($(SPLIT_SORT),true)
+define split-sort
+%.sorted_split_$1.bam.md5 : %.bam.md5
+	$$(call LSCRIPT_MEM,7G,10G,"$$(CHECK_MD5) cat <($$(SAMTOOLS) view -H $$(<M)) <($$(SAMTOOLS) view $$(<M) | awk 'NR % $$(NUM_SORT_SPLITS) == $1') | $$(SAMTOOLS) view -Sub - | $$(SAMTOOLS) sort -m 4G - $$(@:.bam.md5=) && $$(MD5)")
+endef
+$(foreach i,$(SORT_SPLIT_SEQ),$(eval $(call split-sort,$i)))
+
+%.sorted.bam.md5 : $(foreach i,$(SORT_SPLIT_SEQ),%.sorted_split_$i.bam.md5)
+	$(call LSCRIPT_MEM,8G,10G,"$(CHECK_MD5) $(SAMTOOLS) merge -h <($(SAMTOOLS) view -H $(<M)) $(@M) $(^M) && $(MD5) && $(RM) $^ $(^M) $(@:.sorted.bam=) $(@:.sorted.bam.md5=)")
+else
 %.sorted.bam.md5 : %.bam.md5
-	$(call LSCRIPT_MEM,9G,14G,"$(CHECK_MD5) $(call SORT_SAM_MEM,8G) I=$(<:.md5=) O=$(@:.md5=) SO=coordinate &> $(LOG) && $(MD5) && $(RM) $< $(<:.md5=)")
+	$(call LSCRIPT_MEM,20G,25G,"$(CHECK_MD5) $(call SORT_SAM_MEM,19G,4500000) I=$(<:.md5=) O=$(@:.md5=) SO=coordinate &> $(LOG) && $(MD5) && $(RM) $< $(<:.md5=)")
+endif
+
 
 # reorder only if necessary
 #%.reord.bam : %.bam
