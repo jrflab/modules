843b2025fed027d89521bad480a40753fc912efd
raylim <raylim@mm.st>
2013-12-12 19:19:22 -0500

    pyrohmm

---
+++ pyroHMMVar.mk
@@ -0,0 +1,19 @@
+# run pyrohmmvar: realignment-based variant calling method for 454 and ion torrent
+
+include ~/share/modules/Makefile.inc
+
+LOGDIR = log/pyrohmm.$(NOW)
+
+PYROHMMVAR = $(HOME)/share/usr/bin/pyrohmmvar
+PYROHMMVAR_MODEL = $(HOME)/share/reference/pyrohmm_parameter_config
+
+.SECONDARY:
+.DELETE_ON_ERROR: 
+.PHONY: all
+
+all : $(foreach sample,$(SAMPLES),pyrohmm/tables/$(sample).txt)
+
+pyrohmm/tables/%.txt : bam/%.bam bam/%.bam.bai
+	$(call LSCRIPT_MEM,6G,10G,"$(PYROHMMVAR) -b $< -f $(REF_FASTA) -m $(PYROHMMVAR_MODEL) > $@")
+
+
