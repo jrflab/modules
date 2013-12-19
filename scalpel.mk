6cc87c832500d04d71600a30cc09289facf8accf
raylim <raylim@mm.st>
2013-12-19 11:35:14 -0500

    outdir

--- scalpel.mk
+++ scalpel.mk
@@ -22,7 +22,7 @@ all : $(foreach pair,$(SAMPLE_PAIRS),scalpel/$(pair)/somatic.indel.txt)
 
 define scalpel-tumor-normal
 scalpel/$1_$2/somatic.indel.txt : bam/$1.bam bam/$2.bam
-	$$(call LSCRIPT_MEM,8G,10G,"$$(SCALPEL) --somatic --tumor $$(word 1,$$^) --normal $$(word 2,$$^) $$(SCALPEL_OPTS)")
+	$$(call LSCRIPT_MEM,8G,10G,"$$(SCALPEL) --somatic --tumor $$(word 1,$$^) --normal $$(word 2,$$^) $$(SCALPEL_OPTS) --dir $(@D)")
 endef
 $(foreach i,$(SETS_SEQ),\
 	$(foreach tumor,$(call get_tumors,$(set.$i)), \
