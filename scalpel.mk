22dcfa2fd4e4a7e404d79948869a4f56dfdceed5
raylim <raylim@mm.st>
2013-12-20 19:06:44 -0500

    scalpel 2 vcf

--- scalpel.mk
+++ scalpel.mk
@@ -4,6 +4,7 @@
 include ~/share/modules/Makefile.inc
 
 LOGDIR = log/scalpel.$(NOW)
+
 SCALPEL = $(HOME)/share/usr/scalpel-0.1.1/scalpel
 SCALPEL_OPTS = --ref $(REF_FASTA)
 ifeq ($(EXOME),true)
@@ -13,18 +14,33 @@ ifdef TARGETS_FILE
 SCALPEL_OPTS += --bed $(TARGETS_FILE)
 endif
 
+SCALPEL2VCF = $(PERL) $(HOME)/share/scripts/scapelToVcf.pl
+
+FILTER_SUFFIX := dp_ft.dbsnp.nsfp.chasm.fathmm
 
 .SECONDARY:
 .DELETE_ON_ERROR:
 .PHONY: all
 
-all : $(foreach pair,$(SAMPLE_PAIRS),scalpel/$(pair)/somatic.indel.txt)
+
+all : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).scalpel.$(FILTER_SUFFIX).vcf)
 
 define scalpel-tumor-normal
-scalpel/$1_$2/somatic.indel.txt : bam/$1.bam bam/$2.bam
+scalpel/$1_$2/somatic.5x.indel.txt : bam/$1.bam bam/$2.bam
 	$$(call LSCRIPT_MEM,8G,10G,"$$(SCALPEL) --somatic --tumor $$(word 1,$$^) --normal $$(word 2,$$^) $$(SCALPEL_OPTS) --dir $$(@D)")
 endef
 $(foreach i,$(SETS_SEQ),\
 	$(foreach tumor,$(call get_tumors,$(set.$i)), \
 		$(eval $(call scalpel-tumor-normal,$(tumor),$(call get_normal,$(set.$i))))))
+
+define scalpel2vcf-tumor-normal
+vcf/$1_$2.scalpel.vcf : scalpel/$1_$2/somatic.5x.indel.txt
+	$$(INIT) $$(SCALPEL2VCF) -f $$(REF_FASTA) -t $1 -n $2 < $$< > $$@
+endef
+$(foreach i,$(SETS_SEQ),\
+	$(foreach tumor,$(call get_tumors,$(set.$i)), \
+		$(eval $(call scalpel2vcf-tumor-normal,$(tumor),$(call get_normal,$(set.$i))))))
+
+include ~/share/modules/vcftools.mk
+
 
