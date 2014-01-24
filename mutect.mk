6efd32ec2addb74022f8dfff1165bbc8134196fd
raylim <raylim@mm.st>
2014-01-24 15:59:58 -0500

    pass filter

--- mutect.mk
+++ mutect.mk
@@ -32,11 +32,11 @@ VPATH ?= bam
 all : mutect_vcfs mutect_tables ext_output mut_report
 
 
-FILTER_SUFFIX := som_ad_ft.dbsnp.nsfp.chasm.fathmm
+FILTER_SUFFIX := som_ad_ft.pass.dbsnp.nsfp.chasm.fathmm
 EFF_TYPES = silent missense nonsilent_cds nonsilent
 ANN_TYPES = eff # annotated
 VCF_SUFFIXES = $(foreach ann,$(ANN_TYPES),mutect.$(FILTER_SUFFIX).$(ann))
-TABLE_SUFFIXES = $(foreach eff,$(EFF_TYPES),$(foreach ann,$(ANN_TYPES),mutect.$(FILTER_SUFFIX).$(ann).tab.$(eff).pass.novel))
+TABLE_SUFFIXES = $(foreach eff,$(EFF_TYPES),$(foreach ann,$(ANN_TYPES),mutect.$(FILTER_SUFFIX).$(ann).tab.$(eff).novel))
 
 #VCFS = $(foreach suff,$(VCF_SUFFIXES),$(foreach tumor,$(TUMOR_SAMPLES),vcf/$(tumor)_$(normal_lookup.$(tumor)).$(suff).vcf))
 VCFS = $(foreach suff,$(VCF_SUFFIXES),$(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).$(suff).vcf))
