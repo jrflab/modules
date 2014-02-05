20ec28be5cc464a04894f39f6b9fc7bf622c3dd1
raylim <raylim@mm.st>
2014-02-05 17:08:47 -0500

    remove intermediates

--- defuse.mk
+++ defuse.mk
@@ -37,10 +37,10 @@ endif
 tables : $(foreach sample,$(SAMPLES),defuse/tables/$(sample).defuse_results.txt)
 
 defuse/%.defuse_timestamp : fastq/%.1.fastq.gz.md5 fastq/%.2.fastq.gz.md5
-	$(INIT) $(CHECK_MD5) $(DEFUSE) -c $(DEFUSE_CONFIG_FILE) -1 $(word 1,$(^M)) -2 $(word 2,$(^M)) -o $(@D)/$* $(DEFUSE_OPTS) &> $(LOG) && touch $@ && $(RM) -r defuse/$*/jobs
+	$(INIT) $(CHECK_MD5) $(DEFUSE) -c $(DEFUSE_CONFIG_FILE) -1 $(word 1,$(^M)) -2 $(word 2,$(^M)) -o $(@D)/$* $(DEFUSE_OPTS) &> $(LOG) && touch $@
 
 defuse/tables/%.defuse_results.txt : defuse/%.defuse_timestamp
-	$(INIT) $(PERL) $(DEFUSE_FILTER) defuse/$*/results.filtered.tsv > $@ 2> $(LOG)
+	$(INIT) $(PERL) $(DEFUSE_FILTER) defuse/$*/results.filtered.tsv > $@ 2> $(LOG) && rm -r defuse/$*
 
 defuse/tables/all.defuse_results.txt : $(foreach sample,$(SAMPLES),defuse/tables/$(sample).defuse_results.txt)
 	$(INIT) head -1 $< > $@ && for x in $^; do sed '1d' $$x >> $@; done
