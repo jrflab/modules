a7cfc67a61f5cb68d474433207fe683be4d3a2b5
raylim <raylim@mm.st>
2014-02-10 13:55:04 -0500

    all tables

--- soapFuse.mk
+++ soapFuse.mk
@@ -10,13 +10,13 @@ SOAPFUSE = $(HOME)/usr/SOAPfuse-v1.26/SOAPfuse-RUN.pl
 SOAPFUSE_CONFIG = $(HOME)/share/usr/SOAPfuse-v1.26/config/config.txt
 PREPARE_SOAPFUSE = $(HOME)/share/scripts/prepareSoapFuse.pl
 
-SOAPFUSE_SAMPLES_FILE = samples.soapfuse.txt
-
 .SECONDARY:
 .DELETE_ON_ERROR:
-.PHONY: all
+.PHONY: all sample_tables soapfuse
 
-all : $(foreach sample,$(SAMPLES),soapfuse/$(sample).timestamp)
+all : soapfuse/tables/all.sfuse.txt soapfuse/tables/all.sfuse.isoforms.txt
+sample_tables : $(foreach sample,$(SAMPLES),soapfuse/tables/$(sample).sfuse.txt soapfuse/tables/$(sample).sfuse.isoforms.txt)
+soapfuse : $(foreach sample,$(SAMPLES),soapfuse/$(sample).timestamp)
 
 soapfuse/%.timestamp : soapfuse/sample_lists/%.txt
 	$(call LSCRIPT_NAMED_PARALLEL_MEM,$*_soapfuse,4,2G,2.5G,"$(SOAPFUSE) -c $(SOAPFUSE_CONFIG) -fd $(@D) -l $< -o $(@D)/$* && touch $@")
@@ -29,3 +29,11 @@ soapfuse/sample_lists/%.txt : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
 	READLEN=`zcat $< | head -2 | sed 1d | wc -c`; \
 	echo -e "$*\t$*\t$*\t$$READLEN" > $@
 
+soapfuse/tables/%.sfuse.txt : soapfuse/%.timestamp
+	$(INIT) ln -t $(@D) soapfuse/$*/final_fusion_genes/$*/$*.final.Fusion.specific.for.genes
+
+soapfuse/tables/%.sfuse.isoforms.txt : soapfuse/%.timestamp
+	$(INIT) ln -t $(@D) soapfuse/$*/final_fusion_genes/$*/$*.final.Fusion.specific.for.trans
+
+soapfuse/tables/all.sfuse.%.txt : $(foreach sample,$(SAMPLES),soapfuse/tables/$(sample).sfuse.%.txt)
+	$(INIT) head -1 $< | sed 's/^/Sample\t/;' > $@ && for i in $^; do sed "1d; s/^/$$(basename $${i%%.sfuse.$*.txt})\t/" $$i >> $@; done
