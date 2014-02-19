9f6735b90594fd46e2074071975104bf1c208c26
raylim <raylim@mm.st>
2014-02-19 18:29:32 -0500

    md5

--- extractFastq.mk
+++ extractFastq.mk
@@ -15,11 +15,11 @@ LOGDIR ?= log/fastq.$(NOW)
 
 EXTRACT_TOOL ?= PICARD
 
-all : $(foreach sample,$(SAMPLES),fastq/$(sample).1.fastq.gz)
+all : $(foreach sample,$(SAMPLES),fastq/$(sample).1.fastq.gz.md5)
 
 ifeq (${EXTRACT_TOOL},PICARD)
-fastq/%.1.fastq.gz fastq/%.2.fastq.gz : unprocessed_bam/%.bam
-	$(call LSCRIPT_MEM,10G,20G,"$(SAM_TO_FASTQ) I=$< FASTQ=>(gzip -c > fastq/$*.1.fastq.gz) SECOND_END_FASTQ=>(gzip -c > fastq/$*.2.fastq.gz)")
+fastq/%.1.fastq.gz.md5 fastq/%.2.fastq.gz.md5 : unprocessed_bam/%.bam
+	$(call LSCRIPT_MEM,10G,20G,"$(SAM_TO_FASTQ) I=$< FASTQ=>(gzip -c > fastq/$*.1.fastq.gz) SECOND_END_FASTQ=>(gzip -c > fastq/$*.2.fastq.gz) && md5sum fastq/$*.1.fastq.gz > fastq/$*.1.fastq.gz.md5 && md5sum fastq/$*.2.fastq.gz > fastq/$*.2.fastq.gz.md5")
 else
 fastq/%.1.fastq.gz fastq/%.2.fastq.gz : unprocessed_bam/%.bam
 	$(call LSCRIPT_MEM,10G,20G,"$(BAM2FASTQ) -o fastq/$*#.fastq $< && mv fastq/$*_1.fastq fastq/$*.1.fastq && mv fastq/$*_2.fastq fastq/$*.2.fastq")
