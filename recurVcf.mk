3987b2809fea5f182eb6352550a790bc4c06118a
raylim <raylim@mm.st>
2014-02-25 16:22:22 -0500

    log

--- recurVcf.mk
+++ recurVcf.mk
@@ -5,15 +5,17 @@
 include ~/share/modules/Makefile.inc
 include ~/share/modules/gatk.inc
 
-.DELETE_ON_ERROR:
-.SECONDARY: 
-.PHONY : all
+LOGDIR = log/recur_vcf.$(NOW)
 
 SET_VCF_SUFFIXES = gatk_snps.dp_ft.som_ft
 PAIR_VCF_SUFFIXES = som_sniper.ss_dp_ft.ss_ft.pass mutect.som_ad_ft.pass
 SAMPLE_SET_PAIR_VCF = $(foreach suff,$(SET_VCF_SUFFIXES),vcf/$(get_set.$1).$(suff).vcf) $(foreach suff,$(PAIR_VCF_SUFFIXES),vcf/$(get_pair.$1).$(suff).vcf)
 
 RECUR_VCF = $(RSCRIPT) $(HOME)/share/scripts/recurVcf.R
+
+.DELETE_ON_ERROR:
+.SECONDARY: 
+.PHONY : all
 
 all : $(foreach sample,$(TUMOR_SAMPLES),recur_pos/$(sample).recur.txt)
 
