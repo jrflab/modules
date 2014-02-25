7dc87abb5815efe2d5b1aadd1f7deeb0ba4db4ca
raylim <raylim@mm.st>
2014-02-25 17:54:45 -0500

    bed

--- recurVcf.mk
+++ recurVcf.mk
@@ -20,7 +20,7 @@ RECUR_VCF = $(RSCRIPT) $(HOME)/share/scripts/recurVcf.R
 all : $(foreach sample,$(TUMOR_SAMPLES),recur_pos/$(sample).recur.txt)
 
 define recur-pos-tumor
-recur_pos/$1.recur.txt : $$(call SAMPLE_SET_PAIR_VCF,$1)
+recur_pos/$1.recur.bed : $$(call SAMPLE_SET_PAIR_VCF,$1)
 	$$(INIT) $$(RECUR_VCF) --tumor $1 --outFile $$@ $$^
 endef
 $(foreach tumor,$(TUMOR_SAMPLES),$(eval $(call recur-pos-tumor,$(tumor))))
