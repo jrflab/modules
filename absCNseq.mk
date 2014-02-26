8d4e207729d0ead31b90da37f72f9c8507e72e9a
raylim <raylim@mm.st>
2014-02-26 14:12:33 -0500

    output

--- absCNseq.mk
+++ absCNseq.mk
@@ -2,7 +2,11 @@
 include ~/share/modules/Makefile.inc
 include ~/share/modules/gatk.inc
 
+LOGDIR = log/absCN.$(NOW)
 ABS_CN_SEQ = $(RSCRIPT) $(HOME)/share/scripts/absCNseq.R
+
+# file containing positions: chr:start-stop
+SNV_POS_FILE = snv_posn.intervals
 
 .DELETE_ON_ERROR:
 .SECONDARY: 
@@ -10,5 +14,11 @@ ABS_CN_SEQ = $(RSCRIPT) $(HOME)/share/scripts/absCNseq.R
 
 all : $(foreach pair,$(SAMPLE_PAIRS),absCN/$(pair).absCN.txt)
 
-absCN/%.absCN.txt absCN/%.absSNV.txt : varscan/segment/%.varscan2copynumber.txt tables/%.mutect.som_ad_ft.pass.dbsnp.nsfp.chasm.fathmm.eff.opl_tab.txt
+define abs-gatk-tumor-normal
+absCN/$1_$2.gatk.vcf : bam/$1.bam bam/$2.bam
+	$$(call LSCRIPT_MEM,9G,12G,"$$(call GATK_MEM,8G) -T UnifiedGenotyper -O $$@ -I $$< -I $$(<<) -R $$(REF_FASTA)  --output_mode EMIT_ALL_SITES -L $$(SNV_POS_FILE)")
+endef
+$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call abs-gatk-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+
+absCN/%.absCN.txt absCN/%.absSNV.txt : varscan/segment/%.varscan2copynumber.txt absCN/%.gatk.vcf
 	$(call LSCRIPT_MEM,4G,6G,"$(ABS_CN_SEQ) --outPrefix absCN/$* $^")
