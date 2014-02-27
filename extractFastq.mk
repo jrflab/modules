81efe119e9e1b4fc485e4716c8dcd7b5f7a41e17
raylim <raylim@mm.st>
2014-02-26 19:42:24 -0500

    bamToFastq

--- extractFastq.mk
+++ extractFastq.mk
@@ -6,6 +6,7 @@ include ~/share/modules/Makefile.inc
 include ~/share/modules/hg19.inc
 
 SAM_TO_FASTQ = $(JAVA) -Xmx4G -jar $(JARDIR)/SamToFastq.jar VALIDATION_STRINGENCY=LENIENT
+BAM_TO_FASTQ = $(HOME)/share/usr/bin/bamToFastq
 
 LOGDIR ?= log/fastq.$(NOW)
 
@@ -13,7 +14,7 @@ LOGDIR ?= log/fastq.$(NOW)
 
 .SECONDARY:
 
-EXTRACT_TOOL ?= PICARD
+EXTRACT_TOOL ?= bamToFastq
 
 all : $(foreach sample,$(SAMPLES),fastq/$(sample).1.fastq.gz.md5)
 
@@ -22,5 +23,6 @@ fastq/%.1.fastq.gz.md5 fastq/%.2.fastq.gz.md5 : unprocessed_bam/%.bam
 	$(call LSCRIPT_MEM,10G,20G,"$(SAM_TO_FASTQ) I=$< FASTQ=>(gzip -c > fastq/$*.1.fastq.gz) SECOND_END_FASTQ=>(gzip -c > fastq/$*.2.fastq.gz) && md5sum fastq/$*.1.fastq.gz > fastq/$*.1.fastq.gz.md5 && md5sum fastq/$*.2.fastq.gz > fastq/$*.2.fastq.gz.md5")
 else
 fastq/%.1.fastq.gz fastq/%.2.fastq.gz : unprocessed_bam/%.bam
-	$(call LSCRIPT_MEM,10G,20G,"$(BAM2FASTQ) -o fastq/$*#.fastq $< && mv fastq/$*_1.fastq fastq/$*.1.fastq && mv fastq/$*_2.fastq fastq/$*.2.fastq")
+	$(call LSCRIPT_MEM,10G,20G,"$(BAM_TO_FASTQ) -i $< -fq1 >(gzip -c > fastq/$*.1.fastq.gz) -fq2 >(gzip -c > fastq/$*.2.fastq.gz) \
+		&& md5sum fastq/$*.1.fastq.gz > fastq/$*.1.fastq.gz.md5 && md5sum fastq/$*.2.fastq.gz > fastq/$*.2.fastq.gz.md5")
 endif
