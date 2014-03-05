3e0b6848b47ea5cafb82de197f2e895df93f2df6
raylim <raylim@mm.st>
2014-03-04 19:46:45 -0500

    oncofuse

--- soapFuse.mk
+++ soapFuse.mk
@@ -58,11 +58,4 @@ soapfuse/alltables/all.isoform_sfuse%coord.txt : soapfuse/alltables/all.isoform_
 soapfuse/alltables/all.%.nft.txt : soapfuse/alltables/all.%.txt
 	$(INIT) $(SOAPFUSE_NORMAL_FILTER) $(SOAPFUSE_NORMAL_FILTER_OPTS) $(NORMAL_SOAPFUSE_RESULTS) $< > $@
 
-%.oncofuse.txt : %.coord.txt
-	$(call LSCRIPT_MEM,8G,12G,"$(call ONCOFUSE_MEM,7G) $< coord $(ONCOFUSE_TISSUE_TYPE) $@")
-
-%.oncofuse.merged.txt : %.txt %.oncofuse.txt 
-	$(INIT) head -1 $< | sed 's/^/RowID\t/' > $<.tmp && awk 'BEGIN {OFS = "\t" } NR > 1 { print NR-1, $$0 }' $< >> $<.tmp ;\
-		cut -f 2- $(<<) > $(<<).tmp; \
-		$(RSCRIPT) $(MERGE) -X --byColX 1 --byColY 1 -H $<.tmp $(<<).tmp > $@ && rm -f $<.tmp $(<<).tmp
-
+include ~/share/modules/oncofuse.mk
