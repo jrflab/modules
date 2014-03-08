dd6098008d068c7c6020a1fd079e8551b9aa79c4
raylim <limr@mskcc.org>
2014-03-07 19:33:03 -0500

    expands

---
+++ expands.mk
@@ -0,0 +1,24 @@
+SHELL=$(HOME)/share/scripts/Rshell
+.SHELLFLAGS = -s -m $(MEM) -n $(@F) -e 
+
+.ONESHELL:
+.DELETE_ON_ERROR:
+.SECONDARY:
+.PHONY: all
+
+MEM = 3G
+
+all : $(foreach pair,$(SAMPLE_PAIRS),expands/rdata/$(pair).Rdata)
+
+expands/rdata/%.Rdata : mutect/tables/%.mutect.txt varscan/segment/%.varscan2copynumber.txt
+	library(expands)
+	snv <- read.table("$<", header = T, sep = "\t")
+	colnames(snv)[1:2] <- c("chr", "startpos")
+	cbs <- read.table("$(<<)", header = T, sep = "\t")
+	cbs <- transform(cbs, CN_estimate = 2^Segmented)
+	colnames(cbs)[c(2,3,4)] <- c("chr", "startpos", "endpos")
+	dir.create("$(@D)", recursive = T)
+	save(snv, cbs, file = "$@")
+
+
+
