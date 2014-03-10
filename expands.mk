07cb114919165aa000343d21b5b55389b3987ecd
raylim <raylim@mm.st>
2014-03-10 18:39:00 -0400

    reject

--- expands.mk
+++ expands.mk
@@ -2,21 +2,24 @@
 
 include ~/share/modules/Makefile.inc
 
-SHELL=$(HOME)/share/scripts/Rshell
-.SHELLFLAGS = -s -m $(MEM) -n $(@F) -e 
+LOGDIR = log/expands.$(NOW)
+
+SHELL = $(HOME)/share/scripts/Rshell
+.SHELLFLAGS = -s -m $(MEM) -n $(@F) -l $(LOGDIR) -e 
 
 .ONESHELL:
 .DELETE_ON_ERROR:
 .SECONDARY:
 .PHONY: all
 
-MEM = 3G
+MEM = 9G
 
 all : $(foreach pair,$(SAMPLE_PAIRS),expands/rdata/$(pair).cbs_snv.Rdata)
 
 expands/rdata/%.cbs_snv.Rdata : mutect/tables/%.mutect.txt varscan/segment/%.varscan2copynumber.txt
 	library(expands)
 	snv <- read.table("$<", header = T, sep = "\t")
+	snv <- subset(snv, judgement != "REJECT")
 	colnames(snv)[1:2] <- c("chr", "startpos")
 	cbs <- read.table("$(<<)", header = T, sep = "\t")
 	cbs <- transform(cbs, CN_estimate = 2^Segmented)
