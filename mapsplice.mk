95a80000f43480ea829dc863ba28e0f921bb87dd
raylim <raylim@mm.st>
2014-04-03 17:23:59 -0400

    map splice

---
+++ mapsplice.mk
@@ -0,0 +1,27 @@
+# Run VarScan on tumour-normal matched pairs
+# Detect point mutations
+##### DEFAULTS ######
+
+REF ?= hg19
+LOGDIR = log/mapsplice.$(NOW)
+
+##### MAKE INCLUDES #####
+include ~/share/modules/Makefile.inc
+
+MAPSPLICE = $(PYTHON) $(HOME)/share/usr/MapSplice-v2.1.7/mapsplice.py
+MAPSPLICE_OPTS = -c $(MAPSPLICE_REF_DIR) -x $(MAPSPLICE_REF_BASENAME) --bam
+
+ifeq ($(PHRED64),true)
+MAPSPLICE_OPTS += --qual-scal phred64
+else
+MAPSPLICE_OPTS += --qual-scal phred33
+endif
+
+.DELETE_ON_ERROR:
+.SECONDARY: 
+.PHONY: all
+
+all : $(foreach sample,$(SAMPLES),mapsplice/$(sample).mapsplice_timestamp)
+
+mapsplice/%.mapsplice_timestamp : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
+	$(call LSCRIPT_PARALLEL_MEM,6,2G,3G,"$(MAPSPLICE) $(MAPSPLICE_OPTS) -p 6 -o mapsplice/$* -1 <(gzip -cd $<) -2 <(gzip -cd $(<<)) && touch $@")
