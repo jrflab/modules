30afec0066e87b0bc2e0709a5847bb4f68f8b49d
raylim <raylim@mm.st>
2014-04-04 17:38:52 -0400

    tmp extraction

--- mapsplice.mk
+++ mapsplice.mk
@@ -24,4 +24,9 @@ endif
 all : $(foreach sample,$(SAMPLES),mapsplice/$(sample).mapsplice_timestamp)
 
 mapsplice/%.mapsplice_timestamp : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
-	$(call LSCRIPT_PARALLEL_MEM,6,2G,3G,"mkdir -p mapsplice/$*; $(MAPSPLICE) $(MAPSPLICE_OPTS) -p 6 -o mapsplice/$* -1 <(gzip -cd $<) -2 <(gzip -cd $(<<)) && touch $@")
+	$(call LSCRIPT_PARALLEL_MEM,6,2G,3G,"TMP1=`mktemp --tmpdir=$(TMP)`; \
+		TMP2=`mktemp --tmpdir=$(TMP)`; \
+		zcat $< > $$TMP1 && zcat $(<<) $$TMP2; \
+		mkdir -p mapsplice/$*; \
+		$(MAPSPLICE) $(MAPSPLICE_OPTS) -p 6 -o mapsplice/$* -1 $$TMP1 -2 $$TMP2 \
+			&& touch $@ && rm -f $$TMP1 $$TMP2")
