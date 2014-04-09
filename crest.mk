511dcfd3dec496e97a18a7281e240f0487d66bdd
raylim <raylim@mm.st>
2014-04-09 13:50:00 -0400

    prefix

--- crest.mk
+++ crest.mk
@@ -22,19 +22,19 @@ all : $(foreach sample,$(SAMPLES),crest/sv/$(sample).predSV.txt)
 endif
 
 define sclip-chr
-crest/sclip/%.bam.$1.sclip.txt crest/sclip/%.bam.$1.cover : bam/%.bam
-	$$(call LSCRIPT_MEM,4G,6G,"$$(EXTRACT_SCLIP) -i $$< --ref_genome $$(REF_FASTA) -r $1 -o $$(@D)")
+crest/sclip/%.$1.sclip.txt crest/sclip/%.$1.cover : bam/%.bam
+	$$(call LSCRIPT_MEM,4G,6G,"$$(EXTRACT_SCLIP) -i $$< --ref_genome $$(REF_FASTA) -r $1 -o $$(@D) -p $$*")
 endef
 $(foreach chr,$(CHROMOSOMES),$(eval $(call sclip-chr,$(chr))))
 
-crest/sclip/%.bam.cover : $(foreach chr,$(CHROMOSOMES),crest/sclip/%.bam.$(chr).cover)
+crest/sclip/%.cover : $(foreach chr,$(CHROMOSOMES),crest/sclip/%.$(chr).cover)
 	$(INIT) cat $^ > $@
 
-crest/sclip/%.bam.sclip.txt : $(foreach chr,$(CHROMOSOMES),crest/sclip/%.bam.$(chr).sclip.txt)
+crest/sclip/%.sclip.txt : $(foreach chr,$(CHROMOSOMES),crest/sclip/%.$(chr).sclip.txt)
 	$(INIT) cat $^ > $@
 
 define sv-tumor-normal-chr
-crest/sv/$1_$2.$3.predSV.txt : bam/$1.bam bam/$2.bam crest/sclip/$1.bam.cover 
+crest/sv/$1_$2.$3.predSV.txt : bam/$1.bam bam/$2.bam crest/sclip/$1.cover 
 	$$(call LSCRIPT_MEM,4G,6G,"$$(CREST) -p crest/sv/$1_$2.$3 -f $$(<<<) -d $$< -g $$(<<) --ref_genome $$(REF_FASTA) -t $$(REF_2BIT) -r $3")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
@@ -42,7 +42,7 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 		$(eval $(call sv-tumor-normal-chr,$(tumor.$(pair)),$(normal.$(pair)),$(chr)))))
 
 define sv-chr
-crest/sv/%.$1.predSV.txt : bam/%.bam crest/sclip/%.bam.cover 
+crest/sv/%.$1.predSV.txt : bam/%.bam crest/sclip/%.cover 
 	$$(call LSCRIPT_MEM,4G,6G,"$$(CREST) -p crest/sv/$$*.$1 -f $$(<<) -d $$< --ref_genome $$(REF_FASTA) -t $$(REF_2BIT) -r $1")
 endef
 $(foreach chr,$(CHROMOSOMES),$(eval $(call sv-chr,$(chr))))
