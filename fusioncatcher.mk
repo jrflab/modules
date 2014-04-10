4302f58711f51c1316a99945283288c41fbe5e32
raylim <raylim@mm.st>
2014-04-10 19:46:34 -0400

    fusioncatcher

---
+++ fusioncatcher.mk
@@ -0,0 +1,20 @@
+# Run fusioncatcher
+##### DEFAULTS ######
+
+REF ?= hg19
+LOGDIR = log/fusioncatcher.$(NOW)
+
+##### MAKE INCLUDES #####
+include ~/share/modules/Makefile.inc
+
+FUSIONCATCHER = $(HOME)/usr/fusioncatcher/bin/fusioncatcher
+FUSIONCATCHER_OPTS = -d /home/limr/usr/fusioncatcher/data
+
+.DELETE_ON_ERROR:
+.SECONDARY: 
+.PHONY: all
+
+all : $(foreach sample,$(SAMPLES),fusioncatcher/$(sample).fusioncatcher_timestamp)
+
+fusioncatcher/%.fusioncatcher_timestamp : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
+	$(call LSCRIPT_PARALLEL_MEM,8,1G,2G,"$(FUSIONCATCHER) $(FUSIONCATCHER_OPTS) -p 8 -o $(@D)/$* -i $<$(,)$(<<)")
