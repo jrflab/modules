d583760f3a5d0c0f27139ca5885e833e746bfc86
raylim <raylim@mm.st>
2014-05-07 15:33:42 -0400

    named

--- tophatFusion.mk
+++ tophatFusion.mk
@@ -16,7 +16,7 @@ all : $(foreach sample,$(SAMPLES),tophat/$(sample)/fusions.out)
 
 tophat/%/fusions.out : fastq/%.1.fastq.gz fastq/%.2.fastq.gz tophat/ins_size/%.insert_size.txt
 	DIST_OPTS=`perl -e '$$text = do {local $$/ ; <>}; $$text =~ m/Read length: mean (\d+).*\nRead span: mean (\d+).*STD=(\d+)/; print "--mate-inner-dist " . ($$2 - $$1 * 2) . " --mate-std-dev $$3";' $(word 3,$^)`; \
-	$(call LSCRIPT_PARALLEL_MEM,4,6G,10G,"$(TOPHAT) $(TOPHAT_OPTS) -p 4 $$DIST_OPTS -o $(@D) $(BOWTIE_REF) $(<) $(word 2,$(^))")
+	$(call LSCRIPT_NAMED_PARALLEL_MEM,$*_tophat,4,6G,10G,"$(TOPHAT) $(TOPHAT_OPTS) -p 4 $$DIST_OPTS -o $(@D) $(BOWTIE_REF) $(<) $(word 2,$(^))")
 
 tophat/ins_size/%.insert_size.txt : bam/%.bam
 	$(call LSCRIPT,"$(SAMTOOLS) view $< | $(GET_INSERT_SIZE) - > $@")
