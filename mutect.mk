64c7682d1d8735893e7b941f638c2f4da5aea7b5
raylim <raylim@mm.st>
2014-05-07 12:12:03 -0400

    target_ft

--- mutect.mk
+++ mutect.mk
@@ -32,7 +32,7 @@ VPATH ?= bam
 all : mutect_vcfs mutect_tables ext_output mut_report
 
 
-FILTER_SUFFIX := som_ad_ft.pass.dbsnp.nsfp.eff.chasm.fathmm.transfic
+FILTER_SUFFIX := som_ad_ft.target_ft.pass.dbsnp.nsfp.eff.chasm.fathmm.transfic
 EFF_TYPES = silent missense nonsilent_cds nonsilent
 VCF_SUFFIXES = mutect.$(FILTER_SUFFIX)
 TABLE_SUFFIXES = $(foreach eff,$(EFF_TYPES),mutect.$(FILTER_SUFFIX).tab.$(eff).novel)
