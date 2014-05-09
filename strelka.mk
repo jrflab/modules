40e4503c66a718d141337f064a69056eff2cc892
raylim <raylim@mm.st>
2014-05-08 20:01:18 -0400

    fields

--- strelka.mk
+++ strelka.mk
@@ -11,6 +11,7 @@ CONFIGURE_STRELKA = $(HOME)/share/usr/bin/configureStrelkaWorkflow.pl
 STRELKA_CONFIG = $(HOME)/share/usr/etc/strelka_config.ini
 
 VCF_GEN_IDS = DP FDP SDP SUBDP AU CU GU TU
+VCF_FIELDS += QSS TQSS NT QSS_NT TQSS_NT SGT SOMATIC
 
 .DELETE_ON_ERROR:
 .SECONDARY:
