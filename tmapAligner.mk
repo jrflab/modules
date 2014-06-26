98de6be0e6fc2245b0159ad1e455a37e47907fda
raylim <raylim@mm.st>
2014-06-26 19:32:27 -0400

    split bams

--- tmapAligner.mk
+++ tmapAligner.mk
@@ -61,5 +61,25 @@ tmap/sam/%.header.sam : unprocessed_bam/%.bam
 tmap/bam/%.$(TMAP_MODE).bam.md5 : tmap/sam/%.header.sam unprocessed_bam/%.bam
 	$(call LSCRIPT_PARALLEL_MEM,4,6G,8G,"$(SAMTOOLS) reheader $^ | $(TMAP) $(TMAP_MODE) $(TMAP_OPTS) -f $(REF_FASTA) -i bam -s $(@M) -o 1 -n 4 && $(MD5)")
 
+ifdef SPLIT_SAMPLES
+define bam-header
+tmap/bam/$1.header.sam : $$(foreach split,$2,tmap/bam/$$(split).$$(TMAP_MODE).sorted.bam.md5)
+	$$(INIT) $$(SAMTOOLS) view -H $$(<M) | grep -v '^@RG' > $$@.tmp; \
+	for bam in $$(^M); do $$(SAMTOOLS) view -H $$$$bam | grep '^@RG' >> $$@.tmp; done; \
+	uniq $$@.tmp > $$@ && $$(RM) $$@.tmp
+endef
+$(foreach sample,$(SPLIT_SAMPLES),$(eval $(call bam-header,$(sample),$(split_lookup.$(sample)))))
+
+define merged-bam
+tmap/bam/$1.$$(TMAP_MODE).sorted.bam.md5 : tmap/bam/$1.header.sam $$(foreach split,$2,tmap/bam/$$(split).$$(TMAP_MODE).sorted.bam.md5)
+	if [ `echo "$$(filter %.bam,$$(^M))" | wc -w` -gt 1 ]; then \
+		$$(call LSCRIPT_MEM,12G,15G,"$$(SAMTOOLS) merge -f -h $$< $$(@M) $$(filter %.bam,$$(^M)) && $$(MD5) && $$(RM) $$(^M) $$^"); \
+	else \
+		ln -f $$(word 2,$$(^M)) $$(@M) && ln -f $$(word 2,$$^) $$@; \
+	fi
+endef
+$(foreach sample,$(SPLIT_SAMPLES),$(eval $(call merged-bam,$(sample),$(split_lookup.$(sample)))))
+endif
+
 include ~/share/modules/processBamMD5.mk
 include ~/share/modules/fastq.mk
