2a2d29dff8e579d464787aece9fd41d11ef71408
raylim <raylim@mm.st>
2014-09-10 13:11:59 -0400

    x

--- copy_number/gistic.mk
+++ copy_number/gistic.mk
@@ -16,14 +16,9 @@ PE := 1
 export LD_LIBRARY_PATH = /home/limr/usr/MATLAB/v714/runtime/glnxa64:/home/limr/usr/MATLAB/v714/bin/glnxa64:/home/limr/usr/MATLAB/v714/sys/os/glnxa64:/home/limr/usr/MATLAB/v714/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/home/limr/usr/MATLAB/v714/sys/java/jre/glnxa64/jre/lib/amd64/server:/home/limr/usr/MATLAB/v714/sys/java/jre/glnxa64/jre/lib/amd64
 export XAPPLRESDIR = /home/limr/usr/MATLAB/v714/X11/app-defaults
 MCR_DIR = $(HOME)/share/usr/MATLAB
-<<<<<<< HEAD
-GISTIC = $(HOME)/share/usr/gistic_2_0_21/gp_gistic2_from_seg
-GISTIC_OPTS = -genegistic 0 -smallmem 1 -maxseg 5000 -savegene 1 -saveseg 1 -savedata 0 -v 30 -ta 0.4 -td 0.4 -js 15 -qvt 0.25 -conf 0.99 -broad 1 -brlen 0.5 -rx 0
-=======
 GISTIC = $(HOME)/usr/gistic_2_0_21/gp_gistic2_from_seg
 GISTIC_THRESHOLD ?= 0.3
 GISTIC_OPTS = -genegistic 0 -smallmem 1 -maxseg 5000 -savegene 1 -saveseg 1 -savedata 0 -v 30 -ta $(GISTIC_THRESHOLD) -td $(GISTIC_THRESHOLD) -js 15 -qvt 0.25 -conf 0.99 -broad 1 -brlen 0.5 -rx 0
->>>>>>> 98e34afdfe12a0b13d71c09e33727af39e32d5af
 DGV_FILE = $(HOME)/share/reference/GRCh37_hg19_variants_2013-07-23.txt
 
 CNV_SIZES = 100000 300000
