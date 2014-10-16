3cf20c4f5f3aba16485e3751529f0317149787f0
raylim <raylim@mm.st>
2014-10-16 19:20:37 -0400

    mutect variant caller

--- variant_callers/somatic/mutectVariantCaller.mk
+++ variant_callers/somatic/mutectVariantCaller.mk
@@ -14,3 +14,5 @@ mutect_tables : $(foreach suff,$(TABLE_SUFFIXES),$(foreach pair,$(SAMPLE_PAIRS),
 	$(foreach suff,$(TABLE_SUFFIXES),alltables/allTN.$(suff).txt)
 ext_output : $(foreach pair,$(SAMPLE_PAIRS),mutect/tables/$(pair).mutect.txt)
 mut_report : mutect/report/report.timestamp mutect/lowAFreport/report.timestamp mutect/highAFreport/report.timestamp
+
+include ~/share/modules/variant_callers/somatic/mutect.mk
