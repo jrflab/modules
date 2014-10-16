3572394b344c324a1d77e56efd3a891b9d5c9916
raylim <raylim@mm.st>
2014-10-16 19:18:25 -0400

    mutect variant caller

---
+++ variant_callers/somatic/mutectVariantCaller.mk
@@ -0,0 +1,16 @@
+# Run mutect on tumour-normal matched pairs
+# Detect point mutations
+##### DEFAULTS ######
+
+LOGDIR = log/mutect.$(NOW)
+
+.DELETE_ON_ERROR:
+.SECONDARY: 
+.PHONY : all mutect_vcfs mutect_tables ext_output mut_report
+
+all : mutect_vcfs mutect_tables ext_output mut_report
+mutect_vcfs : $(VCFS) $(addsuffix .idx,$(VCFS))
+mutect_tables : $(foreach suff,$(TABLE_SUFFIXES),$(foreach pair,$(SAMPLE_PAIRS),tables/$(pair).$(suff).txt)) \
+	$(foreach suff,$(TABLE_SUFFIXES),alltables/allTN.$(suff).txt)
+ext_output : $(foreach pair,$(SAMPLE_PAIRS),mutect/tables/$(pair).mutect.txt)
+mut_report : mutect/report/report.timestamp mutect/lowAFreport/report.timestamp mutect/highAFreport/report.timestamp
