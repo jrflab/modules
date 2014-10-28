2112b48dc451558393dc8144b5f53e026d02e08a
raylim <raylim@mm.st>
2014-10-28 18:18:06 -0400

    mutect

--- mut_sigs/emu.mk
+++ mut_sigs/emu.mk
@@ -16,6 +16,8 @@ EMU_REF_DIR = $(MAPSPLICE_REF_DIR)
 
 all : emu/mutations.txt emu/cnv.txt emu/emu_results_bic.txt
 
+include ~/share/modules/variant_callers/somatic/mutect.mk
+
 emu/mutations.txt : alltables/allTN.mutect.$(FILTER_SUFFIX).tab.txt
 	$(INIT) awk 'BEGIN {OFS = "\t"} NR > 1 { sub("X", "23", $$3); sub("Y", "24", $$3); sub("MT", "25", $$3); print $$1 "_" $$2, $$3, $$4, $$6 ">" $$7 }' $< > $@
 
@@ -31,4 +33,3 @@ emu/mutations.txt.mut.matrix : emu/mutations.txt emu/cnv.txt
 emu/emu_results_bic.txt : emu/mutations.txt.mut.matrix
 	$(call LSCRIPT_MEM,4G,8G,"$(EMU) --mut $< --opp human-exome --pre emu/emu_results")
 
-include ~/share/modules/variant_callers/somatic/mutect.mk
