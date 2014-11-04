67179841f209a4b77d7f9c1149601b4a639f3f0a
raylim <raylim@mm.st>
2014-11-04 18:18:58 -0500

    targets

--- variant_callers/somatic/mutectVariantCaller.mk
+++ variant_callers/somatic/mutectVariantCaller.mk
@@ -9,10 +9,12 @@ LOGDIR = log/mutect.$(NOW)
 .PHONY : all mutect_vcfs mutect_tables ext_output mut_report
 
 all : mutect_vcfs mutect_tables ext_output mut_report
+
+include ~/share/modules/variant_callers/somatic/mutect.mk
+
 mutect_vcfs : $(VCFS) $(addsuffix .idx,$(VCFS))
-mutect_tables : $(foreach suff,$(TABLE_SUFFIXES),$(foreach pair,$(SAMPLE_PAIRS),tables/$(pair).$(suff).txt)) \
-	$(foreach suff,$(TABLE_SUFFIXES),alltables/allTN.$(suff).txt)
+mutect_tables : $(foreach suff,$(MUTECT_TABLE_SUFFIXES),$(foreach pair,$(SAMPLE_PAIRS),tables/$(pair).$(suff).txt)) \
+	$(foreach suff,$(MUTECT_TABLE_SUFFIXES),alltables/allTN.$(suff).txt)
 ext_output : $(foreach pair,$(SAMPLE_PAIRS),mutect/tables/$(pair).mutect.txt)
 mut_report : mutect/report/report.timestamp mutect/lowAFreport/report.timestamp mutect/highAFreport/report.timestamp
 
-include ~/share/modules/variant_callers/somatic/mutect.mk
