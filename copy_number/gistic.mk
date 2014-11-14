42454828d19535df1181e6d79d042fbdf7e65c40
Raymond Lim <raylim@mm.st>
2014-11-14 13:22:35 -0500

    threshold and JS

--- copy_number/gistic.mk
+++ copy_number/gistic.mk
@@ -18,7 +18,8 @@ export XAPPLRESDIR = /home/limr/usr/MATLAB/v714/X11/app-defaults
 MCR_DIR = $(HOME)/share/usr/MATLAB
 GISTIC = $(HOME)/share/usr/gistic_2_0_21/gp_gistic2_from_seg
 GISTIC_THRESHOLD ?= 0.3
-GISTIC_OPTS = -genegistic 0 -smallmem 1 -maxseg 5000 -savegene 1 -saveseg 1 -savedata 0 -v 30 -ta $(GISTIC_THRESHOLD) -td $(GISTIC_THRESHOLD) -js 15 -qvt 0.25 -conf 0.99 -broad 1 -brlen 0.5 -rx 0
+GISTIC_JS ?= 15
+GISTIC_OPTS = -genegistic 0 -smallmem 1 -maxseg 5000 -savegene 1 -saveseg 1 -savedata 0 -v 30 -ta $(GISTIC_THRESHOLD) -td $(GISTIC_THRESHOLD) -js $(GISTIC_THRESHOLD) -qvt 0.25 -conf 0.99 -broad 1 -brlen 0.5 -rx 0
 DGV_FILE = $(HOME)/share/reference/GRCh37_hg19_variants_2013-07-23.txt
 
 CNV_SIZES = 100000 300000
