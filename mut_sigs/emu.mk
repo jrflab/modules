6269f25edc23bbab45ad28999743051b8114c20b
Raymond Lim <raylim@mm.st>
2014-11-18 12:54:05 -0500

    alexandrov

--- mut_sigs/emu.mk
+++ mut_sigs/emu.mk
@@ -18,13 +18,13 @@ all : emu/mutations.txt emu/cnv.txt emu/emu_results_bic.txt
 include ~/share/modules/variant_callers/somatic/mutect.mk
 
 emu/mutations.txt : alltables/allTN.mutect.$(FILTER_SUFFIX).tab.txt
-	$(INIT) awk 'NR > 1 { sub("X", "23", $$3); sub("Y", "24", $$3); sub("MT", "25", $$3); print $$1 "_" $$2, $$3, $$4, $$6 ">" $$7 }' $< > $@
+	$(INIT) awk 'NR > 1 { sub("X", "23", $$3); sub("Y", "24", $$3); sub("MT", "25", $$3); print $$1 "_" $$2, $$3, $$4, $$6 ">" $$7 }' $< | cat - $(EMU_REF_MUTATIONS) > $@
 
 emu/cnv.txt : $(foreach pair,$(SAMPLE_PAIRS),freec/$(pair)/$(tumor.$(pair)).bam_CNVs)
 	$(INIT) rm -f $@; for x in $^; do \
 		sample=`echo $$x | sed 's:freec/::; s:/.*::'`; \
 		awk -v sample=$$sample 'NR > 1 { sub("chr", "", $$1); sub("X", "23" , $$1); sub("Y", "24", $$1); sub("MT", "25", $$1); print sample, $$1, $$2, $$3, $$4; }' $$x >> $@; \
-	done
+	done && cat $(EMU_REF_CNV) >> $@
 
 emu/mutations.txt.mut.matrix : emu/mutations.txt emu/cnv.txt
 	$(call LSCRIPT_MEM,4G,8G,"$(EMU_PREPARE) --chr $(EMU_REF_DIR) --cnv $(<<) --mut $< --pre $(@D) --regions $(EMU_TARGETS_FILE)")
