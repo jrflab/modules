f06d6f78bc1dcfb01d295822204ece94fc49192b
Raymond Lim <raylim@mm.st>
2015-02-05 19:18:20 -0500

    prefix

--- mut_sigs/emuAbsolute.mk
+++ mut_sigs/emuAbsolute.mk
@@ -24,34 +24,37 @@ endif
 
 all : $(SUBCLONAL) $(CLONAL) emu/cnv.txt 
 
-emu_absolute/subclonal_mutations.txt : $(foreach pair,$(SAMPLE_PAIRS),absolute/tables/$(pair).absolute.txt)
-	$(INIT) rm -f $@ && for x in $^; do sed 1d $$x | awk '$$139 == "snv" && $$141 == "TRUE" {print $$1,$$6,$$7,$$140}' >> $@; done
-
-emu_absolute/clonal_mutations.txt : $(foreach pair,$(SAMPLE_PAIRS),absolute/tables/$(pair).absolute.txt)
-	$(INIT) rm -f $@ && for x in $^; do sed 1d $$x | awk '$$139 == "snv" && $$142 == "TRUE" {print $$1,$$6,$$7,$$140}' >> $@; done
+emu_absolute/mutations.txt : $(foreach pair,$(SAMPLE_PAIRS),absolute/tables/$(pair).absolute.txt)
+	$(INIT) rm -f $@ && \
+		for x in $^; do \
+		sed 1d $$x | awk '$$139 == "snv" && $$141 == "TRUE" {print $$1_subclonal,$$6,$$7,$$140}' >> $@ && \
+		sed 1d $$x | awk '$$139 == "snv" && $$142 == "TRUE" {print $$1_clonal,$$6,$$7,$$140}' >> $@; \
+		done
 
 emu_absolute/cnv.txt : $(foreach pair,$(SAMPLE_PAIRS),freec/$(pair)/$(tumor.$(pair)).bam_CNVs)
 	$(INIT) rm -f $@; for x in $^; do \
 		sample=`echo $$x | sed 's:freec/::; s:/.*::'`; \
-		awk -v sample=$$sample 'NR > 1 { sub("chr", "", $$1); sub("X", "23" , $$1); sub("Y", "24", $$1); sub("MT", "25", $$1); print sample, $$1, $$2, $$3, $$4; }' $$x >> $@; \
+		awk -v sample=$${sample}_clonal 'NR > 1 { sub("chr", "", $$1); sub("X", "23" , $$1); sub("Y", "24", $$1); sub("MT", "25", $$1); print sample, $$1, $$2, $$3, $$4; }' $$x >> $@; \
+		awk -v sample=$${sample}_subclonal 'NR > 1 { sub("chr", "", $$1); sub("X", "23" , $$1); sub("Y", "24", $$1); sub("MT", "25", $$1); print sample, $$1, $$2, $$3, $$4; }' $$x >> $@; \
 	done && cat $(EMU_REF_CNV) >> $@
 
-emu_absolute/%_mutations.txt.mut.matrix : emu_absolute/%_mutations.txt emu_absolute/cnv.txt
+emu/mutations.txt.mut.matrix : emu/mutations.txt emu/cnv.txt
 	$(call LSCRIPT_MEM,4G,8G,"$(EMU_PREPARE) --chr $(EMU_REF_DIR) --cnv $(<<) --mut $< --pre $(@D) --regions $(EMU_TARGETS_FILE)")
 
-emu_absolute/%_emu_results_bic.txt : emu_absolute/%_mutations.txt.mut.matrix
-	$(call LSCRIPT_MEM,4G,8G,"$(EMU) --mut $< --opp human-exome --pre emu_absolute/$*_emu_results")
+emu/emu_results_bic.txt : emu/mutations.txt.mut.matrix
+	$(call LSCRIPT_MEM,4G,8G,"$(EMU) --mut $< --opp human-exome --pre emu/emu_results")
 
 RESULT_TIMESTAMPS = 
 ifdef NUM_SPECTRA
-emu_absolute/%_emu_$(NUM_SPECTRA).timestamp : emu_absolute/%_mutations.txt.mut.matrix
-	$(call LSCRIPT_MEM,4G,8G,"$(EMU) --force $(NUM_SPECTRA) --mut $< --opp human-exome --pre emu_absolute/$*_emu_results && touch $@")
+emu/emu_$(NUM_SPECTRA).timestamp : emu/mutations.txt.mut.matrix
+	$(call LSCRIPT_MEM,4G,8G,"$(EMU) --force $(NUM_SPECTRA) --mut $< --opp human-exome --pre emu/emu_results && touch $@")
 
-RESULT_TIMESTAMPS += emu_absolute/%_emu_$(NUM_SPECTRA).timestamp
+RESULT_TIMESTAMPS += emu/emu_$(NUM_SPECTRA).timestamp
 endif
 
-emu_absolute/sample_pairs.txt : 
-	$(INIT) echo "$(SAMPLE_PAIRS)" | sed 's/ /\n/g' > $@
+emu/sample_pairs.txt : 
+	$(INIT) echo "$(SAMPLE_PAIRS)" | sed 's/ /_subclonal\n/g' > $@ \
+		&& echo "$(SAMPLE_PAIRS)" | sed 's/ /_clonal\n/g' >> $@
 
-emu_absolute/%_report/index.html : emu_absolute/%_emu_results_bic.txt emu_absolute/sample_pairs.txt emu_absolute/%_mutations.txt $(RESULT_TIMESTAMPS)
-	$(call LSCRIPT_MEM,4G,16G,"$(PLOT_EMU) --inPrefix $(<D)/$*_emu_results --outDir $(@D) --sampleSubset $(<<) --mutations $(<<<) --samples $(<<<).samples")
+emu/report/index.html : emu/emu_results_bic.txt emu/sample_pairs.txt emu/mutations.txt $(RESULT_TIMESTAMPS)
+	$(call LSCRIPT_MEM,4G,16G,"$(PLOT_EMU) --inPrefix $(<D)/emu_results --outDir $(@D) --sampleSubset $(<<) --mutations $(<<<) --samples $(<<<).samples")
