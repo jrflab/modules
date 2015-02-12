54395f96dd9d970f48c19f38d38efa547d8b5542
Raymond Lim <raylim@mm.st>
2015-02-12 18:35:11 -0500

    nmf

--- mut_sigs/nmfMutSig.mk
+++ mut_sigs/nmfMutSig.mk
@@ -9,18 +9,19 @@ LOGDIR = log/nmf_mutsig.$(NOW)
 EMU_PREPARE = $(HOME)/usr/bin/EMu-prepare
 MATLAB = /usr/local/bin/matlab -nojvm -nodisplay -nosplash 
 
-PLOT_EMU = $(RSCRIPT) $(HOME)/share/scripts/plotEmuSignatures.R
+NMF_DIR = $(HOME)/usr/nmf_mut_sig
+NMF_TYPES_FILE = $(NMF_DIR)/types.mat
+CREATE_NMF_INPUT = $(HOME)/share/scripts/createNMFinput.m
+RUN_NMF = $(HOME)/share/scripts/runNMF.m
 
-
-NMF_DIR = $(HOME)/usr/wtsi_mut_sig
-PREPARE_NMF_INPUT = $(NMF_DIR)/createWTSIinput.m
-
+NMF_MIN_SIG = 1
+NMF_MAX_SIG = 4
 
 .DELETE_ON_ERROR:
 .SECONDARY: 
 .PHONY: all
 
-ALL := nmf_mutsig/mutations.txt.mut.matrix
+all := nmf_mutsig/mutations.txt.mut.matrix
 
 all : $(ALL)
 
@@ -33,4 +34,9 @@ nmf_mutsig/mutations.txt.mut.matrix : nmf_mutsig/mutations.txt
 	$(INIT) $(EMU_PREPARE) --chr $(EMU_REF_DIR) --mut $< --pre $(@D) --regions $(EMU_TARGETS_FILE)
 
 nmf_mutsig/input.mat : nmf_mutsig/mutations.txt.mut.matrix
-	$(INIT) 
+	$(INIT) $(MATLAB) < $(CREATE_NMF_INPUT) -r "createNMFinput $< $(SAMPLE_FILE) $(NMF_TYPES_FILE) $(PROJECT_NAME) $@"
+
+nmf_mutsig/results.mat : nmf_mutsig/input.mat
+	$(INIT) $(MATLAB) < $(RUN_NMF) -r "runNMF $< $(@:.mat=) $(NMF_DIR) $(NMF_MIN_SIG) $(NMF_MAX_SIG)"
+
+
