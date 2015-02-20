2747e77f62f6b3556c42fd352bc9e78f73f53726
Raymond Lim <raylim@mm.st>
2015-02-20 12:15:22 -0500

    x

--- aligners/tophatAligner.mk
+++ aligners/tophatAligner.mk
@@ -76,15 +76,12 @@ tophat/bam/$1.header.sam : $$(foreach split,$2,tophat/bam/$$(split).tophat.sorte
 	for bam in $$(^M); do $$(SAMTOOLS) view -H $$$$bam | grep '^@RG' >> $$@.tmp; done; \
 	uniq $$@.tmp > $$@ && $$(RM) $$@.tmp
 
-tophat/bam/$1.tophat.merge_sorted.bam.md5 : tophat/bam/$1.header.sam $$(foreach split,$2,tophat/bam/$$(split).tophat.sorted.bam.md5)
+tophat/bam/$1.tophat.sorted.bam.md5 : tophat/bam/$1.header.sam $$(foreach split,$2,tophat/bam/$$(split).tophat.sorted.bam.md5)
 	$$(call LSCRIPT_MEM,12G,15G,"$$(SAMTOOLS) merge -f -h $$< $$(@M) $$(filter %.bam,$$(^M)) && $$(MD5) && $$(RM) $$(^M) $$^")
 endif
 endef
 $(foreach sample,$(SAMPLES),$(eval $(call merged-bam,$(sample),$(split_lookup.$(sample)))))
 endif
-
-%.sorted.bam.md5 : %.merge_sorted.bam.md5
-	$(INIT) mv $< $@ && mv $(<M) $(@M) && sed -i 's/merge_sorted/sorted/' $(@M)
 
 
 include ~/share/modules/fastq_tools/fastq.mk
