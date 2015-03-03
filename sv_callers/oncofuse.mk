37f1a8a8a9d7058e92dc0a80cc7a979bc0f0033f
raylim <raylim@mm.st>
2015-03-03 18:02:23 -0500

    x;

--- sv_callers/oncofuse.mk
+++ sv_callers/oncofuse.mk
@@ -1,8 +1,8 @@
 # run oncofuse
 # hg19 only
 
-
-ONCOFUSE_MEM = $(JAVA7) -Xmx$1 -jar $(HOME)/share/usr/lib/oncofuse-1.0.8.jar
+ONCOFUSE_MEM = $(JAVA7) -Xmx$1 -jar $(HOME)/share/usr/oncofuse-v1.0.6/Oncofuse.jar
+#ONCOFUSE_MEM = $(JAVA7) -Xmx$1 -jar $(HOME)/share/usr/lib/java/oncofuse-1.0.8.jar
 ONCOFUSE_TISSUE_TYPE ?= EPI
 
 %.oncofuse.txt : %.coord.txt
