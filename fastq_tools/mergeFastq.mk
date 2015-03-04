40c9f0efe608840dbf18b99300ac97e55f2ca2b9
Raymond Lim <raylim@mm.st>
2015-03-04 10:46:03 -0500

    bam

--- fastq_tools/mergeFastq.mk
+++ fastq_tools/mergeFastq.mk
@@ -14,7 +14,7 @@ endif
 
 ALIGNER ?= bwamem
 
-merge_fastq : $(foreach sample,$(MERGE_SAMPLES),$(if "$(wildcard bam/$(sample).bam)",merged_bam/$(sample).bam.md5,bam/$(sample).bam.md5))
+merge_fastq : $(foreach sample,$(MERGE_SAMPLES),$(if $(wildcard bam/$(sample).bam),merged_bam/$(sample).bam.md5,bam/$(sample).bam.md5))
 	$(INIT) for bamMd5 in $(filter merged_bam/%.bam.md5,$^); do \
 		cp $$bamMd5 bam/$$(basename $$bamMd5) && \
 		ln -f $${bamMd5%%.md5} bam/$$(basename $${bamMd5%%.md5}); \
