df32b857cb3b349bb84d3ffc5adfcbf6e2c666bb
Raymond Lim <raylim@mm.st>
2015-03-05 17:56:19 -0500

    cuff merged

--- rnaseq/cufflinks.mk
+++ rnaseq/cufflinks.mk
@@ -11,6 +11,7 @@ LOGDIR = log/cufflinks.$(NOW)
 NUM_CORES ?= 8
 CUFFLINKS = $(HOME)/share/usr/bin/cufflinks
 CUFFCOMPARE = $(HOME)/share/usr/bin/cuffcompare
+CUFFMERGE = $(HOME)/share/usr/bin/cuffmerge
 CUFFLINKS_OPTS = -b $(REF_FASTA) -u -g $(GENES_GTF) -p $(NUM_CORES) -u --no-update-check -v
 CUFFCOMPARE_OPTS = -s $(REF_FASTA) -r $(GENES_GTF) -V
 
@@ -18,11 +19,12 @@ CUFFCOMPARE_OPTS = -s $(REF_FASTA) -r $(GENES_GTF) -V
 ..DUMMY := $(shell mkdir -p version; $(CUFFLINKS) &> version/tophat.txt; echo "options: $(CUFFLINKS_OPTS)" >> version/cufflinks.txt)
 .SECONDARY:
 .DELETE_ON_ERROR:
-.PHONY : all_cufflinks cufflinks cuffcmp
+.PHONY : all_cufflinks cufflinks cuffcmp cuffmerge
 
-all_cufflinks : cufflinks cuffcmp
+all_cufflinks : cufflinks cuffcmp cuffmerge
 cufflinks : $(foreach sample,$(SAMPLES),cufflinks/gtf/$(sample).transcripts.gtf)
 cuffcmp : cufflinks/cuffcmp/cc.stats
+cuffmerge : cufflinks/gtf/merged.gtf
 
 cufflinks/gtf/%.transcripts.gtf cufflinks/fpkm_tracking/%.isoforms.fpkm_tracking cufflinks/fpkm_tracking/%.genes.fpkm_tracking : bam/%.bam
 	$(call LSCRIPT_PARALLEL_MEM,$(NUM_CORES),2G,4G,"${CUFFLINKS} ${CUFFLINKS_OPTS} -o cufflinks/$* $<  && \
@@ -33,3 +35,9 @@ cufflinks/gtf/%.transcripts.gtf cufflinks/fpkm_tracking/%.isoforms.fpkm_tracking
 
 cufflinks/cuffcmp/cc.stats : $(foreach sample,$(SAMPLES),cufflinks/gtf/$(sample).transcripts.gtf)
 	$(call LSCRIPT_MEM,10G,20G,"$(CUFFCOMPARE) $(CUFFCOMPARE_OPTS) -o $(@:.stats=) $^")
+
+cufflinks/assembly_list.txt : $(foreach sample,$(SAMPLES),cufflinks/gtf/$(sample).transcripts.gtf)
+	$(INIT) echo "$^" | tr ' ' '\n' > $@
+
+cufflinks/gtf/merged.gtf : cufflinks/assembly_list.txt
+	$(call LSCRIPT_PARALLEL_MEM,8,1G,2.5G,"$(CUFFMERGE) -o $(@D) -g $(GENES_GTF) -p 8
