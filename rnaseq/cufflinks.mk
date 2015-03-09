8dddb33f91656c7d17d9c824ca0f7ac0588414b0
Raymond Lim <raylim@mm.st>
2015-03-09 11:53:32 -0400

    strip

--- rnaseq/cufflinks.mk
+++ rnaseq/cufflinks.mk
@@ -10,20 +10,25 @@ LOGDIR = log/cufflinks.$(NOW)
 
 NUM_CORES ?= 8
 CUFFLINKS = $(HOME)/share/usr/bin/cufflinks
-CUFFCOMPARE = $(HOME)/share/usr/bin/cuffcompare
-CUFFMERGE = $(HOME)/share/usr/bin/cuffmerge
-CUFFDIFF = $(HOME)/share/usr/bin/cuffdiff
-CUFFQUANT = $(HOME)/share/usr/bin/cuffquant
-CUFFNORM = $(HOME)/share/usr/bin/cuffnorm
 CUFFLINKS_OPTS = -b $(REF_FASTA) -u -g $(GENES_GTF) -p $(NUM_CORES) -u --no-update-check -v
-CUFFCOMPARE_OPTS = -s $(REF_FASTA) -r $(GENES_GTF) -V
+CUFFCOMPARE = $(HOME)/share/usr/bin/cuffcompare
+CUFFCOMPARE_OPTS = --no-update-check
+CUFFMERGE = $(HOME)/share/usr/bin/cuffmerge
+CUFFMERGE_OPTS = --no-update-check
+CUFFDIFF = $(HOME)/share/usr/bin/cuffdiff
+CUFFDIFF_OPTS = --no-update-check -v
+CUFFQUANT = $(HOME)/share/usr/bin/cuffquant
+CUFFQUANT_OPTS = --no-update-check -v
+CUFFNORM = $(HOME)/share/usr/bin/cuffnorm
+CUFFNORM_OPTS = --no-update-check -v
+CUFFCOMPARE_OPTS = --no-update-check -s $(REF_FASTA) -r $(GENES_GTF) -V -v
 
 PHENO_FILE ?= pheno.txt
 ifneq ($(wildcard $(PHENO_FILE)),)
   A = $(shell sed '1d' $(PHENO_FILE) | cut -f1)
   B = $(shell sed '1d' $(PHENO_FILE) | cut -f2)
   $(foreach i,$(shell seq 1 $(words $(A))),$(eval pheno.$(word $i,$(B)) += $(word $i,$(A))))
-  PHENOTYPES = $(shell sed '1d' $(PHENO_FILE) | cut -f2 | sort | uniq)
+  PHENOTYPES = $(strip $(shell sed '1d' $(PHENO_FILE) | cut -f2 | sort | uniq))
 endif
 
 ..DUMMY := $(shell mkdir -p version; $(CUFFLINKS) &> version/tophat.txt; echo "options: $(CUFFLINKS_OPTS)" >> version/cufflinks.txt)
@@ -52,21 +57,21 @@ cufflinks/assembly_list.txt : $(foreach sample,$(SAMPLES),cufflinks/gtf/$(sample
 	$(INIT) echo "$^" | tr ' ' '\n' > $@
 
 cufflinks/gtf/merged.gtf : cufflinks/assembly_list.txt
-	$(call LSCRIPT_PARALLEL_MEM,8,1G,2.5G,"$(CUFFMERGE) -o $(@D) -g $(GENES_GTF) -p 8 $<")
+	$(call LSCRIPT_PARALLEL_MEM,8,1G,2.5G,"$(CUFFMERGE) $(CUFFMERGE_OPTS) -o $(@D) -g $(GENES_GTF) -p 8 $<")
 
 cufflinks/cxb/%.cxb : cufflinks/gtf/merged.gtf bam/%.bam
 	$(call LSCRIPT_PARALLEL_MEM,4,1G,2.5G,"mkdir -p cufflinks/$* && \
-	   	$(CUFFQUANT) -o cufflinks/$* -b $(REF_FASTA) -p 4 $^ && \
+	   	$(CUFFQUANT) $(CUFFQUANT_OPTS) -o cufflinks/$* -b $(REF_FASTA) -p 4 $^ && \
 		ln cufflinks/$*/abundances.cxb $@")
 
 cufflinks/cuffdiff/gene_exp.diff : cufflinks/gtf/merged.gtf $(foreach sample,$(SAMPLES),cufflinks/cxb/$(sample).cxb)
-	$(call LSCRIPT_PARALLEL_MEM,8,1G,2.5G,"$(CUFFDIFF) -o $(@D) -p 8 $< \
+	$(call LSCRIPT_PARALLEL_MEM,8,1G,2.5G,"$(CUFFDIFF) $(CUFFDIFF_OPTS) -o $(@D) -p 8 $< \
 		$(foreach pheno,$(PHENOTYPES),$(subst $( ),$(,),\
 			$(foreach s,$(pheno.$(pheno)),cufflinks/cxb/$s.cxb))) \
 		-L $(subst $( ),$(,),$(PHENOTYPES))")
 
 cufflinks/cuffnorm/gene_exp.txt : cufflinks/gtf/merged.gtf $(foreach sample,$(SAMPLES),cufflinks/cxb/$(sample).cxb)
-	$(call LSCRIPT_PARALLEL_MEM,8,1G,2.5G,"$(CUFFNORM) -o $(@D) -p 8 $< \
+	$(call LSCRIPT_PARALLEL_MEM,8,1G,2.5G,"$(CUFFNORM) $(CUFFNORM_OPTS) -o $(@D) -p 8 $< \
 		$(foreach pheno,$(PHENOTYPES),$(subst $( ),$(,),\
 			$(foreach s,$(pheno.$(pheno)),cufflinks/cxb/$s.cxb))) \
 		-L $(subst $( ),$(,),$(PHENOTYPES))")
