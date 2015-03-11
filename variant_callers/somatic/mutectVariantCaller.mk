ba6bf558cd7a71d54f4180abe5d18ffbad440e40
Raymond Lim <raylim@mm.st>
2015-03-11 19:31:11 -0400

    mutect

--- variant_callers/somatic/mutectVariantCaller.mk
+++ variant_callers/somatic/mutectVariantCaller.mk
@@ -6,15 +6,14 @@ LOGDIR = log/mutect.$(NOW)
 
 .DELETE_ON_ERROR:
 .SECONDARY: 
-.PHONY : all mutect_vcfs mutect_tables ext_output mut_report
+.PHONY : mutect mutect_vcfs mutect_tables ext_output mut_report
 
-all : mutect_vcfs mutect_tables ext_output mut_report
+mutect : mutect_vcfs mutect_tables ext_output mut_report
 
 include modules/variant_callers/somatic/mutect.mk
 include modules/variant_callers/somatic/mutect.inc
 include modules/variant_callers/somatic/somaticVariantCaller.inc
 
-#VCFS = $(foreach suff,$(VCF_SUFFIXES),$(foreach tumor,$(TUMOR_SAMPLES),vcf/$(tumor)_$(normal_lookup.$(tumor)).$(suff).vcf))
 mutect_vcfs : $(call VCFS,mutect) $(addsuffix .idx,$(call VCFS,mutect))
 mutect_tables : $(call TABLES,mutect)
 ext_output : $(foreach pair,$(SAMPLE_PAIRS),mutect/tables/$(pair).mutect.txt)
