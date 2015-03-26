8b90f8f5101d00ac4a28340de59b3bb71c044679
Raymond Lim <raylim@mm.st>
2015-03-26 18:57:45 -0400

    merge

--- variant_callers/somatic/mergeStrelkaVarscanIndels.mk
+++ variant_callers/somatic/mergeStrelkaVarscanIndels.mk
@@ -9,7 +9,8 @@ include modules/variant_callers/somatic/somaticVariantCaller.inc
 
 strelka_varscan_merge : strelka_varscan_merge_vcfs strelka_varscan_merge_tables
 strelka_varscan_merge_vcfs : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).strelka_varscan_indels.vcf)
-strelka_varscan_merge_tables : $(call TABLES,strelka_varscan_indels)
+strelka_varscan_merge_tables : $(foreach pair,$(SAMPLE_PAIRS),\
+	$(foreach ext,$(TABLE_EXTENSIONS),tables/$(pair).strelka_varscan_indels.$(ext).txt))
 
 vcf/%.strelka_varscan_indels.vcf : vcf/%.$(call VCF_SUFFIXES,strelka_indels).vcf vcf/%.$(call VCF_SUFFIXES,varscan_indels).vcf
 	$(call LSCRIPT_MEM,8G,10G,"$(call GATK_MEM,8G) -T CombineVariants --variant $< --variant $(<<) -o $@ -genotypeMergeOptions UNIQUIFY -R $(REF_FASTA)")
