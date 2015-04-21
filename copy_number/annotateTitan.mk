4b4744c172e59a140115f9aa0becc61167731397
Raymond Lim <raylim@mm.st>
2015-04-21 19:44:47 -0400

    ))

--- copy_number/annotateTitan.mk
+++ copy_number/annotateTitan.mk
@@ -18,7 +18,7 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 	$(foreach type,varscan_indels varscan_snps,\
 	$(foreach suff,$(call VCF_SUFFIXES,$(type)),\
 	$(foreach ext,$(TABLE_EXTENSIONS),\
-	tables/$(pair).$(suff).titan.$(ext).txt))
+	tables/$(pair).$(suff).titan.$(ext).txt))))
 
 ann_strelka : $(foreach pair,$(SAMPLE_PAIRS),\
 	$(foreach type,strelka_indels strelka_snps,\
@@ -27,14 +27,14 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 	$(foreach type,strelka_indels strelka_snps,\
 	$(foreach suff,$(call VCF_SUFFIXES,$(type)),\
 	$(foreach ext,$(TABLE_EXTENSIONS),\
-	tables/$(pair).$(suff).titan.$(ext).txt))
+	tables/$(pair).$(suff).titan.$(ext).txt))))
 
 ann_mutect : $(foreach pair,$(SAMPLE_PAIRS),\
 	$(foreach suff,$(call VCF_SUFFIXES,mutect),vcf/$(pair).$(suff).titan.vcf)) \
 $(foreach pair,$(SAMPLE_PAIRS),\
 	$(foreach suff,$(call VCF_SUFFIXES,mutect),\
 	$(foreach ext,$(TABLE_EXTENSIONS),\
-	tables/$(pair).$(suff).titan.$(ext).txt))
+	tables/$(pair).$(suff).titan.$(ext).txt)))
 
 $(info vcf/$1.%.titan.vcf : vcf/$1.%.vcf titan/optclust_results_w$(TITAN_WINDOW_SIZE)_p$(DEFAULT_PLOIDY_PRIOR)/$1.titan_seg.txt)
 define titan-pair
