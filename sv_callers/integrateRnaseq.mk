d55b4573cb5f521f083fb4e3bd3ae935b370f26d
Raymond Lim <raylim@mm.st>
2015-04-27 14:10:37 -0400

    max memory = 60g

--- sv_callers/integrateRnaseq.mk
+++ sv_callers/integrateRnaseq.mk
@@ -17,6 +17,6 @@ INTEGRATE = $(HOME)/share/usr/bin/Integrate
 integrate_rnaseq : $(foreach sample,$(SAMPLES),integrate/breakpoints/$(sample).breakpoints.tsv)
 
 integrate/reads/%.reads.txt integrate/sum/%.sum.tsv integrate/exons/%.exons.tsv integrate/breakpoints/%.breakpoints.tsv : tophat/%/accepted_hits.sorted.bam.md5 tophat/%/unmapped.sorted.bam.md5 tophat/%/accepted_hits.sorted.bam.bai tophat/%/unmapped.sorted.bam.bai
-	$(call LSCRIPT_MEM,8G,32G,"mkdir -p integrate/reads integrate/sum integrate/exons integrate/breakpoints; $(INTEGRATE) fusion -reads integrate/reads/$*.reads.txt -sum integrate/reads/$*.sum.tsv -ex integrate/exons/$*.exons.tsv -bk integrate/breakpoints/$*.breakpoints.tsv $(REF_FASTA) $(INTEGRATE_ANN) $(INTEGRATE_BWTS) $(<M) $(<<M)")
+	$(call LSCRIPT_MEM,8G,60G,"mkdir -p integrate/reads integrate/sum integrate/exons integrate/breakpoints; $(INTEGRATE) fusion -reads integrate/reads/$*.reads.txt -sum integrate/reads/$*.sum.tsv -ex integrate/exons/$*.exons.tsv -bk integrate/breakpoints/$*.breakpoints.tsv $(REF_FASTA) $(INTEGRATE_ANN) $(INTEGRATE_BWTS) $(<M) $(<<M)")
 
 include modules/bam_tools/processBam.mk
