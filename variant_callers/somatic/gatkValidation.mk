d3490f649520e63755a26ac4a1d8f8b7834602ea
Raymond Lim <raylim@mm.st>
2015-05-19 16:04:37 -0400

    gatk validation of somatic positions

---
+++ variant_callers/somatic/gatkValidation.mk
@@ -0,0 +1,18 @@
+# use GATK to gather allelic depths at somatic positions
+include modules/Makefile.inc
+include modules/variant_callers/gatk.inc
+include modules/variant_callers/somatic/somaticVariantCaller.inc
+
+LOGDIR ?= log/gatk_validation.$(NOW)
+
+SOMATIC_BED ?= somatic.bed
+
+.DELETE_ON_ERROR:
+.SECONDARY: 
+.PHONY : val_vcfs
+
+val_vcfs : $(foreach sample,$(SAMPLES),vcf/$(sample).gatkval_snp.vcf)
+
+vcf/%.gatkval_snp.vcf : bam/%.bam
+	$(call LSCRIPT_PARALLEL_MEM,4,2.5G,3G,"$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) -I $< -L $(SOMATIC_BED) -o $@ --output_mode EMIT_ALL_SITES")
+
