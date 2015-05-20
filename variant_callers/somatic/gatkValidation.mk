e8896ee3e483b93ccf24f704aaad76dfa334a562
Raymond Lim <raylim@mm.st>
2015-05-20 17:49:36 -0400

    call indels as well for validation

--- variant_callers/somatic/gatkValidation.mk
+++ variant_callers/somatic/gatkValidation.mk
@@ -7,6 +7,7 @@ LOGDIR ?= log/gatk_validation.$(NOW)
 
 SOMATIC_BED ?= somatic.bed
 
+
 .DELETE_ON_ERROR:
 .SECONDARY: 
 .PHONY : val_vcfs
@@ -15,7 +16,7 @@ val_vcfs : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).gatkval.vcf)
 
 define gatkval-tumor-normal
 vcf/$1_$2.gatkval.vcf : bam/$1.bam bam/$2.bam
-	$$(call LSCRIPT_PARALLEL_MEM,4,2.5G,3G,"$$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) -I $$< -I $$(<<) -L $(SOMATIC_BED) -o $$@ --output_mode EMIT_ALL_SITES")
+	$$(call LSCRIPT_PARALLEL_MEM,4,2.5G,3G,"$$(call GATK_MEM,8G) -T UnifiedGenotyper -glm BOTH -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) -I $$< -I $$(<<) -L $(SOMATIC_BED) -o $$@ --output_mode EMIT_ALL_SITES")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),$(eval $(call gatkval-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
 
