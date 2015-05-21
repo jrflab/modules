0dd87d699b7b844fbd9f25ab87cc3e30a47706c4
Raymond Lim <raylim@mm.st>
2015-05-21 18:04:22 -0400

    more mem

--- sv_callers/integrateRnaseq.mk
+++ sv_callers/integrateRnaseq.mk
@@ -17,6 +17,6 @@ INTEGRATE = $(HOME)/share/usr/bin/Integrate
 integrate_rnaseq : $(foreach sample,$(SAMPLES),integrate/breakpoints/$(sample).breakpoints.tsv)
 
 integrate/reads/%.reads.txt integrate/sum/%.sum.tsv integrate/exons/%.exons.tsv integrate/breakpoints/%.breakpoints.tsv : bam/%.bam.md5 bam/%.bam.bai
-	$(call LSCRIPT_MEM,8G,60G,"mkdir -p integrate/reads integrate/sum integrate/exons integrate/breakpoints; $(INTEGRATE) fusion -reads integrate/reads/$*.reads.txt -sum integrate/sum/$*.sum.tsv -ex integrate/exons/$*.exons.tsv -bk integrate/breakpoints/$*.breakpoints.tsv $(REF_FASTA) $(INTEGRATE_ANN) $(INTEGRATE_BWTS) $(<M) $(<M)")
+	$(call LSCRIPT_MEM,8G,80G,"mkdir -p integrate/reads integrate/sum integrate/exons integrate/breakpoints; $(INTEGRATE) fusion -reads integrate/reads/$*.reads.txt -sum integrate/sum/$*.sum.tsv -ex integrate/exons/$*.exons.tsv -bk integrate/breakpoints/$*.breakpoints.tsv $(REF_FASTA) $(INTEGRATE_ANN) $(INTEGRATE_BWTS) $(<M) $(<M)")
 
 include modules/bam_tools/processBam.mk
