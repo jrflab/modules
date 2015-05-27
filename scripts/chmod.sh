1cfbd6d319fe4de50f9b4156a090d7a29507de33
Raymond Lim <raylim@mm.st>
2015-05-27 16:53:20 -0400

    change permissions for data dir

---
+++ scripts/chmod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+chmod ug+rwX -R /ifs/e63data/reis-filho/data &> /dev/null
+chmod ug+rwX -R /ifs/e63data/reis-filho/projects &> /dev/null
+chmod ug+rwX -R /ifs/e63data/reis-filho/reference &> /dev/null
+chmod o+rX -R /ifs/e63data/reis-filho/data &> /dev/null
+chmod o+rX -R /ifs/e63data/reis-filho/projects &> /dev/null
+chmod o+rX -R /ifs/e63data/reis-filho/reference &> /dev/null
