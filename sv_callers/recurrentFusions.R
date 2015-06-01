c8d74bc556a3b34d0dd91852d5c2b9c485358dd1
raylim <raylim@mm.st>
2015-06-01 17:42:06 -0400

    fixed defuse: new oncofuse, bug in recurrentFusions, added merge.R

--- sv_callers/recurrentFusions.R
+++ sv_callers/recurrentFusions.R
@@ -2,7 +2,6 @@
 
 suppressPackageStartupMessages(library("optparse"));
 
-
 options(warn = -1, error = quote({ traceback(); q('no', status = 1) }));
 
 optList <- list(
@@ -37,6 +36,7 @@ Data <- read.table(fn, sep = '\t', header = T, quote = '', comment.char = '', st
 sData <- Data[,c(opt$sampleCol, opt$geneCol1, opt$geneCol2)]
 sData <- t(apply(sData, 1, function(x) c(x[1], sort(c(x[2], x[3])))))
 sData <- sData[!duplicated(sData), ]
+sData <- sData[sData[,2] != "" & sData[,3] != "", ]
 
 genes <- list()
 for (i in 1:nrow(sData)) {
