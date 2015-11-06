37e6c147a1f8822dc0b873037301919c188971ae
raylim <raylim@mm.st>
2015-11-06 15:26:10 -0500

    fix output directory

--- mut_sigs/mutSigReport.mk
+++ mut_sigs/mutSigReport.mk
@@ -13,7 +13,7 @@ define mutsig-report-name-vcfs
 PHONY += $1_mutsig_report 
 $1_mutsig_report : mutsig_report/$1/index.html
 mutsig_report/$1/index.html : $2
-	$$(call LSCRIPT_NAMED_MEM,$1_mutsig_report,8G,20G,"$$(KNIT) $$(MUTSIG_REPORT) $(@D) --outDir $(@D) --name $1 --alexandrovData $$(ALEXANDROV_DATA) $$(if $$(TARGETS_FILE),--targetBed $$(TARGETS_FILE)) $$^")
+	$$(call LSCRIPT_NAMED_MEM,$1_mutsig_report,8G,20G,"$$(KNIT) $$(MUTSIG_REPORT) $$(@D) --outDir $$(@D) --name $1 --alexandrovData $$(ALEXANDROV_DATA) $$(if $$(TARGETS_FILE),--targetBed $$(TARGETS_FILE)) $$^")
 endef
 
 
