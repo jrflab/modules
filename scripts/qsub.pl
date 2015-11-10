70a4eb1ac0c559688c43b6b1314e9fc08399b83d
raylim <raylim@mm.st>
2015-11-10 15:10:29 -0500

    use qsub.pl by default and added file size check across nodes to qsub.pl

--- scripts/qsub.pl
+++ scripts/qsub.pl
@@ -6,16 +6,36 @@ use warnings;
 
 use Schedule::DRMAAc qw/ :all /;
 use File::Temp();
+use Cwd qw/ realpath /;
 use Cwd;
 
 use Getopt::Std;
 my %opt;
-getopts('ho:', \%opt);
+getopts('hco:', \%opt);
 
 my $usage = <<ENDL;
 Usage: perl qsub.pl -h -- [qsub args]
     -o [file]: check file for non-zero size
+    -c: check file across nodes for same file size
 ENDL
+
+sub check_file {
+    my ($cwd, $file) = @_;
+    my @nodes = qw/e01 e02 e03 e04 e05 e06/;
+    my $fileSize = `stat -c\%s $cwd/$file`;
+    chomp $fileSize;
+    #print "checking $cwd/$file on nodes ($fileSize)\n";
+    for my $node (@nodes) {
+        my $nodeFileSize = `ssh $node stat -c\%s $cwd/$file`;
+        chomp $nodeFileSize;
+        if ($fileSize != $nodeFileSize) {
+            #print "$node: file size does not match: $fileSize != $nodeFileSize\n";
+            return 0;
+        }
+    }
+    #print "$cwd/$file: all file sizes match\n";
+    return 1;
+}
 
 sub HELP_MESSAGE {
    print STDERR $usage;
@@ -81,11 +101,25 @@ die drmaa_strerror($error) . "\n" . $diagnosis if $error;
 ($error, $diagnosis) = drmaa_exit();
 die drmaa_strerror($error) . "\n" . $diagnosis if $error;
 
+my $fileStatus = 0;
 if ($opt{o} && (!-e $opt{o} || !-s $opt{o})) {
     sleep 60; # wait for file system to update
     system("rm $opt{o}");
     #print "File not removed\n" if (-e $opt{o});
-    die "$opt{o}: file is size 0";
+    $fileStatus = 99;
+    print STDERR "$opt{o}: file is size 0";
 }
 
-exit $exitStatus + $aborted + $signaled + $coreDumped;
+sleep 20; # wait for file sync
+if ($opt{c} && $opt{o} ne "NULL") {
+    my $i = 0;
+    while (!check_file(getcwd(), $opt{o})) {
+        if ($i++ > 30) {
+            $fileStatus = 77;
+            print STDERR "file sizes do not match across nodes";
+        }
+        sleep 10;
+    }
+}
+
+exit $exitStatus + $aborted + $signaled + $coreDumped + $fileStatus;
