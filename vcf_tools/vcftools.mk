5565d31591f2d7c33c9144cc9d5ce7b77fb4b36b
raylim <raylim@mm.st>
2015-11-13 13:50:58 -0500

    added exac_nontcga and clinvar annotations to vcfs

--- vcf_tools/vcftools.mk
+++ vcf_tools/vcftools.mk
@@ -77,6 +77,9 @@ endif
 %.clinvar.vcf : %.vcf %.vcf.idx 
 	$(call CHECK_VCF,$<,$@,$(call LSCRIPT_CHECK_MEM,9G,12G,"$(call SNP_SIFT_MEM,8G) annotate $(SNP_SIFT_OPTS) $(CLINVAR) $< > $@ && $(RM) $^"))
 
+%.exac_nontcga.vcf : %.vcf %.vcf.idx 
+	$(call CHECK_VCF,$<,$@,$(call LSCRIPT_CHECK_MEM,9G,12G,"$(call SNP_SIFT_MEM,8G) annotate $(SNP_SIFT_OPTS) -info AF $(EXAC_NONTCGA) $< > $@ && $(RM) $^"))
+
 
 # apply overall depth filter
 %.dp_ft.vcf : %.vcf
