1855b21d7a94758bf184620eee65602e0312088f
raylim <raylim@mm.st>
2015-11-16 13:10:51 -0500

    prefix AF field from non-tcga ExAC (ExAC_AF)

--- vcf_tools/vcftools.mk
+++ vcf_tools/vcftools.mk
@@ -78,7 +78,7 @@ endif
 	$(call CHECK_VCF,$<,$@,$(call LSCRIPT_CHECK_MEM,9G,12G,"$(call SNP_SIFT_MEM,8G) annotate $(SNP_SIFT_OPTS) $(CLINVAR) $< > $@ && $(RM) $^"))
 
 %.exac_nontcga.vcf : %.vcf %.vcf.idx 
-	$(call CHECK_VCF,$<,$@,$(call LSCRIPT_CHECK_MEM,9G,12G,"$(call SNP_SIFT_MEM,8G) annotate $(SNP_SIFT_OPTS) -info AF $(EXAC_NONTCGA) $< > $@ && $(RM) $^"))
+	$(call CHECK_VCF,$<,$@,$(call LSCRIPT_CHECK_MEM,9G,12G,"$(call SNP_SIFT_MEM,8G) annotate $(SNP_SIFT_OPTS) -info ExAC_AF $(EXAC_NONTCGA) $< > $@ && $(RM) $^"))
 
 
 # apply overall depth filter
