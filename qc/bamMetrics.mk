05496f835f8d33fa44cf402716d8b0d298fb3fc3
raylim <raylim@mm.st>
2015-12-02 17:17:48 -0500

    fix #73

--- qc/bamMetrics.mk
+++ qc/bamMetrics.mk
@@ -10,37 +10,43 @@ include modules/variant_callers/gatk.inc
 
 .SECONDARY: 
 
-.PHONY: all gc dup
+COLLECT_METRICS = $(JAVA) -Xmx8G -jar $(PICARD_DIR)/CollectMultipleMetrics.jar VALIDATION_STRINGENCY=LENIENT
+COLLECT_WGS_METRICS = $(JAVA) -Xmx8G -jar $(PICARD_JAR) CollectWgsMetrics VALIDATION_STRINGENCY=LENIENT
+COLLECT_GC_METRICS = $(JAVA) -Xmx8G -jar $(PICARD_DIR)/CollectGcBiasMetrics.jar VALIDATION_STRINGENCY=LENIENT
 
-COLLECT_METRICS = $(JAVA) -Xmx8G -jar $(JARDIR)/CollectMultipleMetrics.jar VALIDATION_STRINGENCY=LENIENT
-COLLECT_GC_METRICS = $(JAVA) -Xmx8G -jar $(JARDIR)/CollectGcBiasMetrics.jar VALIDATION_STRINGENCY=LENIENT
+PHONY += bam_metrics
+bam_metrics : summary_metrics gc flagstats wgs_metrics
 
-all : summary_metrics gc flagstats
-
+PHONY += flagstats
 flagstats : $(foreach sample,$(SAMPLES),metrics/$(sample).flagstats)
-
+PHONY += summary_metrics
 summary_metrics : $(foreach sample,$(SAMPLES),metrics/$(sample).alignment_summary_metrics)
-
+PHONY += wgs_metrics
+wgs_metrics : $(foreach sample,$(SAMPLES),metrics/$(sample).wgs_metrics)
+PHONY += dup
 dup : $(foreach sample,$(SAMPLES),metrics/$(sample).dup_metrics)
-
+PHONY += gc
 gc : $(foreach sample,$(SAMPLES),metrics/$(sample).gc_bias_metrics)
 
 metrics/%.alignment_summary_metrics : bam/%.bam
-	$(call LSCRIPT_MEM,12G,13G,"$(COLLECT_METRICS) I=$< O=metrics/$* REFERENCE_SEQUENCE=$(REF_FASTA) &> $(LOGDIR)/$(@F).log")
+	$(call LSCRIPT_MEM,12G,13G,"$(COLLECT_METRICS) I=$< O=metrics/$* REFERENCE_SEQUENCE=$(REF_FASTA)")
+
+metrics/%.wgs_metrics : bam/%.bam
+	$(call LSCRIPT_MEM,12G,13G,"$(COLLECT_WGS_METRICS) I=$< O=$@ REFERENCE_SEQUENCE=$(REF_FASTA)")
 
 metrics/%.gc_bias_metrics : bam/%.bam
-	$(call LSCRIPT_MEM,12G,13G,"$(COLLECT_GC_METRICS) I=$< O=$@ CHART_OUTPUT=$(addsuffix .pdf,$@) REFERENCE_SEQUENCE=$(REF_FASTA) &> $(LOGDIR)/$(@F).log")
+	$(call LSCRIPT_MEM,12G,13G,"$(COLLECT_GC_METRICS) I=$< O=$@ CHART_OUTPUT=$(addsuffix .pdf,$@) REFERENCE_SEQUENCE=$(REF_FASTA)")
 
 metrics/%.flagstats : bam/%.bam
-	$(call LSCRIPT_MEM,2G,3G,"$(SAMTOOLS) flagstat $< > $@ 2> $(LOGDIR)/$(@F).log")
+	$(call LSCRIPT_MEM,2G,3G,"$(SAMTOOLS) flagstat $< > $@")
 	
 bam/%.markdup.bam metrics/%.dup_metrics : bam/%.bam
-	$(call LSCRIPT_MEM,18G,19G,"$(MARK_DUP) I=$< O=bam/$*.markdup.bam METRICS_FILE=metrics/$*.dup_metrics &> $(LOGDIR)/$(@F).log")
+	$(call LSCRIPT_MEM,18G,19G,"$(MARK_DUP) I=$< O=bam/$*.markdup.bam METRICS_FILE=metrics/$*.dup_metrics")
 
 metrics/dup_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample).dup_metrics.txt)
 	$(INIT) grep '^LIBRARY' $< > $@ && \
 	for metrics in $^; do \
 	    grep -A1 '^LIBRARY' $$metrics | sed '1d' >> $@; \
 	done
-# print mean, min, max
-# awk 'BEGIN {min = 1; max = 0; } NR > 1 {if ($8 > max) {max = $8; } if ($8 < min) {min = $8;} total += $8} END {print total / (NR-1), min, max }' metrics/dup_metrics.txt
+
+.PHONY: $(PHONY)
