8a703978aa51d037518455c5b588e167fe15368f
Ino de Bruijn <ino@ino.pm>
2016-01-14 19:18:46 -0500

    Add facets/absolute/custom to excel and plot
    - Get cancer_cell_frac Pr_somatic_clonal and ccf_CI95_low from absolute results.
    - Get facets LOH from facets/geneCN.em.reviewed.txt. Basically the original
      facets file after reviewing
    - Get whatever other type of annotations for the mutations from
      summary/annotation.tsv merge on TUMOR_SAMPLE CHROM POS REF ALT columns
    - Add MUTATION_SUMMARY sheet in excel that lists all NS and S mutations. Remove
      annotations that are not NS and S from | separated columns
    - Add likely_pathogenic
            - for snv: fathmm_pred is "CANCER" and chasm score > 0.3
            - for indel: dbNSFP_PROVEAN_PRED is D or dbNSFP_MutationTaster_pred is
              D
    - Add cancer_gene
            - either cancer_gene_census, kandoth or lawrence is true
    - Plot heatmaps for NS mutations in all samples using the excel file
            - ccf from absolute
            - LOH from facets
            - likely_pathogenic
            - cancer_gene

--- summary/mutationSummary.mk
+++ summary/mutationSummary.mk
@@ -1,7 +1,8 @@
 include modules/Makefile.inc
 include modules/variant_callers/somatic/somaticVariantCaller.inc
 
-LOGDIR ?= log/mutation_summary.$(NOW)
+LOGDIR = log/summary.$(NOW)
+
 
 ALLTABLES_HIGH_MODERATE_MUTECT = alltables/allTN.$(call SOMATIC_VCF_SUFFIXES,mutect).tab.high_moderate.novel.txt
 ALLTABLES_LOW_MODIFIER_MUTECT = alltables/allTN.$(call SOMATIC_VCF_SUFFIXES,mutect).tab.low_modifier.novel.txt
@@ -12,22 +13,31 @@ ALLTABLES_LOW_MODIFIER_STRELKA_VARSCAN = alltables/allTN.strelka_varscan_indels.
 ALLTABLES_SYNONYMOUS_STRELKA_VARSCAN = alltables/allTN.strelka_varscan_indels.tab.synonymous.novel.txt
 ALLTABLES_NONSYNONYMOUS_STRELKA_VARSCAN = alltables/allTN.strelka_varscan_indels.tab.nonsynonymous.novel.txt
 
-#ABSOLUTE_SOMATIC_TXTS ?= $(foreach sample,$(SAMPLE_SETS),absolute/tables/$(set).somatic.txt)
-#ABSOLUTE_SEGMENTS ?= $(foreach sample,$(SAMPLE_SETS),absolute/reviewed/SEG_MAF/$(set)_ABS_MAF.txt)
+# Add optional absolute results to excel
+# the $(wildcard x) syntax is used to check for existence of file
+ABSOLUTE_SOMATIC_TXTS ?= $(wildcard $(foreach set,$(SAMPLE_SETS),absolute/tables/$(set).somatic.txt))
+ABSOLUTE_SEGMENTS ?= $(wildcard $(foreach set,$(SAMPLE_SETS),absolute/reviewed/SEG_MAF/$(set)_ABS_MAF.txt))
+ifneq ($(ABSOLUTE_SOMATIC_TXTS)$(ABSOLUTE_SEGMENTS),)
+EXCEL_ABSOLUTE_PARAMS = --absolute_segments $(subst $(space),$(,),$(strip $(ABSOLUTE_SEGMENTS))) --absolute_somatic_txts $(subst $(space),$(,),$(strip $(ABSOLUTE_SOMATIC_TXTS)))
+endif
+
+# Add optional facets LOH to excel
+EXCEL_FACETS_LOH ?= $(wildcard facets/geneCN.em.reviewed.txt)
+ifneq ($(EXCEL_FACETS_LOH),)
+EXCEL_FACETS_LOH_PARAMS = --facets_em_reviewed $(EXCEL_FACETS_LOH)
+endif
+
+# Add optional annotations file to excel, used to add any other desired columns
+# to the excel file inner merge on TUMOR_SAMPLE CHROM POS REF ALT
+EXCEL_ANNOTATIONS ?= $(wildcard summary/annotation.tsv)
+ifneq ($(EXCEL_ANNOTATIONS),)
+EXCEL_ANNOTATIONS_PARAMS = --annotation_tsv $(EXCEL_ANNOTATIONS)
+endif
+
 
 mutation_summary: summary/mutation_summary.xlsx
 
-#SUMMARY_MERGE_ABSOLUTE ?= false
-
-
-#ifeq ($(EXCEL_MERGE_ABSOLUTE),false)
-summary/mutation_summary.xlsx : $(ALLTABLES_HIGH_MODERATE_MUTECT) $(ALLTABLES_LOW_MODIFIER_MUTECT) $(ALLTABLES_SYNONYMOUS_MUTECT) $(ALLTABLES_NONSYNONYMOUS_MUTECT) $(ALLTABLES_HIGH_MODERATE_STRELKA_VARSCAN) $(ALLTABLES_LOW_MODIFIER_STRELKA_VARSCAN) $(ALLTABLES_SYNONYMOUS_STRELKA_VARSCAN) $(ALLTABLES_NONSYNONYMOUS_STRELKA_VARSCAN)
+summary/mutation_summary.xlsx : $(ALLTABLES_HIGH_MODERATE_MUTECT) $(ALLTABLES_LOW_MODIFIER_MUTECT) $(ALLTABLES_SYNONYMOUS_MUTECT) $(ALLTABLES_NONSYNONYMOUS_MUTECT) $(ALLTABLES_HIGH_MODERATE_STRELKA_VARSCAN) $(ALLTABLES_LOW_MODIFIER_STRELKA_VARSCAN) $(ALLTABLES_SYNONYMOUS_STRELKA_VARSCAN) $(ALLTABLES_NONSYNONYMOUS_STRELKA_VARSCAN) $(ABSOLUTE_SOMATIC_TXTS) $(ABSOLUTE_SEGMENTS) $(EXCEL_FACETS_LOH) $(EXCEL_ANNOTATIONS)
 	$(INIT) unset PYTHONPATH; \
 	source $(ANACONDA_27_ENV)/bin/activate $(ANACONDA_27_ENV); \
-	python modules/scripts/mutation_summary_excel.py $^ $@
-#else
-#summary/mutation_summary.xlsx : $(ALLTABLES_HIGH_MODERATE_MUTECT) $(ALLTABLES_LOW_MODIFIER_MUTECT) $(ALLTABLES_SYNONYMOUS_MUTECT) $(ALLTABLES_NONSYNONYMOUS_MUTECT) $(ALLTABLES_HIGH_MODERATE_STRELKA_VARSCAN) $(ALLTABLES_LOW_MODIFIER_STRELKA_VARSCAN) $(ALLTABLES_SYNONYMOUS_STRELKA_VARSCAN) $(ALLTABLES_NONSYNONYMOUS_STRELKA_VARSCAN) $(ABSOLUTE_SOMATIC_TXTS) $(ABSOLUTE_SEGMENTS)
-#	$(INIT) unset PYTHONPATH; \
-	source $(ANACONDA_27_ENV)/bin/activate $(ANACONDA_27_ENV); \
-	python modules/scripts/mutation_summary_excel.py --absolute_somatic_txts $(subst $(space),$(,),$(strip $(ABSOLUTE_SOMATIC_TXTS))) --absolute_segments $(subst $(space),$(,),$(strip $(ABSOLUTE_SEGMENTS))) $(wordlist 1,8,$^) $@
-#endif
+	python modules/scripts/mutation_summary_excel.py --output_tsv_dir $(@D)/tsv $(EXCEL_ABSOLUTE_PARAMS) $(EXCEL_FACETS_LOH_PARAMS) $(EXCEL_ANNOTATIONS_PARAMS) $(wordlist 1,8,$^) $@
