6c03110f9c8f5175e7b607d7cd2ab4699041f2bf
raylim <raylim@mm.st>
2016-02-16 16:44:48 -0500

    vcf2maf and mutation-signatures for mutect vcfs

--- mut_sigs/mutSigReport.mk
+++ mut_sigs/mutSigReport.mk
@@ -10,8 +10,14 @@ MUTSIG_REPORT = modules/mut_sigs/mutSigReport.Rmd
 KNIT = $(RSCRIPT) modules/scripts/knit.R
 ALEXANDROV_DATA = $(HOME)/share/reference/sanger_30_mutsig_prob.txt
 
-mutect_mutsig_reports : $(foreach pair,$(SAMPLE_PAIRS),mutsig_report/mutect/$(pair)_mutsig_report.timestamp)
+MAKE_TRINUC_MAF = $(PYTHON) $(HOME)/share/usr/mutation-signatures/make_trinuc_maf.py
+MUT_SIG_MAIN = $(PYTHON) $(HOME)/share/usr/mutation-signatures/main.py
+MUT_SIGS = $(HOME)/share/usr/mutation-signatures/Stratton_signatures30.txt
+
+mutect_mutsig_reports : $(foreach pair,$(SAMPLE_PAIRS),mutsig_report2/mutect/$(pair)_mutsig_report.timestamp)
 
 mutsig_report/mutect/%_mutsig_report.timestamp : $(foreach suff,$(call SOMATIC_VCF_SUFFIXES,mutect),vcf/%.$(suff).vcf)
 	$(call LSCRIPT_NAMED_MEM,$*_mutect_mutsig_report,8G,40G,"$(KNIT) $(MUTSIG_REPORT) $(@D)/$* --outDir $(@D) --name $* --alexandrovData $(ALEXANDROV_DATA) $(if $(TARGETS_FILE),--targetBed $(TARGETS_FILE)) $^ && touch $@")
 
+mutsig_report2/mutect/%_mutsig_report.timestamp : $(foreach pair,maf/$(pair).$(call SOMATIC_FILTER_SUFFIX,mutect).pass.maf)
+	$(call LSCRIPT_NAMED_MEM,$*_mutect_mutsig_report2,8G,12G,"$(MUT_SIG_MAIN) $(MUT_SIGS) $< $(@D)/$* && touch $@")
--- vcf_tools/vcftools.mk
+++ vcf_tools/vcftools.mk
@@ -367,12 +367,39 @@ CN_BREAST_BED = $(foreach set,$(CN_BREAST_SUBTYPES), $(HOME)/share/reference/ann
 ifdef SAMPLE_PAIRS
 ANNOTATE_FACETS_VCF = $(RSCRIPT) modules/copy_number/annotateFacets2Vcf.R
 define annotate-facets-pair
-vcf/$1_$2.%.facets.vcf : vcf/$1_$2.%.vcf facets/$1_$2.cncf.txt
+vcf/$1.%.facets.vcf : vcf/$1.%.vcf facets/$1.cncf.txt
 	$$(call LSCRIPT_MEM,4G,6G,"$$(ANNOTATE_FACETS_VCF) --facetsFile $$(<<) --outFile $$@ $$<")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),$(eval $(call annotate-facets-pair,$(pair))))
 endif
 
+ifdef SAMPLE_PAIRS
+define vcf2maf-tumor-normal
+maf/$1_$2.%.maf : vcf/$1_$2.%.vcf
+	$$(call LSCRIPT_MEM,9G,12G,"$$(VCF2MAF) --tumor-id $1 --normal-id $2 --ref-fasta $$(REF_FASTA) --vep-path $$(VEP_PATH) --vep-data $$(VEP_DATA)")
+endef
+$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call vcf2maf-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+
+allmaf/allTN.%.maf : $(foreach pair,$(SAMPLE_PAIRS),maf/$(pair).%.maf)
+	$(INIT) \
+	{ \
+	head -2 $< \
+	sed 1,2d $^ \
+	} > $@
+endif
+
+define vcf2maf-sample
+maf/$1.%.maf : vcf/$1.%.vcf
+	$$(call LSCRIPT_MEM,9G,12G,"$$(VCF2MAF) --tumor-id $1 --ref-fasta $$(REF_FASTA) --vep-path $$(VEP_PATH) --vep-data $$(VEP_DATA)")
+endef
+$(foreach sample,$(SAMPLES),$(eval $(call vcf2maf-sample,$(sample))))
+
+allmaf/all.%.maf : $(foreach sample,$(SAMPLES),maf/$(sample).%.maf)
+	$(INIT) \
+	{ \
+	head -2 $< \
+	sed 1,2d $^ \
+	} > $@
 
 endif
 VCFTOOLS_MK = true
