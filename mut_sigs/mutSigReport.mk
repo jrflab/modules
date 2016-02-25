f1aa034f985a3e135c3636100d1aa007ac432af1
raylim <raylim@mm.st>
2016-02-25 18:03:09 -0500

    typo for function call

--- mut_sigs/mutSigReport.mk
+++ mut_sigs/mutSigReport.mk
@@ -3,22 +3,25 @@ include modules/Makefile.inc
 include modules/variant_callers/variantCaller.inc
 include modules/variant_callers/somatic/somaticVariantCaller.inc
 
-
 LOGDIR = log/mutsig_report.$(NOW)
 
-MUTSIG_REPORT = modules/mut_sigs/mutSigReport2.Rmd
 VCF2VRANGES = $(RSCRIPT) modules/mut_sigs/vcf2VRanges.R
 KNIT = $(RSCRIPT) modules/scripts/knit.R
 ALEXANDROV_DATA = $(HOME)/share/reference/sanger_30_mutsig_prob.txt
+MUTSIG_REPORT = modules/mut_sigs/mutSigReport2.Rmd
+MUTSIG_REPORT_OPTS = --name $(PROJECT_NAME) \
+					 --alexandrovData $(ALEXANDROV_DATA) \
+					 $(if $(TARGETS_FILE),--targetBed $(TARGETS_FILE))
 
 
 .SECONDARY:
 .DELETE_ON_ERROR:
+.PHONY: mutect_mutsig_reports
 
 mutect_mutsig_reports : mutsig_report/mutect/mutsig_report.timestamp
 
 mutsig_report/mutect/mutsig_report.timestamp : $(foreach pair,$(SAMPLE_PAIRS),mutsig_report/vrange/$(pair).$(call SOMATIC_VCF_SUFFIXES,mutect).VRanges.Rdata)
-	$(call LSCRIPT_NAMED_PALLLEL_MEM,$*_mutect_mutsig_report,4,3G,5G,"$(KNIT) $(MUTSIG_REPORT) $(@D)/$* --ncores 4 --outDir $(@D) --name $(PROJECT_NAME) --alexandrovData $(ALEXANDROV_DATA) $(if $(TARGETS_FILE),--targetBed $(TARGETS_FILE)) $^ && touch $@")
+	$(call LSCRIPT_NAMED_PARALLEL_MEM,mutect_mutsig_report,4,3G,5G,"$(KNIT) $(MUTSIG_REPORT) $(@D) --ncores 4 --outDir $(@D) $(MUTSIG_REPORT_OPTS) $^ && touch $@")
 
 mutsig_report/vrange/%.VRanges.Rdata : vcf/%.vcf
 	$(call LSCRIPT_MEM,7G,10G,"$(VCF2VRANGES) --genome $(REF) --outFile $@ $<")
