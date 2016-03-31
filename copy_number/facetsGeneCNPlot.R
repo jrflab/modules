714d12ab83872d34f6a2500136f5221ef460212a
Ino de Bruijn <ino@ino.pm>
2016-03-30 20:06:12 -0400

    facetsGeneCNPlot update screenshot, add image test
    - change some styling parameters for facetsGeneCNPlot
    - Uploads image to clbin if test image differs, convert pdf to png if necessary

--- copy_number/facetsGeneCNPlot.R
+++ copy_number/facetsGeneCNPlot.R
@@ -45,7 +45,7 @@ plot_heatmap <- function(facets_tab, plot_file, sample_column_postfix, fontfamil
     if (is.null(sample_names)) { sample_names <- list(sort(colnames(mm)[sapply(colnames(mm), function(x) {grepl(paste(sample_column_postfix,"$",sep=""), x)})])) }
     chrsep <- cumsum(rle(mm$chrom)$lengths)
     chrmid <- c(0,chrsep[-length(chrsep)]) + (rle(mm$chrom)$lengths/2)
-    pdf(plot_file, width=12, height=20*length(sample_names))
+    pdf(plot_file, width=12, height=.8*length(sample_names[[1]]))
     par(mfrow=c(length(sample_names),1), mar=c(8,.5*(max(sapply(sample_names,nchar))-nchar(sample_column_postfix)),1,2))
     lapply(sample_names, function(x, mm) {
         mm2 <- mm[,rev(x)]; #for (i in 1:ncol(mm2)) { mm2[,i] <- as.numeric(mm2[,i]) }
@@ -54,7 +54,7 @@ plot_heatmap <- function(facets_tab, plot_file, sample_column_postfix, fontfamil
         for (i in (chrsep*2)-1) { abline(v=i/((max(chrsep)-1)*2), col="grey") }
         for (i in seq(-1, max(((2*(ncol(mm2)-1))+1),1), 2)) { abline(h=i/(2*(ncol(mm2)-1)), col="white", lwd=2)}
     axis(1,at=chrmid/(max(chrsep)-1), label=rle(mm$chrom)$values, cex.axis=0.8, tick=F, family=fontfamily)
-    axis(2,at=seq(0,1,1/max((ncol(mm2)-1),1)), label=sub(sample_column_postfix, "", colnames(mm2)), las=2, cex.axis=1, tick=F, family=fontfamily)
+    axis(2,at=seq(0,1,1/max((ncol(mm2)-1),1)), label=sub("T_.*N$", "", sub(sample_column_postfix, "", colnames(mm2))), las=2, cex.axis=1, tick=F, family=fontfamily)
     }, mm)
     legend("bottom", inset=c(0,-.4), legend=c("Homozygous deletion", "Loss", "Gain", "Amplification"),
            fill=c("red", "darksalmon", "lightblue", "blue"), xpd=T, ncol=2)
