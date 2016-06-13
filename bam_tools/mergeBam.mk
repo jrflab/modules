2830a755c4d55eaabd3f4c4446cbbb169685d846
raylim <raylim@mm.st>
2016-06-13 17:17:15 -0400

    merge bam files using a merge yaml file

--- bam_tools/mergeBam.mk
+++ bam_tools/mergeBam.mk
@@ -7,18 +7,28 @@ LOGDIR = log/merge.$(NOW)
 .DELETE_ON_ERROR: 
 .PHONY : merged_bam
 
-merged_bam : $(foreach sample,$(SPLIT_SAMPLES),bam/$(sample).bam bam/$(sample).bam.bai)
+merged_bam : $(foreach sample,$(MERGE_SAMPLES),bam/$(sample).bam bam/$(sample).bam.bai)
 
 define merged-bam
-merged_bam/$1.header.sam : $$(foreach split,$$(split.$1),bam/$$(split).bam)
-	$$(INIT) $$(SAMTOOLS) view -H $$< | grep -v '^@RG' > $$@.tmp; \
-	for bam in $$^; do $$(SAMTOOLS) view -H $$$$bam | grep '^@RG' >> $$@.tmp; done; \
-	uniq $$@.tmp > $$@ && $$(RM) $$@.tmp
+%.header.sam : %.bam
+	$$(INIT) $$(SAMTOOLS2) view -H $$< > $$@
 
-merged_bam/$1.bam : merged_bam/$1.header.sam $$(foreach split,$$(split.$1),bam/$$(split).bam)
-	$$(call LSCRIPT_MEM,12G,15G,"$$(SAMTOOLS) merge -f -h $$< $$(@) $$(filter %.bam,$$^)")
+merged_bam/$1.header.sam : $$(merge.$1:.bam=.header.sam)
+	$$(call LSCRIPT_MEM,8G,9G,"$$(call PICARD,MergeSamFiles,9G) $$(foreach sam,$$^,I=$$(sam) ) O=$$@")
+
+merged_bam/$1.bam : merged_bam/$1.header.sam $$(merge.$1)
+	$$(call LSCRIPT_MEM,12G,15G,"$$(SAMTOOLS2) merge -f -h $$< $$(@) $$(filter %.bam,$$^)")
 endef
-$(foreach sample,$(SPLIT_SAMPLES),$(eval $(call merged-bam,$(sample))))
+define rename-bam
+bam/$1.bam : $2
+	$$(INIT) ln -f $$< $$@
+endef
+$(foreach sample,$(MERGE_SAMPLES),\
+	$(if $(word 2,$(merge.$(sample))),\
+	$(eval $(call merged-bam,$(sample))),\
+	$(if $(merge.$(sample)),\
+	$(eval $(call rename-bam,$(sample),$(merge.$(sample)))))))
+
 
 bam/%.bam : merged_bam/%.rg.bam
 	$(INIT) ln -f $< $@
