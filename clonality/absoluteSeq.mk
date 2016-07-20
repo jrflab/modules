9a04c755d756b5a38b436ef73919a891aa69bd6b
hxrts <bermans@mskcc.org>
2016-07-20 14:51:45 -0400

    respect special characters in column names

--- clonality/absoluteSeq.mk
+++ clonality/absoluteSeq.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 include modules/variant_callers/somatic/somaticVariantCaller.inc
 
 LOGDIR = log/absoluteSeq.$(NOW)
-MEM := 2G
+MEM := 4G
 PE := 1
 SHELL = modules/scripts/Rshell
 .SHELLFLAGS = $(if $(findstring true,$(USE_CLUSTER)),-s) -m $(MEM) -p $(PE) -n $(@F) -l $(LOGDIR) -e 
@@ -29,54 +29,66 @@ TITAN_ESTIMATE_FILE ?= $(TITAN_RESULTS_DIR)/titan_summary.txt
 
 USE_ONCOSCAN_COPYNUM ?= false
 
-USE_FACETS_COPYNUM ?= false
+USE_FACETS_COPYNUM ?= true
 
 define LIB_INIT
-library(ABSOLUTE)
-library(dplyr)
-library(readr)
+pacman::p_load(ABSOLUTE, dplyr, stringr, readr)
 endef
 
-
 absolute/tables/%.somatic.txt : tables/%.$(call SOMATIC_VCF_SUFFIXES,mutect).tab.txt tables/%.strelka_varscan_indels.tab.txt
+
 	$(R_INIT)
 	$(LIB_INIT)
+
 	tn <- unlist(strsplit("$*", '_'))
-	snvs <- read.table("$<", header = T, sep = '\t', comment.char = '', as.is = T)
+	snvs <- read.table("$<", header = T, sep = '\t', comment.char = '', as.is = T, check.names = F)
 	snvs.tref <- c()
 	snvs.talt <- c()
+
 	if (nrow(snvs) > 0) {
 		snvs.tref <- sapply(strsplit(snvs[[paste(tn[1], ".AD", sep = '')]], ','), function (x) x[1])
 		snvs.talt <- sapply(strsplit(snvs[[paste(tn[1], ".AD", sep = '')]], ','), function (x) x[2])
 	}
-	indels <- read.table("$(<<)", header = T, sep = '\t', comment.char = '', as.is = T)
+
+	indels <- read.table("$(<<)", header = T, sep = '\t', comment.char = '', as.is = T, check.names = F)
 	indels.tref <- c()
 	indels.talt <- c()
 	if (nrow(indels) > 0) {
 		indels.tref <- sapply(strsplit(indels[[paste(tn[1], ".AD", sep = '')]], ','), function (x) x[1])
 		indels.talt <- sapply(strsplit(indels[[paste(tn[1], ".AD", sep = '')]], ','), function (x) x[2])
 	}
-	chr <- c(snvs[["X.CHROM"]], indels[["X.CHROM"]])
-	chr <- as.integer(sub('X', '23', chr))
-	ref <- c(snvs[["REF"]], indels[["REF"]])
-	alt <- c(snvs[["ALT"]], indels[["ALT"]])
-	genes <- c(snvs[['ANN....GENE']], indels[['ANN....GENE']])
-	pos <- c(snvs[["POS"]], indels[["POS"]])
+
+	chr       <- c(snvs[["#CHROM"]], indels[["#CHROM"]])
+	chr       <- as.integer(sub('X', '23', chr))
+	ref       <- c(snvs[["REF"]], indels[["REF"]])
+	alt       <- c(snvs[["ALT"]], indels[["ALT"]])
+	genes     <- c(snvs[['ANN[*].GENE']], indels[['ANN[*].GENE']])
+	pos       <- c(snvs[["POS"]], indels[["POS"]])
 	tRefCount <- c(snvs.tref, indels.tref)
-	tAltCount = c(snvs.talt, indels.talt)
-	type = c(rep('snv', nrow(snvs)), rep('indel', nrow(indels)))
-	Data <- data.frame(Sample = "$*", Gene = genes, Chromosome = chr, Position = pos, Ref = ref, Alt = alt, tRefCount = tRefCount, tAltCount = tAltCount, Type = type, stringsAsFactors = F)
-	Data[["Gene"]] <- sub('\\|.*', '', Data[["Gene"]])
-	Data <- subset(Data, Gene != ".")
-	x <- with(Data, paste(Chromosome, Position, sep = ":"))
+	tAltCount <- c(snvs.talt, indels.talt)
+	type      <- c(rep('snv', nrow(snvs)), rep('indel', nrow(indels)))
+
+	Data <-
+		data_frame( Sample           = "$*",
+					Gene             = sub('\\|.*', '', genes),
+					Chromosome       = chr,
+					Position         = pos,
+					Ref              = ref,
+					Alt              = alt,
+					tRefCount        = tRefCount,
+					tAltCount        = tAltCount,
+					Type             = type ) %>%
+		filter(Gene!=".")
+
+	x <- with(Data, paste(Chromosome, Position, sep=":"))
 	Data <- subset(Data, !duplicated(x))
-	write.table(Data, file = "$@", sep = '\t', quote = F, row.names = F)
+	write_tsv(Data, path="$@")
 
 
 absolute/maf/%.maf.txt : absolute/tables/%.somatic.txt
 	$(R_INIT)
 	$(LIB_INIT)
-	X <- read.table("$(<)", header = T, sep = '\t', comment.char = '', as.is = T)
+	X <- read.table("$(<)", header = T, sep = '\t', comment.char = '', as.is = T, check.names = F)
 	Data <- with(X, data.frame(Tumor_Sample_Barcode = Sample, Hugo_Symbol = Gene, t_ref_count = tRefCount, t_alt_count = tAltCount, dbSNP_Val_Status = "validated", Chromosome = Chromosome, Start_position = Position, stringsAsFactors = F))
 	write.table(Data, file = "$@", sep = '\t', quote = F, row.names = F)
 
@@ -112,14 +124,14 @@ absolute/segment/%.seg.txt : facets/cncf/%.cncf.txt
 	$(LIB_INIT)
 	cncf <-
 		read.table("$<", stringsAsFactors=F, header=T, sep="\t") %>%
-		select(ID, Chromosome=chrom, Start=loc.start, End=loc.end, seg, Num_Probes=num.mark, nhet, Segment_mean=cnlr.median, mafR, segclust, cnlr.median.clust, cf.em, tcn.em, lcn.em)
+		select(ID, Chromosome=chrom, Start=loc.start, End=loc.end, seg, Num_Probes=num.mark, nhet, Segment_Mean=cnlr.median, mafR, segclust, cnlr.median.clust, cf.em, tcn.em, lcn.em)
 
-	cncf %>% write_tsv("$@")
+	cncf %>% write_tsv(path="$@")
 else
 absolute/segment/%.seg.txt : varscan/segment/%.collapsed_seg.txt
 	$(R_INIT)
 	$(LIB_INIT)
-	X <- read.table("$<", header = T, sep = '\t', as.is = T)
+	X <- read.table("$<", header = T, sep = '\t', as.is = T, check.names = F)
 	colnames(X) <- c('Chromosome', 'Start', 'End', 'Num_Probes', 'Segment_Mean')
 	write.table(X, file = "$@", row.names = F, quote = F, sep = '\t')
 endif
@@ -129,7 +141,7 @@ $(info absolute/results/%.ABSOLUTE.RData : absolute/segment/%.seg.txt absolute/m
 absolute/results/%.ABSOLUTE.RData : absolute/segment/%.seg.txt absolute/maf/%.maf.txt $(TITAN_ESTIMATE_FILE)
 	$(R_INIT)
 	$(LIB_INIT)
-	titanResults <- read.table("$(<<<)", row.names = 1, header = T)
+	titanResults <- read.table("$(<<<)", row.names = 1, header = T, check.names = F)
 	sigma.p <- 0
 	max.sigma.h <- 0.07
 	avgTumorPloidyEst <- titanResults["$*", "avgTumorPloidyEst"]
@@ -189,7 +201,7 @@ absolute/tables/%.absolute.txt : absolute/reviewed/all.seq.ABSOLUTE.table.txt ab
 	$(R_INIT)
 	$(LIB_INIT)
 	fn <- 'absolute/reviewed/SEG_MAF/$*_ABS_MAF.txt'
-	absData <- read.table(fn, sep = '\t', header = T, as.is = T)
+	absData <- read.table(fn, sep = '\t', header = T, as.is = T, check.names = F)
 	tn <- absData[['sample']][1]
 	somatic <- read.table("$(<<)", header = T, sep = '\t', comment.char = '', as.is = T)
 	absData[, "refSeq"] <- somatic[, "Ref"]
