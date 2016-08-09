1f4bc07af5fdc3673453aa793c236c4f3c397276
raylim <raylim@mm.st>
2016-08-09 14:17:29 -0400

    merge configure script from master

--- test/scripts/test_configure.sh
+++ test/scripts/test_configure.sh
@@ -9,6 +9,6 @@ TEST_DATA_DIR=${DIR}/test_data
 TEST_DATA_OUT_DIR=${DIR}/test_data/output
 
 mkdir -p ${DIR}/output
-python ${SCRIPT_DIR}/configure.py --config_file ${DIR}/../../config.yaml --project_config_file ${DIR}/../../default_project_config.yaml --samples_file ${TEST_DATA_DIR}/samples.yaml --sample_fastq_file ${TEST_DATA_DIR}/sample.fastq.yaml --out_file ${OUT_DIR}/project_config.inc
+python ${SCRIPT_DIR}/configure.py --project_config_file ${DIR}/../../default_project_config.yaml --samples_file ${TEST_DATA_DIR}/samples.yaml --sample_fastq_file ${TEST_DATA_DIR}/sample.fastq.yaml --out_file ${OUT_DIR}/project_config.inc
 compare_files ${OUT_DIR}/project_config.inc ${TEST_DATA_OUT_DIR}/project_config.inc
 
