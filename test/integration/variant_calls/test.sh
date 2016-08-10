e976152245ca3312901d0e03f4d02ed342d00395
raylim <raylim@mm.st>
2016-08-10 16:30:31 -0400

    integration test for variant calling

---
+++ test/integration/variant_calls/test.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/../../test_functions.sh
+set -e
+
+echo "TEST VARIANT CALLING test_data"
+cd ${DIR}/test_data
+# create project_config.inc
+python modules/configure.py
+echo "==mutect=="
+echo "run"
+make mutect && echo "success" || (echo "failed" && exit 1)
+echo "check files"
+for x in output/vcf/*mutect*.vcf; do
+    diff_files $x vcf/$(basename $x)
+done
+echo "==strelka=="
+echo "run"
+make strelka && echo "success" || (echo "failed" && exit 1)
+echo "check files"
+for x in output/vcf/*.strelka_{snps,indels}.vcf; do
+    diff_files $x vcf/$(basename $x)
+done
+echo "==varscan=="
+echo "run"
+make varscanTN && echo "success" || (echo "failed" && exit 1)
+echo "check files"
+for x in output/vcf/*.varscan_{snps,indels}.vcf; do
+    diff_files $x vcf/$(basename $x)
+done
+echo "==hotspot=="
+echo "run"
+make hotspot && echo "success" || (echo "failed" && exit 1)
+echo "check files"
+for x in output/vcf_ann/*hotspot*.vcf; do
+    diff_files $x vcf_ann/$(basename $x)
+done
+make ann_somatic_vcf && echo "success" || (echo "failed" && exit 1)
+for x in output/vcf_ann/*.vcf; do
+    diff_files $x vcf_ann/$(basename $x)
+done
+make mutation_summary && echo "success" || (echo "failed" && exit 1)
+echo "check files"
+compare_files output/summary/mutation_summary.xlsx summary/mutation_summary.xlsx
+exit 0
