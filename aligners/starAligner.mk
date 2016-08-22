2fc664038df89ca61e6f61b907e9ff2d3b6476f7
raylim <raylim@mm.st>
2016-08-22 11:04:47 -0400

    add star fusion aligner workflow

--- aligners/starAligner.mk
+++ aligners/starAligner.mk
@@ -1,10 +1,5 @@
-# This module is for the Tophat aligner
+# This module is for the star aligner
 # input: $(SAMPLES) 
-# Options: 
-# 	BAM_PHRED64 = true/false
-# 	NO_NOVEL_SPLICING = true/false
-# 	NUM_CORES = 4
-#	INNER_MATE_DIST = 200
 include modules/Makefile.inc
 
 BAM_NO_REALN = true
@@ -20,30 +15,37 @@ LOGDIR = log/star.$(NOW)
 SEQ_PLATFROM = illumina
 
 STAR_CHIMERIC ?= true
+STAR_CHIMERIC_OPTS = --twopassMode Basic --outReadsUnmapped None --chimSegmentMin 12 --chimJunctionOverhangMin 12 \
+					 --alignSJDBoverhangMin 10 --alignMatesGapMax 200000 --alignIntronMax 200000 \
+					 --chimSegmentReadGapMax parameter 3 --alignSJstitchMismatchNmax 5 -1 5 5 \
+					 --chimOutType WithinBAM
 STAR = $(HOME)/share/usr/bin/STAR
 STAR_OPTS = --genomeDir $(STAR_REF_DIR) --outSAMtype BAM SortedByCoordinate \
-			$(if $(findstring true,$(STAR_CHIMERIC)),--chimSegmentMin 20 --chimOutType WithinBAM) \
+			$(if $(findstring true,$(STAR_CHIMERIC)),$(STAR_CHIMERIC_OPTS)) \
 			--quantMode GeneCounts
 
 
 $(if $(STAR_REF_DIR),,$(error no STAR genome provided))
 
-PHONY += star_bams
+PHONY += star_bams star star_chimeric_junctions
+star : star_bams $(if $(findstring true,$(STAR_CHIMERIC)),star_chimeric_junctions)
 star_bams : $(foreach sample,$(SAMPLES),bam/$(sample).bam bam/$(sample).bam.bai)
+star_chimeric_junctions : $(foreach sample,$(SAMPLES),star/$(sample).Chimeric.out.junction)
 
 define align-split-fastq
-star/$2.Aligned.sortedByCoord.out.bam : $3
+star/$2.star_align_timestamp : $3
 	$$(call LSCRIPT_PARALLEL_MEM,4,6G,10G,"$$(STAR) $$(STAR_OPTS) \
 		--outFileNamePrefix star/$2. --runThreadN 4 \
 		--outSAMattrRGline \"ID:$2\" \"LB:$1\" \"SM:$1\" \"PL:$${SEQ_PLATFORM}\" \
-		--readFilesIn $$^ --readFilesCommand zcat")
+		--readFilesIn $$^ --readFilesCommand zcat && touch $@")
+star/$2.Aligned.sortedByCoord.out.bam : star/$2.star_align_timestamp
+star/$2.Chimeric.out.junction : star/$2.star_align_timestamp
 endef
 $(foreach ss,$(SPLIT_SAMPLES),\
 	$(if $(fq.$(ss)),\
 	$(eval $(call align-split-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
 
-
-star/%.Aligned.sortedByCoord.out.bam : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
+star/%.Aligned.sortedByCoord.out.bam star/%.Chimeric.out.junction : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
 	$(call LSCRIPT_PARALLEL_MEM,4,6G,10G,"$(STAR) $(STAR_OPTS) \
 		--outFileNamePrefix star/$*. --runThreadN 4 \
 		--readFilesIn $^ --readFilesCommand zcat")
@@ -54,6 +56,12 @@ star/bam/%.star.sorted.bam : star/%.Aligned.sortedByCoord.out.bam
 bam/%.bam : star/bam/%.star.$(BAM_SUFFIX)
 	$(INIT) ln -f $(<) $(@) 
 
+define merged-chimeric-junction
+star/$1.Chimeric.out.junction : $$(foreach split,$$(split.$1),star/$$(split).Chimeric.out.junction)
+	$$(INIT) sort -V $$^ > $$@
+endef
+$(foreach sample,$(SAMPLES),\
+	$(eval $(call merged-chimeric-junction,$(sample))))
 
 include modules/fastq_tools/fastq.mk
 include modules/bam_tools/processBam.mk
---
+++ aligners/starFusionAligner.mk
@@ -0,0 +1,17 @@
+# star alignment + star fusion
+#
+include modules/Makefile.inc
+
+STAR_CHIMERIC = true
+
+STAR_FUSION = $(HOME)/share/usr/STAR-Fusion/STAR-Fusion
+
+$(if $(STAR_CTAT_DIR),,$(error no STAR CTAT dir))
+
+PHONY += star_fusion
+star_fusion : $(foreach sample,$(SAMPLES),star_fusion/$(sample).star_fusion_timestamp)
+
+star_fusion/%.star_fusion_timestamp : star/%.Chimeric.out.junction
+	$(call LSCRIPT_MEM,8G,12G,"$(STAR_FUSION) --genome_lib_dir $(STAR_CTAT_DIR) -J $< --output_dir $(@D)/$*.star_fusion && touch $@")
+
+include modules/aligners/starAligner.mk
