f9a0bd90d9fe546e9929f16db16a22c691b3cc49
raylim <raylim@mm.st>
2016-09-02 12:03:12 -0400

    filter bam files for paired reads only when extracting fastqs

--- fastq_tools/extractFastq.mk
+++ fastq_tools/extractFastq.mk
@@ -4,22 +4,15 @@
 
 include modules/Makefile.inc
 
-SAM_TO_FASTQ = $(JAVA) -Xmx4G -jar $(JARDIR)/SamToFastq.jar VALIDATION_STRINGENCY=LENIENT
-
 LOGDIR ?= log/extract_fastq.$(NOW)
 
 .DELETE_ON_ERROR:
 .SECONDARY:
 .PHONY: extract_fastq
 
-EXTRACT_TOOL ?= bamToFastq
+VPATH = rawdata unprocessed_bam
 
 extract_fastq : $(foreach sample,$(SAMPLES),fastq/$(sample).1.fastq.gz)
 
-ifeq (${EXTRACT_TOOL},PICARD)
-fastq/%.1.fastq.gz fastq/%.2.fastq.gz : bam/%.bam
-	$(call LSCRIPT_MEM,10G,20G,"$(SAM_TO_FASTQ) I=$< FASTQ=>(gzip -c > fastq/$*.1.fastq.gz) SECOND_END_FASTQ=>(gzip -c > fastq/$*.2.fastq.gz)")
-else
-fastq/%.1.fastq.gz fastq/%.2.fastq.gz : bam/%.bam
-	$(call LSCRIPT_PARALLEL_MEM,4,4G,9G,"$(SAMTOOLS2) sort -T bam/$* -O bam -n -@ 4 -m 6G $< | $(SAMTOOLS2) fastq -f 2 -1 >(gzip -c > fastq/$*.1.fastq.gz) -2 >(gzip -c > fastq/$*.2.fastq.gz) -")
-endif
+fastq/%.1.fastq.gz fastq/%.2.fastq.gz : %.bam
+	$(call LSCRIPT_PARALLEL_MEM,4,4G,9G,"$(SAMTOOLS2) sort -T $(<D)/$* -O bam -n -@ 4 -m 6G $< | $(SAMTOOLS2) fastq -f 1 -1 >(gzip -c > fastq/$*.1.fastq.gz) -2 >(gzip -c > fastq/$*.2.fastq.gz) -")
