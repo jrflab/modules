2d31ce06b37829e632b43bb8d725ca8375e1a0ec
Raymond Lim <raylim@mm.st>
2016-09-23 21:17:03 -0400

    use specified SNV_TYPE

--- variant_callers/somatic/somaticVariants.mk
+++ variant_callers/somatic/somaticVariants.mk
@@ -3,13 +3,13 @@
 include modules/Makefile.inc
 LOGDIR = log/strelkaVarscan.$(NOW)
 
-SNV_TYPE ?= mutect2_snps
+SNV_TYPE ?= mutect_snps
 INDEL_TYPE ?= somatic_indels
 VARIANT_TYPES ?= $(SNV_TYPE) $(INDEL_TYPE)
 
 .PHONY : somatic_variants somatic_indels somatic_snvs
 somatic_variants: somatic_snvs somatic_indels
-somatic_snvs : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).mutect2_snps.vcf)
+somatic_snvs : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).$(SNV_TYPE).vcf)
 somatic_indels : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).somatic_indels.vcf)
 #strelka_varscan_merge_mafs : $(foreach pair,$(SAMPLE_PAIRS),maf/$(pair).strelka_varscan_indels.vcf)
 
@@ -21,6 +21,7 @@ vcf/%.somatic_indels.vcf : vcf/%.mutect_indels.vcf vcf/%.strelka_varscan_indels.
 vcf/%.strelka_varscan_indels.vcf : vcf/%.varscan_indels.vcf vcf/%.strelka_indels.vcf
 	$(call LSCRIPT_MEM,9G,12G,"grep -P '^#' $< > $@ && $(BEDTOOLS) intersect -a $< -b $(<<) >> $@")
 
+include modules/variant_callers/somatic/mutect.mk
 include modules/variant_callers/somatic/mutect2.mk
 include modules/variant_callers/somatic/strelka.mk
 include modules/variant_callers/somatic/varscanTN.mk
