05c309dd57e399a7c6fb0f63b510624ee069ab38
raylim <raylim@mm.st>
2016-10-05 14:00:03 -0400

    fix #218
    remove facets diff tests

--- test/integration/variant_calls/test.sh
+++ test/integration/variant_calls/test.sh
@@ -7,14 +7,11 @@ echo "TEST VARIANT CALLING test_data"
 cd ${DIR}/test_data
 # create project_config.inc
 python modules/configure.py
-echo "==somatic_variants: mutect2 + strelka + varscan=="
+echo "==somatic_variants: mutect + strelka + varscan=="
 echo "run"
 make -j8 somatic_variants && echo "success" || (echo "failed" && exit 1)
 echo "check files"
-for x in output/vcf/*mutect*.vcf; do
-    compare_vcf $x vcf/$(basename $x)
-done
-for x in output/vcf/*somatic_indels*.vcf; do
+for x in output/vcf/*.vcf; do
     compare_vcf $x vcf/$(basename $x)
 done
 echo "==hotspot=="
@@ -28,12 +25,13 @@ echo "==facets copy number=="
 echo "run"
 make -j8 facets && echo "success" || (echo "failed" && exit 1)
 echo "check files"
-for x in output/facets/cncf/*.txt; do
-    diff_files $x facets/cncf/$(basename $x)
-done
-for x in output/facets/*.txt; do
-    diff_files $x facets/$(basename $x)
-done
+# can't check cncf files (non-deterministic)
+#for x in output/facets/cncf/*.txt; do
+    #diff_files $x facets/cncf/$(basename $x)
+#done
+#for x in output/facets/*.txt; do
+    #diff_files $x facets/$(basename $x)
+#done
 echo "==somatic annotations=="
 echo "run"
 make -j8 ann_somatic_vcf && echo "success" || (echo "failed" && exit 1)
