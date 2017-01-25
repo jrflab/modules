195cf9a30c73e2ea9678a17fbada09a034c688fa
raylim <raylim@mm.st>
2017-01-25 17:39:27 -0500

    downsample bams properly for msisensor

--- variant_callers/somatic/msisensor.mk
+++ variant_callers/somatic/msisensor.mk
@@ -14,12 +14,18 @@ PHONY += msisensor
 msisensor: msisensor/msi.tsv
 
 define msisensor-tumor-normal
-msisensor/$1_$2.msi : bam/$1.dcov400.bam bam/$2.dcov400.bam bam/$1.dcov400.bam.bai bam/$2.dcov400.bam.bai
+msisensor/$1_$2.msi : bam/$1.ds.bam bam/$2.ds.bam bam/$1.ds.bam.bai bam/$2.ds.bam.bai
 	$$(call LSCRIPT_PARALLEL_MEM,8,1G,1.2G,"$$(MSISENSOR) msi $$(MSISENSOR_OPTS) -n $$(<<) -t $$< -b 8 -o $$@")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),$(eval $(call msisensor-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
 
 msisensor/msi.tsv : $(foreach pair,$(SAMPLE_PAIRS),msisensor/$(pair).msi)
 	$(INIT) (head -1 $< | sed 's/^/sample\t/'; for x in $^; do sed "1d; s/^/$$(basename $$x)\t/" $$x; done | sed 's/_.*msi//' ) > $@
+
+bam/%.ds.bam : metrics/hs_metrics.tsv bam/%.bam
+	   $(call LSCRIPT_MEM,4G,6G,\
+		   "ds=\`py 'round(500 / pandas.read_table(\"$<\", index_col=0).ix[\"$*\", \"MEAN_TARGET_COVERAGE\"], 2)'\`; \
+		   if [ \$$(echo \"\$$ds >= 1\" | bc) -eq 1 ]; then ln -s \$$(readlink -f $(<<)) $@; else \
+		   samtools view -hb -s \$$ds $(<<) > $@; fi")
 
 include modules/bam_tools/processBam.mk
