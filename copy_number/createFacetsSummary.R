d5c21a6cf137b7c3843f639748f8df4dd38caf79
raylim <raylim@mm.st>
2017-02-21 10:49:53 -0500

    flag <= 0.3 purity

--- copy_number/createFacetsSummary.R
+++ copy_number/createFacetsSummary.R
@@ -48,6 +48,6 @@ for (facetsFile in facetsFiles) {
     Df[n, 'ploidy'] <- fit$ploidy
     Df[n, 'dipLogR'] <- fit$dipLogR
 }
-Df <- mutate(Df, bad = purity < 0.3 | is.na(purity))
+Df <- mutate(Df, bad = purity <= 0.3 | is.na(purity))
 
 write.table(Df, file = opt$outFile, sep = '\t', quote = F, row.names = F)
