978b7b5ac627d7d6a861a8e69717f6cf1b0d33e9
raylim <raylim@mm.st>
2017-03-13 17:58:58 -0400

    min mapq and baseq of 15 for facets snp-pileup

--- copy_number/facets.mk
+++ copy_number/facets.mk
@@ -30,7 +30,7 @@ FACETS_OPTS = --cval2 $(FACETS_CVAL2) --cval1 $(FACETS_CVAL1) --genome $(REF) \
 			  $(if $(facets_purity.$1),--purity $(facets_purity.$1))
 
 SNP_PILEUP = snp-pileup
-SNP_PILEUP_OPTS = -A --min-map-quality=0 --min-base-quality=0 --gzip --max-depth=15000
+SNP_PILEUP_OPTS = -A --min-map-quality=15 --min-base-quality=15 --gzip --max-depth=15000
 
 # convert old facets basecount files to snp-pileup
 CONVERT_BASECOUNT ?= false
