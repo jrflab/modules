0cf84117f0dfa9232e5070310ea8ef1feee7d192
raylim <raylim@mm.st>
2017-03-29 19:25:07 -0400

    decrease AF thresholds of snp calling for snp clustering to reduce noise
    previous thresholds resulted in potentially noisy clustering of samples

--- contamination/clusterSampleVcf.R
+++ contamination/clusterSampleVcf.R
@@ -33,9 +33,9 @@ gt <- geno(vcf)$GT
 ad <- geno(vcf)$AD
 af <- structure(sapply(ad, function(x) x[2] / sum(x)), dim = dim(ad))
 X <- matrix(0, nrow = nrow(gt), ncol = ncol(gt), dimnames = list(rownames(gt), colnames(gt)))
-X[is.na(af)] <- 0
-X[af > 0.45 & af < 0.55] <- 1
-X[af > 0.95] <- 2
+X[is.na(af)] <- NA
+X[af > 0.15 & af < 0.95] <- 1
+X[af >= 0.95] <- 2
 X[!gt %in% c("0/0", "0/1", "1/1")] <- NA
 #plot(hclust(dist(t(X), method = 'manhattan')))
 
