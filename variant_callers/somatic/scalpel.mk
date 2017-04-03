aa3719963a7067ac75ce915c58d6107b7bc23662
raylim <raylim@mm.st>
2017-04-03 12:40:54 -0400

    clean up scalpel workflow

--- variant_callers/somatic/scalpel.mk
+++ variant_callers/somatic/scalpel.mk
@@ -3,9 +3,19 @@
 
 include modules/Makefile.inc
 include modules/variant_callers/gatk.inc
-include modules/variant_callers/somatic/scalpel.inc
 
 LOGDIR = log/scalpel.$(NOW)
+
+NUM_SCALPEL_CHUNKS = 2
+SCALPEL_CHUNKS = $(shell seq -f "%03g" $(NUM_SCALPEL_CHUNKS))
+
+SCALPEL_MIN_COV ?= 5
+SCALPEL_DIR = $(HOME)/share/usr/scalpel-0.5.3
+SCALPEL_DISCOVERY = export LD_LIBRARY_PATH=$(LD_LIBRARY_PATH):$(SCALPEL_DIR)/bamtools-2.3.0/lib/; $(PERL) $(SCALPEL_DIR)/scalpel-discovery
+SCALPEL_OPTS = --ref $(REF_FASTA) --format vcf --covthr $(SCALPEL_MIN_COV)
+
+
+SCALPEL2VCF = $(PERL) modules/variant_callers/somatic/scalpelToVcf.pl
 
 ..DUMMY := $(shell mkdir -p version; echo "$(SCALPEL) $(SCALPEL_OPTS) > version/scalpel.txt")
 
@@ -19,42 +29,38 @@ scalpel : scalpel_vcfs #scalpel_mafs
 scalpel_vcfs : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).scalpel_indels.vcf)
 #scalpel_mafs : $(foreach pair,$(SAMPLE_PAIR),maf/$(pair).scalpel_indels.maf)
 
-ifdef BED_FILES
-define scalpel-bed-tumor-normal
-scalpel/$2_$3/$1/somatic.$(SCALPEL_MIN_COV)x.indel.annovar : bam/$2.bam bam/$3.bam
-	$$(call LSCRIPT_NAMED_PARALLEL_MEM,$2_$3_$1_scalpel,2,4G,7G,"$$(SCALPEL) --somatic --numprocs 2 --tumor $$(<) --normal $$(<<) $$(SCALPEL_OPTS) --bed $$(BED_DIR)/$1 --dir $$(@D) && \
-		if [! -e $$@ ]; then cp $$(@D)/main/$$(@F) $$@; fi")
-endef
-$(foreach bed,$(BED_FILES),\
-	$(foreach pair,$(SAMPLE_PAIRS),\
-		$(eval $(call scalpel-bed-tumor-normal,$(bed),$(tumor.$(pair)),$(normal.$(pair))))))
-
-define merge-scalpel-tumor-normal
-scalpel/$1_$2/somatic.$(SCALPEL_MIN_COV)x.indel.annovar : $$(foreach bed,$$(BED_FILES),scalpel/$1_$2/$$(bed)/somatic.$(SCALPEL_MIN_COV)x.indel.annovar)
-	$$(INIT) grep '^#chr' $$< > $$@ && sed '/^#/d' $$^ >> $$@
-endef
-$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call merge-scalpel-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
-
-else # dont split across exome bed files
-
-define scalpel-tumor-normal
-scalpel/$1_$2/somatic.$(SCALPEL_MIN_COV)x.indel.annovar : bam/$1.dcov.bam bam/$2.dcov.bam
-	$$(call LSCRIPT_NAMED_PARALLEL_MEM,$1_$2_scalpel,8,1G,4G,"$$(SCALPEL) --somatic --numprocs 8 --tumor $$(<M) --normal $$(<<M) $$(SCALPEL_OPTS) --dir $$(@D) && \
-		if [! -e $$@ ]; then cp $$(@D)/main/$$(@F) $$@; fi")
-endef
-$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call scalpel-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+ifdef TARGETS_FILE
+scalpel/interval_chunk/chunk.timestamp : $(TARGETS_FILE)
+	$(INIT) $(SPLIT_BED) --out_prefix $(@D)/chunk --num_chunks $(NUM_SCALPEL_CHUNKS) $<  && touch $@
+genome_sliding_window.bed : $(REF_DICT)
+	$(INIT) bedtools makewindows -g <('s/.*SN:\([^\s]\+\)\sLN:\([0-9]\+\).*/\1\t\2/' $<) -w 10000 -s 9900 > $@
+else
+scalpel/interval_chunk/chunk.timestamp : genome_sliding_window.bed
+	$(INIT) $(SPLIT_BED) --out_prefix $(@D)/chunk $(NUM_SCALPEL_CHUNKS) $<  && touch $@
 endif
 
-# convert annovar table to vcf and remove dcov bams
-define scalpel2vcf-tumor-normal
-vcf/$1_$2.scalpel_indels.vcf : scalpel/$1_$2/somatic.$(SCALPEL_MIN_COV)x.indel.annovar bam/$1.dcov.bam bam/$2.dcov.bam
-	$$(INIT) tempf=`mktemp` && \
-		$$(SCALPEL2VCF) -f $$(REF_FASTA) -t $1 -n $2 < $$< > $$$$tempf 2> $$(LOG) \
-		&& (grep '^#' $$$$tempf; grep -v '^#' $$$$tempf | sort -V)  > $$@ 2>> $$(LOG) \
-		&& rm $$$$tempf 2>> $$(LOG) \
-		&& rm $$(<<) $$(<<<) 2>> $$(LOG)
+
+define scalpel-interval-chunk
+scalpel/interval_chunk/chunk$1.bed : scalpel/interval_chunk/chunk.timestamp
 endef
-$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call scalpel2vcf-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+$(foreach i,$(SCALPEL_CHUNKS),$(eval $(call scalpel-interval-chunk,$i)))
+
+define scalpel-chunk-tumor-normal
+scalpel/$2_$3/$1/scalpel.timestamp : scalpel/interval_chunk/chunk$1.bed bam/$2.bam bam/$3.bam
+	$$(call LSCRIPT_NAMED_PARALLEL_MEM,$2_$3_$1_scalpel,4,2G,3G,"$$(SCALPEL_DISCOVERY) --somatic --numprocs 4 --tumor $$(<<) --normal $$(<<<) $$(SCALPEL_OPTS) --bed $$(<) --dir $$(@D) && touch $$@")
+scalpel/$2_$3/$1/somatic.indel.vcf: scalpel/$2_$3/$1/scalpel.timestamp 
+	if [ ! -e $$@ ]; then cp $$(@D)/main/$$(@F) $$@; fi
+endef
+$(foreach chunk,$(SCALPEL_CHUNKS), \
+	$(foreach pair,$(SAMPLE_PAIRS),\
+		$(eval $(call scalpel-chunk-tumor-normal,$(chunk),$(tumor.$(pair)),$(normal.$(pair))))))
+
+define scalpel-tumor-normal
+vcf/$1_$2.scalpel_indels.vcf : $$(foreach chunk,$$(SCALPEL_CHUNKS),scalpel/$1_$2/$$(chunk)/somatic.indel.vcf)
+	$$(call LSCRIPT_MEM,4G,8G,"(grep '^#' $$<; cat $$^ | grep -v '^#' | $$(VCF_SORT) $$(REF_DICT) -) > $$@")
+endef
+$(foreach pair,$(SAMPLE_PAIRS),\
+		$(eval $(call scalpel-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
 
 include modules/vcf_tools/vcftools.mk
 include modules/bam_tools/processBam.mk
