ffe0e194ec8cfb5514bf45f5e04ae6d13802f349
raylim <raylim@mm.st>
2017-05-26 17:01:31 -0400

    updated contest to gatk version

--- contamination/contest.mk
+++ contamination/contest.mk
@@ -1,8 +1,9 @@
 # This module runs ContEst on snp vcf files from gatk
-# Author: inodb
+# Author: inodb, limr
 
 ##### MAKE INCLUDES #####
 include modules/Makefile.inc
+include modules/variant_callers/gatk.inc
 
 LOGDIR = log/contest.$(NOW)
 
@@ -10,18 +11,16 @@ LOGDIR = log/contest.$(NOW)
 .DELETE_ON_ERROR:
 .PHONY: contest
 
-contest : contest/snp_vcf/all_contamination.txt $(foreach sample,$(SAMPLES),contest/snp_vcf/$(sample)_contamination.txt)
+contest : contest/all_contest.txt
 
-CONTEST_JAR ?= ~/share/usr/contest-1.0.24530-bin/ContEst.jar
-CONTEST_REF_FA ?= ~/share/reference/GATK_bundle/2.3/human_g1k_v37.fasta
-CONTEST_REF_VCF ?= ~/share/reference/hg19_population_stratified_af_hapmap_3.3.vcf
-CONTEST_MEM = $(JAVA) -Xmx$(1) -jar $(CONTEST_JAR) -R $(CONTEST_REF_FA) -B:pop$(,)vcf $(CONTEST_REF_VCF) -T Contamination -BTI genotypes
+# ContEst doing on-the-fly genotyping
+define contest-tumor-normal
+contest/$1_$2.contest.txt : bam/$1.bam bam/$2.bam
+	$$(call LSCRIPT_MEM,12G,12G,"$$(call GATK_MEM2,4G) -T ContEst -I:eval $$(<) -I:genotype $$(<<) -pf $$(HAPMAP_POP_FILE) -o $$(@) -R $$(REF_FASTA)")
+endef
+$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call contest-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
 
-# ContEst on gatk snp_vcf folder
-contest/snp_vcf/%_contamination.txt : bam/%.bam snp_vcf/%.snps.vcf
-	$(call LSCRIPT_MEM,7G,8G,"$(MKDIR) contest/snp_vcf; $(call CONTEST_MEM,2G) -I $(<) -B:genotypes$(,)vcf $(<<) -o $(@)")
-
-contest/snp_vcf/all_contamination.txt : $(foreach sample,$(SAMPLES),contest/snp_vcf/$(sample)_contamination.txt)
+contest/all_contest.txt : $(foreach pair,$(SAMPLE_PAIRS),contest/$(pair).contest.txt)
 	( \
 		head -1 $< | sed "s/^/sample\t/"; \
 		for s in $(^); do \
