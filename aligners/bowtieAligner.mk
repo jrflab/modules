35ac2cc375720600cc44dc4dbbf9af1a7f73b718
raylim <raylim@mm.st>
2017-10-09 16:03:39 -0400

    fix bowtie alignment

--- aligners/bowtieAligner.mk
+++ aligners/bowtieAligner.mk
@@ -40,22 +40,22 @@ BAMS = $(foreach sample,$(SAMPLES),bam/$(sample).bam)
 bowtie_bams : $(BAMS) $(addsuffix .bai,$(BAMS))
 
 # memory for human genome: ~3.2G
-bowtie/bam/%.bwt.bam : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
+bowtie/bam/%.bowtie.bam : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
 	$(call RUN,-n 4 -s 1G -m 1.5G,"LBID=`echo \"$*\" | sed 's/_[A-Za-z0-9]\+//'`; \
 		$(BOWTIE) $(BOWTIE_OPTS) --rg-id $* --rg \"LB:\$${LBID}\" --rg \"PL:${SEQ_PLATFORM}\" --rg \"SM:\$${LBID}\" -p $(NUM_CORES) \
 		-1 $< -2 $(<<) | $(SAMTOOLS) view -bhS - > $(@)")
 
-bowtie/bam/%.bwt.bam : fastq/%.fastq.gz
+bowtie/bam/%.bowtie.bam : fastq/%.fastq.gz
 	$(call RUN,-n 4 -s 1G -m 1.5G,"LBID=`echo \"$*\" | sed 's/_[A-Za-z0-9]\+//'`; \
 		$(BOWTIE) $(BOWTIE_OPTS) --rg-id $* --rg \"LB:\$${LBID}\" --rg \"PL:${SEQ_PLATFORM}\" --rg \"SM:\$${LBID}\" -p $(NUM_CORES) \
 		-U $(<) | $(SAMTOOLS) view -bhS - > $(@) ")
 
 
-bam/%.bam : bowtie/bam/%.bwt.$(BAM_SUFFIX)
+bam/%.bam : bowtie/bam/%.bowtie.$(BAM_SUFFIX)
 	$(call RUN,,"ln -f $(<) $(@) ")
 
 define align-split-fastq
-bowtie/bam/$2.bwt.bam : $3
+bowtie/bam/$2.bowtie.bam : $3
 	$$(call RUN,-n 4 -s 1G -m 1.5G,"$$(BOWTIE) $$(BOWTIE_OPTS) \
 		--rg-id $2 --rg \"LB:$1\" --rg \"PL:$${SEQ_PLATFORM}\" --rg \"SM:$1\" \
 		-p $$(NUM_CORES) $$(if $$(<<),-1 $$(<) -2 $$(<<),-U $$<) | $$(SAMTOOLS) view -bhS - > $$(@)")
