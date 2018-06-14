d1039316805ea4737dceee554c41488064e0cac6
David Brown <ndbrown6@users.noreply.github.com>
2018-06-13 23:45:27 -0400

    Typo fixed with --keep-dup

--- variant_callers/somatic/macsTN.mk
+++ variant_callers/somatic/macsTN.mk
@@ -12,7 +12,7 @@ VPATH = macs2
 
 define macs2-case-control
 macs2/$1_$2.timestamp : bam/$1.bam bam/$2.bam
-	$$(call RUN,-c -s 12G -m 16G,"macs2 callpeak -t bam/$1.bam -c bam/$2.bam -f BAM -g hs -keep-dup all --outdir macs2 -n $1_$2 -B --verbose 2 --nomodel -p 0.1 && touch macs2/$1_$2.timestamp")
+	$$(call RUN,-c -s 12G -m 16G,"macs2 callpeak -t bam/$1.bam -c bam/$2.bam -f BAM -g hs --keep-dup all --outdir macs2 -n $1_$2 -B --verbose 2 --nomodel -p 0.1 && touch macs2/$1_$2.timestamp")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
 		$(eval $(call macs2-case-control,$(tumor.$(pair)),$(normal.$(pair)))))
