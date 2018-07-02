05d9c125f611a4bbb5859de36fc241d74e0cad7f
David Brown <ndbrown6@users.noreply.github.com>
2018-07-02 19:02:28 -0400

    Update blastReads.mk

--- fastq_tools/blastReads.mk
+++ fastq_tools/blastReads.mk
@@ -11,4 +11,4 @@ VPATH = unmapped_reads
 blast_reads : $(foreach sample,$(SAMPLES),unmapped_reads/$(sample).blast)
 
 unmapped_reads/%.blast : unmapped_reads/%.fasta
-	$(call RUN,-n 32 -s 4G -m 9G -w 360,"blastn -num_threads 32 -evalue 0.001 -word_size 28 -db ~/share/reference/ncbi_nt/nt -query $< -outfmt 7 -out unmapped_reads/$*.blast")
+	$(call RUN,-n 32 -s 3G -m 4G -w 360,"blastn -num_threads 32 -evalue 0.001 -word_size 28 -db ~/share/reference/ncbi_nt/nt -query $< -outfmt 7 -out unmapped_reads/$*.blast")
