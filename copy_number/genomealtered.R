6d72a389d88f827a4e21026bec754ad4b1df17dd
David Brown <ndbrown6@users.noreply.github.com>
2018-07-25 19:11:16 -0400

    Fixed NA when no CNA

--- copy_number/genomealtered.R
+++ copy_number/genomealtered.R
@@ -22,8 +22,15 @@ gamma = 1
 x = fit$cncf[,"cnlr.median"]
 absolute_copies = round(((((2^(x/gamma))*(alpha*psi+(1-alpha)*2)) - ((1-alpha)*2))/alpha))
 index = absolute_copies!=round(psi)
-genome_footprint = sum(fit$cncf[!is.na(index),"end"]-fit$cncf[!is.na(index),"start"], na.rm=TRUE)
-genome_altered = sum((fit$cncf[index,"end"] - fit$cncf[index,"start"]), na.rm=TRUE)/genome_footprint
+if (sum(index, na.rm=TRUE)!=0) {
+        index = which(index)
+        genome_footprint = sum(fit$cncf[,"end"]-fit$cncf[,"start"], na.rm=TRUE)
+        genome_altered = sum((fit$cncf[index,"end"] - fit$cncf[index,"start"]), na.rm=TRUE)/genome_footprint
+        genome_altered = ifelse(is.nan(genome_altered), 0, genome_altered)
+        genome_altered = ifelse(is.na(genome_altered), 0, genome_altered)
+} else {
+        genome_altered = 0
+}
 cat(paste0(gsub("facets/cncf/","", gsub(".Rdata", "", opt$file_in)), "\t", genome_altered), file = opt$file_out, append=FALSE)
 cat("\n", file = opt$file_out, append=TRUE)
 
