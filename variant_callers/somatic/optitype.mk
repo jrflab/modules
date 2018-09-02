9e159c5147b38d8ce0e4d87ba369831be86b42be
David Brown <ndbrown6@gmail.com>
2018-09-02 19:41:31 -0400

    optitype

--- variant_callers/somatic/optitype.mk
+++ variant_callers/somatic/optitype.mk
@@ -8,7 +8,7 @@ LOGDIR ?= log/hla_optitype.$(NOW)
 
 VPATH = bam
 
-hla_optitype : $(foreach sample,$(SAMPLES),hla_optitype/$(sample).bam)
+hla_optitype : $(foreach sample,$(SAMPLES),hla_optitype/$(sample).taskcomplete)
 
 define hla-optitype
 hla_optitype/%.bam : %.bam
@@ -19,6 +19,8 @@ hla_optitype/%_1.fastq hla_optitype/%_2.fastq : hla_optitype/%.bam
 	$$(call RUN,-n 4 -s 12G -m 16G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate \
 								 	/home/${USER}/share/usr/anaconda-envs/optitype && \
 								 	$(SAMTOOLS2) sort -T $$(<D)/$$* -O bam -n -@ 4 -m 6G $$< | $(SAMTOOLS2) fastq -f 1 -1 > hla_optitype/$$*.1.fastq -2 > hla_optitype/$$*.2.fastq -")	
+hla_optitype/%.taskcomplete : hla_optitype/%_1.fastq hla_optitype/%_2.fastq
+	$$(call RUN,-s 1G -m 1G,"touch hla_optitype/$$*.taskcomplete")
 endef
 $(foreach pair,$(SAMPLES),\
 		$(eval $(call hla-optitype,$sample)))
