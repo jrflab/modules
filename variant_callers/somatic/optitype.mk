0f346fb003f3f9c44afd6fa1f1ce13c514ad5b75
David Brown <ndbrown6@gmail.com>
2018-09-02 19:59:35 -0400

    optitype

--- variant_callers/somatic/optitype.mk
+++ variant_callers/somatic/optitype.mk
@@ -21,7 +21,7 @@ hla_optitype/%_1.fastq hla_optitype/%_2.fastq : hla_optitype/%.bam
 								 	/home/${USER}/share/usr/anaconda-envs/optitype && \
 								 	$(SAMTOOLS2) sort -T $$(<D)/$$* -O bam -n -@ 4 -m 6G $$< | $(SAMTOOLS2) fastq -f 1 -1 > hla_optitype/$$*.1.fastq -2 > hla_optitype/$$*.2.fastq -")
 
-hla_optitype/%_1.razers3.bam hla_optitype/%_2.razers3.bam : hla_optitype/%_1.fastq hla_optitype/%_2.fastq
+hla_optitype/%_1_razers3.bam hla_optitype/%_2_razers3.bam : hla_optitype/%_1.fastq hla_optitype/%_2.fastq
 	$$(call RUN,-n 4 -s 12G -m 16G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate \
 								 	/home/${USER}/share/usr/anaconda-envs/optitype && \
 								 	razers3 -i 95 -m 1 -dr 0 -o hla_optitype/$$*_1_razers3.bam /home/${USER}/share/usr/anaconda-envs/optitype/share/optitype-1.3.2-1/data/hla_reference_dna.fasta hla_optitype/$$*_1.fastq && \
