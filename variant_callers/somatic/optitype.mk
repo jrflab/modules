05ee873454d747e3931cffb32185ee5ad269f9d9
David Brown <ndbrown6@gmail.com>
2018-09-02 01:20:23 -0400

    Added new functionalities
    Added makefiles for polysolver and optitype

---
+++ variant_callers/somatic/optitype.mk
@@ -0,0 +1,4 @@
+include modules/Makefile.inc
+
+LOGDIR ?= log/hla_optitype.$(NOW)
+PHONY += hla_optitype 
---
+++ variant_callers/somatic/polysolver.mk
@@ -0,0 +1,4 @@
+include modules/Makefile.inc
+
+LOGDIR ?= log/hla_polysolver.$(NOW)
+PHONY += hla_polysolver
