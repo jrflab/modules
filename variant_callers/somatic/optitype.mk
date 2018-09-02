2dea7f76a2ce2685892aeb5b50a9b6266ad2aa98
David Brown <ndbrown6@gmail.com>
2018-09-02 19:05:28 -0400

    optitype

--- variant_callers/somatic/optitype.mk
+++ variant_callers/somatic/optitype.mk
@@ -1,20 +1,16 @@
 include modules/Makefile.inc
 
 LOGDIR ?= log/hla_optitype.$(NOW)
-PHONY += hla_optitype
-
-hla_optitype : $(foreach sample,$(SAMPLES),hla_optitype/$(sample).fastq)
-
-define hla-optitype
-hla_optitype/%fastq : bam/%.bam
-	$$(call RUN,-n 8 -s 12G -m 24G, "source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate \
-									 /home/${USER}/share/usr/anaconda-envs/optitype && \
-								 	 if [ ! -d hla_optitype/$* ]; then mkdir hla_optitype/$*; fi && \
-								 	 razers3 -i 95 -m 1 -dr 0 -o hla_optitype/$*/$*.bam /home/${USER}/share/usr/anaconda-envs/optitype/share/optitype-1.3.2-1/data/hla_reference_dna.fasta hla_optitype/$*/$*.fastq")
-endef
-$(foreach sample,$(SAMPLES),\
-		$(eval $(call hla-optitype,$(sample))))
 
 .DELETE_ON_ERROR:
 .SECONDARY:
-.PHONY: $(PHONY)
+.PHONY: hla_optitype
+
+VPATH = bam
+
+hla_optitype : $(foreach sample,$(SAMPLES),hla_optitype/$(sample).bam)
+
+hla_optitype/%.bam : %.bam
+	$(call RUN,-n 4 -s 4G -m 9G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate \
+								 /home/${USER}/share/usr/anaconda-envs/optitype && \
+								 razers3 -i 95 -m 1 -dr 0 -o hla_optitype/$*.bam /home/${USER}/share/usr/anaconda-envs/optitype/share/optitype-1.3.2-1/data/hla_reference_dna.fasta hla_optitype/$*.fastq")
