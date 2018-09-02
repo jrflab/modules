901698015bd6a3efd3cf0e31e023b86be0b5e8af
David Brown <ndbrown6@gmail.com>
2018-09-02 18:56:31 -0400

    optitype

--- variant_callers/somatic/optitype.mk
+++ variant_callers/somatic/optitype.mk
@@ -12,7 +12,7 @@ hla_optitype/%/%fastq : bam/%.bam
 								 	 if [ ! -d hla_optitype/$* ]; then mkdir hla_optitype/$*; fi && \
 								 	 razers3 -i 95 -m 1 -dr 0 -o hla_optitype/$*/$*.bam /home/${USER}/share/usr/anaconda-envs/optitype/share/optitype-1.3.2-1/data/hla_reference_dna.fasta hla_optitype/$*/$*.fastq")
 endef
-$(foreach pair,$(SAMPLES),\
+$(foreach sample,$(SAMPLES),\
 		$(eval $(call hla-optitype,$(sample),$(sample))))
 
 .DELETE_ON_ERROR:
