8efa8843499cc24aab25cee4bd2bff32034c4ace
David Brown <ndbrown6@gmail.com>
2018-09-02 16:21:10 -0400

    polysolver

--- variant_callers/somatic/polysolver.mk
+++ variant_callers/somatic/polysolver.mk
@@ -7,12 +7,12 @@ hla_polysolver : $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/winners.h
 
 define hla-polysolver
 hla_polysolver/$1_$2/winners.hla.txt : bam/$1.bam bam/$2.bam
-	$$(call RUN,-n 8 -s 12G -m 24G -v /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/, \
-									  "source activate /home/${USER}/share/usr/anaconda-envs/hla-polysolver && \
-									   export CONDA_PREFIX=/home/${USER}/share/usr/anaconda-envs/hla-polysolver && \
-									   export PERL5LIB=/home/${USER}/share/usr/anaconda-envs/hla-polysolver/lib/perl5/5.22.0/ && \
-								 	   if [ ! -d hla_polysolver/$1_$2 ]; then mkdir hla_polysolver/$1_$2; fi && \
-								 	   shell_call_hla_type bam/$2.bam Unknown 1 hg19 STDFQ 0 hla-polysolver/$1_$2")
+	$$(call RUN,-n 8 -s 12G -m 24G , "source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/activate \
+									  /home/${USER}/share/usr/anaconda-envs/hla-polysolver/ && \
+									  export CONDA_PREFIX=/home/${USER}/share/usr/anaconda-envs/hla-polysolver/ && \
+									  export PERL5LIB=/home/${USER}/share/usr/anaconda-envs/hla-polysolver/lib/perl5/5.22.0/ && \
+								 	  if [ ! -d hla_polysolver/$1_$2 ]; then mkdir hla_polysolver/$1_$2; fi && \
+								 	  shell_call_hla_type bam/$2.bam Unknown 1 hg19 STDFQ 0 hla-polysolver/$1_$2")
 
 #hla_polysolver/$1_$2/$1_$2.taskcomplete : hla_polysolver/$1_$2/winners.hla.txt
 #	$$(call RUN,-s 1G -m 1G,"echo $$< $$(<<) > hla_polysolver/$1_$2/$1_$2.taskcomplete")
