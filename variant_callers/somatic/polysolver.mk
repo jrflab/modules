648971d3651f2f951ca8798bca9d7a4da0a230f1
David Brown <ndbrown6@gmail.com>
2018-09-02 01:46:56 -0400

    Polysolver
    Fixed typo

--- variant_callers/somatic/polysolver.mk
+++ variant_callers/somatic/polysolver.mk
@@ -8,7 +8,7 @@ hla_polysolver : $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/$(pair).t
 define hla-polysolver
 hla_polysolver/$1_$2/winners.hla.txt : bam/$1.bam bam/$2.bam
 	$$(call RUN,-c -s 8G -m 12G,"export CONDA_PREFIX=/home/${USER}/share/usr/anaconda-envs/hla-polysolver && PERL5LIB=$CONDA_PREFIX/lib/perl5/5.22.0/ && mkdir hla_polysolver/$1_$2 && \
-								 shell_call_hla_type bam/$2.bam Unknown 1 hg19 STDFQ 0 hla-polysolver/$1_$2"
+								 shell_call_hla_type bam/$2.bam Unknown 1 hg19 STDFQ 0 hla-polysolver/$1_$2")
 
 hla_polysolver/$1_$2/$1_$2.taskcomplete : hla_polysolver/$1_$2/winners.hla.txt
 	$$(call RUN,-c -s 1G -m 1G,"echo $$< $$(<<) > hla_polysolver/$1_$2/$1_$2.taskcomplete")
