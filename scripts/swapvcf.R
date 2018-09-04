53d220b0842dcd71543d9dd007041076a8fc6e13
David Brown <ndbrown6@gmail.com>
2018-09-04 18:44:25 -0400

    Lancet

--- scripts/swapvcf.R
+++ scripts/swapvcf.R
@@ -6,20 +6,23 @@ if (!interactive()) {
     options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
 }
 
-args_list <- list(make_option("--file_in", default = NA, type = 'character', help = "input file name"))
+args_list <- list(make_option("--file", default = NA, type = 'character', help = "input file name"),
+				  make_option("--tumor", default = NA, type = 'character', help = "tumor sample name"),
+				  make_option("--normal", default = NA, type = 'character', help = "normal sample name"),)
 				  
 parser <- OptionParser(usage = "%prog", option_list = args_list)
 arguments <- parse_args(parser, positional_arguments = T)
 opt <- arguments$options
 
-vcf = read.table(file=opt$file_in, header=FALSE, sep="\t", comment.char="#", stringsAsFactors=FALSE)
+vcf = read.table(file=opt$file, header=FALSE, sep="\t", comment.char="#", stringsAsFactors=FALSE)
 n = ncol(vcf)
 n1 = vcf[,n,drop=TRUE]
 n2 = vcf[,n-1,drop=TRUE]
 vcf[,n-1] = n1
 vcf[,n] = n2
-system(paste0("grep '#' ", opt$file_in, " > ", opt$file_in, ".tmp"))
-write.table(vcf, file=paste0(opt$file_in, ".tmp"), append=TRUE, quote=FALSE, sep="\t", row.names=FALSE, col.names=FALSE)
+colnames(vcf) = c("#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT", opt$tumor, opt$normal)
+system(paste0("grep '##' ", opt$file_in, " > ", opt$file_in, ".tmp"))
+write.table(vcf, file=paste0(opt$file_in, ".tmp"), append=TRUE, quote=FALSE, sep="\t", row.names=FALSE, col.names=TRUE)
 #file.copy(from=paste0(opt$file_in, ".tmp"), to=opt$file_in, overwrite=TRUE)
 
 warnings()
