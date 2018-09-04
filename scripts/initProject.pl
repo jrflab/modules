e841b717982dcca90c18ad7e96543f31535b4bfa
David Brown <ndbrown6@gmail.com>
2018-09-04 11:42:34 -0400

    Minor changes in initProject.pl

--- scripts/initProject.pl
+++ scripts/initProject.pl
@@ -5,24 +5,9 @@ use warnings;
 use Cwd;
 use File::Copy;
 
-# create repo
-# my $repoName = getcwd;
-# $repoName =~ s:.*/projects/::;
-# $repoName =~ s:.*/data/::;
-# $repoName =~ s:.*kinglab/::;
-# $repoName =~ s:/:_:g;
-
-# system "bb create -c --owner jrflab --protocol ssh $repoName";
-# system "git init";
-# system "git remote add origin git\@bitbucket.org:jrflab/$repoName.git";
-
 my $MAKEFILE = <<ENDL;
 include modules/Makefile
 ENDL
-
-# unless (-d "modules") {
-#    system "git clone --recursive git\@github.com:jrflab/modules.git -b master";
-# }
 
 unless (-e "Makefile") {
     open OUT, ">Makefile";
@@ -41,10 +26,3 @@ unless (-e "summary_config.yaml") {
 unless (-e "sample_attr.yaml") {
     copy("modules/default_sample_attr.yaml", "sample_attr.yaml") or die "Unable to create sample_attr.yaml: $!";
 }
-#close OUT;
-#system "git add Makefile";
-#system "git add project_config.yaml";
-#system "git add summary_config.yaml";
-#system "git commit -m 'makefile, summary_config.yaml, project_config.yaml'";
-#system "git push --set-upstream origin master";
-
