05fb199dd2735be4aac0ea75de8e213b42059a28
David Brown <ndbrown6@gmail.com>
2018-09-05 21:46:05 -0400

    % in dexseq

--- rnaseq/dexseq.mk
+++ rnaseq/dexseq.mk
@@ -6,10 +6,10 @@ PHONY += dex_seq
 dex_seq : $(foreach sample,$(TUMOR_SAMPLES),dex_seq/$(sample).taskcomplete)
 
 define exon-count
-dex_seq/$1.txt : star/bam/$1.star.sorted.filtered.bam
+dex_seq/%.txt : star/bam/%.star.sorted.filtered.bam
 	$$(call RUN,-c -s 8G -m 12G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules.0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/dexseq && \
 								 /home/${USER}/share/usr/anaconda-envs/dexseq/lib/R/library/DEXSeq/python_scripts/dexseq_count.py -f bam /home/${USER}/share/reference/Ensembl/Homo_sapiens.GRCh37.75.gff star/bam/$1.star.sorted.filtered.bam dex_seq/$1.txt")
-dex_seq/$1.taskcomplete : dex_seq/$1.txt
+dex_seq/%.taskcomplete : dex_seq/%.txt
 	$$(call RUN,-c -s 1G -m 1G,"echo $$<")
 endef
 $(foreach sample,$(TUMOR_SAMPLES),\
