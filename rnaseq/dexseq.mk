02dc1ba71167e32aefac707baf04e9b99cef5391
David Brown <ndbrown6@gmail.com>
2018-09-05 21:59:10 -0400

    changed dex_seq to dexseq

--- rnaseq/dexseq.mk
+++ rnaseq/dexseq.mk
@@ -1,15 +1,15 @@
 include modules/Makefile.inc
 
 LOGDIR ?= log/exon_counts.$(NOW)
-PHONY += dex_seq
+PHONY += dexseq
 
-dex_seq : $(foreach sample,$(TUMOR_SAMPLES),dex_seq/$(sample).taskcomplete)
+dexseq : $(foreach sample,$(TUMOR_SAMPLES),dex_seq/$(sample).taskcomplete)
 
 define exon-count
-dex_seq/%.txt : star/bam/%.star.sorted.filtered.bam
+dexseq/%.txt : star/bam/%.star.sorted.filtered.bam
 	$$(call RUN,-c -s 8G -m 12G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules.0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/dexseq && \
-								 /home/${USER}/share/usr/anaconda-envs/dexseq/lib/R/library/DEXSeq/python_scripts/dexseq_count.py -f bam /home/${USER}/share/reference/Ensembl/Homo_sapiens.GRCh37.75.gff $$< dex_seq/$$*.txt")
-dex_seq/%.taskcomplete : dex_seq/%.txt
+								 /home/${USER}/share/usr/anaconda-envs/dexseq/lib/R/library/DEXSeq/python_scripts/dexseq_count.py -f bam /home/${USER}/share/reference/Ensembl/Homo_sapiens.GRCh37.75.gff $$< dexseq/$$*.txt")
+dexseq/%.taskcomplete : dexseq/%.txt
 	$$(call RUN,-c -s 1G -m 1G,"touch $$<")
 endef
 $(foreach sample,$(TUMOR_SAMPLES),\
