a7fabf07e3a441653f3df55dde8b20e61ff22674
David Brown <ndbrown6@gmail.com>
2018-09-08 16:30:17 -0400

    updated sufam

--- variant_callers/updateSamples.R
+++ variant_callers/updateSamples.R
@@ -12,5 +12,5 @@ parser <- OptionParser(usage = "%prog", option_list = args_list)
 arguments <- parse_args(parser, positional_arguments = T)
 opt <- arguments$options
 
-all_vars = read.csv(file=paste0("sufam/", opt$patient, ".txt"), header=TRUE, sep="\t", stringsAsFactors=FALSE)
-write.csv(all_vars, file=paste0("sufam/", opt$patient, ".tsv"))
+vars = read.csv(file=paste0("sufam/", opt$patient, ".txt"), header=TRUE, sep="\t", stringsAsFactors=FALSE)
+write.table(vars, file=paste0("sufam/", opt$patient, ".tsv"), col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
