8f90c98e55456d1eb2ae9a310c55df868d716883
David Brown <ndbrown6@gmail.com>
2018-09-09 02:47:45 -0400

    %

--- clonality/pyclone.mk
+++ clonality/pyclone.mk
@@ -3,10 +3,10 @@ include modules/Makefile.inc
 LOGDIR ?= log/pyclone.$(NOW)
 PHONY += pyclone
 
-pyclone : $(foreach sample,$(NORMAL_SAMPLES),sufam/$(sample)/$(sample).taskcomplete)
+pyclone : $(foreach sample,$(NORMAL_SAMPLES),sufam/$(sample).taskcomplete)
 
 define make-input-pyclone
-pyclone/%/%.taskcomplete : sufam/%.tsv
+pyclone/%taskcomplete : sufam/%.tsv
 	$$(call RUN,-c -s 4G -m 6G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate && \
 								$(RSCRIPT) modules/clonality/pyclone_make_input.R --file_name $$< && \
 								touch $$*")
