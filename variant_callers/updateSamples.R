0e5d1e9b2d3a07c166192dc41902a1485811e279
David Brown <ndbrown6@gmail.com>
2018-09-08 20:07:40 -0400

    loh status in updateSamples.R
    LOH status is true only if qt==q2

--- variant_callers/updateSamples.R
+++ variant_callers/updateSamples.R
@@ -17,7 +17,9 @@ index = grep("MAF", colnames(vars))
 sample_names = unlist(lapply(strsplit(colnames(vars)[index], "_"), function(x) {return(x[2])}))
 sample_names[1] = opt$patient
 
-## run sufam
+#====================================
+# sufam
+#====================================
 chr = vars$Chromosome
 pos = vars$Position
 id = rep(".", nrow(vars))
@@ -30,6 +32,9 @@ vcf = cbind(chr, pos, id, ref, alt, qual, filter, info)
 colnames(vcf) = c("#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO")
 write.table(vcf, file=paste0("sufam/", opt$patient, ".vcf"), sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
 
+#====================================
+# dp and maf
+#====================================
 for (i in 1:length(sample_names)) {
 	system(paste0("source ~/share/usr/anaconda/bin/activate ~/share/usr/anaconda-envs/sufam-dev && sufam ~/share/reference/GATK_bundle/2.3/human_g1k_v37.fa sufam/", opt$patient, ".vcf bam/", sample_names[i], ".bam > sufam/", sample_names[i], ".tmp"))
 	tmp = read.csv(file=paste0("sufam/", sample_names[i], ".tmp"), header=TRUE, sep="\t", stringsAsFactors=FALSE)
@@ -41,10 +46,12 @@ for (i in 1:length(sample_names)) {
 	vars[,index[i]] = tmp[,"val_maf"]
 }
 
+#====================================
+# qt and q2
+#====================================
 q_t = rep(2, nrow(vars))
 q_2 = rep(1, nrow(vars))
 for (i in 2:length(sample_names)) {
-	## fix qt and q2
 	load(paste0("ascat/ascat/", sample_names[i], "_", sample_names[1], ".RData"))
 	Chromosomes = tmp2$SNPpos[tmp3$seg[,"start"],1]
 	Chromosomes[Chromosomes==23] = "X"
@@ -69,5 +76,23 @@ q_2[is.na(q_2)] = 1
 colnames(q_t) = colnames(q_2) = c("N", sample_names[2:length(sample_names)])
 colnames(q_t) = paste0("qt_", colnames(q_t))
 colnames(q_2) = paste0("q2_", colnames(q_2))
+
+#====================================
+# loh
+#====================================
+for (i in 2:length(sample_names)) {
+	loh = rep(0, nrow(vars))
+	for (j in 1:nrow(vars)) {
+		if (qt[j,i]==q2[j,i]) {
+			loh[j] = 1
+		}
+	}
+	vars[,paste0("LOH_", sample_names[i])] = loh
+}
+
+#====================================
+# ccf
+#====================================
+
 
 write.table(vars, file=paste0("sufam/", opt$patient, ".tsv"), col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
