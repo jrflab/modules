de0f6811bcbf2f2ac0bc9aacfc9b1184641d244e
David Brown <ndbrown6@gmail.com>
2018-09-09 02:52:55 -0400

    environment in pyclone.mk

--- clonality/pyclone.mk
+++ clonality/pyclone.mk
@@ -7,7 +7,7 @@ pyclone : $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample).taskcomplete)
 
 define make-input-pyclone
 pyclone/%.taskcomplete : sufam/%.tsv
-	$$(call RUN,-c -s 4G -m 6G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate && \
+	$$(call RUN,-c -s 4G -m 6G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
 								$(RSCRIPT) modules/clonality/pyclone_make_input.R --file_name $$< && \
 								touch pyclone/$$*.taskcomplete")
 
