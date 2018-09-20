d67fe05517686dd422633ae68a240fed30571211
David Brown <ndbrown6@gmail.com>
2018-09-20 10:45:13 -0400

    update to pyclone

--- clonality/tsvforpyclone.R
+++ clonality/tsvforpyclone.R
@@ -21,8 +21,8 @@ ref_counts = round((1-mutation_summary[,paste0("MAF_", opt$sample_name)])*mutati
 var_counts = round((mutation_summary[,paste0("MAF_", opt$sample_name)])*mutation_summary[,paste0("DP_", opt$sample_name)])
 var_counts[var_counts<5] = 0
 normal_cn = rep(2, length(mutation_id))
-minor_cn = mutation_summary[,paste0("qt_", opt$sample_name)] - mutation_summary[,paste0("q2_", opt$sample_name)]
-major_cn = mutation_summary[,paste0("q2_", opt$sample_name)]
+minor_cn = rep(0, length(mutation_id))
+major_cn = mutation_summary[,paste0("qt_", opt$sample_name)]
 sample_summary = data.frame(mutation_id, ref_counts, var_counts, normal_cn, minor_cn, major_cn)
 write.table(sample_summary, paste0(gsub(".tsv", "/", gsub("sufam/", "pyclone/", opt$file_name)), opt$sample_name, ".tsv"), sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE, append=FALSE)
 
