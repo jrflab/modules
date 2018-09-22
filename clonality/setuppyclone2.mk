01f644c6a9f32abc0656d500c401bc98b1b36d7b
David Brown <ndbrown6@gmail.com>
2018-09-22 15:37:30 -0400

    update to pyclone single sample

--- clonality/setuppyclone2.mk
+++ clonality/setuppyclone2.mk
@@ -1,23 +1,24 @@
 include modules/Makefile.inc
 
-LOGDIR ?= log/setup_pyclone.$(NOW)
+LOGDIR ?= log/setup_pyclone2.$(NOW)
 PHONY += pyclone
 
-setup_pyclone : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(normal.$(pair))/$(tumor.$(pair)).yaml)
+setup_pyclone2 : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/$(pair).yaml)
 
 define make-input-pyclone
-pyclone/$2/$1.tsv : sufam/$2.tsv
+pyclone/$1_$2/$1_$2.tsv : summary/tsv/mutation_summary.tsv ascat/ascat/$1_$2.RData
 	$$(call RUN,-c -s 4G -m 6G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
-								if [ ! -d pyclone/$2 ]; then mkdir pyclone/$2; fi && \
-								$(RSCRIPT) modules/clonality/tsvforpyclone.R --file_name sufam/$2.tsv --sample_name $1")
+								if [ ! -d pyclone/$1_$2 ]; then mkdir pyclone/$1_$2; fi && \
+								$(RSCRIPT) modules/clonality/tsvforpyclone2.R --tumor_name $1 --normal_name $2")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
-	$(eval $(call make-input-pyclone,$(tumor.$(pair)),$(normal.$(pair)))))
+		$(eval $(call make-input-pyclone,$(tumor.$(pair)),$(normal.$(pair)))))
 
 define build-mutations-file
-pyclone/$2/$1.yaml : pyclone/$2/$1.tsv
+pyclone/$1_$2/$1_$2.yaml : pyclone/$1_$2/$1_$2.tsv
 	$$(call RUN,-c -s 4G -m 6G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
-								PyClone build_mutations_file --in_file pyclone/$2/$1.tsv --out_file pyclone/$2/$1.yaml --prior total_copy_number")
+								PyClone build_mutations_file --in_file pyclone/$1_$2/$1_$2.tsv --out_file pyclone/$1_$2/$1_$2.yaml --prior total_copy_number")
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
-	$(eval $(call build-mutations-file,$(tumor.$(pair)),$(normal.$(pair)))))
+		$(eval $(call build-mutations-file,$(tumor.$(pair)),$(normal.$(pair)))))
+
---
+++ clonality/tsvforpyclone2.R
@@ -0,0 +1,53 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library("optparse"))
+
+optList = list(make_option("--tumor_name", default = NULL, help = "tumor name"),
+			   make_option("--normal_name", default = NULL, help = "normal name"))
+
+parser = OptionParser(usage = "%prog [options] mutation_file", option_list = optList)
+arguments = parse_args(parser, positional_arguments = T)
+opt = arguments$options
+mutation_summary = read.csv(file="summary/tsv/mutation_summary.tsv", header=TRUE, sep="\t", stringsAsFactors=FALSE)
+mutation_summary = subset(mutation_summary, TUMOR_SAMPLE==opt$tumor_name & NORMAL_SAMPLE==opt$normal_name)
+index = grepl("mutect", mutation_summary[,"variantCaller"]) | grepl("varscan", mutation_summary[,"variantCaller"])
+mutation_summary = mutation_summary[index,,drop=FALSE]
+index = mutation_summary[,"Variant_Classification"] %in% c("Frame_Shift_Del", "Frame_Shift_Ins", "In_Frame_Del", "In_Frame_Ins", "Missense_Mutation", "Nonsense_Mutation", "Nonstop_Mutation")
+mutation_summary = mutation_summary[index,,drop=FALSE]
+index = mutation_summary[,"CHROM"] %in% c(1:23, "X")
+mutation_summary = mutation_summary[index,,drop=FALSE]
+
+Chromosome = mutation_summary[,"CHROM"]
+Chromosome[Chromosome=="X"] = 23
+Chromosome = as.numeric(Chromosome)
+Position = as.numeric(mutation_summary[,"POS"])
+load(paste0("ascat/ascat/", opt$tumor_name, "_", opt$normal_name, ".RData"))
+Chromosomes = tmp2$SNPpos[tmp3$seg[,"start"],1]
+Start = tmp2$SNPpos[tmp3$seg[,"start"],2]
+End = tmp2$SNPpos[tmp3$seg[,"end"],2]
+qt = tmp3$seg[,"nA"] + tmp3$seg[,"nB"]
+q2 = apply(tmp3$seg[,c("nA","nB")], 1, max)
+index = rep(NA, nrow(mutation_summary))
+for (j in 1:nrow(mutation_summary)) {
+	indx = which(Chromosomes==Chromosome[j] & Start<=Position[j] & End>=Position[j])
+	if (length(indx)!=0) {
+		index[j] = indx
+	} else {
+		index[j] = NA
+	}
+}
+q_t = qt[index]
+q_2 = q2[index]
+q_t[is.na(q_t)] = 2
+q_2[is.na(q_2)] = 1
+
+mutation_id = paste0(mutation_summary[,"SYMBOL"], "_", mutation_summary[,"HGVSp_Short"])
+ref_counts = round((1-mutation_summary[,"TUMOR_MAF"])*mutation_summary[,"TUMOR_DP"])
+var_counts = round(mutation_summary[,"TUMOR_MAF"]*mutation_summary[,"TUMOR_DP"])
+normal_cn = rep(2, nrow(mutation_summary))
+minor_cn = rep(0, nrow(mutation_summary))
+major_cn = q_t
+
+mutation_summary = cbind(mutation_id, ref_counts, var_counts, normal_cn, minor_cn, major_cn)
+colnames(mutation_summary) = c("mutation_id", "ref_counts", "var_counts", "normal_cn", "minor_cn", "major_cn")
+write.table(mutation_summary, file=paste0("pyclone/", opt$tumor_name, "_", opt$normal_name, "/", opt$tumor_name, "_", opt$normal_name, ".tsv"), col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
