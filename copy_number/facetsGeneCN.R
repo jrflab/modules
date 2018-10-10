16ceadf3db7e87142bef3ef2d69828137c574862
David Brown <ndbrown6@gmail.com>
2018-10-10 09:41:38 -0400

    substiture reference flat file for MySQL query in facets

--- copy_number/facetsGeneCN.R
+++ copy_number/facetsGeneCN.R
@@ -25,57 +25,62 @@ optList <- list(
 				make_option("--mysqlUser", default = 'embl', help = "MySQL server username"),
 				make_option("--mysqlPassword", default = NULL, help = "MySQL server password"),
 				make_option("--mysqlDb", default = 'homo_sapiens_core_75_37', help = "MySQL server database"),
-				make_option("--genesFile", default = NULL, help = "list of genes to include (hgnc symbols)"))
-parser <- OptionParser(usage = "%prog [options] [facets files]", option_list = optList);
+				make_option("--genesFile", default = NULL, help = "list of genes to include (hgnc symbols)"),
+				make_option("--annotFile", default = NULL, help = "file with annotations to replace MySQL sever query"))
+parser <- OptionParser(usage = "%prog [options] [facets files]", option_list = optList)
 
-arguments <- parse_args(parser, positional_arguments = T);
-opt <- arguments$options;
+arguments <- parse_args(parser, positional_arguments = T)
+opt <- arguments$options
 
 if (length(arguments$args) < 1) {
 	cat("Need facets output files\n")
-	print_help(parser);
-	stop();
+	print_help(parser)
+	stop()
 } else if (is.null(opt$outFile)) {
 	cat("Need output prefix\n")
-	print_help(parser);
-	stop();
+	print_help(parser)
+	stop()
 } else {
 	facetsFiles <- arguments$args
 }
 
-connect <- function() dbConnect(MySQL(), host = opt$mysqlHost, port = opt$mysqlPort, user = opt$mysqlUser, password = opt$mysqlPassword, dbname = opt$mysqlDb)
-cat('Connecting to ensembl ... ')
-mydb <- connect()
-on.exit(dbDisconnect(mydb))
+if (is.null(opt$annotFile)) {
+	connect <- function() dbConnect(MySQL(), host = opt$mysqlHost, port = opt$mysqlPort, user = opt$mysqlUser, password = opt$mysqlPassword, dbname = opt$mysqlDb)
+	cat('Connecting to ensembl ... ')
+	mydb <- connect()
+	on.exit(dbDisconnect(mydb))
 
-query <- "select r.name as chrom,
-g.seq_region_start as start,
-g.seq_region_end as end,
-x.display_label as hgnc,
-k.band as band
-from gene as g
-join seq_region as r on g.seq_region_id = r.seq_region_id
-join xref as x on g.display_xref_id = x.xref_id
-left join karyotype k on g.seq_region_id = k.seq_region_id
-and ((g.seq_region_start >= k.seq_region_start and g.seq_region_start <= k.seq_region_end)
-or (g.seq_region_end >= k.seq_region_start and g.seq_region_end <= k.seq_region_end))
-where x.external_db_id = 1100;"
-repeat {
-	rs <- try(dbSendQuery(mydb, query), silent = T)
-	if (is(rs, "try-error")) {
-		cat("Lost connection to mysql db ... ")
-		mydb <- connect()
-		cat("reconnected\n")
-	} else {
-		break
+	query <- "select r.name as chrom,
+	g.seq_region_start as start,
+	g.seq_region_end as end,
+	x.display_label as hgnc,
+	k.band as band
+	from gene as g
+	join seq_region as r on g.seq_region_id = r.seq_region_id
+	join xref as x on g.display_xref_id = x.xref_id
+	left join karyotype k on g.seq_region_id = k.seq_region_id
+	and ((g.seq_region_start >= k.seq_region_start and g.seq_region_start <= k.seq_region_end)
+	or (g.seq_region_end >= k.seq_region_start and g.seq_region_end <= k.seq_region_end))
+	where x.external_db_id = 1100;"
+	repeat {
+		rs <- try(dbSendQuery(mydb, query), silent = T)
+		if (is(rs, "try-error")) {
+			cat("Lost connection to mysql db ... ")
+			mydb <- connect()
+			cat("reconnected\n")
+		} else {
+			break
+		}
 	}
+	genes <- dbFetch(rs, -1)
+} else {
+	genes = read.csv(file=opt$annotFile, header=TRUE, sep="\t", stringsAsFactors=FALSE)
 }
-genes <- dbFetch(rs, -1)
 cat(paste("Found", nrow(genes), "records\n"))
 
 genes %<>% filter(chrom %in% as.character(c(1:22, "X", "Y"))) %>%
-	filter(!duplicated(hgnc)) %>% 
-	arrange(as.integer(chrom), start, end)
+		   filter(!duplicated(hgnc)) %>%
+		   arrange(as.integer(chrom), start, end)
 
 if (!is.null(opt$genesFile)) {
 	g <- scan(opt$genesFile, what = 'character')
