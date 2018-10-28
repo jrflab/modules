2c8b95947fc831212ba05bfa969e6cc1cedf476b
David Brown <ndbrown6@gmail.com>
2018-10-27 22:26:22 -0400

    Update variant_callers/sufammultiSample.mk

--- variant_callers/sufammultiSample.mk
+++ variant_callers/sufammultiSample.mk
@@ -12,9 +12,9 @@ sufam/%.txt : summary/tsv/mutation_summary.tsv
 								$(RSCRIPT) modules/variant_callers/combineSamples.R --patient $$*")
 
 sufam/%.tsv : sufam/%.txt
-	$$(call RUN,-c -s 4G -m 6G,"source /home/${USER}/share/usr/opt/miniconda/bin/activate /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/ && \
-																							  export R_LIBS=/home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:/home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$R_LIBS && \
-																							  $(RSCRIPT) modules/variant_callers/updateSamples.R --patient $$*")
+	$$(call RUN,-c -s 4G -m 6G --default_env /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/,"source /home/${USER}/share/usr/opt/miniconda/bin/activate /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/ && \
+								export R_LIBS=/home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:/home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$R_LIBS && \
+								$(RSCRIPT) modules/variant_callers/updateSamples.R --patient $$*")
 	
 endef
 $(foreach sample,$(NORMAL_SAMPLES),\
