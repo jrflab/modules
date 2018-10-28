07cd89843f43c5ed78da2b258b774419d82f1ad9
David Brown <ndbrown6@gmail.com>
2018-10-27 22:22:18 -0400

    Update variant_callers/sufammultiSample.mk

--- variant_callers/sufammultiSample.mk
+++ variant_callers/sufammultiSample.mk
@@ -12,7 +12,7 @@ sufam/%.txt : summary/tsv/mutation_summary.tsv
 								$(RSCRIPT) modules/variant_callers/combineSamples.R --patient $$*")
 
 sufam/%.tsv : sufam/%.txt
-	$$(call RUN,-c -s 4G -m 6G -v /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5,"source /home/${USER}/share/usr/opt/miniconda/bin/activate /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/ && \
+	$$(call RUN,-c -s 4G -m 6G,"source /home/${USER}/share/usr/opt/miniconda/bin/activate /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/ && \
 																							  export R_LIBS=/home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:/home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$R_LIBS && \
 																							  $(RSCRIPT) modules/variant_callers/updateSamples.R --patient $$*")
 	
