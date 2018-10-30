68b7ab5fb3a66968a1ca4550f171eeb785c3e20b
David Brown <ndbrown6@gmail.com>
2018-10-30 15:30:46 -0400

    Update variant_callers/sufammultiSample.mk

--- variant_callers/sufammultiSample.mk
+++ variant_callers/sufammultiSample.mk
@@ -1,7 +1,6 @@
 include modules/Makefile.inc
 
-RUNNING_ENV = /home/$(USER)/share/usr/anaconda-envs/jrflab-modules-0.1.5
-DEFAULT_ENV = /home/$(USER)/share/usr/opt/miniconda/bin/activate
+DEFAULT_ENV = /home/$(USER)/share/usr/anaconda-envs/jrflab-modules-0.1.5
 
 LOGDIR ?= log/sufam_multisample.$(NOW)
 PHONY += sufam
@@ -13,7 +12,7 @@ sufam/%.txt : summary/tsv/mutation_summary.tsv
 	$$(call RUN,-c -s 4G -m 6G,"$(RSCRIPT) modules/variant_callers/combineSamples.R --patient $$*")
 
 sufam/%.tsv : sufam/%.txt
-	$$(call RUN,-c -s 4G -m 6G --env $$(RUNNING_ENV) --default_env $$(DEFAULT_ENV),"$(RSCRIPT) modules/variant_callers/updateSamples.R --patient $$*")
+	$$(call RUN,-c -s 4G -m 6G --env $$(DEFAULT_ENV),"$(RSCRIPT) modules/variant_callers/updateSamples.R --patient $$*")
 	
 endef
 $(foreach sample,$(NORMAL_SAMPLES),\
