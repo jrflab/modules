46a4949f79648e040856bcd2bfba3e44ef64e816
David Brown <ndbrown6@gmail.com>
2018-11-03 17:41:49 -0400

    Update bam_tools/checkBam.mk

--- bam_tools/checkBam.mk
+++ bam_tools/checkBam.mk
@@ -3,9 +3,9 @@ include modules/Makefile.inc
 LOGDIR ?= log/check_bam.$(NOW)
 PHONY += check_bam
 
-check_bam : $(foreach sample,$(SAMPLES),check_bam/$(sample).txt)
+check_bam : $(foreach sample,$(SAMPLES),check_bam/$(sample).txt) check_bam/summary.txt
 
-CHECK ?= $(wildcard $(foreach set,$(SAMPLES),check_bam/$(set).txt))
+CHECK_BAM ?= $(wildcard $(foreach set,$(SAMPLES),check_bam/$(set).txt))
 
 define check-bam
 check_bam/%.txt : bam/%.bam
@@ -13,6 +13,9 @@ check_bam/%.txt : bam/%.bam
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call check-bam,$(sample))))
+		
+check_bam/summary.txt: $(wildcard check_bam/$(SAMPLES).txt)
+	$(call RUN,-n 1 -s 2G -m 4G,"cat $(CHECK_BAM) > check_bam/summary.txt &&")
 
 .DELETE_ON_ERROR:
 .SECONDARY:
