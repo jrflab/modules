5a7377b11589dc73e3ce42340658a3ef43537d7c
David Brown <ndbrown6@gmail.com>
2018-11-03 14:53:09 -0400

    Update summary/hotspotSummary.mk

--- summary/hotspotSummary.mk
+++ summary/hotspotSummary.mk
@@ -5,7 +5,7 @@ PHONY += hotspot summary summary/tsv
 
 HOTSPOT ?= $(wildcard $(foreach set,$(SAMPLE_PAIRS),summary/tsv/$(set).tsv))
 
-hotspot_summary : summary/hotspot_summary.xlsx
+hotspot_summary : summary/tsv/hotspot_summary.tsv summary/hotspot_summary.xlsx
 
 summary/tsv/hotspot_summary.tsv : $(wildcard hotspot/$(SAMPLE_PAIRS).txt)
 	$(call RUN,-n 1 -s 4G -m 4G,"$(RSCRIPT) modules/summary/hotspotsummary.R --in_file '$(HOTSPOT)' --out_file summary/tsv/hotspot_summary.tsv")
