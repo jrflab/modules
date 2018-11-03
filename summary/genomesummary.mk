61684267f90815ff43f164878a81744798a4603b
David Brown <ndbrown6@gmail.com>
2018-11-03 15:27:27 -0400

    Update summary/genomesummary.mk

--- summary/genomesummary.mk
+++ summary/genomesummary.mk
@@ -10,7 +10,7 @@ MYRIAD_SCORE ?= $(wildcard $(foreach set,$(SAMPLE_PAIRS),genome_stats/$(set).mrs
 
 genome_summary : genome_stats/lst_score.tsv genome_stats/genome_altered.tsv genome_stats/ntai_score.tsv genome_stats/myriad_score.tsv summary/tsv/genome_summary.tsv summary/genome_summary.xlsx
 
-genome_stats/lst_score.tsv genome_stats/genome_altered.tsv genome_stats/ntai_score.tsv genome_stats/myriad_score.tsv genome_stats/genome_summary.tsv : 
+genome_stats/lst_score.tsv genome_stats/genome_altered.tsv genome_stats/ntai_score.tsv genome_stats/myriad_score.tsv summary/tsv/genome_summary.tsv : 
 	$(call RUN,-n 1 -s 4G -m 4G,"cat $(LST_SCORE) > genome_stats/lst_score.tsv && \
 				     			 cat $(GENOME_ALTERED) > genome_stats/genome_altered.tsv && \
 				     			 cat $(NTAI_SCORE) > genome_stats/ntai_score.tsv && \
