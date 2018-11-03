bb14395f83778d705d4cc5bfd1d41792bf44e480
David Brown <ndbrown6@gmail.com>
2018-11-03 17:34:04 -0400

    Update bam_tools/checkBam.mk

--- bam_tools/checkBam.mk
+++ bam_tools/checkBam.mk
@@ -9,7 +9,7 @@ CHECK ?= $(wildcard $(foreach set,$(SAMPLES),check_bam/$(set).txt))
 
 define check-bam
 check_bam/%.txt : bam/%.bam
-	$$(call RUN,-c -n 1 -s 2G -m 4G," if [ -f $$(<) ]; then touch $$(<<); fi")
+	$$(call RUN,-c -n 1 -s 2G -m 4G," if [ -f $$(<) ]; then touch $$(*); fi")
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call check-bam,$(sample))))
