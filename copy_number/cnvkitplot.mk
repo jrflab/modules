3192673b8ee6df1a4eb5a8cee96ff6daaaaf043e
David Brown <ndbrown6@gmail.com>
2018-11-04 19:28:34 -0500

    Update copy_number/cnvkitplot.mk

--- copy_number/cnvkitplot.mk
+++ copy_number/cnvkitplot.mk
@@ -8,7 +8,7 @@ cnvkit : $(foreach sample,$(SAMPLES),cnvkit/plot/$(sample).ontarget.pdf cnvkit/p
 
 define cnvkit-plot
 cnvkit/plot/%.ontarget.pdf cnvkit/plot/%.offtarget.pdf : cnvkit/cnr/%.cnr
-	$$(call RUN,-c -s 4G -m 6G,"$(RSCRIPT) modules/copy_number/cnvkitplot.R --in_file $$(<)")
+	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 4G -m 6G,"$(RSCRIPT) modules/copy_number/cnvkitplot.R --in_file $$(<)")
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call cnvkit-plot,$(sample))))
