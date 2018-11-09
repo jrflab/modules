ea652e8c1af42668c4354e6969f73e14e2f67d03
David Brown <ndbrown6@gmail.com>
2018-11-09 13:03:30 -0500

    ASCAT

--- copy_number/ascat.mk
+++ copy_number/ascat.mk
@@ -39,7 +39,7 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 
 define ascat-plot-aspcf
 ascat/log2nbaf/$1_$2.pdf : ascat/mad/$1_$2.RData
-	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 3G -m 6G,"$(RSCRIPT) modules/copy_number/ascat.R --type plot-aspcf --file_in $$< --file_out ascat/log2nbaf/$1_$2.pdf --gamma $${aspcf_gamma.$1}")
+	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 3G -m 6G,"$(RSCRIPT) modules/copy_number/ascat.R --type plot-aspcf --file_in $$< --file_out ascat/log2nbaf/$1_$2.pdf")
 	
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
