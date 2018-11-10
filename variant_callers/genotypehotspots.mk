1978e4b05fbce00355d92a6ca0818dc54c733207
David Brown <ndbrown6@gmail.com>
2018-11-10 16:31:03 -0500

    Update variant_callers/genotypehotspots.mk

--- variant_callers/genotypehotspots.mk
+++ variant_callers/genotypehotspots.mk
@@ -6,15 +6,15 @@ SUFAM_OPTS = --mpileup-parameters='-A -q 15 -Q 15 -d 15000'
 LOGDIR ?= log/genotype_hotspots.$(NOW)
 PHONY += hotspot
 
-genotype_hotspots : $(foreach pair,$(SAMPLE_PAIRS),hotspot/$(pair).txt)
+genotype_hotspots : $(foreach sample,$(SAMPLES),hotspot/$(sample).txt)
 
 define genotype-hotspots
-hotspot/$1_$2.txt : bam/$1.bam bam/$2.bam
-	$$(call RUN,-v $$(SUFAM_ENV) -c -s 2G -m 4G -w 2880,"sufam --sample_name $1 $$(SUFAM_OPTS) $$(REF_FASTA) modules/reference/hotspots/hotspot-dedup.vcf bam/$1.bam > hotspot/$1_$2.txt")
+hotspot/%.txt : bam/%.bam
+	$$(call RUN,-v $$(SUFAM_ENV) -c -s 2G -m 4G -w 2880,"sufam --sample_name $$(*) $$(SUFAM_OPTS) $$(REF_FASTA) modules/reference/hotspots/hotspot-dedup.vcf bam/$$(*).bam > hotspot/$$(*).txt")
 	
 endef
-$(foreach pair,$(SAMPLE_PAIRS),\
-		$(eval $(call genotype-hotspots,$(tumor.$(pair)),$(normal.$(pair)))))
+ $(foreach sample,$(SAMPLES),\
+		$(eval $(call genotype-hotspots,$(sample))))
 
 .DELETE_ON_ERROR:
 .SECONDARY:
