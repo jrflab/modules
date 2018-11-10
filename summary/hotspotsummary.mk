c27497de44f3744173fe4ec4a80a7c94d48516c5
David Brown <ndbrown6@gmail.com>
2018-11-10 16:32:31 -0500

    Update summary/hotspotsummary.mk

--- summary/hotspotsummary.mk
+++ summary/hotspotsummary.mk
@@ -3,11 +3,11 @@ include modules/Makefile.inc
 LOGDIR ?= log/hotspot_summary.$(NOW)
 PHONY += hotspot summary summary/tsv
 
-HOTSPOT ?= $(wildcard $(foreach set,$(SAMPLE_PAIRS),hotspot/$(set).txt))
+HOTSPOT ?= $(wildcard $(foreach sample,$(SAMPLES),hotspot/$(sample).txt))
 
 hotspot_summary : summary/tsv/hotspot_summary.tsv summary/hotspot_summary.xlsx
 
-summary/tsv/hotspot_summary.tsv : $(wildcard hotspot/$(SAMPLE_PAIRS).txt)
+summary/tsv/hotspot_summary.tsv : $(wildcard hotspot/$(SAMPLES).txt)
 	$(call RUN,-n 1 -s 4G -m 4G,"$(RSCRIPT) modules/summary/hotspotsummary.R --in_file '$(HOTSPOT)' --out_file summary/tsv/hotspot_summary.tsv")
 		
 summary/hotspot_summary.xlsx : summary/tsv/hotspot_summary.tsv
