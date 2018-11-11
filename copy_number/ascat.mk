232fd80190121976364b9b9829ae8972a41a2951
David Brown <ndbrown6@gmail.com>
2018-11-10 22:07:25 -0500

    Update copy_number/ascat.mk

--- copy_number/ascat.mk
+++ copy_number/ascat.mk
@@ -50,7 +50,7 @@ ascat/ascat/$1_$2.pdf : ascat/mad/$1_$2.RData
 	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 3G -m 6G,"export rho=NA; && \
 																   if [ ! -z ${ascat_rho.$1} ]; then export rho=$${ascat_rho.$1}; else export rho=NA; fi; && \
 																   if [ ! -z ${ascat_psi.$1} ]; then export psi=$${ascat_psi.$1}; else export psi=NA; fi; && \
-																   $(RSCRIPT) modules/copy_number/ascat.R --type run-ascat --file_in $$< --file_out ascat/ascat/$1_$2.pdf --rho ${rho} --psi ${psi}")
+																   $(RSCRIPT) modules/copy_number/ascat.R --type run-ascat --file_in $$< --file_out ascat/ascat/$1_$2.pdf --rho $(rho) --psi ${psi}")
 	
 ascat/total/$1_$2.pdf : facets/cncf/$1_$2.Rdata ascat/ascat/$1_$2.pdf
 	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 6G -m 12G,"$(RSCRIPT) modules/copy_number/ascat.R --type total-copy --file_in $$< --file_out ascat/total/$1_$2.pdf")	
