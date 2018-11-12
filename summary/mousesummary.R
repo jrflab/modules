e1032a9da4f2491179dffe7206022e48b2346866
David Brown <ndbrown6@gmail.com>
2018-11-12 18:46:41 -0500

    Update summary/mousesummary.R

--- summary/mousesummary.R
+++ summary/mousesummary.R
@@ -28,5 +28,13 @@ AD = do.call(cbind, AD)
 colnames(AD) = paste0("AD_", sample_names)
 MAF = do.call(cbind, MAF)
 colnames(MAF) = paste0("MAF_", sample_names)
-res = cbind(DP, AD, MAF)
+vcf = read.table(file="sufam/PDX.vcf", header=FALSE, sep="\t", comment.char="#", stringsAsFactors=FALSE)
+chr = vcf[,1]
+pos = vcf[,2]
+ref = vcf[,4]
+alt = vcf[,5]
+gene_symbol = unlist(lapply(strsplit(vcf[,7], "p.", fixed=TRUE), function(x) { x[1] }))
+hgvsp_short = paste0("p.", unlist(lapply(strsplit(vcf[,7], "p.", fixed=TRUE), function(x) { x[2] })))
+res = cbind(chr, pos, ref, alt, gene_symbol, hgvsp_short, DP, AD, MAF)
+colnames(res)[1:6] = c("Chromosome", "Position", "Reference_Allele", "Alternate_Allele", "Gene_Symbol", "HGVSp")
 write.table(res, file=out_file_name, sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
