f327c010effe8d16be91316121faaa5a91ac54ec
David Brown <ndbrown6@gmail.com>
2018-11-12 16:09:38 -0500

    Update variant_callers/genotypepdx.mk

--- variant_callers/genotypepdx.mk
+++ variant_callers/genotypepdx.mk
@@ -6,11 +6,15 @@ SUFAM_OPTS = --mpileup-parameters='-A -q 15 -Q 15 -d 15000'
 LOGDIR ?= log/genotype_pdx.$(NOW)
 PHONY += sufam
 
+summary_to_vcf : sufam/PDX.vcf
 genotype_pdx : $(foreach sample,$(sample_category.mouse),sufam/$(sample).txt)
 
+sufam/PDX.vcf : summary/tsv/mutation_summary.tsv
+	$(call RUN, -c -s 8G -m 16G,"$(RSCRIPT) modules/variant_callers/genotypepdx.R")
+
 define genotype-pdx
-sufam/%.txt : bam/%.bam
-	$$(call RUN,-v $$(SUFAM_ENV) -c -s 2G -m 4G -w 2880,"sufam --sample_name $$(*) $$(SUFAM_OPTS) $$(REF_FASTA) modules/reference/hotspots/hotspot-dedup.vcf bam/$$(*).bam > sufam/$$(*).txt")
+sufam/%.txt : bam/%.bam sufam/PDX.vcf
+	$$(call RUN,-v $$(SUFAM_ENV) -c -s 2G -m 4G -w 2880,"sufam --sample_name $$(*) $$(SUFAM_OPTS) $$(REF_FASTA) sufam/PDX.vcf bam/$$(*).bam > sufam/$$(*).txt")
 	
 endef
  $(foreach sample,$(sample_category.mouse),\
