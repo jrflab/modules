b072ac680bf828997e4439ddab34b2889de950d5
David Brown <ndbrown6@gmail.com>
2018-11-12 20:59:45 -0500

    Update clonality/runpyclone.mk

--- clonality/runpyclone.mk
+++ clonality/runpyclone.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/run_pyclone.$(NOW)
 PHONY += pyclone
 
-run_pyclone : $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone.tsv) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_density.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_matrix.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_vaf_coordinates.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_scatter.pdf)
+run_pyclone : $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone.tsv) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_density.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_matrix.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_vaf_coordinates.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_scatter.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_cluster_density.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/parallel_cluster_coordinates.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/pyclone_cluster_scatter.pdf)
 
 define run-pyclone
 pyclone/%/trace/alpha.tsv.bz2 : pyclone/%/config.yaml
@@ -38,6 +38,21 @@ pyclone/%/pyclone_scatter.pdf : pyclone/%/trace/alpha.tsv.bz2
 	$$(call RUN,-s 4G -m 6G,"export DISPLAY='localhost:10.0' && \
 							 source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
 							 PyClone plot_loci --config_file pyclone/$$*/config.yaml --plot_file pyclone/$$*/pyclone_scatter.pdf --plot_type vaf_scatter --burnin 1000 --min_cluster_size 2 --max_clusters 5")
+
+pyclone/%/pyclone_cluster_density.pdf : pyclone/%/trace/alpha.tsv.bz2
+	$$(call RUN,-s 4G -m 6G,"export DISPLAY='localhost:10.0' && \
+							 source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
+							 PyClone plot_clusters --config_file pyclone/$$*/config.yaml --plot_file pyclone/$$*/pyclone_cluster_density.pdf --plot_type density --burnin 1000 --min_cluster_size 2 --max_clusters 5")
+
+pyclone/%/parallel_cluster_coordinates.pdf : pyclone/%/trace/alpha.tsv.bz2
+	$$(call RUN,-s 4G -m 6G,"export DISPLAY='localhost:10.0' && \
+							 source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
+							 PyClone plot_clusters --config_file pyclone/$$*/config.yaml --plot_file pyclone/$$*/parallel_cluster_coordinates.pdf --plot_type parallel_coordinates --burnin 1000 --min_cluster_size 2 --max_clusters 5")
+
+pyclone/%/pyclone_cluster_scatter.pdf : pyclone/%/trace/alpha.tsv.bz2
+	$$(call RUN,-s 4G -m 6G,"export DISPLAY='localhost:10.0' && \
+							 source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
+							 PyClone plot_clusters --config_file pyclone/$$*/config.yaml --plot_file pyclone/$$*/pyclone_cluster_scatter.pdf --plot_type scatter --burnin 1000 --min_cluster_size 2 --max_clusters 5")
 
 endef
 $(foreach sample,$(NORMAL_SAMPLES),\
