eba39c9a6ca5a4fadd22b5383b0b909efe88de7f
David Brown <ndbrown6@gmail.com>
2018-11-15 13:42:38 -0500

    Update pyclonelocidensity.R

--- clonality/pyclonelocidensity.R
+++ clonality/pyclonelocidensity.R
@@ -34,8 +34,8 @@ for (i in 1:length(file_names)) {
 for (i in 1:length(file_names)) {
 	ccf[[i]] = ccf[[i]][-(1:opt$burnin),,drop=FALSE]
 }
-pdf(file=paste0("pyclone/", opt$sample_name, "/plots/by_loci_density.pdf"), width=8)
-par(mar=c(6.1, 6.5, 4.1, 1.1))
+pdf(file=paste0("pyclone/", opt$sample_name, "/plots/by_loci_density.pdf"), width=15)
+par(mar=c(6.1, 6.5, 4.1, 1.1), mfcol=c(1,2))
 for (i in 1:length(feature_names)) {
 	tmp = tmp2 = z = list()
 	for (j in 1:length(ccf)) {
@@ -51,15 +51,23 @@ for (i in 1:length(feature_names)) {
 		}
 		tmp2[[j]] = post_density(z[[j]])
 	}
+	
 	plot(0, 0, type="n", axes=FALSE, frame.plot=FALSE, main=sub("_", " ", feature_names[i], fixed=TRUE), xlab="", ylab="", xlim=c(0,1), ylim=c(0,1.1), cex.main=2)
 	for (j in 1:length(tmp)) {
 		index = tmp[[j]]$x>1 | tmp[[j]]$x<0
 		points(tmp[[j]]$x[!index], ((tmp[[j]]$y-min(tmp[[j]]$y))/(max(tmp[[j]]$y)-min(tmp[[j]]$y)))[!index], type="l", lwd=3, col=hex_cols(j))
 		points(rep(mean(ccf[[j]][,i]),2), c(-1,1), type="l", col=hex_cols(j), lty=1, lwd=1)
-		
+	}
+	axis(1, at=seq(from=0, to=1, by=.2), labels=seq(from=0, to=1, by=.2), cex.axis=1.5, padj=0.25, lwd = 1.25, lwd.ticks = 1.15)
+    axis(2, at=seq(from=0, to=1, by=.2), labels=seq(from=0, to=1, by=.2), cex.axis=1.5, las=1, lwd = 1.25, lwd.ticks = 1.15)
+    mtext(side=1, text="CCF", line=4, cex=1.5)
+    mtext(side=2, text="Density", line=4, cex=1.5)
+    
+	plot(0, 0, type="n", axes=FALSE, frame.plot=FALSE, main=sub("_", " ", feature_names[i], fixed=TRUE), xlab="", ylab="", xlim=c(0,1), ylim=c(0,1.1), cex.main=2)
+	for (j in 1:length(tmp2)) {
 		index = tmp2[[j]]$x>1 | tmp2[[j]]$x<0
-		points(tmp2[[j]]$x[!index], ((tmp2[[j]]$y-min(tmp2[[j]]$y))/(max(tmp2[[j]]$y)-min(tmp2[[j]]$y)))[!index], type="l", lwd=3, col=hex_cols(j+length(tmp)))
-		points(rep(mean(z[[j]]),2), c(-1,1), type="l", col=hex_cols(j+length(tmp)), lwd=1)
+		points(tmp2[[j]]$x[!index], ((tmp2[[j]]$y-min(tmp2[[j]]$y))/(max(tmp2[[j]]$y)-min(tmp2[[j]]$y)))[!index], type="l", lwd=3, col=hex_cols(j))
+		points(rep(mean(z[[j]]),2), c(-1,1), type="l", col=hex_cols(j), lty=1, lwd=1)
 	}
     axis(1, at=seq(from=0, to=1, by=.2), labels=seq(from=0, to=1, by=.2), cex.axis=1.5, padj=0.25, lwd = 1.25, lwd.ticks = 1.15)
     axis(2, at=seq(from=0, to=1, by=.2), labels=seq(from=0, to=1, by=.2), cex.axis=1.5, las=1, lwd = 1.25, lwd.ticks = 1.15)
