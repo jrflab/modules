f885b07bd122836e8bfa966bb19e6dbe63fae166
David Brown <ndbrown6@gmail.com>
2018-11-14 23:16:21 -0500

    Update pyclonelocidensity.R

--- clonality/pyclonelocidensity.R
+++ clonality/pyclonelocidensity.R
@@ -40,7 +40,7 @@ for (i in 1:length(feature_names)) {
 	for (j in 1:length(ccf)) {
 		tmp[[j]] = post_density(ccf[[j]][,i])
 	}
-	plot(0, 0, type="n", axes=FALSE, frame.plot=FALSE, main=sub("_", " ", feature_names[i], fixed=TRUE), xlab="", ylab="", xlim=c(0,.95), ylim=c(0,1.1)
+	plot(0, 0, type="n", axes=FALSE, frame.plot=FALSE, main=sub("_", " ", feature_names[i], fixed=TRUE), xlab="", ylab="", xlim=c(0,.95), ylim=c(0,1.1))
 	for (j in 1:length(tmp)) {
 		points(tmp[[j]]$x, (tmp[[j]]$y-min(tmp[[j]]$y))/(max(tmp[[j]]$y)-min(tmp[[j]]$y)), type="l", lwd=3, col=hex_cols(j))
 	}
