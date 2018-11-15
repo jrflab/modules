ea85b847a8c34e4c4f62bb18c357d546f8ff03f4
David Brown <ndbrown6@gmail.com>
2018-11-14 23:07:59 -0500

    Update pyclonelocidensity.R

--- clonality/pyclonelocidensity.R
+++ clonality/pyclonelocidensity.R
@@ -41,12 +41,12 @@ for (i in 1:length(feature_names)) {
 	for (j in 1:length(ccf)) {
 		tmp[[j]] = post_density(ccf[[j]][,i])
 	}
-	plot(0, 0, type="n", axes=FALSE, frame.plot=FALSE, main=sub("_", " ", feature_names[i], fixed=TRUE), xlab="", ylab="", xlim=c(0,1), ylim=c(0,1))
+	plot(0, 0, type="n", axes=FALSE, frame.plot=FALSE, main=sub("_", " ", feature_names[i], fixed=TRUE), xlab="", ylab="", xlim=c(0,.9), ylim=c(0,1.2))
 	for (j in 1:length(tmp)) {
 		points(tmp[[j]]$x, (tmp[[j]]$y-min(tmp[[j]]$y))/(max(tmp[[j]]$y)-min(tmp[[j]]$y)), type="l", lwd=3, col=hex_cols(j))
 	}
-    axis(1, at=NULL, cex.axis=1.5, padj=0.25, lwd = 1.25, lwd.ticks = 1.15)
-    axis(2, at=NULL, cex.axis=1.5, las=1, lwd = 1.25, lwd.ticks = 1.15)
+    axis(1, at=seq(from=0, to=1, by=.2), labels=seq(from=0, to=1, by=.2), cex.axis=1.5, padj=0.25, lwd = 1.25, lwd.ticks = 1.15)
+    axis(2, at=seq(from=0, to=1, by=.2), labels=seq(from=0, to=1, by=.2), cex.axis=1.5, las=1, lwd = 1.25, lwd.ticks = 1.15)
     mtext(side=1, text="CCF", line=4, cex=1.5)
     mtext(side=2, text="Density", line=4, cex=1.5)
 }
