6472f60f8d124e975edb2aa445bab748f64750c2
David Brown <ndbrown6@gmail.com>
2018-11-14 23:23:39 -0500

    Update pyclonelocidensity.R

--- clonality/pyclonelocidensity.R
+++ clonality/pyclonelocidensity.R
@@ -42,7 +42,8 @@ for (i in 1:length(feature_names)) {
 	}
 	plot(0, 0, type="n", axes=FALSE, frame.plot=FALSE, main=sub("_", " ", feature_names[i], fixed=TRUE), xlab="", ylab="", xlim=c(0,1), ylim=c(0,1.1))
 	for (j in 1:length(tmp)) {
-		points(tmp[[j]]$x, (tmp[[j]]$y-min(tmp[[j]]$y))/(max(tmp[[j]]$y)-min(tmp[[j]]$y)), type="l", lwd=3, col=hex_cols(j), xlim=c(0.01,.99))
+		index = tmp[[j]]$x>1 | tmp[[j]]$x<0
+		points(tmp[[j]]$x[!index], ((tmp[[j]]$y-min(tmp[[j]]$y))/(max(tmp[[j]]$y)-min(tmp[[j]]$y)))[!index], type="l", lwd=3, col=hex_cols(j))
 	}
     axis(1, at=seq(from=0, to=1, by=.2), labels=seq(from=0, to=1, by=.2), cex.axis=1.5, padj=0.25, lwd = 1.25, lwd.ticks = 1.15)
     axis(2, at=seq(from=0, to=1, by=.2), labels=seq(from=0, to=1, by=.2), cex.axis=1.5, las=1, lwd = 1.25, lwd.ticks = 1.15)
