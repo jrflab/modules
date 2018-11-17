3bf8962f6531aac46f80bea0e9a5c9c22167af9f
David Brown <ndbrown6@gmail.com>
2018-11-16 20:21:06 -0500

    Update plotpyclone.mk

--- clonality/plotpyclone.mk
+++ clonality/plotpyclone.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/plot_pyclone.$(NOW)
 PHONY += pyclone
 
-plot_pyclone : $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/plots/by_loci_density.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/plots/all_loci_density.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/plots/all_loci_scatter.pdf)
+plot_pyclone : $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/plots/by_loci_density.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/plots/all_loci_density.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/plots/all_loci_scatter.pdf) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/summary.tsv)
 
 define plot-pyclone
 pyclone/%/plots/by_loci_density.pdf : pyclone/%/pyclone.tsv
@@ -14,7 +14,7 @@ pyclone/%/plots/all_loci_density.pdf : pyclone/%/pyclone.tsv
 	$$(call RUN,-s 4G -m 6G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.6/bin/activate /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.6/ && \
 							 $(RSCRIPT) modules/clonality/pyclonealldensity.R --sample_name $$* --burnin 5000")
 							 
-pyclone/%/plots/all_loci_scatter.pdf : pyclone/%/pyclone.tsv
+pyclone/%/plots/all_loci_scatter.pdf pyclone/%/summary.tsv : pyclone/%/pyclone.tsv sufam/%.tsv
 	$$(call RUN,-s 4G -m 6G,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.6/bin/activate /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.6/ && \
 							 $(RSCRIPT) modules/clonality/pyclonelociscatter.R --sample_name $$* --burnin 5000")
 
