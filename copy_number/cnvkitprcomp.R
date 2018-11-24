d96bad7c1764e7051e2379317f8ecc4dc60d49bc
David Brown <ndbrown6@gmail.com>
2018-11-24 18:24:15 -0500

    Update cnvkitprcomp.R

--- copy_number/cnvkitprcomp.R
+++ copy_number/cnvkitprcomp.R
@@ -14,8 +14,9 @@ parser <- OptionParser(usage = "%prog", option_list = args_list)
 arguments <- parse_args(parser, positional_arguments = T)
 opt <- arguments$options
 
-col = "steelblue"
-pch = 1
+bg = "grey90"
+col = "grey50"
+pch = 21
 cex = 1
 
 in_file = unlist(strsplit(x=opt$in_file, split=" ", fixed=TRUE))
@@ -25,14 +26,14 @@ depth = list()
 for (i in 1:length(in_file)) {
 	print(i)
 	data = read.csv(file=in_file[i], header=TRUE, sep="\t", stringsAsFactors=FALSE)
-	index = data[,"chromosome"] %in% c(as.character(1:22), "X")
+	index = data[,"chromosome"] %in% as.character(1:22)
 	depth[[i]] = as.numeric(data[index,"depth"])
 }
 depth = do.call(cbind, depth)
 pca = prcomp(t(depth), center=TRUE, scale.=TRUE)
 pdf(file=out_file, width=9, height=9)
 par(mar = c(6.1, 6.5, 4.1, 1.1))
-plot(x=pca$x[,1], y=pca$x[,2], col = col, pch = pch, cex = cex, axes = FALSE, frame.plot = FALSE, main = "", xlab = "", ylab = "")
+plot(x=pca$x[,1], y=pca$x[,2], col = col, bg = bg, pch = pch, cex = cex, lwd = .1, axes = FALSE, frame.plot = FALSE, main = "", xlab = "", ylab = "")
 axis(1, at = NULL, cex.axis = 1.5, padj = 0.25, lwd=1.25, lwd.ticks=1.15)
 axis(2, at = NULL, cex.axis = 1.5, las = 1, lwd=1.25, lwd.ticks=1.15)
 mtext(side = 1, text = "PC 1", line = 4, cex = 1.5)
