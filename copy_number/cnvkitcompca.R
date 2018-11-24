14bf5258a9196ba8421c21015f90f597d924a2aa
David Brown <ndbrown6@gmail.com>
2018-11-24 18:39:27 -0500

    Update cnvkitcompca.R

--- copy_number/cnvkitcompca.R
+++ copy_number/cnvkitcompca.R
@@ -38,7 +38,7 @@ for (i in 1:length(in_file_tumor)) {
 depth_t = do.call(cbind, depth_t)
 
 pca_n = prcomp(t(depth_n), center=TRUE, scale.=TRUE)
-pca_t = predict(object=pca_n, newdata=t(pca_t))
+pca_t = predict(object=pca_n, newdata=t(depth_t))
 x = c(pca_n$x[,1], pca_t[,1])
 y = c(pca_n$x[,2], pca_t[,2])
 bg = c(rep("grey90", nrow(pca_n$x)), rep("salmon", nrow(pca_t)))
