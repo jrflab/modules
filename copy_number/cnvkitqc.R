413941bd708b551d284ecf1d98a925e00ee7c0b1
David Brown <ndbrown6@gmail.com>
2018-11-24 21:53:03 -0500

    Update cnvkitqc.R

--- copy_number/cnvkitqc.R
+++ copy_number/cnvkitqc.R
@@ -7,14 +7,18 @@ if (!interactive()) {
     options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
 }
 
-args_list <- list(make_option("--in_file", default = NA, type = 'character', help = "input file name"),
-				  make_option("--out_file", default = NA, type = 'character', help = "output file name"),
-				  make_option("--option", default = NA, type = 'character', help = "0/1 on- or offtarget"))
+args_list <- list(make_option("--normal_files", default = NA, type = 'character', help = "normal input files"),
+				  make_option("--tumor_files", default = NA, type = 'character', help = "tumor input files"),
+				  make_option("--out_file", default = NA, type = 'character', help = "output file"),
+				  make_option("--option", default = NA, type = 'character', help = "1-0 for ontarget or offtarget"))
 parser <- OptionParser(usage = "%prog", option_list = args_list)
 arguments <- parse_args(parser, positional_arguments = T)
 opt <- arguments$options
 
-in_file = unlist(strsplit(x=opt$in_file, split=" ", fixed=TRUE))
+normal_files = unlist(strsplit(x=opt$normal_files, split=" ", fixed=TRUE))
+normal_samples = gsub(pattern=".cnr", replacement="", x=gsub(pattern="cnvkit/cnr/", replacement="", x=normal_files, fixed=TRUE), fixed=TRUE)
+tumor_files = unlist(strsplit(x=opt$tumor_files, split=" ", fixed=TRUE))
+tumor_samples = gsub(pattern=".cnr", replacement="", x=gsub(pattern="cnvkit/cnr/", replacement="", x=tumor_files, fixed=TRUE), fixed=TRUE)
 out_file = opt$out_file
 
 'MAD' <- function(x)
@@ -31,21 +35,31 @@ out_file = opt$out_file
 	return(invisible(q2))
 }
 
-'IQR' <- function(x)
+'MIQR' <- function(x)
 {
 	x = na.omit(x)
 	iq = stats::IQR(abs(x[1:(length(x)-1)] - x[2:length(x)]))
 	return(invisible(iq))
 }
 
-data = matrix(NA, nrow=length(in_file), ncol=3)
-for (i in 1:length(in_file)) {
+data = matrix(NA, nrow=length(c(normal_samples, tumor_samples)), ncol=3, dimnames=list(c(normal_samples, tumor_samples), c("MAD", "MAPD", "IQR")))
+for (i in 1:length(normal_files)) {
 	print(i)
-	data = read.csv(file=in_file[i], header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	data = read.csv(file=file[i], header=TRUE, sep="\t", stringsAsFactors=FALSE)
 	index = data[,"chromosome"] %in% 1:22 & data[,"gene"] == ifelse(opt$option==1, "-", "Antitarget")
-	data[i,1] = MAD(data[index,"log2"])
-	data[i,2] = MAPD(data[index,"log2"])
-	data[i,3] = IQR(data[index,"log2"])
+	data[normal_samples[i],1] = MAD(data[index,"log2"])
+	data[normal_samples[i],2] = MAPD(data[index,"log2"])
+	data[normal_samples[i],3] = MIQR(data[index,"log2"])
 }
+for (i in 1:length(tumor_files)) {
+	print(i)
+	data = read.csv(file=file[i], header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	index = data[,"chromosome"] %in% 1:22 & data[,"gene"] == ifelse(opt$option==1, "-", "Antitarget")
+	data[tumor_samples[i],1] = MAD(data[index,"log2"])
+	data[tumor_samples[i],2] = MAPD(data[index,"log2"])
+	data[tumor_samples[i],3] = MIQR(data[index,"log2"])
+}
+
 colnames(data) = c("MAD", "MAPD", "IQR")
-write.table(data, file=opt_file, sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
+data = cbind("SAMPLE_NAME"=c(normal_samples, tumor_samples), "SAMPLE_TYPE"=c(rep("N", length(normal_samples)), rep("T", length(tumor_samples))), data)
+write.table(data, file=out_file, sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
