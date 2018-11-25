3a6f02b62ef9b7bc918833bfaa168c90b7d56857
David Brown <ndbrown6@gmail.com>
2018-11-24 21:53:06 -0500

    Update cnvkitqc.mk

--- copy_number/cnvkitqc.mk
+++ copy_number/cnvkitqc.mk
@@ -3,14 +3,15 @@ include modules/Makefile.inc
 LOGDIR ?= log/cnvkit_qc.$(NOW)
 PHONY += cnvkit cnvkit/qc
 
-CNVKIT_CNR ?= $(wildcard $(foreach sample,$(SAMPLES),cnvkit/cnr/$(sample).cnr))
+CNVKIT_NORMAL ?= $(wildcard $(foreach sample,$(NORMAL_SAMPLES),cnvkit/cnr/$(sample).cnr))
+CNVKIT_TUMOR ?= $(wildcard $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnr/$(sample).cnr))
 
 cnvkit : cnvkit/qc/ontarget.tsv cnvkit/qc/offtarget.tsv
 
 cnvkit/qc/ontarget.tsv : $(wildcard cnvkit/cnr/$(SAMPLES).cnr)
-	$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 8G -m 16G,"$(RSCRIPT) modules/copy_number/cnvkitqc.R --in_file '$(CNVKIT_CNR)' --out_file cnvkit/qc/ontarget.tsv --option 1")
+	$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 8G -m 16G,"$(RSCRIPT) modules/copy_number/cnvkitqc.R --normal_files '$(CNVKIT_NORMAL)' --tumor_files '$(CNVKIT_TUMOR)' --out_file cnvkit/qc/ontarget.tsv --option 1")
 	
 cnvkit/qc/offtarget.tsv : $(wildcard cnvkit/cnr/$(SAMPLES).cnr)
-	$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 8G -m 16G,"$(RSCRIPT) modules/copy_number/cnvkitqc.R --in_file '$(CNVKIT_CNR)' --out_file cnvkit/qc/offtarget.tsv --option 0")
+	$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 8G -m 16G,"$(RSCRIPT) modules/copy_number/cnvkitqc.R --normal_files '$(CNVKIT_NORMAL)' --tumor_files '$(CNVKIT_TUMOR)' --out_file cnvkit/qc/offtarget.tsv --option 0")
 				
 .PHONY: $(PHONY)
