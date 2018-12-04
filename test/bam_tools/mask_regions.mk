58c2db155bb2156af8d6bc88f903e89e55d29c6a
David Brown <ndbrown6@gmail.com>
2018-12-04 10:49:31 -0500

    Update mask_regions.mk

--- test/bam_tools/mask_regions.mk
+++ test/bam_tools/mask_regions.mk
@@ -7,7 +7,7 @@ mask : $(foreach sample,$(SAMPLES),masked/$(sample).bam)
 
 define bedtools-mask
 masked/%.bam : bam/%.bam
-	$$(call RUN,-c -s 6G -m 8G -w 7200,"bedtools intersect -abam $$(<) -b $$(ONTARGET_FILE) -v > masked/$$(*).bam && \
+	$$(call RUN,-c -s 6G -m 8G,"bedtools intersect -abam $$(<) -b $$(ONTARGET_FILE) -v > masked/$$(*).bam && \
 								samtools index masked/$$(*).bam && \
 								cp masked/$$(*).bam.bai masked/$$(*).bai")
 
