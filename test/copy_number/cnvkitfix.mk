42620fc793646b319902ec9b0c06a35576873139
David Brown <ndbrown6@gmail.com>
2018-12-12 21:42:43 -0500

    Update cnvkitfix.mk

--- test/copy_number/cnvkitfix.mk
+++ test/copy_number/cnvkitfix.mk
@@ -8,7 +8,7 @@ cnvkit : $(foreach pair,$(SAMPLE_PAIRS),cnvkit/cnr/$(pair).cnr)
 
 define cnvkit-cnr
 cnvkit/cnr/$1_$2.cnr : cnvkit/cnn/tumor/$1.targetcoverage.cnn cnvkit/cnn/tumor/$1.antitargetcoverage.cnn cnvkit/reference/$2.cnr
-	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) cnvkit/reference/$2.cnr -o cnvkit/cnr/$$(*).cnr")
+	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) cnvkit/reference/$2.cnr -o cnvkit/cnr/$1_$2.cnr")
 	
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
