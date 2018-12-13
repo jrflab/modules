88c98078bf4c3f190b2427b6e3b7b1a49075e611
David Brown <ndbrown6@gmail.com>
2018-12-12 22:55:10 -0500

    Update cnvkitfix.R

--- test/copy_number/cnvkitfix.R
+++ test/copy_number/cnvkitfix.R
@@ -17,5 +17,5 @@ tumor_sample = opt$tumor
 normal_samples = unlist(strsplit(x=opt$normals, " ", fixed=TRUE))
 
 for (i in 1:length(normal_samples)) {
-	system(paste0("cnvkit.py fix cnvkit/cnn/tumor/", tumor_sample, ".targetcoverage.cnn  cnvkit/cnn/tumor/", tumor_sample, ".antitargetcoverage.cnn cnvkit/reference/", normal_samples[i], ".cnr -o cnvkit/cnr/", tumor_sample, ".cnr"))
+	system(paste0("cnvkit.py fix cnvkit/cnn/tumor/", tumor_sample, ".targetcoverage.cnn  cnvkit/cnn/tumor/", tumor_sample, ".antitargetcoverage.cnn cnvkit/reference/", normal_samples[i], ".cnr -o cnvkit/cnr/", tumor_sample, "_", normal_samples[i], ".cnr"))
 }
