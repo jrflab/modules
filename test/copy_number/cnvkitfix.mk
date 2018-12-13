fb7f839d75e3252ea02bf222580cd8d38fc19061
David Brown <ndbrown6@gmail.com>
2018-12-12 20:30:24 -0500

    CNVkit

--- test/copy_number/cnvkitfix.mk
+++ test/copy_number/cnvkitfix.mk
@@ -4,16 +4,15 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/cnvkit_fix.$(NOW)
 PHONY += cnvkit cnvkit/cnr
 
-cnvkit : $(foreach sample,$(SAMPLES),cnvkit/cnr/$(sample).cnr)
+cnvkit : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnr/$(sample).timestamp)
 
 define cnvkit-cnr
-cnvkit/cnr/%.cnr : cnvkit/cnn/tumor/%.targetcoverage.cnn cnvkit/cnn/tumor/%.antitargetcoverage.cnn cnvkit/reference/combined_reference.cnr
-	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) cnvkit/reference/combined_reference.cnr -o cnvkit/cnr/$$(*).cnr")
+cnvkit/cnr/%.timestamp : cnvkit/cnn/tumor/%.targetcoverage.cnn cnvkit/cnn/tumor/%.antitargetcoverage.cnn $(wildcard cnvkit/reference/$(NORMAL_SAMPLES).cnr)
+	$$(call RUN,-c -s 6G -m 8G,"for i in $$(NORMAL_SAMPLES); do \
+									cnvkit.py fix $$(<) $$(<<) cnvkit/reference/$i.cnr -o cnvkit/cnr/$$(*)_$i.cnr")
 	
-cnvkit/cnr/%.cnr : cnvkit/cnn/normal/%.targetcoverage.cnn cnvkit/cnn/normal/%.antitargetcoverage.cnn cnvkit/reference/combined_reference.cnr
-	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) cnvkit/reference/combined_reference.cnr -o cnvkit/cnr/$$(*).cnr")
 endef
- $(foreach sample,$(SAMPLES),\
+ $(foreach sample,$(TUMOR_SAMPLES),\
 		$(eval $(call cnvkit-cnr,$(sample))))
 				
 .PHONY: $(PHONY)
--- test/copy_number/cnvkitreference.mk
+++ test/copy_number/cnvkitreference.mk
@@ -8,7 +8,7 @@ cnvkit : $(foreach sample,$(NORMAL_SAMPLES),cnvkit/reference/$(sample).cnr)
 
 define cnvkit-reference
 cnvkit/reference/%.cnr : cnvkit/cnn/normal/%.targetcoverage.cnn cnvkit/cnn/normal/%.antitargetcoverage.cnn
-	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py reference cnvkit/cnn/$$(*).* --no-edge -o cnvkit/reference/$$(*).cnr")
+	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py reference cnvkit/cnn/normal/$$(*).*.cnn -f $(REF_FASTA) --no-edge -o cnvkit/reference/$$(*).cnr")
 	
 endef
  $(foreach sample,$(NORMAL_SAMPLES),\
