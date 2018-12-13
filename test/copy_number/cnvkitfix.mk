563f954f9fa9d6eb9feadcaaa9ca28d8b7e79843
David Brown <ndbrown6@gmail.com>
2018-12-12 23:00:07 -0500

    CNVkit

--- test/copy_number/cnvkitfix.mk
+++ test/copy_number/cnvkitfix.mk
@@ -4,11 +4,11 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/cnvkit_fix.$(NOW)
 PHONY += cnvkit cnvkit/cnr
 
-cnvkit : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnr/$(sample).cnr)
+cnvkit : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnr/$(sample).timestamp)
 
 define cnvkit-cnr
-cnvkit/cnr/%.cnr : cnvkit/cnn/tumor/%.targetcoverage.cnn cnvkit/cnn/tumor/%.antitargetcoverage.cnn
-	$$(call RUN,-c -s 6G -m 8G,"$(Rscript) modules/test/copy_number/cnvkitfix.R --tumor $$(*) --normals $$(NORMAL_SAMPLES)")
+cnvkit/cnr/%.timestamp : cnvkit/cnn/tumor/%.targetcoverage.cnn cnvkit/cnn/tumor/%.antitargetcoverage.cnn
+	$$(call RUN,-c -s 6G -m 8G,"$(Rscript) modules/test/copy_number/cnvkitfix.R --tumor $$(*) --normals $$(NORMAL_SAMPLES) && touch cnvkit/cnr/$$(*).timestamp")
 	
 endef
  $(foreach sample,$(TUMOR_SAMPLES),\
