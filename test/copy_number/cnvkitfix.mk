366ba07c1af05b51e2e428ea4f4f8c0d66f9c9d8
David Brown <ndbrown6@gmail.com>
2018-12-12 22:54:09 -0500

    Update cnvkitfix.mk

--- test/copy_number/cnvkitfix.mk
+++ test/copy_number/cnvkitfix.mk
@@ -8,7 +8,7 @@ cnvkit : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnr/$(sample).cnr)
 
 define cnvkit-cnr
 cnvkit/cnr/%.cnr : cnvkit/cnn/tumor/%.targetcoverage.cnn cnvkit/cnn/tumor/%.antitargetcoverage.cnn
-	$$(call RUN,-c -s 6G -m 8G,"$(Rscript) modules/test/copy_number/cnvkitfix.R --tumor $$(*) --normals '$NORMAL_SAMPLES'")
+	$$(call RUN,-c -s 6G -m 8G,"$(Rscript) modules/test/copy_number/cnvkitfix.R --tumor $$(*) --normals $$(NORMAL_SAMPLES)")
 	
 endef
  $(foreach sample,$(TUMOR_SAMPLES),\
