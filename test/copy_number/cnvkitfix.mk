5c327c1bd9c4c07c248938287fe35ddfbe9467a9
David Brown <ndbrown6@gmail.com>
2018-12-12 20:39:41 -0500

    Update cnvkitfix.mk

--- test/copy_number/cnvkitfix.mk
+++ test/copy_number/cnvkitfix.mk
@@ -9,7 +9,7 @@ cnvkit : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnr/$(sample).timestamp)
 define cnvkit-cnr
 cnvkit/cnr/%.timestamp : cnvkit/cnn/tumor/%.targetcoverage.cnn cnvkit/cnn/tumor/%.antitargetcoverage.cnn $(wildcard cnvkit/reference/$(NORMAL_SAMPLES).cnr)
 	$$(call RUN,-c -s 6G -m 8G,"for i in $$(NORMAL_SAMPLES); do \
-									cnvkit.py fix $$(<) $$(<<) cnvkit/reference/$$i.cnr -o cnvkit/cnr/$$(*)_$i.cnr && \
+									cnvkit.py fix $$(<) $$(<<) cnvkit/reference/$$(i).cnr -o cnvkit/cnr/$$(*)_$i.cnr && \
 									touch cnvkit/cnr/$$(*).timestamp")
 	
 endef
