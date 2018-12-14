0ca3d4e34c6500e801b5c9e80b47ac75b53fc46d
David Brown <ndbrown6@gmail.com>
2018-12-14 14:40:55 -0500

    Update cnvkitcoverage.mk

--- test/copy_number/cnvkitcoverage.mk
+++ test/copy_number/cnvkitcoverage.mk
@@ -10,7 +10,7 @@ define cnvkit-tumor-cnn
 cnvkit/cnn/tumor/%.targetcoverage.cnn : bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE) -o cnvkit/cnn/tumor/$$(*).targetcoverage.tmp && \
 									 source $(HOME)/share/usr/opt/miniconda/bin/activate /home/brownd7/share/usr/anaconda-envs/jrflab-modules-0.1.5 && \
-									 export $R_LIBS='/lila/data/reis-filho/usr/lib64/R/library:$R_LIBS' && \
+									 export R_LIBS='/lila/data/reis-filho/usr/lib64/R/library:$(R_LIBS)' && \
 									 $(RSCRIPT) modules/test/copy_number/cnvkitcoverage.R --file cnvkit/cnn/tumor/$$(*).targetcoverage.tmp && \
 									 rm cnvkit/cnn/tumor/$$(*).targetcoverage.tmp")
 
