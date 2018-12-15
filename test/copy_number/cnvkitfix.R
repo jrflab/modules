4fcd73a31ab759c977b1426daca5d39cbd56e9e0
David Brown <ndbrown6@gmail.com>
2018-12-14 23:40:49 -0500

    Update cnvkitfix.R

--- test/copy_number/cnvkitfix.R
+++ test/copy_number/cnvkitfix.R
@@ -16,14 +16,14 @@ opt = arguments$options
 tumor_sample = opt$tumor
 normal_samples = unlist(strsplit(x=opt$normals, " ", fixed=TRUE))
 
-file.copy(from=paste0("cnvkit/cnn/tumor/", tumor_sample, ".A.targetcoverage.cnn"), to=paste0("cnvkit/cnn/tumor/", tumor_sample, ".targetcoverage.cnn"))
+file.copy(from=paste0("cnvkit/cnn/tumor/", tumor_sample, ".antitargetcoverage.cnn"), to=paste0("cnvkit/cnn/tumor/", tumor_sample, ".A.antitargetcoverage.cnn"))
 for (i in 1:length(normal_samples)) {
-	system(paste0("cnvkit.py fix cnvkit/cnn/tumor/", tumor_sample, ".A.targetcoverage.cnn  cnvkit/cnn/tumor/", tumor_sample, ".antitargetcoverage.cnn cnvkit/reference/", normal_samples[i], ".A.cnr -o cnvkit/cnr/", tumor_sample, "_", normal_samples[i], ".A.cnr"))
+	system(paste0("cnvkit.py fix cnvkit/cnn/tumor/", tumor_sample, ".A.targetcoverage.cnn  cnvkit/cnn/tumor/", tumor_sample, ".A.antitargetcoverage.cnn cnvkit/reference/", normal_samples[i], ".A.cnr -o cnvkit/cnr/", tumor_sample, "_", normal_samples[i], ".A.cnr"))
 }
-file.remove(paste0("cnvkit/cnn/tumor/", tumor_sample, ".targetcoverage.cnn"))
+file.remove(paste0("cnvkit/cnn/tumor/", tumor_sample, ".A.antitargetcoverage.cnn"))
 
-file.copy(from=paste0("cnvkit/cnn/tumor/", tumor_sample, ".B.targetcoverage.cnn"), to=paste0("cnvkit/cnn/tumor/", tumor_sample, ".targetcoverage.cnn"))
+file.copy(from=paste0("cnvkit/cnn/tumor/", tumor_sample, ".antitargetcoverage.cnn"), to=paste0("cnvkit/cnn/tumor/", tumor_sample, ".A.antitargetcoverage.cnn"))
 for (i in 1:length(normal_samples)) {
-	system(paste0("cnvkit.py fix cnvkit/cnn/tumor/", tumor_sample, ".B.targetcoverage.cnn  cnvkit/cnn/tumor/", tumor_sample, ".antitargetcoverage.cnn cnvkit/reference/", normal_samples[i], ".B.cnr -o cnvkit/cnr/", tumor_sample, "_", normal_samples[i], ".B.cnr"))
+	system(paste0("cnvkit.py fix cnvkit/cnn/tumor/", tumor_sample, ".B.targetcoverage.cnn  cnvkit/cnn/tumor/", tumor_sample, ".B.antitargetcoverage.cnn cnvkit/reference/", normal_samples[i], ".B.cnr -o cnvkit/cnr/", tumor_sample, "_", normal_samples[i], ".B.cnr"))
 }
-file.remove(paste0("cnvkit/cnn/tumor/", tumor_sample, ".targetcoverage.cnn"))
+file.remove(paste0("cnvkit/cnn/tumor/", tumor_sample, ".B.antitargetcoverage.cnn"))
