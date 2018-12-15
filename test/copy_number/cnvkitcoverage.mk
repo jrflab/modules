d45cb8964292bb7f6d043cb7a02b56b4bcff54ac
David Brown <ndbrown6@gmail.com>
2018-12-14 22:42:02 -0500

    cnvkit

--- test/copy_number/cnvkitcoverage.mk
+++ test/copy_number/cnvkitcoverage.mk
@@ -4,14 +4,14 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/cnvkit_coverage_test.$(NOW)
 PHONY += cnvkit cnvkit/cnn cnvkit/cnn/tumor cnvkit/cnn/normal
 
-cnvkit : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnn/tumor/$(sample).targetcoverage.cnn cnvkit/cnn/tumor/$(sample).antitargetcoverage.cnn) $(foreach sample,$(NORMAL_SAMPLES),cnvkit/cnn/normal/$(sample).targetcoverage.cnn cnvkit/cnn/normal/$(sample).antitargetcoverage.cnn)
+cnvkit : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnn/tumor/$(sample).A.targetcoverage.cnn cnvkit/cnn/tumor/$(sample).B.targetcoverage.cnn cnvkit/cnn/tumor/$(sample).antitargetcoverage.cnn) $(foreach sample,$(NORMAL_SAMPLES),cnvkit/cnn/normal/$(sample).A.targetcoverage.cnn cnvkit/cnn/normal/$(sample).B.targetcoverage.cnn cnvkit/cnn/normal/$(sample).antitargetcoverage.cnn)
 
 define cnvkit-tumor-cnn
-cnvkit/cnn/tumor/%.targetcoverage.cnn : bam/%.bam
-	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE) -o cnvkit/cnn/tumor/$$(*).targetcoverage.tmp && \
-									 source $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.6/bin/activate $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.6 && \
-									 $(RSCRIPT) modules/test/copy_number/cnvkitcoverage.R --file cnvkit/cnn/tumor/$$(*).targetcoverage.tmp && \
-									 rm -rf cnvkit/cnn/tumor/$$(*).targetcoverage.tmp")
+cnvkit/cnn/tumor/%.A.targetcoverage.cnn : bam/%.bam
+	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_A) -o cnvkit/cnn/tumor/$$(*).A.targetcoverage.cnn")
+	
+cnvkit/cnn/tumor/%.B.targetcoverage.cnn : bam/%.bam
+	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_B) -o cnvkit/cnn/tumor/$$(*).B.targetcoverage.cnn")
 
 cnvkit/cnn/tumor/%.antitargetcoverage.cnn : bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o cnvkit/cnn/tumor/$$(*).antitargetcoverage.cnn")
@@ -20,11 +20,11 @@ endef
 		$(eval $(call cnvkit-tumor-cnn,$(sample))))
 		
 define cnvkit-normal-cnn
-cnvkit/cnn/normal/%.targetcoverage.cnn : bam/%.bam
-	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE) -o cnvkit/cnn/normal/$$(*).targetcoverage.tmp && \
-									 source $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.6/bin/activate $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.6 && \
-									 $(RSCRIPT) modules/test/copy_number/cnvkitcoverage.R --file cnvkit/cnn/normal/$$(*).targetcoverage.tmp && \
-									 rm -rf cnvkit/cnn/normal/$$(*).targetcoverage.tmp")
+cnvkit/cnn/normal/%.A.targetcoverage.cnn : bam/%.bam
+	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_A) -o cnvkit/cnn/normal/$$(*).A.targetcoverage.cnn")
+	
+cnvkit/cnn/normal/%.B.targetcoverage.cnn : bam/%.bam
+	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_B) -o cnvkit/cnn/normal/$$(*).B.targetcoverage.cnn")
 
 cnvkit/cnn/normal/%.antitargetcoverage.cnn : bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o cnvkit/cnn/normal/$$(*).antitargetcoverage.cnn")
