f2f607f063190b11cdbeead9acaf26bd50a41f90
David Brown <ndbrown6@gmail.com>
2018-12-26 14:20:26 -0500

    Update qdnaseq.mk

--- test/copy_number/qdnaseq.mk
+++ test/copy_number/qdnaseq.mk
@@ -7,13 +7,13 @@ qdnaseq : $(foreach sample,$(SAMPLES),qdnaseq/readcounts/$(sample).pdf qdnaseq/i
 
 define qdnaseq-log2ratio
 qdnaseq/readcounts/%.pdf qdnaseq/isobars/%.pdf qdnaseq/variance/%.pdf qdnaseq/log2ratio/%.pdf qdnaseq/bed/%.bed : bam/%.bam
-	$$(call RUN,-c -s 12G -m 24G -w 7200,"source $(HOME)/share/usr/opt/miniconda/bin/activate /home/brownd7/share/usr/anaconda-envs/jrflab-modules-0.1.5 && \
-										  export CPATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/include:$(CPATH)' && \
-										  export LIBRARY_PATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LIBRARY_PATH)' && \
-										  export LD_LIBRARY_PATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LD_LIBRARY_PATH)' && \
-										  export R_LIBS='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$(R_LIBS)' && \
-										  export R_LIBS='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:$(R_LIBS)' && \
-										  $(RSCRIPT) modules/test/copy_number/qdnaseq.R --sample $$(*)")
+	$$(call RUN,-c -n 16 -s 2G -m 3G -w 7200,"source $(HOME)/share/usr/opt/miniconda/bin/activate /home/brownd7/share/usr/anaconda-envs/jrflab-modules-0.1.5 && \
+										  	  export CPATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/include:$(CPATH)' && \
+										  	  export LIBRARY_PATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LIBRARY_PATH)' && \
+										  	  export LD_LIBRARY_PATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LD_LIBRARY_PATH)' && \
+										  	  export R_LIBS='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$(R_LIBS)' && \
+										  	  export R_LIBS='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:$(R_LIBS)' && \
+										  	  $(RSCRIPT) modules/test/copy_number/qdnaseq.R --sample $$(*)")
 										  
 endef
  $(foreach sample,$(SAMPLES),\
@@ -22,11 +22,3 @@ endef
 .DELETE_ON_ERROR:
 .SECONDARY:
 .PHONY: $(PHONY)
-
-
-
-               
-                
-                
-                
-                
