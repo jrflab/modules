a6b5f3d7d61b75d74ef3dc781de2dba41f70debf
David Brown <ndbrown6@gmail.com>
2018-12-26 13:01:48 -0500

    QDNAseq

---
+++ test/copy_number/qdnaseq.R
@@ -0,0 +1,52 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library("optparse"))
+suppressPackageStartupMessages(library("QDNAseq"))
+
+if (!interactive()) {
+    options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
+}
+
+args_list = list(make_option("--sample", default = NA, type = 'character', help = "sample name"))
+				  
+parser = OptionParser(usage = "%prog", option_list = args_list)
+arguments = parse_args(parser, positional_arguments = T)
+opt = arguments$options
+
+bins = getBinAnnotations(binSize=15, genome="hg19")
+readCounts = binReadCounts(bins=bins, bamfiles=paste0("bam/", opt$sample, ".bam"),
+						   isPaired=TRUE,
+       					   isProperPair=TRUE,
+        				   minMapq=30, pairedEnds=TRUE)
+       
+       
+# read counts versus genomic coordinates
+pdf(file=paste0("qdnaseq/readcounts/", opt$sample,".pdf"), width=14, height=9)
+plot(readCounts, logTransform=FALSE, ylim=c(-50, 200))
+highlightFilters(readCounts, logTransform=FALSE, residual=TRUE, blacklist=TRUE)
+dev.off()
+
+readCountsFiltered = applyFilters(readCounts, residual=TRUE, blacklist=TRUE)
+
+# GC content versus mappability
+pdf(file=paste0("qdnaseq/isobars/", opt$sample, ".pdf"), width=7, height=7)
+isobarPlot(readCountsFiltered)
+dev.off()
+
+readCountsFiltered = estimateCorrection(readCountsFiltered)
+
+# variance versus bin coverage
+pdf(file=paste0("qdnaseq/variance/", opt$sample, ".pdf"), width=7, height=7)
+noisePlot(readCountsFiltered)
+dev.off()
+
+copyNumbers = correctBins(readCountsFiltered)
+copyNumbersSmooth = smoothOutlierBins(copyNumbersNormalized)
+
+# log2 ratio versus genomic coordinates
+pdf(file=paste0("qdnaseq/log2ratio/", opt$sample, ".pdf"), width=14, height=9)
+plot(copyNumbersSmooth)
+dev.off()
+
+# write log2 ratio to file
+exportBins(copyNumbersSmooth, file=paste0("qdnaseq/bed/", opt$sample, ".bed"), format="bed")
--- test/copy_number/qdnaseq.mk
+++ test/copy_number/qdnaseq.mk
@@ -1,8 +1,17 @@
 include modules/Makefile.inc
 
 LOGDIR ?= log/qdnaseq.$(NOW)
-PHONY += qdnaseq
+PHONY += qdnaseq qdnaseq/readcounts qdnaseq/isobars qdnaseq/variance qdnaseq/log2ratio qdnaseq/bed
+
+qdnaseq : $(foreach sample,$(SAMPLES),qdnaseq/readcounts/$(sample).pdf qdnaseq/isobars/$(sample).pdf qdnaseq/variance/$(sample).pdf qdnaseq/log2ratio/$(sample).pdf qdnaseq/bed/$(sample).bed)
+
+define qdnaseq-log2ratio
+qdnaseq/readcounts/%.pdf qdnaseq/isobars/%.pdf qdnaseq/variance/%.pdf qdnaseq/log2ratio/%.pdf qdnaseq/bed/%.bed : bam/%.bam
+	$$(call RUN,-c -s XXG -m XXG -w 7200 -v ~/share/usr/anaconda-envs/qdnaseq-1.16.0/,"$(RSCRIPT) modules/test/copy_number/qdnaseq.R --sample $$(*)")
+	
 
 .DELETE_ON_ERROR:
 .SECONDARY:
 .PHONY: $(PHONY)
+
+
