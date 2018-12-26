dc6af01d04200c3a8cae0cec700c7b44f78327ed
David Brown <ndbrown6@gmail.com>
2018-12-26 18:34:14 -0500

    Update qdnaseq.mk

--- test/copy_number/qdnaseq.mk
+++ test/copy_number/qdnaseq.mk
@@ -5,15 +5,12 @@ PHONY += qdnaseq qdnaseq/readcounts qdnaseq/isobars qdnaseq/variance qdnaseq/log
 
 qdnaseq : $(foreach sample,$(SAMPLES),qdnaseq/readcounts/$(sample).pdf qdnaseq/isobars/$(sample).pdf qdnaseq/variance/$(sample).pdf qdnaseq/log2ratio/$(sample).pdf qdnaseq/bed/$(sample).bed)
 
-QDNASEQ_ENV = $(HOME)/share/usr/opt/miniconda
+DEFAULT_ENV = $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.6
+QDNASEQ_ENV = $(HOME)/share/usr/anaconda-envs/qdnaseq
 
 define qdnaseq-log2ratio
 qdnaseq/readcounts/%.pdf qdnaseq/isobars/%.pdf qdnaseq/variance/%.pdf qdnaseq/log2ratio/%.pdf qdnaseq/bed/%.bed : bam/%.bam
-	$$(call RUN,-c -n 16 -s 1G -m 3G -w 7200 --default_env $$(QDNASEQ_ENV) -v $$(QDNASEQ_ENV),"export CPATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/include:$(CPATH) && \
-	export LIBRARY_PATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LIBRARY_PATH) && \
-	export LD_LIBRARY_PATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LD_LIBRARY_PATH) && \
-	export R_LIBS=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:$(R_LIBS) && \
-	$(RSCRIPT) modules/test/copy_number/qdnaseq.R --sample $$(*) --binsize $(QDNASEQ_BINSIZE)")
+	$$(call RUN,-c -n 16 -s 1G -m 3G -w 7200 --default_env $$(DEFAULT_ENV) -v $$(QDNASEQ_ENV),"$(RSCRIPT) modules/test/copy_number/qdnaseq.R --sample $$(*) --binsize $(QDNASEQ_BINSIZE)")
 										  
 endef
  $(foreach sample,$(SAMPLES),\
