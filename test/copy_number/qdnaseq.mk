4749de01c9f1495266e0a97b880d7a83650d9913
David Brown <ndbrown6@gmail.com>
2018-12-26 17:25:54 -0500

    Update qdnaseq.mk

--- test/copy_number/qdnaseq.mk
+++ test/copy_number/qdnaseq.mk
@@ -8,11 +8,11 @@ qdnaseq : $(foreach sample,$(SAMPLES),qdnaseq/readcounts/$(sample).pdf qdnaseq/i
 define qdnaseq-log2ratio
 qdnaseq/readcounts/%.pdf qdnaseq/isobars/%.pdf qdnaseq/variance/%.pdf qdnaseq/log2ratio/%.pdf qdnaseq/bed/%.bed : bam/%.bam
 	$$(call RUN,-c -n 16 -s 2G -m 3G -w 7200,"source $(HOME)/share/usr/opt/miniconda/bin/activate /home/brownd7/share/usr/anaconda-envs/jrflab-modules-0.1.5 && \
-										  	  export CPATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/include:$(CPATH)' && \
-										  	  export LIBRARY_PATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LIBRARY_PATH)' && \
-										  	  export LD_LIBRARY_PATH='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LD_LIBRARY_PATH)' && \
-										  	  export R_LIBS='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$(R_LIBS)' && \
-										  	  export R_LIBS='$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:$(R_LIBS)' && \
+										  	  export CPATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/include:$(CPATH) && \
+										  	  export LIBRARY_PATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LIBRARY_PATH) && \
+										  	  export LD_LIBRARY_PATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LD_LIBRARY_PATH) && \
+										  	  export R_LIBS=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$(R_LIBS) && \
+										  	  export R_LIBS=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:$(R_LIBS) && \
 										  	  $(RSCRIPT) modules/test/copy_number/qdnaseq.R --sample $$(*) --binsize $(QDNASEQ_BINSIZE)")
 										  
 endef
