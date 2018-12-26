4d3674306272319e007dcc2be6f89d05ebfe6d53
David Brown <ndbrown6@gmail.com>
2018-12-26 17:50:18 -0500

    Update qdnaseq.mk

--- test/copy_number/qdnaseq.mk
+++ test/copy_number/qdnaseq.mk
@@ -5,14 +5,16 @@ PHONY += qdnaseq qdnaseq/readcounts qdnaseq/isobars qdnaseq/variance qdnaseq/log
 
 qdnaseq : $(foreach sample,$(SAMPLES),qdnaseq/readcounts/$(sample).pdf qdnaseq/isobars/$(sample).pdf qdnaseq/variance/$(sample).pdf qdnaseq/log2ratio/$(sample).pdf qdnaseq/bed/$(sample).bed)
 
+QDNASEQ_ENV = $(HOME)/share/usr/opt/miniconda
+
 define qdnaseq-log2ratio
 qdnaseq/readcounts/%.pdf qdnaseq/isobars/%.pdf qdnaseq/variance/%.pdf qdnaseq/log2ratio/%.pdf qdnaseq/bed/%.bed : bam/%.bam
-	$$(call RUN,-c -n 16 -s 2G -m 3G -w 7200 -v $(HOME)/share/usr/opt/miniconda/,"source $(HOME)/share/usr/opt/miniconda/bin/activate $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5 && \
-										  	  export CPATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/include:$(CPATH) && \
-										  	  export LIBRARY_PATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LIBRARY_PATH) && \
-										  	  export LD_LIBRARY_PATH=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$(LD_LIBRARY_PATH) && \
-										  	  export R_LIBS=$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:$(R_LIBS) && \
-										  	  $(RSCRIPT) modules/test/copy_number/qdnaseq.R --sample $$(*) --binsize $(QDNASEQ_BINSIZE)")
+	$$(call RUN,-c -n 16 -s 2G -m 3G -w 7200 -v $$(SUFAM_ENV),"source $$(HOME)/share/usr/opt/miniconda/bin/activate $$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5 && \
+										  	  export CPATH=$$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/include:$$(CPATH) && \
+										  	  export LIBRARY_PATH=$$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$$(LIBRARY_PATH) && \
+										  	  export LD_LIBRARY_PATH=$$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib:$$(LD_LIBRARY_PATH) && \
+										  	  export R_LIBS=$$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/usr/R/library:$$(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.5/lib/R/library:$$(R_LIBS) && \
+										  	  $$(RSCRIPT) modules/test/copy_number/qdnaseq.R --sample $$(*) --binsize $$(QDNASEQ_BINSIZE)")
 										  
 endef
  $(foreach sample,$(SAMPLES),\
