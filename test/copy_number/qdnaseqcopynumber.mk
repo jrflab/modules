38b4359ebea50f05b21a381e121f8e3fec152ff8
David Brown <ndbrown6@gmail.com>
2018-12-27 12:15:19 -0500

    QDNAseq

--- test/copy_number/qdnaseqcopynumber.mk
+++ test/copy_number/qdnaseqcopynumber.mk
@@ -2,7 +2,7 @@ include modules/Makefile.inc
 include modules/genome_inc/b37.inc
 
 LOGDIR ?= log/qdnaseq_copynumber.$(NOW)
-PHONY += qdnaseq qdnaseq/copynumber qdnaseq/copynumber/log2ratio qdnaseq/copynumber/segmented
+PHONY += qdnaseq qdnaseq/copynumber qdnaseq/copynumber/log2ratio qdnaseq/copynumber/segmented qdnaseq/copynumber/pcf
 
 qdnaseq : $(foreach sample,$(SAMPLES),qdnaseq/copynumber/log2ratio/$(sample).pdf qdnaseq/copynumber/segmented/$(sample).RData)
 
@@ -14,8 +14,9 @@ endef
 		$(eval $(call qdnaseq-plot-log2ratio,$(sample))))
 		
 define qdnaseq-segment-log2ratio
-qdnaseq/copynumber/segmented/%.RData : qdnaseq/bed/%.bed
-	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 10G -m 12G,"$(RSCRIPT) modules/test/copy_number/qdnaseqsegment.R --sample $$(*)")
+qdnaseq/copynumber/segmented/%.RData qdnaseq/copynumber/pcf/%.pdf : qdnaseq/bed/%.bed
+	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 10G -m 12G,"$(RSCRIPT) modules/test/copy_number/qdnaseqsegment.R --sample $$(*) && \
+																	 $(RSCRIPT) modules/test/copy_number/qdnaseqplot.R --sample $$(*) --type 'segmented'")
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call qdnaseq-segment-log2ratio,$(sample))))
--- test/copy_number/qdnaseqplot.R
+++ test/copy_number/qdnaseqplot.R
@@ -16,7 +16,30 @@ parser = OptionParser(usage = "%prog", option_list = args_list)
 arguments = parse_args(parser, positional_arguments = T)
 opt = arguments$options
 
+'prunesegments.cn' <- function(x, n=10)
+{
+	cnm = matrix(NA, nrow=nrow(x), ncol=nrow(x))
+	for (j in 1:nrow(x)) {
+		cnm[,j] = abs(2^x[j,"Log2Ratio"] - 2^x[,"Log2Ratio"])
+	}
+	cnt = hclust(as.dist(cnm), "average")
+	cnc = cutree(tree=cnt, k=n)
+	for (j in unique(cnc)) {
+		indx = which(cnc==j)
+		if (length(indx)>2) {
+			mcl = mean(x[indx,"Log2Ratio"])
+			scl = sd(x[indx,"Log2Ratio"])
+			ind = which(x[indx,"Log2Ratio"]<(mcl+1.96*scl) & x[indx,"Log2Ratio"]>(mcl-1.96*scl))
+			x[indx[ind],"Log2Ratio"] = mean(x[indx[ind],"Log2Ratio"])
+		} else {
+			x[indx,"Log2Ratio"] = mean(x[indx,"Log2Ratio"])
+		}
+	}
+	return(invisible(x))
+}
+
 if (opt$type=="raw") {
+
 	infile = paste0("qdnaseq/bed/", opt$sample, ".bed")
 	outfile = paste0("qdnaseq/copynumber/log2ratio/", opt$sample, ".pdf")
 	data = read.table(file=infile, header=FALSE, sep="\t", skip=1, stringsAsFactors=FALSE)[,c(1,2,3,5),drop=FALSE]
@@ -47,7 +70,46 @@ if (opt$type=="raw") {
 	axis(1, at = .5*(start+end), labels=labels, cex.axis = 1.15, las = 1)
 	box(lwd=2.5)
 	dev.off()
-} else if (opt$type=="segmented") {
 	
+} else if (opt$type=="segmented") {
+
+	infile = paste0("qdnaseq/copynumber/segmented/", opt$sample, ".RData")
+	outfile = paste0("qdnaseq/copynumber/pcf/", opt$sample, ".pdf")
+	load(infile)
+	
+	segmented = prunesegments.cn(x=segmented, n=7)
+	lo2ratio = winsorize(data=data[,c("Chromosome","Start","Log2Ratio")], tau=2.5, k=15, verbose=FALSE)
+	end = NULL
+	for (j in 1:22) {
+		end = c(end, max(CytoBand$End[CytoBand$Chromosome==j]))
+	}
+	end = cumsum(end)
+	start = rep(0, 22)
+	start[2:22] = end[1:21]+1
+	for (j in 1:22) {
+		segmented[tmp[,"Chromosome"]==j,"Start"] = segmented[segmented[,"Chromosome"]==j,"Start"] + start[j]
+		segmented[segmented[,"Chromosome"]==j,"End"] = segmented[segmented[,"Chromosome"]==j,"End"] + start[j]
+		data[data[,"Chromosome"]==j,"Start"] = data[data[,"Chromosome"]==j,"Start"] + start[j]
+	}
+	col = "grey80"
+	pdf(file=outfile, width=18, height=7)
+	par(mar=c(5, 5, 4, 2)+.1)
+	plot(data[,"Start"], data[,"Log2Ratio"], type="p", pch=".", cex=2, col=col, axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-2,2))
+ 	for (j in 1:nrow(segmented)) {
+ 		lines(x=c(segmented[j,"Start"], segmented[j,"End"]), y=rep(segmented[j,"Log2Ratio"],2), lty=1, lwd=2.75, col="red")
+ 	}
+ 	axis(2, at = NULL, cex.axis = 1.15, las = 1)
+	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
+	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
+	abline(v=1, col="goldenrod3")
+	abline(h=0, col="red")
+	for (j in 2:22) {
+		v = start[j]
+		abline(v=v, col="goldenrod3")
+	}
+	abline(v=max(data[,"Start"]), col="goldenrod3")
+	axis(1, at = .5*(start+end), labels=c(1:22), cex.axis = 0.85, las = 1)
+	box(lwd=2.5)
+	dev.off()
 
 }
