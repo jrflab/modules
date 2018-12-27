69a4481d73e8eb6888f30e05d6540f895973f6cf
David Brown <ndbrown6@gmail.com>
2018-12-27 11:30:16 -0500

    Update qdnaseqplot.R

--- test/copy_number/qdnaseqplot.R
+++ test/copy_number/qdnaseqplot.R
@@ -24,7 +24,7 @@ col = rep("#9F6986", nrow(data))
 col[(data[,"Chromosome"]%%2)==1] = "#CECAC5"
 pdf(file=outfile, width=18, height=7)
 par(mar=c(5, 5, 4, 2)+.1)
-plot(data[,"Log2Ratio"], type="p", pch=".", cex=1, col=col, axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
+plot(data[,"Log2Ratio"], type="p", pch=".", cex=1, col=col, axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-2,2))
 axis(2, at = NULL, cex.axis = 1.15, las = 1)
 mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
 mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
@@ -43,6 +43,6 @@ for (j in 1:max(data[,"Chromosome"])) {
 }
 labels = 1:max(data[,"Chromosome"])
 labels[labels==23] = "X"
-axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
+axis(1, at = .5*(start+end), labels=labels, cex.axis = 1.15, las = 1)
 box(lwd=2.5)
 dev.off()
