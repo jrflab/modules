bae715192f8825bc4168d8ca9a48b52ad74ec3ed
David Brown <ndbrown6@gmail.com>
2018-12-27 12:21:18 -0500

    Update qdnaseqcopynumber.mk

--- test/copy_number/qdnaseqcopynumber.mk
+++ test/copy_number/qdnaseqcopynumber.mk
@@ -15,11 +15,14 @@ endef
 		
 define qdnaseq-segment-log2ratio
 qdnaseq/copynumber/segmented/%.RData qdnaseq/copynumber/pcf/%.pdf : qdnaseq/bed/%.bed
-	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 10G -m 12G,"$(RSCRIPT) modules/test/copy_number/qdnaseqsegment.R --sample $$(*) && \
-																	 $(RSCRIPT) modules/test/copy_number/qdnaseqplot.R --sample $$(*) --type 'segmented'")
+	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 10G -m 12G,"$(RSCRIPT) modules/test/copy_number/qdnaseqsegment.R --sample $$(*)")
+	
+qdnaseq/copynumber/pcf/%.pdf : qdnaseq/copynumber/segmented/%.RData
+	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 10G -m 12G,"$(RSCRIPT) modules/test/copy_number/qdnaseqplot.R --sample $$(*) --type 'segmented'")
+
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call qdnaseq-segment-log2ratio,$(sample))))
+		
 
-				
 .PHONY: $(PHONY)
