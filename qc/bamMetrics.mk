7f9cd03421dd6e4ce76048c0a112b35bb6d76392
David Brown <ndbrown6@gmail.com>
2018-12-27 19:20:11 -0500

    Update bamMetrics.mk

--- qc/bamMetrics.mk
+++ qc/bamMetrics.mk
@@ -1,22 +1,15 @@
-## defaults
-LOGDIR = metrics/log
-
-## includes
 include modules/Makefile.inc
 include modules/variant_callers/gatk.inc
 
+LOGDIR ?= log/bam_metrics.$(NOW)
+PHONY += metrics
 
-.DELETE_ON_ERROR:
-
-.SECONDARY: 
-
-COLLECT_METRICS = $(JAVA) -Xmx8G -jar $(PICARD_DIR)/CollectMultipleMetrics.jar VALIDATION_STRINGENCY=LENIENT
-COLLECT_WGS_METRICS = $(JAVA) -Xmx8G -jar $(PICARD_JAR) CollectWgsMetrics VALIDATION_STRINGENCY=LENIENT
-COLLECT_GC_METRICS = $(JAVA) -Xmx8G -jar $(PICARD_DIR)/CollectGcBiasMetrics.jar VALIDATION_STRINGENCY=LENIENT
+COLLECT_METRICS = $(JAVA) -Xmx12G -jar $(PICARD_DIR)/CollectMultipleMetrics.jar VALIDATION_STRINGENCY=LENIENT
+COLLECT_WGS_METRICS = $(JAVA) -Xmx12G -jar $(PICARD_JAR) CollectWgsMetrics VALIDATION_STRINGENCY=LENIENT
+COLLECT_GC_METRICS = $(JAVA) -Xmx12G -jar $(PICARD_DIR)/CollectGcBiasMetrics.jar VALIDATION_STRINGENCY=LENIENT
 
 SUMMARIZE_IDXSTATS = python modules/qc/summarize_idxstats.py
 
-PHONY += bam_metrics
 bam_metrics : summary_metrics gc flagstats wgs_metrics
 
 PHONY += flagstats
@@ -29,38 +22,29 @@ PHONY += dup
 dup : $(foreach sample,$(SAMPLES),metrics/$(sample).dup_metrics)
 PHONY += gc
 gc : $(foreach sample,$(SAMPLES),metrics/$(sample).gc_bias_metrics)
-PHONY += idxstats
-idxstats : metrics/idxstats_summary.tsv $(foreach sample,$(SAMPLES),metrics/$(sample).idxstats)
 
 metrics/%.alignment_summary_metrics : bam/%.bam
-	$(call RUN,-s 12G -m 13G,"$(COLLECT_METRICS) I=$< O=metrics/$* REFERENCE_SEQUENCE=$(REF_FASTA)")
+	$(call RUN,-s 18G -m 24G -w 7200,"$(COLLECT_METRICS) I=$< O=metrics/$* REFERENCE_SEQUENCE=$(REF_FASTA)")
 
 metrics/wgs_metrics_summary.tsv : $(foreach sample,$(SAMPLES),metrics/$(sample).wgs_metrics)
 	$(INIT) (grep GENOME_TERRITORY $< | sed 's/^/SAMPLE\t/'; for x in $(SAMPLES); do grep -A1 GENOME_TERRITORY metrics/$$x.wgs_metrics | sed 1d | sed "s/^/$$x\t/" ; done) > $@
 
 metrics/%.wgs_metrics : bam/%.bam
-	$(call RUN,-s 12G -m 13G,"$(COLLECT_WGS_METRICS) I=$< O=$@ REFERENCE_SEQUENCE=$(REF_FASTA)")
+	$(call RUN,-s 18G -m 24G -w 7200,"$(COLLECT_WGS_METRICS) I=$< O=$@ REFERENCE_SEQUENCE=$(REF_FASTA)")
 
 metrics/%.gc_bias_metrics : bam/%.bam
-	$(call RUN,-s 12G -m 13G,"$(COLLECT_GC_METRICS) I=$< O=$@ CHART_OUTPUT=$(addsuffix .pdf,$@) REFERENCE_SEQUENCE=$(REF_FASTA)")
+	$(call RUN,-s 18G -m 24G -w 7200,"$(COLLECT_GC_METRICS) I=$< O=$@ CHART_OUTPUT=$(addsuffix .pdf,$@) REFERENCE_SEQUENCE=$(REF_FASTA)")
 
 metrics/%.flagstats : bam/%.bam
-	$(call RUN,-s 2G -m 3G,"$(SAMTOOLS) flagstat $< > $@")
+	$(call RUN,-s 18G -m 24G -w 7200,"$(SAMTOOLS) flagstat $< > $@")
 	
 bam/%.markdup.bam metrics/%.dup_metrics : bam/%.bam
-	$(call RUN,-s 18G -m 19G,"$(MARK_DUP) I=$< O=bam/$*.markdup.bam METRICS_FILE=metrics/$*.dup_metrics")
+	$(call RUN,-s 18G -m 24G -w 7200,"$(MARK_DUP) I=$< O=bam/$*.markdup.bam METRICS_FILE=metrics/$*.dup_metrics")
 
 metrics/dup_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample).dup_metrics.txt)
 	$(INIT) grep '^LIBRARY' $< > $@ && \
 	for metrics in $^; do \
 	    grep -A1 '^LIBRARY' $$metrics | sed '1d' >> $@; \
 	done
-
-metrics/%.idxstats : bam/%.bam bam/%.bam.bai
-	$(call RUN,,"samtools idxstats $< > $@")
-
-metrics/idxstats_summary.tsv : $(foreach sample,$(SAMPLES),metrics/$(sample).idxstats)
-	$(INIT) $(SUMMARIZE_IDXSTATS) --chromosomes $(CHROMOSOMES) --excel_file $(@:.tsv=.xlsx) --project_name $(PROJECT_NAME)  $^ > $@ 2> $(LOG)
-
 
 .PHONY: $(PHONY)
