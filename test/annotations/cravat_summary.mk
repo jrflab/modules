2e90e4af884a6a548ca62a4a9ee806589c0fd7e9
David Brown <ndbrown6@gmail.com>
2019-01-06 16:29:29 -0500

    Update cravat_summary.mk

--- test/annotations/cravat_summary.mk
+++ test/annotations/cravat_summary.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/cravat_summary.$(NOW)
 PHONY += cravat summary summary/tsv
 
-cravat : summary/tsv/cravat_summary.tsv  summary/tsv/cravat_summary.xlsx
+cravat : summary/tsv/cravat_summary.tsv  summary/cravat_summary.xlsx
 
 summary/tsv/cravat_summary.tsv : $(wildcard cravat/$(SAMPLES).txt)
 	$(call RUN,-c -s 24G -m 48G -w 7200,"$(RSCRIPT) modules/test/annotations/cravat_summary.R --sample_names '$(SAMPLES)'")
