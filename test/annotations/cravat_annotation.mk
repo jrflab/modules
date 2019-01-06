e73bc8cd1ea271300398fdfef7646e2747781a26
David Brown <ndbrown6@gmail.com>
2019-01-05 23:03:32 -0500

    Update cravat_annotation.mk

--- test/annotations/cravat_annotation.mk
+++ test/annotations/cravat_annotation.mk
@@ -19,8 +19,8 @@ cravat/%.cravat.vcf : cravat/%.vcf cravat/%.maf
 	$$(call RUN,-c -s 9G -m 12G -v $$(DEFAULT_ENV),"$(RSCRIPT) modules/test/annotations/filter_vcf.R --sample_name $$(*)")
 
 cravat/%.tsv: cravat/%.cravat.vcf
-	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"source activate $$(CRAVAT_ENV) && \
-												   cravat $$(*).cravat.vcf -n $$(*) -a clinvar cosmic dbsnp gnomad hgvs -v -l hg19 -t text")
+	$$(call RUN,-c -s 9G -m 12G -v $$(DEFAULT_ENV),"source activate $$(CRAVAT_ENV) && \
+												   cravat cravat/$$(*).cravat.vcf -n $$(*) -d cravat -a clinvar cosmic dbsnp gnomad hgvs -v -l hg19 -t text")
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call cravat-annotation,$(sample))))
