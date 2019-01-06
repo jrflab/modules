d27c190ddf7b03a11350765cea8f46ebb9b63486
David Brown <ndbrown6@gmail.com>
2019-01-05 19:09:01 -0500

    Update cravat_annotation.mk

--- test/annotations/cravat_annotation.mk
+++ test/annotations/cravat_annotation.mk
@@ -10,7 +10,7 @@ CRAVAT_ENV = $(HOME)/share/usr/anaconda-envs/open-cravat
 
 define cravat-annotation
 cravat/%.vcf : vcf_ann/%.gatk_snps.vcf vcf_ann/%.gatk_indels.vcf
-	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"$(RSCRIPT) modules/test/annotation/combine_vcf.R --sample_name $(<)")
+	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"$(RSCRIPT) modules/test/annotation/combine_vcf.R --sample_name $$(<)")
 
 #cravat/%.xlsx : cravat/%.vcf
 #	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"source activate $$(CRAVAT_ENV) && \
