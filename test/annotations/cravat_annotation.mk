d4545d1bfd1e5a82557b0a3921509e4e802b06e7
David Brown <ndbrown6@gmail.com>
2019-01-05 21:38:11 -0500

    Update cravat_annotation.mk

--- test/annotations/cravat_annotation.mk
+++ test/annotations/cravat_annotation.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/cravat.$(NOW)
 PHONY += cravat
 
-cravat : $(foreach sample,$(SAMPLES),cravat/$(sample).vcf)
+cravat : $(foreach sample,$(SAMPLES),cravat/$(sample).maf)
 
 DEFAULT_ENV = $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.6
 CRAVAT_ENV = $(HOME)/share/usr/anaconda-envs/open-cravat
@@ -11,6 +11,9 @@ CRAVAT_ENV = $(HOME)/share/usr/anaconda-envs/open-cravat
 define cravat-annotation
 cravat/%.vcf : vcf_ann/%.gatk_snps.vcf vcf_ann/%.gatk_indels.vcf
 	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"$(RSCRIPT) modules/test/annotations/combine_vcf.R --sample_name $$(*)")
+	
+cravat/%.maf : cravat/%.vcf
+	$$(call RUN,-s 9G -m 12G,"$$(VCF2MAF) --input-vcf $$< --tumor-id $$(*) $$(if $$(EXAC_NONTCGA),--filter-vcf $$(EXAC_NONTCGA)) --ref-fasta $$(REF_FASTA) --vep-path $$(VEP_PATH) --vep-data $$(VEP_DATA) --tmp-dir `mktemp -d` --output-maf $$@")
 
 #cravat/%.xlsx : cravat/%.vcf
 #	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"source activate $$(CRAVAT_ENV) && \
