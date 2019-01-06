b33f21e36873324ed0e2385b8aa1453035ce9488
David Brown <ndbrown6@gmail.com>
2019-01-06 16:15:19 -0500

    Update cravat_summary.mk

--- test/annotations/cravat_summary.mk
+++ test/annotations/cravat_summary.mk
@@ -6,7 +6,7 @@ PHONY += cravat summary summary/tsv
 cravat : summary/tsv/cravat_summary.tsv
 
 summary/tsv/cravat_summary.tsv : $(wildcard cravat/$(SAMPLES).txt)
-	$$(call RUN,-c -s 24G -m 48G -w 7200,"$(RSCRIPT) modules/test/annotations/cravat_summary.R --sample_names '$(SAMPLES)'")
+	$(call RUN,-c -s 24G -m 48G -w 7200,"$(RSCRIPT) modules/test/annotations/cravat_summary.R --sample_names '$(SAMPLES)'")
 	
 
 .DELETE_ON_ERROR:
