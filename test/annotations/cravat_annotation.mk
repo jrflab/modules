8dfc6ad1c85d9f720e6c602330baeb23cae4f4df
David Brown <ndbrown6@gmail.com>
2019-01-05 19:05:11 -0500

    added carvat annotation script

---
+++ test/annotations/cravat_annotation.mk
@@ -0,0 +1,23 @@
+include modules/Makefile.inc
+
+LOGDIR ?= log/cravat.$(NOW)
+PHONY += cravat
+
+cravat : $(foreach sample,$(SAMPLES),cravat/$(sample).vcf)
+
+DEFAULT_ENV = $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.6
+CRAVAT_ENV = $(HOME)/share/usr/anaconda-envs/open-cravat
+
+define cravat-annotation
+cravat/%.vcf : vcf_ann/%.gatk_snps.vcf vcf_ann/%.gatk_indels.vcf
+	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"$(RSCRIPT) modules/test/annotation/combine_vcf.R --sample_name $(<)")
+
+#cravat/%.xlsx : cravat/%.vcf
+#	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"source activate $$(CRAVAT_ENV) && \
+																 ")
+endef
+ $(foreach sample,$(SAMPLES),\
+		$(eval $(call cravat-annotation,$(sample))))
+		
+.PHONY: $(PHONY)
+
