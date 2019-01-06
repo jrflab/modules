46a51a76fc296a83c212be2ca50d11fcd62afe53
David Brown <ndbrown6@gmail.com>
2019-01-05 22:58:10 -0500

    Update cravat_annotation.mk

--- test/annotations/cravat_annotation.mk
+++ test/annotations/cravat_annotation.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/cravat.$(NOW)
 PHONY += cravat
 
-cravat : $(foreach sample,$(SAMPLES),cravat/$(sample).vcf cravat/$(sample).maf cravat/$(sample).cravat.vcf)
+cravat : $(foreach sample,$(SAMPLES),cravat/$(sample).vcf cravat/$(sample).maf cravat/$(sample).cravat.vcf cravat/$(sample).tsv)
 
 DEFAULT_ENV = $(HOME)/share/usr/anaconda-envs/jrflab-modules-0.1.6
 CRAVAT_ENV = $(HOME)/share/usr/anaconda-envs/open-cravat
@@ -18,9 +18,9 @@ cravat/%.maf : cravat/%.vcf
 cravat/%.cravat.vcf : cravat/%.vcf cravat/%.maf
 	$$(call RUN,-c -s 9G -m 12G -v $$(DEFAULT_ENV),"$(RSCRIPT) modules/test/annotations/filter_vcf.R --sample_name $$(*)")
 
-#cravat/%.xlsx : cravat/%.vcf
-#	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"source activate $$(CRAVAT_ENV) && \
-#																 ")
+cravat/%.tsv: cravat/%.cravat.vcf
+	$$(call RUN,-c -s 6G -m 8G -v $$(DEFAULT_ENV),"source activate $$(CRAVAT_ENV) && \
+												   cravat $$(*).cravat.vcf -n $$(*) -a clinvar cosmic dbsnp gnomad hgvs -v -l hg19 -t text")
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call cravat-annotation,$(sample))))
