64d4f2a825ae51d72d5ae3e6a16ad2feb4f51224
David Brown <ndbrown6@gmail.com>
2019-01-17 22:50:09 -0500

    sufam

--- variant_callers/updatesamples.R
+++ variant_callers/updatesamples.R
@@ -44,42 +44,46 @@ for (i in 1:length(sample_names)) {
  		system(paste0("source ~/share/usr/anaconda/bin/activate ~/share/usr/anaconda-envs/sufam-dev && sufam ~/share/reference/GATK_bundle/2.3/human_g1k_v37.fa sufam/", opt$sample_set, ".vcf bam/", sample_names[i], ".bam > sufam/", sample_names[i], ".mat"))
  	}
  	tmp = read.csv(file=paste0("sufam/", sample_names[i], ".mat"), header=TRUE, sep="\t", stringsAsFactors=FALSE)
- 	## fix depth
- 	index = grep("DP", colnames(vars))
- 	vars[,index[i]] = tmp[,"cov"]
- 	## fix maf
- 	index = grep("MAF", colnames(vars))
- 	vars[,index[i]] = tmp[,"val_maf"]
+ 	index = paste0("DP_", sample_names[i])
+ 	vars[,index] = tmp[,"cov"]
+ 	index = paste0("MAF_", sample_names[i])
+ 	vars[,index] = tmp[,"val_maf"]
 }
  
 #====================================
 # qt and q2
 #====================================
-q_t = rep(2, nrow(vars))
-q_2 = rep(1, nrow(vars))
-for (i in 2:length(sample_names)) {
-	load(paste0("ascat/ascat/", sample_names[i], "_", sample_names[1], ".RData"))
-	Chromosomes = tmp2$SNPpos[tmp3$seg[,"start"],1]
-	Chromosomes[Chromosomes==23] = "X"
-	Start = tmp2$SNPpos[tmp3$seg[,"start"],2]
-	End = tmp2$SNPpos[tmp3$seg[,"end"],2]
-	qt = tmp3$seg[,"nA"] + tmp3$seg[,"nB"]
-	q2 = apply(tmp3$seg[,c("nA","nB")], 1, max)
-	index = rep(NA, nrow(vars))
-	for (j in 1:nrow(vars)) {
-		indx = which(Chromosomes==vars[j,"Chromosome"] & Start<=vars[j,"Position"] & End>=vars[j,"Position"])
-		if (length(indx)!=0) {
-			index[j] = indx
-		} else {
-			index[j] = NA
+q_t = q_2 = NULL
+for (i in 1:length(sample_names)) {
+	file_names = dir(path="ascat/ascat", pattern=".RData", full.names=TRUE)
+	index = grep(sample_names[i], file_names, fixed=TRUE)
+	if (length(index)==1) {
+		load(file_names[index])
+		Chromosomes = tmp2$SNPpos[tmp3$seg[,"start"],1]
+		Chromosomes[Chromosomes==23] = "X"
+		Start = tmp2$SNPpos[tmp3$seg[,"start"],2]
+		End = tmp2$SNPpos[tmp3$seg[,"end"],2]
+		qt = tmp3$seg[,"nA"] + tmp3$seg[,"nB"]
+		q2 = apply(tmp3$seg[,c("nA","nB")], 1, max)
+		index = rep(NA, nrow(vars))
+		for (j in 1:nrow(vars)) {
+			indx = which(Chromosomes==vars[j,"Chromosome"] & Start<=vars[j,"Position"] & End>=vars[j,"Position"])
+			if (length(indx)!=0) {
+				index[j] = indx
+			} else {
+				index[j] = NA
+			}
 		}
+		q_t = cbind(q_t, qt[index])
+		q_2 = cbind(q_2, q2[index])
+	} else {
+		q_t = cbind(q_t, rep(2, nrow(vars)))
+		q_2 = cbind(q_2, rep(1, nrow(vars)))
 	}
-	q_t = cbind(q_t, qt[index])
-	q_2 = cbind(q_2, q2[index])
 }
 q_t[is.na(q_t)] = 2
 q_2[is.na(q_2)] = 1
-colnames(q_t) = colnames(q_2) = c("N", sample_names[2:length(sample_names)])
+colnames(q_t) = colnames(q_2) = sample_names
 colnames(q_t) = paste0("qt_", colnames(q_t))
 colnames(q_2) = paste0("q2_", colnames(q_2))
 vars = cbind(vars, q_t, q_2)
@@ -107,52 +111,66 @@ pr_somatic_clonal = NULL
 ll = NULL
 sq = NULL
 clonal_status = NULL
-for (i in 2:length(sample_names)) {
-	load(paste0("ascat/ascat/", sample_names[i], "_", sample_names[1], ".RData"))
-	f_hat = vars[,paste0("MAF_", sample_names[i])]
-	n = vars[,paste0("DP_", sample_names[i])]
-	qt = vars[,paste0("qt_", sample_names[i])]
-	qt[qt>10] = 10
-	q2 = vars[,paste0("q2_", sample_names[i])]
-	q2[q2>10] = 10
-	alpha = seq(.1, to=.9, length=50)
-	alpha_hat = list()
-	indx = f_hat>.2
-	if (sum(indx)==0) {
+for (i in 1:length(sample_names)) {
+	file_names = dir(path="ascat/ascat", pattern=".RData", full.names=TRUE)
+	index = grep(sample_names[i], file_names, fixed=TRUE)
+	if (length(index)==1) {
+		load(file_names[index])
+		f_hat = vars[,paste0("MAF_", sample_names[i])]
+		n = vars[,paste0("DP_", sample_names[i])]
+		qt = vars[,paste0("qt_", sample_names[i])]
+		qt[qt>10] = 10
+		q2 = vars[,paste0("q2_", sample_names[i])]
+		q2[q2>10] = 10
+		alpha = seq(.1, to=.9, length=50)
+		alpha_hat = list()
 		indx = f_hat>.1
+		if (sum(indx)>5) {
+			for (j in 1:length(alpha)) {
+				alpha_hat[[j]] = cancercellFraction(f_hat[indx], n[indx], qt[indx], q2[indx], alpha[j], e=0.01)
+			}
+			LL = unlist(lapply(alpha_hat, function(x) {sum(x[,"LL"])}))
+			pdf(file=paste0("sufam/", sample_names[i], ".pdf"))
+			plot(alpha, LL, type="o", col="steelblue", axes=FALSE, frame.plot=FALSE, xlab="", ylab="")
+			axis(1, at = NULL, cex.axis = 1.5, padj = 0.25)
+    		axis(2, at = NULL, cex.axis = 1.5, las = 1)
+    		mtext(side = 1, text = expression(alpha), line = 4, cex = 1.5)
+    		mtext(side = 2, text = expression(Sigma~"LL"), line = 4, cex = 1.5)
+    		index = which.max(LL)
+    		title(main = paste0("alpha* = ", signif(alpha[index], 3)), cex.main = 1.5)
+    		box(lwd = 2)
+			dev.off()
+			index = which.max(LL)
+			alpha_hat = cancercellFraction(f_hat, n, qt, q2, ifelse((alpha[index]-.25)<=0, alpha[index], alpha[index]-.25), e=0.01)
+			cancer_cell_fraction = cbind(cancer_cell_fraction, alpha_hat[,"cancer_cell_frac"])
+			ccf_95CI_low = cbind(ccf_95CI_low, alpha_hat[,"ccf_95CI_low"])
+			ccf_95CI_high = cbind(ccf_95CI_high, alpha_hat[,"ccf_95CI_high"])
+			pr_somatic_clonal = cbind(pr_somatic_clonal, alpha_hat[,"Pr_somatic_clonal"])
+			ll = cbind(ll, alpha_hat[,"LL"])
+			sq = cbind(sq, alpha_hat[,"sq"])
+			clonal_estimate = rep("Subclonal", nrow(vars))
+			clonal_estimate[cancer_cell_fraction[,i]>.75 | pr_somatic_clonal[,i]>.5 | ccf_95CI_low[,i]>.9] = "Clonal"
+			clonal_status = cbind(clonal_status, clonal_estimate)
+		} else {
+			cancer_cell_fraction = cbind(cancer_cell_fraction, rep(NA, nrow(vars)))
+			ccf_95CI_low = cbind(ccf_95CI_low, rep(NA, nrow(vars)))
+			ccf_95CI_high = cbind(ccf_95CI_high, rep(NA, nrow(vars)))
+			pr_somatic_clonal = cbind(pr_somatic_clonal, rep(NA, nrow(vars)))
+			ll = cbind(ll, rep(NA, nrow(vars)))
+			sq = cbind(sq, rep(NA, nrow(vars)))
+			clonal_status = cbind(clonal_status, rep(NA, nrow(vars)))
+		}
+	} else {
+		cancer_cell_fraction = cbind(cancer_cell_fraction, rep(NA, nrow(vars)))
+		ccf_95CI_low = cbind(ccf_95CI_low, rep(NA, nrow(vars)))
+		ccf_95CI_high = cbind(ccf_95CI_high, rep(NA, nrow(vars)))
+		pr_somatic_clonal = cbind(pr_somatic_clonal, rep(NA, nrow(vars)))
+		ll = cbind(ll, rep(NA, nrow(vars)))
+		sq = cbind(sq, rep(NA, nrow(vars)))
+		clonal_status = cbind(clonal_status, rep(NA, nrow(vars)))
 	}
-	if (sum(indx)<5) {
-		indx = f_hat>0
-	}
-	for (j in 1:length(alpha)) {
-		alpha_hat[[j]] = cancercellFraction(f_hat[indx], n[indx], qt[indx], q2[indx], alpha[j], e=0.01)
-	}
-	LL = unlist(lapply(alpha_hat, function(x) {sum(x[,"LL"])}))
-	
-	pdf(file=paste0("sufam/", sample_names[i], "_", sample_names[1], ".pdf"))
-	plot(alpha, LL, type="o", col="steelblue", axes=FALSE, frame.plot=FALSE, xlab="", ylab="")
-	axis(1, at = NULL, cex.axis = 1.5, padj = 0.25)
-    axis(2, at = NULL, cex.axis = 1.5, las = 1)
-    mtext(side = 1, text = expression(alpha), line = 4, cex = 1.5)
-    mtext(side = 2, text = expression(Sigma~"LL"), line = 4, cex = 1.5)
-    index = which.max(LL)
-    title(main = paste0("alpha* = ", signif(alpha[index], 3)), cex.main = 1.5)
-    box(lwd = 2)
-	dev.off()
-	
-	index = which.max(LL)
-	alpha_hat = cancercellFraction(f_hat, n, qt, q2, ifelse((alpha[index]-.25)<=0, alpha[index], alpha[index]-.25), e=0.01)
-	cancer_cell_fraction = cbind(cancer_cell_fraction, alpha_hat[,"cancer_cell_frac"])
-	ccf_95CI_low = cbind(ccf_95CI_low, alpha_hat[,"ccf_95CI_low"])
-	ccf_95CI_high = cbind(ccf_95CI_high, alpha_hat[,"ccf_95CI_high"])
-	pr_somatic_clonal = cbind(pr_somatic_clonal, alpha_hat[,"Pr_somatic_clonal"])
-	ll = cbind(ll, alpha_hat[,"LL"])
-	sq = cbind(sq, alpha_hat[,"sq"])
-	clonal_estimate = rep("Subclonal", nrow(vars))
-	clonal_estimate[cancer_cell_fraction>.75 | pr_somatic_clonal>.5 | ccf_95CI_low>.9] = "Clonal"
-	clonal_status = cbind(clonal_status, clonal_estimate)
 }
-colnames(cancer_cell_fraction) = colnames(ccf_95CI_low) = colnames(ccf_95CI_high) = colnames(pr_somatic_clonal) = colnames(ll) = colnames(sq) = colnames(clonal_status) = sample_names[2:length(sample_names)]
+colnames(cancer_cell_fraction) = colnames(ccf_95CI_low) = colnames(ccf_95CI_high) = colnames(pr_somatic_clonal) = colnames(ll) = colnames(sq) = colnames(clonal_status) = sample_names
 colnames(cancer_cell_fraction) = paste0("CCF_", colnames(cancer_cell_fraction))
 colnames(ccf_95CI_low) = paste0("CCF_95CI_Low_", colnames(ccf_95CI_low))
 colnames(ccf_95CI_high) = paste0("CCF_95CI_High_", colnames(ccf_95CI_high))
@@ -169,4 +187,4 @@ vars = cbind(vars, cancer_cell_fraction,
 				   sq,
 				   clonal_status)
 
-write.table(vars, file=paste0("sufam/", opt$patient, ".tsv"), col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
+write.table(vars, file=paste0("sufam/", opt$sample_set, ".tsv"), col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE)
