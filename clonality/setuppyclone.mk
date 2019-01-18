1a74732e67f53a5aa326bc72b53dc9e9dae4103a
David Brown <ndbrown6@gmail.com>
2019-01-18 00:20:53 -0500

    Update setuppyclone.mk

--- clonality/setuppyclone.mk
+++ clonality/setuppyclone.mk
@@ -8,7 +8,7 @@ setup_pyclone : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(normal.$(pair))/$(tumor
 define make-input-pyclone
 pyclone/$2/$1.yaml : $(wildcard $(foreach set,$(SAMPLE_SETS),sufam/$(set).tsv))
 	$$(call RUN,-c -s 4G -m 6G,"if [ ! -d pyclone/$2 ]; then mkdir pyclone/$2; fi && \
-								if [ ! -d pyclone/$2/$1.tsv ]; then  $(RSCRIPT) modules/clonality/tsvforpyclone.R --sample_name $1 --normal_name $2; fi && \
+								if [ ! -f pyclone/$2/$1.tsv ]; then  $(RSCRIPT) modules/clonality/tsvforpyclone.R --sample_name $1 --normal_name $2; fi && \
 								source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
 								PyClone build_mutations_file --in_file pyclone/$2/$1.tsv --out_file pyclone/$2/$1.yaml --prior total_copy_number")
 
