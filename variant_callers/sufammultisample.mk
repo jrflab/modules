ca8061d64b4bcde4257a3171317f12660612690b
David Brown <ndbrown6@gmail.com>
2019-01-17 22:51:29 -0500

    Update sufammultisample.mk

--- variant_callers/sufammultisample.mk
+++ variant_callers/sufammultisample.mk
@@ -12,16 +12,16 @@ define combine-samples
 sufam/%.txt : summary/tsv/mutation_summary.tsv
 	$$(call RUN,-c -s 4G -m 6G,"$(RSCRIPT) modules/variant_callers/combinesamples.R --sample_set $$*")
 
-#sufam/%.tsv : sufam/%.txt
-#	$$(call RUN,-c -s 4G -m 6G,"$(RSCRIPT) modules/variant_callers/updatesamples.R --sample_set $$*")
+sufam/%.tsv : sufam/%.txt
+	$$(call RUN,-c -s 4G -m 6G,"$(RSCRIPT) modules/variant_callers/updatesamples.R --sample_set $$*")
 	
 endef
 $(foreach set,$(SAMPLE_SETS),\
 		$(eval $(call combine-samples,$(set))))
 
-#summary/sufam_summary.xlsx : $(wildcard $(foreach set,$(SAMPLE_SETS),sufam/$(set).tsv))
-#	$(call RUN,-c -s 12G -m 16G,"export R_LIBS='/lila/data/reis-filho/usr/anaconda-envs/jrflab-modules-0.1.4/lib/R/library:/lila/data/reis-filho/usr/lib64/R/library' &&\
-#								 $(RSCRIPT) modules/summary/sufamsummary.R --in_file '$(SUFAM_SUMMARY)'")
+summary/sufam_summary.xlsx : $(wildcard $(foreach set,$(SAMPLE_SETS),sufam/$(set).tsv))
+	$(call RUN,-c -s 12G -m 16G,"export R_LIBS='/lila/data/reis-filho/usr/anaconda-envs/jrflab-modules-0.1.4/lib/R/library:/lila/data/reis-filho/usr/lib64/R/library' &&\
+								 $(RSCRIPT) modules/summary/sufamsummary.R --in_file '$(SUFAM_SUMMARY)'")
 
 .DELETE_ON_ERROR:
 .SECONDARY:
