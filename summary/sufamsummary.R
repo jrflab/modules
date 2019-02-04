c949bf869e9885ccc8b578e9b510bfce87db1461
David Brown <ndbrown6@gmail.com>
2019-02-04 16:26:30 -0500

    Update sufamsummary.R

--- summary/sufamsummary.R
+++ summary/sufamsummary.R
@@ -8,16 +8,15 @@ if (!interactive()) {
     options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
 }
 
-args_list <- list(make_option("--in_file", default = NA, type = 'character', help = "input file name"))
+args_list <- list(make_option("--sample_sets", default = NA, type = 'character', help = "sample sets file names"))
 parser <- OptionParser(usage = "%prog", option_list = args_list)
 arguments <- parse_args(parser, positional_arguments = T)
 opt <- arguments$options
 
-file_names = unlist(strsplit(x=opt$in_file, split=" ", fixed=TRUE))
-sample_names = gsub(".tsv", "", x=gsub("sufam/", "", file_names, fixed=TRUE), fixed=TRUE)
+sample_names = unlist(strsplit(x=opt$sample_sets, split=" ", fixed=TRUE))
 list_of_dfs = list()
-for (i in 1:length(file_names)) {
-	sample_vars = read_tsv(file=file_names[i])
+for (i in 1:length(sample_names)) {
+	sample_vars = read_tsv(file=paste0("sufam/", sample_names[i], ".tsv"))
 	col_names = colnames(sample_vars)
 	sample_vars = as.data.frame(sample_vars)
 	sample_vars[sample_vars=="" | sample_vars==" " | is.na(sample_vars)] = "NA"
