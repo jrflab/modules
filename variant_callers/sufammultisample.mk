ab81c2421db7ae61296817c047e3becfc72eae5d
David Brown <ndbrown6@gmail.com>
2019-02-04 16:14:02 -0500

    Update sufammultisample.mk

--- variant_callers/sufammultisample.mk
+++ variant_callers/sufammultisample.mk
@@ -37,7 +37,7 @@ endif
 
 summary/sufam_summary.xlsx : $(wildcard $(foreach set,$(SAMPLE_SETS),sufam/$(set).tsv))
 	$(call RUN,-c -s 12G -m 16G,"export R_LIBS='/lila/data/reis-filho/usr/anaconda-envs/jrflab-modules-0.1.4/lib/R/library:/lila/data/reis-filho/usr/lib64/R/library' &&\
-								 $(RSCRIPT) modules/summary/sufamsummary.R --in_file '$$(SUFAM_SUMMARY)'")
+								 $(RSCRIPT) modules/summary/sufamsummary.R --in_file $$(SUFAM_SUMMARY)")
 
 .DELETE_ON_ERROR:
 .SECONDARY:
