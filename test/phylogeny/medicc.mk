99c6e9340efd1df0b92ff6b91ded4e2abec8b259
David Brown <ndbrown6@gmail.com>
2019-02-05 22:26:12 -0500

    Update medicc.mk

--- test/phylogeny/medicc.mk
+++ test/phylogeny/medicc.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/medicc.$(NOW)
 PHONY += medicc medicc/mad medicc/mpcf medicc/ascat
 
-medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData)
+medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach pairs,$(SAMPLE_PAIRS),medicc/ascat/$(pairs).pdf)
 
 define combine-samples
 medicc/mad/%.RData : $(wildcard $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).Rdata))
