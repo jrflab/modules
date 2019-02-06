6250d4c1d7c4535af1b9db06b301917827119bdb
David Brown <ndbrown6@gmail.com>
2019-02-05 23:23:16 -0500

    medicc

---
+++ test/phylogeny/initmedicc.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library("optparse"))
+
+if (!interactive()) {
+    options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
+}
+
+args_list <- list(make_option("--sample_set", default = NA, type = 'character', help = "sample names set"))
+				  
+parser <- OptionParser(usage = "%prog", option_list = args_list)
+arguments <- parse_args(parser, positional_arguments = T)
+opt <- arguments$options
+
+load(paste0("medicc/mpcf", opt$sample_set, ".RData"))
+q1 = qt-q2
+index = !apply(q2, 1, function(x) { any(is.na(x)) }) & !apply(q1, 1, function(x) { any(is.na(x)) })
+q2 = q2[index,,drop=FALSE]
+q1 = q1[index,,drop=FALSE]
+tmp = tmp[index,,drop=FALSE]
+q2[q2>4] = 4
+q1[q1>4] = 4
+desc = cbind(paste0("chrom", unique(tmp[,"Chromosome"])),
+			 paste0("major_chr", unique(tmp[,"Chromosome"]), ".fasta"),
+			 paste0("minor_chr", unique(tmp[,"Chromosome"]), ".fasta"))
+write.table(desc, file=paste0("medicc/medicc/", opt$sample_set, "/desc.txt"), sep=" ", col.names=FALSE, row.names=FALSE, quote=FALSE, append=FALSE)
+for (i in unique(tmp[,"Chromosome"])) {
+	cat(">diploid\n", file=paste0("medicc/medicc/", opt$sample_set, "/major_chr", i, ".fasta"), append=FALSE)
+	cat(paste0(rep(1, sum(tmp[,"Chromosome"]==i)), collapse=""), "\n", file=paste0("medicc/medicc/", opt$sample_set, "/major_chr", i, ".fasta"), append=TRUE)
+	for (j in 1:ncol(q2)) {
+		cat(paste0(">", gsub("-", "_", colnames(q2)[j]), "\n"), file=paste0("medicc/medicc/", opt$sample_set, "/major_chr", i, ".fasta"), append=TRUE)
+		cat(paste0(q2[tmp[,"Chromosome"]==i,j], collapse=""), "\n", file=paste0("medicc/medicc/", opt$sample_set, "/major_chr", i, ".fasta"), append=TRUE)
+	}
+	
+	
+	cat(">diploid\n", file=paste0("medicc/medicc/", opt$sample_set, "/minor_chr", i, ".fasta"), append=FALSE)
+	cat(paste0(rep(1, sum(tmp[,"Chromosome"]==i)), collapse=""), "\n", file=paste0("medicc/medicc/", opt$sample_set, "/minor_chr", i, ".fasta"), append=TRUE)
+	for (j in 1:ncol(q1)) {
+		cat(paste0(">", gsub("-", "_", colnames(q1)[j]), "\n"), file=paste0("medicc/medicc/", opt$sample_set, "/minor_chr", i, ".fasta"), append=TRUE)
+		cat(paste0(q1[tmp[,"Chromosome"]==i,j], collapse=""), "\n", file=paste0("medicc/medicc/", opt$sample_set, "/minor_chr", i, ".fasta"), append=TRUE)
+	}
+}
--- test/phylogeny/medicc.mk
+++ test/phylogeny/medicc.mk
@@ -1,9 +1,9 @@
 include modules/Makefile.inc
 
 LOGDIR ?= log/medicc.$(NOW)
-PHONY += medicc medicc/mad medicc/mpcf
+PHONY += medicc medicc/mad medicc/mpcf medicc/medicc
 
-medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData)
+medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/desc.txt)
 
 define combine-samples
 medicc/mad/%.RData : $(wildcard $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).Rdata))
@@ -17,11 +17,17 @@ define ascat-mpcf
 medicc/mpcf/%.RData : medicc/mad/%.RData
 	$$(call RUN,-c -s 8G -m 12G -v $(ASCAT_ENV),"if [ ! -d medicc/ascat ]; then mkdir medicc/ascat; fi && \
 												 $(RSCRIPT) modules/test/phylogeny/segmentsamples.R --sample_set $$* --normal_samples '$(NORMAL_SAMPLES)' --gamma '$${mpcf_gamma}' --nlog2 '$${mpcf_nlog2}' --nbaf '$${mpcf_nbaf}'")
-
 endef
 $(foreach set,$(SAMPLE_SETS),\
 		$(eval $(call ascat-mpcf,$(set))))
 
+define medicc-init
+medicc/medicc/%/desc.txt : medicc/mpcf/%.RData
+	$$(call RUN,-c -s 8G -m 12G -v $(ASCAT_ENV),"$(RSCRIPT) modules/test/phylogeny/initmedicc.R --sample_set $$*")
+
+endef
+$(foreach set,$(SAMPLE_SETS),\
+		$(eval $(call medicc-init,$(set))))
 
 #define
 # initial run of MEDICC
@@ -29,7 +35,7 @@ $(foreach set,$(SAMPLE_SETS),\
 #endef
 #$(foreach set,$(SAMPLE_SETS),\
 #		$(eval $(call combine-samples-pdx,$(set))))
-#
+
 #define
 # bootstrapped runs of MEDICC
 # 
@@ -41,4 +47,3 @@ $(foreach set,$(SAMPLE_SETS),\
 .DELETE_ON_ERROR:
 .SECONDARY:
 .PHONY: $(PHONY)
-
