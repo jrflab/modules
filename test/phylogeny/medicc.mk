9efe7323ab2db6db4acd9d096fbfc0effcf31335
David Brown <ndbrown6@gmail.com>
2019-02-07 13:37:05 -0500

    Update medicc.mk

--- test/phylogeny/medicc.mk
+++ test/phylogeny/medicc.mk
@@ -5,8 +5,9 @@ INDEX = $(seq -f "%03g" 1 100)
 LOGDIR ?= log/medicc.$(NOW)
 PHONY += medicc medicc/mad medicc/mpcf medicc/medicc medicc/boot
 
-medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$(INDEX)) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$INDEX/tree_final.new)
+medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$(INDEX))
 
+# $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$INDEX/tree_final.new)
 
 define combine-samples
 medicc/mad/%.RData : $(wildcard $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).Rdata))
@@ -45,9 +46,9 @@ define boot-medicc
 $(wildcard medicc/boot/%/$(INDEX)) : medicc/mpcf/%.RData
 	$$(call RUN,-c -s 8G -m 12G -v $(ASCAT_ENV),"$(RSCRIPT) modules/test/phylogeny/bootstrapmedicc.R --sample_set $$*")
 
-$(wildcard medicc/boot/%/$(INDEX)/tree_final.new) : $(wildcard medicc/boot/%/$(INDEX))
-	$$(call RUN,-c -s 2G -m 4G -n 12 -v $(MEDICC_ENV),"source $(MEDICC_VAR) && \
-												  	   seq -f '%03g' 1 100 | parallel -j 12 'if [ ! -f medicc/boot/$$*/{}/tree_final.new ]; then $(MEDICC_BIN)/medicc.py medicc/boot/$$*/{}/desc.txt medicc/boot/$$*/{}/ -v; fi'")
+# $(wildcard medicc/boot/%/$(INDEX)/tree_final.new) : $(wildcard medicc/boot/%/$(INDEX))
+#	$$(call RUN,-c -s 2G -m 4G -n 12 -v $(MEDICC_ENV),"source $(MEDICC_VAR) && \
+#												  	   seq -f '%03g' 1 100 | parallel -j 12 'if [ ! -f medicc/boot/$$*/{}/tree_final.new ]; then $(MEDICC_BIN)/medicc.py medicc/boot/$$*/{}/desc.txt medicc/boot/$$*/{}/ -v; fi'")
 endef
 $(foreach set,$(SAMPLE_SETS),\
 		$(eval $(call boot-medicc,$(set))))
