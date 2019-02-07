550b18b1b87bdf782b6bbff01a142d810f77b5fa
David Brown <ndbrown6@gmail.com>
2019-02-07 13:17:41 -0500

    Update medicc.mk

--- test/phylogeny/medicc.mk
+++ test/phylogeny/medicc.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/medicc.$(NOW)
 PHONY += medicc medicc/mad medicc/mpcf medicc/medicc medicc/boot
 
-medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$(foreach index,$(seq -f '%03g' 1 100),index)) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$(foreach index,$(seq -f '%03g' 1 100),index)/tree_final.new)
+medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$(foreach index,$(seq -f '%03g' 1 100),$index)) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$(foreach index,$(seq -f '%03g' 1 100),$index)/tree_final.new)
 
 define combine-samples
 medicc/mad/%.RData : $(wildcard $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).Rdata))
