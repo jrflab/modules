b948f29eb6ae3a6a046bf809617b0e6c63b154d7
David Brown <ndbrown6@gmail.com>
2019-02-07 13:15:56 -0500

    updates

--- test/phylogeny/medicc.mk
+++ test/phylogeny/medicc.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/medicc.$(NOW)
 PHONY += medicc medicc/mad medicc/mpcf medicc/medicc medicc/boot
 
-medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/100) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/100/tree_final.new)
+medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$(foreach index,$(seq -f '%03g' 1 100),index)) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/$(foreach index,$(seq -f '%03g' 1 100),index)/tree_final.new)
 
 define combine-samples
 medicc/mad/%.RData : $(wildcard $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).Rdata))
@@ -44,7 +44,7 @@ medicc/boot/%/100 : medicc/mpcf/%.RData
 
 medicc/boot/%/100/tree_final.new : medicc/boot/%/100
 	$$(call RUN,-c -s 2G -m 4G -n 12 -v $(MEDICC_ENV),"source $(MEDICC_VAR) && \
-												  	  seq -f '%03g' 1 100 | parallel -j 12 'if [ ! -f medicc/boot/$$*/{}/tree_final.new ]; then $(MEDICC_BIN)/medicc.py medicc/boot/$$*/{}/desc.txt medicc/boot/$$*/{}/ -v; fi'")
+												  	   seq -f '%03g' 1 100 | parallel -j 12 'if [ ! -f medicc/boot/$$*/{}/tree_final.new ]; then $(MEDICC_BIN)/medicc.py medicc/boot/$$*/{}/desc.txt medicc/boot/$$*/{}/ -v; fi'")
 endef
 $(foreach set,$(SAMPLE_SETS),\
 		$(eval $(call boot-medicc,$(set))))
@@ -53,3 +53,9 @@ $(foreach set,$(SAMPLE_SETS),\
 .DELETE_ON_ERROR:
 .SECONDARY:
 .PHONY: $(PHONY)
+
+
+
+
+
+
