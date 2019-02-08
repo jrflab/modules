48ebdf26eecc304a48fdcb769ec7d1be34bbcf44
David Brown <ndbrown6@gmail.com>
2019-02-07 21:19:09 -0500

    Update fixBam.mk

--- bam_tools/fixBam.mk
+++ bam_tools/fixBam.mk
@@ -13,7 +13,7 @@ define fix-bam
 unprocessed_bam/%.ubam : unprocessed_bam/%.bam
 	$$(call RUN,-c -n 1 -s 12G -m 18G -w 7200,"java -Djava.io.tmpdir=$(TMPDIR) -Xmx16G -jar $$(PICARD_JAR) RevertSam I=$$(<) O=unprocessed_bam/$$(*).ubam SANITIZE=true MAX_DISCARD_FRACTION=0.005 ATTRIBUTE_TO_CLEAR=XT ATTRIBUTE_TO_CLEAR=XN ATTRIBUTE_TO_CLEAR=AS ATTRIBUTE_TO_CLEAR=OC ATTRIBUTE_TO_CLEAR=OP SORT_ORDER=queryname RESTORE_ORIGINAL_QUALITIES=true REMOVE_DUPLICATE_INFORMATION=true REMOVE_ALIGNMENT_INFORMATION=true TMP_DIR=$(TMPDIR)")
 unprocessed_bam/%.fixed.bam : unprocessed_bam/%.ubam
-	$$(call RUN, -c -n 1 -s 12G -m 18G -w 7200,"java -Djava.io.tmpdir=$(TMPDIR) -Xmx16G -jar $$(PICARD_JAR) MergeBamAlignment R=$$(REF_FASTA) UNMAPPED_BAM=$$(<) ALIGNED_BAM=unprocessed_bam/$$(*).bam O=unprocessed_bam/$$(*).fixed.bam CREATE_INDEX=true ADD_MATE_CIGAR=true CLIP_ADAPTERS=true CLIP_OVERLAPPING_READS=true INCLUDE_SECONDARY_ALIGNMENTS=false MAX_INSERTIONS_OR_DELETIONS=-1 TMP_DIR=$(TMPDIR)")
+	$$(call RUN, -c -n 1 -s 12G -m 18G -w 7200,"java -Djava.io.tmpdir=$(TMPDIR) -Xmx16G -jar $$(PICARD_JAR) MergeBamAlignment R=$$(DMP_FASTA) UNMAPPED_BAM=$$(<) ALIGNED_BAM=unprocessed_bam/$$(*).bam O=unprocessed_bam/$$(*).fixed.bam CREATE_INDEX=true ADD_MATE_CIGAR=true CLIP_ADAPTERS=true CLIP_OVERLAPPING_READS=true INCLUDE_SECONDARY_ALIGNMENTS=false MAX_INSERTIONS_OR_DELETIONS=-1 TMP_DIR=$(TMPDIR)")
 unprocessed_bam/%.dedup.bam : unprocessed_bam/%.fixed.bam
 	$$(call RUN, -c -n 1 -s 12G -m 18G -w 7200,"java -Djava.io.tmpdir=$(TMPDIR) -Xmx16G -jar $$(PICARD_JAR) MarkDuplicates I=$$(<) O=unprocessed_bam/$$(*).dedup.bam M=unprocessed_bam/$$(*).txt TMP_DIR=$(TMPDIR)")
 fixed_bam/%.bam : unprocessed_bam/%.dedup.bam
