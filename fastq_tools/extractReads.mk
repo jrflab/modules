27f0d4100f1468200a09f7d6b81f54fec214f863
David Brown <ndbrown6@gmail.com>
2019-02-07 22:11:37 -0500

    Update extractReads.mk

--- fastq_tools/extractReads.mk
+++ fastq_tools/extractReads.mk
@@ -7,7 +7,7 @@ extract_unmapped : $(foreach sample,$(SAMPLES),unmapped_reads/$(sample).bam)
 
 define extract-unmapped-reads
 unmapped_reads/%.bam : bam/%.bam
-	$(call RUN,-n 4 -s 4G -m 9G,"$(SAMTOOLS2) view -f 0x04 -h -@ 4 -b $< -o unmapped_reads/$*.bam")
+	$(call RUN,-n 4 -s 4G -m 9G,"$(SAMTOOLS2) view -f 0x04 -h -@ 4 -b $$< -o unmapped_reads/$$*.bam")
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call extract-unmapped-reads,$(sample))))
