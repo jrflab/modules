2744f0a1f3c2365822e25c30d2ec408dc5aa706d
David Brown <ndbrown6@gmail.com>
2019-02-16 15:15:05 -0500

    Update cnvkitfix.mk

--- copy_number/cnvkitfix.mk
+++ copy_number/cnvkitfix.mk
@@ -4,16 +4,14 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/cnvkit_fix.$(NOW)
 PHONY += cnvkit cnvkit/cnr
 
-cnvkit_fix : $(foreach sample,$(SAMPLES),cnvkit/cnr/$(sample).cnr)
+cnvkit_fix : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnr/$(sample).cnr)
 
 define cnvkit-cnr
 cnvkit/cnr/%.cnr : cnvkit/cnn/tumor/%.targetcoverage.cnn cnvkit/cnn/tumor/%.antitargetcoverage.cnn cnvkit/reference/combined_reference.cnr
 	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) cnvkit/reference/combined_reference.cnr -o cnvkit/cnr/$$(*).cnr")
 	
-cnvkit/cnr/%.cnr : cnvkit/cnn/normal/%.targetcoverage.cnn cnvkit/cnn/normal/%.antitargetcoverage.cnn cnvkit/reference/combined_reference.cnr
-	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) cnvkit/reference/combined_reference.cnr -o cnvkit/cnr/$$(*).cnr")
 endef
- $(foreach sample,$(SAMPLES),\
+ $(foreach sample,$(TUMOR_SAMPLES),\
 		$(eval $(call cnvkit-cnr,$(sample))))
 				
 .PHONY: $(PHONY)
