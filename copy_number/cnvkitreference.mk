ff7c1e926041b6718fe89d673e1bf7c739284d0b
David Brown <ndbrown6@gmail.com>
2019-02-16 15:11:00 -0500

    Update cnvkitreference.mk

--- copy_number/cnvkitreference.mk
+++ copy_number/cnvkitreference.mk
@@ -4,7 +4,7 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/cnvkit_reference.$(NOW)
 PHONY += cnvkit cnvkit/reference
 
-cnvkit : cnvkit/reference/combined_reference.cnr
+cnvkit_reference : cnvkit/reference/combined_reference.cnr
 
 cnvkit/reference/combined_reference.cnr : $(wildcard cnvkit/cnn/normal/$(NORMAL_SAMPLES).targetcoverage.cnn) $(wildcard cnvkit/cnn/normal/$(NORMAL_SAMPLES).antitargetcoverage.cnn)
 	$(call RUN,-n 1 -s 12G -m 16G,"cnvkit.py reference cnvkit/cnn/normal/*.cnn -f $(REF_FASTA) --no-edge -o cnvkit/reference/combined_reference.cnr")
