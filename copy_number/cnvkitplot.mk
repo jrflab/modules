33b39d34e9f0f85a293621d3fd6b4adc5885bf0e
David Brown <ndbrown6@gmail.com>
2019-02-16 15:16:45 -0500

    Update cnvkitplot.mk

--- copy_number/cnvkitplot.mk
+++ copy_number/cnvkitplot.mk
@@ -4,13 +4,13 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/cnvkit_plot.$(NOW)
 PHONY += cnvkit cnvkit/plot
 
-cnvkit : $(foreach sample,$(SAMPLES),cnvkit/plot/$(sample).ontarget.pdf cnvkit/plot/$(sample).offtarget.pdf)
+cnvkit_plot : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/plot/$(sample).ontarget.pdf cnvkit/plot/$(sample).offtarget.pdf)
 
 define cnvkit-plot
 cnvkit/plot/%.ontarget.pdf cnvkit/plot/%.offtarget.pdf : cnvkit/cnr/%.cnr
 	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 4G -m 6G,"$(RSCRIPT) modules/copy_number/cnvkitplot.R --in_file $$(<)")
 endef
- $(foreach sample,$(SAMPLES),\
+ $(foreach sample,$(TUMOR_SAMPLES),\
 		$(eval $(call cnvkit-plot,$(sample))))
 				
 .PHONY: $(PHONY)
