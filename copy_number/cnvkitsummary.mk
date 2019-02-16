301afbc4ded0862b6ab2c936f77d2ac27a9e4681
David Brown <ndbrown6@gmail.com>
2019-02-16 17:39:14 -0500

    Update cnvkitsummary.mk

--- copy_number/cnvkitsummary.mk
+++ copy_number/cnvkitsummary.mk
@@ -6,7 +6,7 @@ PHONY += cnvkit cnvkit/summary
 
 cnvkit_summary : cnvkit/summary/bygene.txt cnvkit/summary/bygene.RData
 
-cnvkit/summary/bygene.txt cnvkit/summary/bygene.RData : $(foreach pair,$(TUMOR_SAMPLES),cnvkit/called/$(sample).RData)
+cnvkit/summary/bygene.txt cnvkit/summary/bygene.RData : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/called/$(sample).RData)
 	$(call RUN,-c -s 24G -m 48G,"mkdir -p cnvkit/summary && \
 							 	 $(RSCRIPT) modules/copy_number/cnvkitsummary.R --sample_names '$(TUMOR_SAMPLES)'")
 												 
