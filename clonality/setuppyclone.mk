512ecb8fab332eb67f5526264003dafafd315e0a
David Brown <ndbrown6@gmail.com>
2019-02-28 18:26:11 -0500

    Update

--- clonality/setuppyclone.mk
+++ clonality/setuppyclone.mk
@@ -3,26 +3,27 @@ include modules/Makefile.inc
 LOGDIR ?= log/setup_pyclone.$(NOW)
 PHONY += pyclone
 
-setup_pyclone : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(normal.$(pair))/$(tumor.$(pair)).yaml) $(foreach sample,$(NORMAL_SAMPLES),pyclone/$(sample)/config.yaml)
+setup_pyclone : $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/config.yaml)
 
 define make-input-pyclone
-pyclone/$2/$1.yaml : $(wildcard $(foreach set,$(SAMPLE_SETS),sufam/$(set).tsv))
-	$$(call RUN,-c -s 4G -m 6G,"if [ ! -d pyclone/$2 ]; then mkdir pyclone/$2; fi && \
-								if [ ! -f pyclone/$2/$1.tsv ]; then  $(RSCRIPT) modules/clonality/tsvforpyclone.R --sample_name $1 --normal_name $2; fi && \
-								source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
-								PyClone build_mutations_file --in_file pyclone/$2/$1.tsv --out_file pyclone/$2/$1.yaml --prior total_copy_number")
+pyclone/%/config.yaml : sufam/%.tsv
+	$$(call RUN,-c -s 4G -m 6G,"mkdir -p pyclone/$$(*) && \
+								$(RSCRIPT) modules/clonality/tsvforpyclone.R --sample_set $$(*) --normal_samples $(NORMAL_SAMPLES) && \
+								touch pyclone/$$(*)/config.yaml")
+#								source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
+#								PyClone build_mutations_file --in_file pyclone/$2/$1.tsv --out_file pyclone/$2/$1.yaml --prior total_copy_number")
 
 endef
-$(foreach pair,$(SAMPLE_PAIRS),\
-	$(eval $(call make-input-pyclone,$(tumor.$(pair)),$(normal.$(pair)))))
+$(foreach set,$(SAMPLE_SETS),\
+		$(eval $(call make-input-pyclone,$(set))))
 
-define make-config-yaml
-pyclone/%/config.yaml : $(wildcard $(foreach set,$(SAMPLE_SETS),sufam/$(set).tsv))
-	$$(call RUN,-c -s 4G -m 6G,"if [ ! -d pyclone/$$(*) ]; then mkdir pyclone/$$(*); fi && \
-								$(RSCRIPT) modules/clonality/pycloneconfig.R --sample_name $$*")
-endef
-$(foreach sample,$(NORMAL_SAMPLES),\
-		$(eval $(call make-config-yaml,$(sample))))
+#define make-config-yaml
+#pyclone/%/config.yaml : $(wildcard $(foreach set,$(SAMPLE_SETS),sufam/$(set).tsv))
+#	$$(call RUN,-c -s 4G -m 6G,"if [ ! -d pyclone/$$(*) ]; then mkdir pyclone/$$(*); fi && \
+#								$(RSCRIPT) modules/clonality/pycloneconfig.R --sample_name $$*")
+#endef
+#$(foreach sample,$(NORMAL_SAMPLES),\
+#		$(eval $(call make-config-yaml,$(sample))))
 		
 .DELETE_ON_ERROR:
 .SECONDARY:
--- clonality/tsvforpyclone.R
+++ clonality/tsvforpyclone.R
@@ -2,33 +2,40 @@
 
 suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("readr"))
+suppressPackageStartupMessages(library("dplyr"))
+suppressPackageStartupMessages(library("magrittr"))
 
-optList = list(make_option("--sample_name", default = NULL, help = "sample name"),
-			   make_option("--normal_name", default = NULL, help = "normal name"))
+optList = list(make_option("--sample_set", default = NULL, help = "sample set name"),
+			   make_option("--normal_samples", default = NULL, help = "normal sample names"))
 
 parser = OptionParser(usage = "%prog [options] mutation_file", option_list = optList)
 arguments = parse_args(parser, positional_arguments = T)
 opt = arguments$options
-file_names = dir(path="sufam", pattern=".tsv", full.names=TRUE)
-index = grep(opt$sample_name, file_names, fixed=TRUE)
-mutation_summary = read_tsv(file=file_names[index])
-col_names = colnames(mutation_summary)
-mutation_summary = data.frame(mutation_summary)
-colnames(mutation_summary) = col_names
-index = grep("qt", colnames(mutation_summary))
-flag = apply(mutation_summary[,index,drop=FALSE], 1, function(x) { sum(x==0)==0 })
-mutation_summary = mutation_summary[flag,,drop=FALSE]
-index = grep("q2", colnames(mutation_summary))
-flag = apply(mutation_summary[,index,drop=FALSE], 1, function(x) { sum(x==0)==0 })
-mutation_summary = mutation_summary[flag,,drop=FALSE]
 
-mutation_id = paste0(mutation_summary[,"Gene_Symbol"], "_", mutation_summary[,"HGVSp"])
-ref_counts = round((1-mutation_summary[,paste0("MAF_", opt$sample_name)])*mutation_summary[,paste0("DP_", opt$sample_name)])
-var_counts = round((mutation_summary[,paste0("MAF_", opt$sample_name)])*mutation_summary[,paste0("DP_", opt$sample_name)])
-var_counts[var_counts<5] = 0
-normal_cn = rep(2, length(mutation_id))
-minor_cn = rep(0, length(mutation_id))
-major_cn = mutation_summary[,paste0("qt_", opt$sample_name)]
-sample_summary = data.frame(mutation_id, ref_counts, var_counts, normal_cn, minor_cn, major_cn)
-write.table(sample_summary, paste0("pyclone/", opt$normal_name, "/", opt$sample_name, ".tsv"), sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE, append=FALSE)
 
+tumor_samples = unlist(strsplit(opt$sample_set, split="_", fixed=TRUE))
+normal_sample = unlist(strsplit(opt$normal_samples, split=" ", fixed=TRUE))
+normal_sample = tumor_samples[tumor_samples %in% normal_sample]
+tumor_samples = tumor_samples[!(tumor_samples %in% normal_sample)]
+
+mutation_summary = read_tsv(file=paste0("sufam/", opt$sample_set, ".tsv"))
+for (i in 1:length(tumor_samples)) {
+	mutation_id = paste0(mutation_summary$Gene_Symbol, "_", mutation_summary$HGVSp)
+	fsq = mutation_summary %>%
+		  .[[paste0("MAF_", tumor_samples[i])]]
+	qt = mutation_summary %>%
+		  .[[paste0("qt_", tumor_samples[i])]]
+	n = mutation_summary %>%
+		.[[paste0("DP_", tumor_samples[i])]]
+	flag = mutation_summary %>%
+		   .[[paste0("CALL_", tumor_samples[i])]]
+	
+	fsq[flag==0] = 0
+	var_counts = round(fsq*n)
+	ref_counts = round((1-fsq)*n)
+	normal_cn = rep(2, length(mutation_id))
+	minor_cn = rep(0, length(mutation_id))
+	major_cn = qt
+	sample_summary = data.frame(mutation_id, ref_counts, var_counts, normal_cn, minor_cn, major_cn)
+	write.table(sample_summary, paste0("pyclone/", opt$sample_set, "/", tumor_samples[i], ".tsv"), sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE, append=FALSE)
+}
