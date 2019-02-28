a85b8a36696438df6d61970fd300502e53198e57
David Brown <ndbrown6@gmail.com>
2019-02-28 18:56:45 -0500

    Update setuppyclone.mk

--- clonality/setuppyclone.mk
+++ clonality/setuppyclone.mk
@@ -8,10 +8,10 @@ setup_pyclone : $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/config.yaml)
 define make-input-pyclone
 pyclone/%/config.yaml : sufam/%.tsv
 	$$(call RUN, -s 4G -m 6G,"mkdir -p pyclone/$$(*) && \
-								$(RSCRIPT) modules/clonality/tsvforpyclone.R --sample_set $$(*) --normal_samples $(NORMAL_SAMPLES) && \
-								$(RSCRIPT) modules/clonality/pycloneconfig.R --sample_set $$(*) --normal_samples $(NORMAL_SAMPLES)")
-#								source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
-#								PyClone build_mutations_file --in_file pyclone/$2/$1.tsv --out_file pyclone/$2/$1.yaml --prior total_copy_number")
+							  $(RSCRIPT) modules/clonality/tsvforpyclone.R --sample_set $$(*) --normal_samples $(NORMAL_SAMPLES) && \
+							  $(RSCRIPT) modules/clonality/pycloneconfig.R --sample_set $$(*) --normal_samples $(NORMAL_SAMPLES) && \
+							  source /home/$(USER)/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/$(USER)/share/usr/anaconda-envs/PyClone-0.13.1 && \
+							  for i in $(ls pyclone/$$(*)/*.tsv); do j=${i%.*}; PyClone build_mutations_file --in_file $i --out_file $j.yaml --prior total_copy_number; done;")
 
 endef
 $(foreach set,$(SAMPLE_SETS),\
