58baa5600ae571ecff3b7c303c8f019cbc45c9f2
David Brown <ndbrown6@gmail.com>
2019-03-08 11:27:25 -0500

    Update cnvkitreference.mk

--- copy_number/cnvkitreference.mk
+++ copy_number/cnvkitreference.mk
@@ -7,7 +7,7 @@ PHONY += cnvkit cnvkit/reference
 cnvkit_reference : cnvkit/reference/combined_reference.cnr
 
 cnvkit/reference/combined_reference.cnr : $(wildcard cnvkit/cnn/normal/$(NORMAL_SAMPLES).targetcoverage.cnn) $(wildcard cnvkit/cnn/normal/$(NORMAL_SAMPLES).antitargetcoverage.cnn)
-	$(call RUN,-n 1 -s 12G -m 16G,"cnvkit.py reference cnvkit/cnn/normal/*.cnn -f $(REF_FASTA) --no-edge -o cnvkit/reference/combined_reference.cnr")
+	$(call RUN,-n 1 -s 24G -m 32G,"cnvkit.py reference cnvkit/cnn/normal/*.cnn -f $(REF_FASTA) --no-edge -o cnvkit/reference/combined_reference.cnr")
 		
 .PHONY: $(PHONY)
 
