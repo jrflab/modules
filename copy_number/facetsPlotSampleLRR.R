a0e84cea1cb4ae5e86c50a25456c9e6f93f7c677
David Brown <ndbrown6@gmail.com>
2019-03-16 22:20:36 -0400

    Update facetsPlotSampleLRR.R

--- copy_number/facetsPlotSampleLRR.R
+++ copy_number/facetsPlotSampleLRR.R
@@ -13,12 +13,9 @@ plotSampleLRR <- function(x, fit)
     start = c(1, tmp[-length(len)] + 1)
     end = tmp
     mid = start + len/2
-    plot(mat$cnlr, pch = ".", axes = F, cex = 1.5, ylim = c(-max(c(max(abs(mat$cnlr)), 4)),
-                                                            max(c(max(abs(mat$cnlr)), 4))), col = c("grey", "lightblue")[1 + rep(cncf$chrom -
-                                                                                                                         2 * floor(cncf$chrom/2), cncf$num.mark)],
-         ylab = "log-ratio", xlab="Chromosomes")
-     points(rep(cncf$cnlr.median, cncf$num.mark), pch = ".", cex = 2,
-            col = "brown")
+    plot(mat$cnlr, pch = ".", axes = F, cex = 1.5, ylim = c(-5,5), col = c("grey", "lightblue")[1 + rep(cncf$chrom - 2 * floor(cncf$chrom/2), cncf$num.mark)],
+         ylab = expression(Log[2]~"Ratio"), xlab="Chromosomes")
+    points(rep(cncf$cnlr.median, cncf$num.mark), pch = ".", cex = 2, col = "brown")
     labs <- names(mid)
     labs <- sub('21', '', labs)
     labs <- sub('23', 'X', labs)
@@ -26,4 +23,242 @@ plotSampleLRR <- function(x, fit)
     axis(side = 2, cex.axis = 1, las=2)
     abline(h=0, lty=2, col="lightgrey")
     box()
+}
+
+psi <- function (x, z)
+{
+    xwin <- x
+    xwin[x < -z] <- -z
+    xwin[x > z] <- z
+    return(xwin)
+}
+
+numericChrom <- function (chrom) 
+{
+    if (!is.numeric(chrom)) {
+        if (is.factor(chrom)) {
+            chrom <- as.character(chrom)
+        }
+        chrx <- c(which(chrom == "x"), which(chrom == "X"))
+        chrom[chrx] <- 23
+        chry <- c(which(chrom == "y"), which(chrom == "Y"))
+        chrom[chry] <- 24
+        chrom <- as.numeric(chrom)
+    }
+    return(chrom)
+}
+
+numericArms <- function (chrom, char.arms)
+{
+    p.arm <- which(char.arms == "p")
+    q.arm <- which(char.arms == "q")
+    arms <- rep(NA, length(char.arms))
+    arms[p.arm] <- chrom[p.arm] * 2 - 1
+    arms[q.arm] <- chrom[q.arm] * 2
+    return(arms)
+}
+
+madWins <- function (x, tau, k, digits) 
+{
+    xhat <- medianFilter(x, k)
+    d <- x - xhat
+    SD <- mad(d)
+    z <- tau * SD
+    xwin <- xhat + psi(d, z)
+    outliers <- rep(0, length(x))
+    outliers[round(x, digits) > round(xwin, digits)] <- 1
+    outliers[round(x, digits) < round(xwin, digits)] <- -1
+    return(list(ywin = xwin, sdev = SD, outliers = outliers))
+}
+
+medianFilter <- function (x, k) 
+{
+    n <- length(x)
+    filtWidth <- 2 * k + 1
+    if (filtWidth > n) {
+        if (n == 0) {
+            filtWidth <- 1
+        }
+        else if (n%%2 == 0) {
+            filtWidth <- n - 1
+        }
+        else {
+            filtWidth <- n
+        }
+    }
+    runMedian <- runmed(x, k = filtWidth, endrule = "median")
+    return(runMedian)
+}
+
+
+winsorize <- function (data, pos.unit = "bp", arms = NULL, method = "mad", 
+    tau = 2.5, k = 25, gamma = 40, iter = 1, assembly = "hg19", 
+    digits = 4, return.outliers = FALSE, save.res = FALSE, file.names = NULL, 
+    verbose = TRUE) 
+{
+    stopifnot(pos.unit %in% c("bp", "kbp", "mbp"))
+    stopifnot(method %in% c("mad", "pcf"))
+    if (!assembly %in% c("hg19", "hg18", "hg17", "hg16", "mm7", "mm8", "mm9")) {
+        stop("assembly must be one of hg19, hg18, hg17 or hg16", call. = FALSE)
+    }
+    stopifnot(class(data) %in% c("matrix", "data.frame", "character"))
+    isfile <- class(data) == "character"
+    if (!isfile) {
+        stopifnot(ncol(data) >= 3)
+        chrom <- data[, 1]
+        pos <- data[, 2]
+        nSample <- ncol(data) - 2
+        sample.names <- colnames(data)[-c(1:2)]
+    }
+    else {
+        f <- file(data, "r")
+        head <- scan(f, nlines = 1, what = "character", quiet = TRUE, 
+            sep = "\t")
+        if (length(head) < 3) {
+            stop("Data in file must have at least 3 columns", 
+                call. = FALSE)
+        }
+        sample.names <- head[-c(1:2)]
+        nSample <- length(sample.names)
+        chrom.pos <- read.table(file = data, sep = "\t", header = TRUE, 
+            colClasses = c(rep(NA, 2), rep("NULL", nSample)), 
+            as.is = TRUE)
+        chrom <- chrom.pos[, 1]
+        pos <- chrom.pos[, 2]
+    }
+    if (is.factor(chrom)) {
+        chrom <- as.character(chrom)
+    }
+    num.chrom <- numericChrom(chrom)
+    nProbe <- length(num.chrom)
+    if (!is.numeric(pos)) {
+        stop("input in data column 2 (posistions) must be numeric", call. = FALSE)
+    }
+    if (is.null(arms)) {
+        arms <- getArms(num.chrom, pos, pos.unit, get(assembly))
+    }
+    else {
+        if (length(arms) != nProbe) {
+            stop("'arms' must be the same length as number of rows in data", 
+                call. = FALSE)
+        }
+    }
+    num.arms <- numericArms(num.chrom, arms)
+    arm.list <- unique(num.arms)
+    nArm <- length(arm.list)
+    if (!save.res) {
+        wins.data <- matrix(nrow = 0, ncol = nSample)
+        if (return.outliers) {
+            wins.outliers <- matrix(nrow = 0, ncol = nSample)
+        }
+    }
+    else {
+        if (is.null(file.names)) {
+            dir.res <- "Wins_res"
+            if (!dir.res %in% dir()) {
+                dir.create(dir.res)
+            }
+            file.names <- c(paste(dir.res, "/", "wins.data.txt", 
+                sep = ""), paste(dir.res, "/", "wins.outliers.txt", 
+                sep = ""))
+        }
+        else {
+            if (length(file.names) < 2) {
+                stop("'file.names' must be of length 2", call. = FALSE)
+            }
+        }
+    }
+    for (c in 1:nArm) {
+        probe.c <- which(num.arms == arm.list[c])
+        wins.data.c <- matrix(nrow = length(probe.c), ncol = 0)
+        if (return.outliers || save.res) {
+            wins.outliers.c <- matrix(nrow = length(probe.c), 
+                ncol = 0)
+        }
+        if (!isfile) {
+            arm.data <- data[probe.c, -c(1:2), drop = FALSE]
+        }
+        else {
+            arm.data <- read.table(f, nrows = length(probe.c), 
+                sep = "\t", colClasses = c(rep("NULL", 2), rep("numeric", 
+                  nSample)))
+        }
+        if (any(!sapply(arm.data, is.numeric))) {
+            stop("input in data columns 3 and onwards (copy numbers) must be numeric", 
+                call. = FALSE)
+        }
+        for (i in 1:nSample) {
+            y <- arm.data[, i]
+            na <- is.na(y)
+            use.y <- y[!na]
+            ywins <- rep(NA, length(y))
+            outliers <- rep(NA, length(y))
+            wins <- switch(method, mad = madWins(use.y, tau = tau, 
+                k = k, digits = digits), pcf = pcfWins(use.y, 
+                tau = tau, k = k, gamma = gamma, iter = iter, 
+                digits = digits))
+            ywins[!na] <- wins$ywin
+            outliers[!na] <- wins$outliers
+            ywins <- round(ywins, digits = digits)
+            wins.data.c <- cbind(wins.data.c, ywins)
+            if (return.outliers || save.res) {
+                wins.outliers.c <- cbind(wins.outliers.c, outliers)
+            }
+        }
+        if (!save.res) {
+            wins.data <- rbind(wins.data, wins.data.c)
+            if (return.outliers) {
+                wins.outliers <- rbind(wins.outliers, wins.outliers.c)
+            }
+        }
+        else {
+            if (c == 1) {
+                wd <- file(file.names[1], "w")
+                wo <- file(file.names[2], "w")
+            }
+            write.table(data.frame(chrom[probe.c], pos[probe.c], 
+                wins.data.c, stringsAsFactors = FALSE), file = wd, 
+                col.names = if (c == 1) 
+                  c("chrom", "pos", sample.names)
+                else FALSE, row.names = FALSE, quote = FALSE, 
+                sep = "\t")
+            write.table(data.frame(chrom[probe.c], pos[probe.c], 
+                wins.outliers.c, stringsAsFactors = FALSE), file = wo, 
+                col.names = if (c == 1) 
+                  c("chrom", "pos", sample.names)
+                else FALSE, row.names = FALSE, quote = FALSE, 
+                sep = "\t")
+        }
+        if (verbose) {
+            chr <- unique(chrom[probe.c])
+            a <- unique(arms[probe.c])
+            cat(paste("winsorize finished for chromosome arm ", 
+                chr, a, sep = ""), "\n")
+        }
+    }
+    if (isfile) {
+        close(f)
+    }
+    if (!save.res) {
+        wins.data <- data.frame(chrom, pos, wins.data, stringsAsFactors = FALSE)
+        colnames(wins.data) <- c("chrom", "pos", sample.names)
+        if (return.outliers) {
+            wins.outliers <- data.frame(chrom, pos, wins.outliers, 
+                stringsAsFactors = FALSE)
+            colnames(wins.outliers) <- c("chrom", "pos", sample.names)
+            return(list(wins.data = wins.data, wins.outliers = wins.outliers))
+        }
+        else {
+            return(wins.data)
+        }
+    }
+    else {
+        close(wd)
+        close(wo)
+        cat(paste("winsorized data were saved in file", file.names[1]), 
+            sep = "\n")
+        cat(paste("outliers were saved in file", file.names[2]), 
+            sep = "\n")
+        return(invisible(NULL))
+    }
 }
