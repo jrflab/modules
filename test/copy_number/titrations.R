cef18c9c91ec4b168f6fa329f253313a269a3261
David Brown <ndbrown6@gmail.com>
2019-03-21 16:43:43 -0400

    Update titrations.R

--- test/copy_number/titrations.R
+++ test/copy_number/titrations.R
@@ -15,12 +15,12 @@ opt = arguments$options
 tumor_sample = strsplit(x=opt$tumor_normal, split="_", fixed=TRUE)[[1]][1]
 normal_sample = strsplit(x=opt$tumor_normal, split="_", fixed=TRUE)[[1]][2]
 load(paste0("facets/cncf/", opt$tumor_normal, ".Rdata"))
-purity = c(.1, .2, .3, .4, .5, .75, .9)
+purity = c(.1, .2, .3, .4, .5, .7, .9)
 purity = purity[purity < fit$purity]
 for (i in 1:length(purity)) {
-	system(paste0("samtools view -s 0", purity[i], " -b bam/", tumor_sample, ".bam > titration/", tumor_sample, purity[i], ".bam"))
-	system(paste0("samtools merge titration/", tumor_sample, purity[i], ".bam bam/", normal_sample[i], ".bam titration/", tumor_sample, "-", purity[i], ".bam"))
-	system(paste0("samtools index titration/", tumor_sample, "-", purity[i], ".bam"))
-	file.remove(paste0("titration/", tumor_sample, purity[i], ".bam"))
+	system(paste0("samtools view -s ", purity[i], "0 -b bam/", tumor_sample, ".bam > titrations/", tumor_sample, ".", purity[i], ".bam"))
+	system(paste0("samtools merge titration/", tumor_sample, purity[i], ".bam bam/", normal_sample, ".bam titrations/", tumor_sample, "-", purity[i], ".bam"))
+	system(paste0("samtools index titrations/", tumor_sample, "-", purity[i], ".bam"))
+	file.remove(paste0("titrations/", tumor_sample, ".", purity[i], ".bam"))
 	cat(sessionInfo()$R.version$version.string, file=paste0("titrations/", opt$tumor_normal, ".timestamp"))
 }
