a127fadfa8dea24ef34b7b6dcd3626d055872988
David Brown <ndbrown6@gmail.com>
2019-03-23 16:32:37 -0400

    Update titrations.mk

--- test/copy_number/titrations.mk
+++ test/copy_number/titrations.mk
@@ -3,10 +3,10 @@ include modules/Makefile.inc
 LOGDIR ?= log/titrations.$(NOW)
 PHONY += titrations titrations/bam
 
-pyclone : $(foreach pair,$(SAMPLE_PAIRS),titrations/$(pair).timestamp)
+pyclone : $(foreach pair,$(SAMPLE_PAIRS),titrations/bam/$(pair).timestamp)
 
 define make-titrations
-titrations/$1_$2.timestamp : bam/$1.bam bam/$2.bam
+titrations/bam/$1_$2.timestamp : bam/$1.bam bam/$2.bam
 	$$(call RUN, -s 12G -m 16G,"mkdir -p titrations && \
 								mkdir -p titrations/bam && \
 							    $(RSCRIPT) modules/test/copy_number/titrations.R --tumor_normal $1_$2")
