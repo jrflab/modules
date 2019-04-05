5ab2cd109b105f021f3118d64fe7cf170b18c8b8
David Brown <ndbrown6@gmail.com>
2019-04-05 17:52:26 -0400

    pyclone

--- test/clonality/pyclone.mk
+++ test/clonality/pyclone.mk
@@ -8,7 +8,7 @@ pyclone : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/pyclone.tsv)
 define make-pyclone
 pyclone/$1_$2/config.yaml : summary/tsv/mutation_summary.tsv
 	$$(call RUN, -s 16G -m 24G,"mkdir -p pyclone/$1_$2 && \
-							    $(RSCRIPT) modules/test/clonality/tsvtopyclone.R --sample_name $1_$2 --sample_fraction '$${pyclone_fraction.$1}'")
+							    $(RSCRIPT) modules/test/clonality/tsvtopyclone.R --sample_name $1_$2'")
 							    
 pyclone/$1_$2/trace/alpha.tsv.bz2 : pyclone/$1_$2/config.yaml
 	$$(call RUN,-s 8G -m 16G -w 7200,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
@@ -16,7 +16,7 @@ pyclone/$1_$2/trace/alpha.tsv.bz2 : pyclone/$1_$2/config.yaml
 							 		  
 pyclone/$1_$2/pyclone.tsv : pyclone/$1_$2/trace/alpha.tsv.bz2
 	$$(call RUN,-s 8G -m 16G -w 7200,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
-							 		 PyClone build_table --config_file pyclone/$1_$2/config.yaml --out_file pyclone/$1_$2/pyclone.tsv --max_cluster 10 --table_type old_style --burnin 5000")
+							 		 PyClone build_table --config_file pyclone/$1_$2/config.yaml --out_file pyclone/$1_$2/pyclone.tsv --max_cluster 10 --table_type old_style --burnin 50000")
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
--- test/clonality/tsvtopyclone.R
+++ test/clonality/tsvtopyclone.R
@@ -5,8 +5,7 @@ suppressPackageStartupMessages(library("readr"))
 suppressPackageStartupMessages(library("dplyr"))
 suppressPackageStartupMessages(library("magrittr"))
 
-optList = list(make_option("--sample_name", default = NULL, help = "tumor normal sample name"),
-			   make_option("--sample_fraction", default = NULL, help = "fraction of mutations to consider"))
+optList = list(make_option("--sample_name", default = NULL, help = "tumor normal sample name"))
 
 parser = OptionParser(usage = "%prog [options] mutation_file", option_list = optList)
 arguments = parse_args(parser, positional_arguments = T)
@@ -16,20 +15,19 @@ tumor_sample = unlist(strsplit(opt$sample_name, split="_", fixed=TRUE))[1]
 normal_sample = unlist(strsplit(opt$sample_name, split="_", fixed=TRUE))[2]
 sample_fraction = ifelse(is.na(as.numeric(opt$sample_fraction)), 1, as.numeric(opt$sample_fraction))
 
-mutation_summary = read_tsv(file="summary/tsv/mutation_summary.tsv") %>%
+mutation_summary = read_tsv(file="summary/tsv/mutation_summary.tsv", col_types = cols(.default = col_character())) %>%
+				   type_convert() %>%
 				   filter(TUMOR_SAMPLE==tumor_sample) %>%
  				   filter(NORMAL_SAMPLE==normal_sample) %>%
  				   filter(grepl("mutect", variantCaller, fixed=TRUE)) %>%
  				   filter(NORMAL_MAF==0) %>%
- 				   filter(TUMOR_MAF>.05) %>%
- 				   filter(TUMOR_DP<200) %>%
- 				   filter(TUMOR_DP>25) %>%
- 				   filter(NORMAL_DP<100) %>%
- 				   filter(NORMAL_DP>10) %>%
- 				   filter(!(Variant_Classification %in% c("3'Flank", "3'UTR", "5'Flank", "5'UTR", "RNA", "Splice_Region"))) %>%
- 				   filter(CHROM %in% 1:22)
- 				   
-mutation_id = paste0(mutation_summary$CHROM, ":", mutation_summary$POS)
+ 				   filter(TUMOR_MAF>=.05) %>%
+ 				   filter(TUMOR_DP<=500) %>%
+ 				   filter(TUMOR_DP>=50) %>%
+ 				   filter(NORMAL_DP<=500) %>%
+ 				   filter(NORMAL_DP>=10) %>%
+ 				   mutate(UUID = paste0(CHROM, ":", POS))
+ 				    				   
 load(paste0("facets/cncf/", opt$sample_name, ".Rdata"))
 qt = q1 = rep(NA, nrow(mutation_summary))
 for (i in 1:nrow(mutation_summary)) {
@@ -46,17 +44,14 @@ n = as.numeric(mutation_summary$TUMOR_DP)
 var_counts = round(fsq*n)
 ref_counts = round((1-fsq)*n)
 normal_cn = rep(2, nrow(mutation_summary))
-minor_cn = q1
-major_cn = qt-q1
+minor_cn = rep(0, nrow(mutation_summary))
+major_cn = qt
 sample_summary = data.frame(mutation_id, ref_counts, var_counts, normal_cn, minor_cn, major_cn)
-index = sample_summary[,5]==0 & sample_summary[,6]==0
+index = apply(sample_summary, 1, function(x) {any(is.na(x))})
 sample_summary = sample_summary[!index,,drop=FALSE]
-sample_summary = sample_summary[!apply(sample_summary, 1, function(x) {any(is.na(x))}),,drop=FALSE]
-index = sort(sample(x=1:nrow(sample_summary), size=round(sample_fraction*nrow(sample_summary)), replace=FALSE))
-sample_summary = sample_summary[index,,drop=FALSE]
 write.table(sample_summary, paste0("pyclone/", opt$sample_name, "/", tumor_sample,".tsv"), sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE, append=FALSE)
 
-cat("num_iters: 10000\n", file=paste0("pyclone/", opt$sample_name, "/config.yaml"), append = FALSE)
+cat("num_iters: 100000\n", file=paste0("pyclone/", opt$sample_name, "/config.yaml"), append = FALSE)
 cat("\n", file=paste0("pyclone/", opt$sample_name, "/config.yaml"), append = TRUE)
 cat("base_measure_params:\n", file=paste0("pyclone/", opt$sample_name, "/config.yaml"), append = TRUE)
 cat("  alpha: 1\n", file=paste0("pyclone/", opt$sample_name, "/config.yaml"), append = TRUE)
@@ -96,4 +91,4 @@ cat("    tumour_content:\n", file=paste0("pyclone/", opt$sample_name, "/config.y
 cat(paste0("      value: ", ifelse(is.na(fit$purity), 1.0, signif(fit$purity, 2)),"\n"), file=paste0("pyclone/", opt$sample_name, "/config.yaml"), append = TRUE)
 cat("\n", file=paste0("pyclone/", opt$sample_name, "/config.yaml"), append = TRUE)
 cat("    error_rate: 0.01", file=paste0("pyclone/", opt$sample_name, "/config.yaml"), append = TRUE)
-system(paste0("source ~/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate ~/share/usr/anaconda-envs/PyClone-0.13.1 && PyClone build_mutations_file --in_file pyclone/",  opt$sample_name, "/", tumor_sample, ".tsv --out_file pyclone/", opt$sample_name, "/", tumor_sample, ".yaml  --prior parental_copy_number"))
+system(paste0("source ~/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate ~/share/usr/anaconda-envs/PyClone-0.13.1 && PyClone build_mutations_file --in_file pyclone/",  opt$sample_name, "/", tumor_sample, ".tsv --out_file pyclone/", opt$sample_name, "/", tumor_sample, ".yaml  --prior total_copy_number"))
