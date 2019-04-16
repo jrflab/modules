3b87148e21b97118395a4f3a97594b947f46b1a3
David Brown <ndbrown6@gmail.com>
2019-04-15 22:07:46 -0400

    Update pyclone.mk

--- test/clonality/pyclone.mk
+++ test/clonality/pyclone.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/setup_pyclone.$(NOW)
 PHONY += pyclone
 
-pyclone : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/pyclone.tsv)
+pyclone : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/report.tsv) $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/hist_ccf.pdf) $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/hist_std.pdf)
 
 define make-pyclone
 pyclone/$1_$2/config.yaml : summary/tsv/mutation_summary.tsv
@@ -17,6 +17,14 @@ pyclone/$1_$2/trace/alpha.tsv.bz2 : pyclone/$1_$2/config.yaml
 pyclone/$1_$2/pyclone.tsv : pyclone/$1_$2/trace/alpha.tsv.bz2
 	$$(call RUN,-s 16G -m 24G -w 7200,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
 							 		   PyClone build_table --config_file pyclone/$1_$2/config.yaml --out_file pyclone/$1_$2/pyclone.tsv --max_cluster 10 --table_type old_style --burnin 50000")
+							 		   
+pyclone/$1_$2/hist_ccf.pdf pyclone/$1_$2/hist_std.pdf : pyclone/$1_$2/pyclone.tsv
+	$$(call RUN, -s 16G -m 24G,"mkdir -p pyclone/$1_$2 && \
+							    $(RSCRIPT) modules/test/clonality/plotpyclone.R --sample_name $1_$2")
+							    
+pyclone/$1_$2/report.tsv : pyclone/$1_$2/pyclone.tsv
+	$$(call RUN, -s 16G -m 24G,"mkdir -p pyclone/$1_$2 && \
+							    $(RSCRIPT) modules/test/clonality/reportpyclone.R --sample_name $1_$2")
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
