a9e7c776e89e684926ee8f872849752dc2830697
David Brown <ndbrown6@gmail.com>
2019-05-13 14:59:07 -0400

    Update cnvkitplot.R

--- copy_number/cnvkitplot.R
+++ copy_number/cnvkitplot.R
@@ -4,6 +4,35 @@ suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("copynumber"))
 suppressPackageStartupMessages(library("colorspace"))
 suppressPackageStartupMessages(library("ASCAT"))
+suppressPackageStartupMessages(library("CNtu"))
+
+'plot_log2_' <- function(x, title = "")
+{
+   	par(mar=c(5, 5, 4, 2)+.1)
+   	data("CytoBand")
+   	end = NULL
+   	for (i in 1:23) {
+   		end = c(end, max(CytoBand[CytoBand[,1]==i,"End"]))
+   	}
+   	end = cumsum(end)
+   	start = c(1, end[1:22]+1)
+   	CytoBand = cbind(start, end)
+   	index = NULL
+   	for (i in 1:23) {
+   		index = c(index, seq(from = CytoBand[i, "start"], to=CytoBand[i, "end"], length=sum(x$chromosome==i)))
+   	}
+	plot(index, x$log2, type="p", pch=".", cex=1.95, col="grey80", axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,5))
+  	axis(2, at = c(-4, -2, 0, 2, 4), labels = c(-4, -2, 0, 2, 4), cex.axis = 1, las = 1)
+	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
+	abline(v=1, col=transparentRgb("goldenrod3", 255), lty=3, lwd=.5)
+	for (j in 1:23) {
+		abline(v=CytoBand[j,"end"], col=transparentRgb("goldenrod3", 255), lty=3, lwd=.5)
+	}
+	axis(1, at = .5*(CytoBand[,"start"]+CytoBand[,"end"]), labels=c(1:22, "X"), cex.axis = 0.85, las = 1)
+	rect(xleft=1-1e10, xright=CytoBand[23,"end"]+1e10, ybottom=4, ytop=6, col="lightgrey", border="black", lwd=1.5)
+	title(main = title, line=-1, cex.main=.75, font.main=1)
+    box(lwd=1.5)
+}
 
 if (!interactive()) {
     options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
@@ -42,31 +71,8 @@ if (nrow(data)==0) {
 		ontarget = subset(data, data$gene!="Antitarget")
 	}
 	
-	col = rep("#9F6986", nrow(ontarget))
-	col[(ontarget[,"chromosome"]%%2)==1] = "#CECAC5"
-	pdf(file=outfile_on_target, width=14, height=5)
-	par(mar=c(5, 5, 4, 2)+.1)
-	plot(ontarget[,"log2"], type="p", pch=".", cex=2, col=col, axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
-	axis(2, at = NULL, cex.axis = 1.15, las = 1)
-	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
-	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
-	abline(v=1, col="goldenrod3")
-	abline(h=0, col="red")
-	for (j in 2:max(ontarget[,"chromosome"])) {
-		v = min(which(ontarget[,"chromosome"]==j))
-		abline(v=v, col="goldenrod3")
-	}
-	abline(v=max(nrow(ontarget)), col="goldenrod3")
-	start = NULL
-	end = NULL
-	for (j in 1:max(ontarget[,"chromosome"])) {
-		start[j] = min(which(ontarget[,"chromosome"]==j))
-		end[j] = max(which(ontarget[,"chromosome"]==j))
-	}
-	labels = 1:max(ontarget[,"chromosome"])
-	labels[labels==23] = "X"
-	axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
-	box(lwd=2.5)
+	pdf(file=outfile_on_target, width=10, height=4.25)
+	plot_log2_(x=ontarget, title=gsub("cnvkit/cnr/", "", gsub(".cnr", "", opt$in_file, fixed=TRUE), fixed=TRUE))
 	dev.off()
 	
 	if (flag==1) {
@@ -74,34 +80,11 @@ if (nrow(data)==0) {
 	} else if (flag==2) {
 		offtarget = subset(data, data$gene=="Antitarget")
 	}
+	
 	tmp = offtarget[,c("chromosome", "start", "log2"),drop=FALSE]
-	colnames(tmp) = c("Chromosome", "Position", "Log2Ratio")
 	tmp = winsorize(data=tmp, tau=3.5, k=25, verbose=FALSE, return.outliers=TRUE)
 	offtarget[tmp$wins.outliers[,3]!=0,"log2"] = NA
-	col = rep("#9F6986", nrow(offtarget))
-	col[(offtarget[,"chromosome"]%%2)==1] = "#CECAC5"
-	pdf(file=outfile_off_target, width=14, height=5)
-	par(mar=c(5, 5, 4, 2)+.1)
-	plot(offtarget[,"log2"], type="p", pch=".", cex=2, col=col, axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
-	axis(2, at = NULL, cex.axis = 1.15, las = 1)
-	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
-	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
-	abline(v=1, col="goldenrod3")
-	abline(h=0, col="red")
-	for (j in 2:max(offtarget[,"chromosome"])) {
-		v = min(which(offtarget[,"chromosome"]==j))
-		abline(v=v, col="goldenrod3")
-	}
-	abline(v=max(nrow(offtarget)), col="goldenrod3")
-	start = NULL
-	end = NULL
-	for (j in 1:max(offtarget[,"chromosome"])) {
-		start[j] = min(which(offtarget[,"chromosome"]==j))
-		end[j] = max(which(offtarget[,"chromosome"]==j))
-	}
-	labels = 1:max(offtarget[,"chromosome"])
-	labels[labels==23] = "X"
-	axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
-	box(lwd=2.5)
+	pdf(file=outfile_off_target, width=10, height=4.25)
+	plot_log2_(x=offtarget, title=gsub("cnvkit/cnr/", "", gsub(".cnr", "", opt$in_file, fixed=TRUE), fixed=TRUE))
 	dev.off()
 }
