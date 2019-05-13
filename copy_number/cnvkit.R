7d20b6077582c3ee0af3af9dfdf6dd85a17d009d
David Brown <ndbrown6@gmail.com>
2019-05-13 15:38:02 -0400

    Update cnvkit.R

--- copy_number/cnvkit.R
+++ copy_number/cnvkit.R
@@ -4,6 +4,46 @@ suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("copynumber"))
 suppressPackageStartupMessages(library("colorspace"))
 suppressPackageStartupMessages(library("ASCAT"))
+suppressPackageStartupMessages(library("GAP"))
+
+'plot_log2_' <- function(x, y, title = "", alpha=NA, psi=NA)
+{
+   	par(mar=c(5, 5, 4, 2)+.1)
+   	data("CytoBand")
+   	end = NULL
+	for (j in 1:23) {
+		end = c(end, max(CytoBand$End[CytoBand$Chromosome==j]))
+	}
+	end = cumsum(end)
+	start = rep(0, 23)
+	start[2:23] = end[1:22]+1
+	for (j in 1:23) {
+		y[y[,"Chromosome"]==j,"Start"] = y[y[,"Chromosome"]==j,"Start"] + start[j]
+		y[y[,"Chromosome"]==j,"End"] = y[y[,"Chromosome"]==j,"End"] + start[j]
+		x[x[,"chrom"]==j,"pos"] = x[x[,"chrom"]==j,"pos"] + start[j]
+	}
+   	
+   	
+   	
+	plot(x[,"pos"], x[,"Log2Ratio"], type="p", pch=".", cex=1.95, col="grey80", axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,5))
+	for (j in 1:nrow(y)) {
+ 		lines(x=c(y[j,"Start"], y[j,"End"]), y=rep(y[j,"Log2Ratio"],2), lty=1, lwd=2.75, col="red")
+ 	}
+  	axis(2, at = c(-4, -2, 0, 2, 4), labels = c(-4, -2, 0, 2, 4), cex.axis = 1, las = 1)
+	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
+	abline(v=1, col="goldenrod3", lty=3, lwd=.5)
+	abline(h=0, col="red", lty=1, lwd=1)
+	for (j in 2:23) {
+		v = start[j]
+		abline(v=v, col="goldenrod3", lty=3, lwd=.5)
+	}
+	abline(v=max(x[,"pos"]), col="goldenrod3", lty=3, lwd=.5)
+	axis(1, at = .5*(start+end), labels=c(1:22, "X"), cex.axis = 0.85, las = 1)
+	
+	rect(xleft=1-1e10, xright=CytoBand[23,"end"]+1e10, ybottom=4, ytop=6, col="lightgrey", border="black", lwd=1.5)
+	title(main = paste0(title, " | alpha = ", signif(alpha, 3), " | psi = ", signif(psi, 3)), line=-1, cex.main=.75, font.main=1)
+    box(lwd=1.5)
+}
 
 if (!interactive()) {
     options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
@@ -15,7 +55,6 @@ args_list <- list(make_option("--type", default = NA, type = 'character', help =
 parser <- OptionParser(usage = "%prog", option_list = args_list)
 arguments <- parse_args(parser, positional_arguments = T)
 opt <- arguments$options
-load("modules/copy_number/CytoBand.RData")
 
 if (opt$type=="total-copy") {
 	
@@ -55,58 +94,13 @@ if (opt$type=="total-copy") {
 	save(CN, tmp, file=paste0("cnvkit/totalcopy/", opt$sample_name, ".RData"))
 	tmp = prunesegments.cn(x=tmp, n=10)
 	CN = winsorize(data=CN[,c("Chromosome","Position","Log2Ratio")], tau=2.5, k=15, verbose=FALSE)
-	end = NULL
-	for (j in 1:23) {
-		end = c(end, max(CytoBand$End[CytoBand$Chromosome==j]))
-	}
-	end = cumsum(end)
-	start = rep(0, 23)
-	start[2:23] = end[1:22]+1
-	for (j in 1:23) {
-		tmp[tmp[,"Chromosome"]==j,"Start"] = tmp[tmp[,"Chromosome"]==j,"Start"] + start[j]
-		tmp[tmp[,"Chromosome"]==j,"End"] = tmp[tmp[,"Chromosome"]==j,"End"] + start[j]
-		CN[CN[,"chrom"]==j,"pos"] = CN[CN[,"chrom"]==j,"pos"] + start[j]
-	}
-	col = "grey80"
-	pdf(file=paste0("cnvkit/segmented/", opt$sample_name, ".pdf"), height=7*10/7*2/3, width=7*20/7)
-	par(mar=c(5, 5, 4, 2)+.1)
-	plot(CN[,"pos"], CN[,"Log2Ratio"], type="p", pch=".", cex=1.75, col=col, axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
- 	for (j in 1:nrow(tmp)) {
- 		lines(x=c(tmp[j,"Start"], tmp[j,"End"]), y=rep(tmp[j,"Log2Ratio"],2), lty=1, lwd=2.75, col="red")
- 	}
- 	axis(2, at = NULL, cex.axis = 1.15, las = 1)
-	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
-	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
-	abline(v=1, col="goldenrod3")
-	abline(h=0, col="red")
-	for (j in 2:23) {
-		v = start[j]
-		abline(v=v, col="goldenrod3")
-	}
-	abline(v=max(CN[,"pos"]), col="goldenrod3")
-	axis(1, at = .5*(start+end), labels=c(1:22, "X"), cex.axis = 0.85, las = 1)
+	pdf(file=paste0("cnvkit/segmented/", opt$sample_name, ".pdf"), width=10, height=4.25)
 	file_names = dir(path="facets/cncf", pattern=opt$sample_name, full.names=TRUE)
 	file_names = file_names[grep(".Rdata", file_names, fixed=TRUE)]
 	load(file_names)
 	alpha = ifelse(is.na(fit$purity), 1, fit$purity)
-	psi = ifelse(is.na(fit$ploidy), 2, fit$ploid)
-	if (round(psi)==1 | round(psi)==2) {
-		cat5 = c(0, 1, 3, 7)
-	} else if (round(psi)==3) {
-		cat5 = c(0, 1, 4, 9)
-	} else if (round(psi)==4) {
-		cat5 = c(0, 1, 5, 10)
-	} else if (round(psi)==5) {
-		cat5 = c(0, 2, 6, 12)
-	} else if (round(psi)>=6) {
-		cat5 = c(0, 2, 7, 15)
-	} else {
-		cat5 = c(0, 1, 3, 7)
-	}
-	cat5 = log2((alpha*cat5 + (1-alpha)*2)/(alpha*psi + (1-alpha)*2))
-	for (j in 1:length(cat5)) {
-		abline(h=cat5[j], lty=3, lwd=.5, col="steelblue")
-	}
+	psi = ifelse(is.na(fit$ploidy), 2, fit$ploidy)
+	plot_log2_(x=CN, y=tmp, title = opt$sample_name, alpha=alpha, psi=psi)
 	dev.off()
 
 } else if (opt$type=="call-cna") {
