6d703bd0c861840b011db6c0acef19227fee5602
David Brown <ndbrown6@gmail.com>
2019-05-15 13:07:15 -0400

    Update pyclone.mk

--- test/clonality/pyclone.mk
+++ test/clonality/pyclone.mk
@@ -3,16 +3,15 @@ include modules/Makefile.inc
 LOGDIR ?= log/setup_pyclone.$(NOW)
 PHONY += pyclone
 
-pyclone : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/timestamp) #$(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/report/report.tsv) $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/report/histogram_ccf.pdf)
+pyclone : $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/config.yaml) #$(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/report/report.tsv) $(foreach pair,$(SAMPLE_PAIRS),pyclone/$(pair)/report/histogram_ccf.pdf)
 
 define make-pyclone
-pyclone/$1_$2/timestamp : summary/tsv/mutation_summary.tsv
+pyclone/$1_$2/config.yaml : summary/tsv/mutation_summary.tsv
 	$$(call RUN, -s 16G -m 24G,"mkdir -p pyclone/$1_$2 && \
 								mkdir -p pyclone/$1_$2/report && \
-							    $(RSCRIPT) modules/test/clonality/tsvtopyclone.R --sample_name $1_$2 && \
-							    touch pyclone/$1_$2/timestamp")
+							    $(RSCRIPT) modules/test/clonality/tsvtopyclone.R --sample_name $1_$2")
 							    
-#pyclone/$1_$2/trace/alpha.tsv.bz2 : pyclone/$1_$2/config.yaml
+# pyclone/$1_$2/trace/alpha.tsv.bz2 : pyclone/$1_$2/config.yaml
 #	$$(call RUN,-s 16G -m 24G -w 7200,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
 #							 		   PyClone run_analysis --config_file pyclone/$1_$2/config.yaml --seed 0")
 #							 		  
