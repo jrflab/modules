d53144fe377ef0d696de4f7d4f0a1435e9f99a18
David Brown <ndbrown6@gmail.com>
2019-05-16 00:50:36 -0400

    Update ascat.mk

--- copy_number/ascat.mk
+++ copy_number/ascat.mk
@@ -59,9 +59,9 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 
 define ascat-plot-chr
 ascat/bychr/$1_$2/chromosome_23.pdf : facets/cncf/$1_$2.Rdata ascat/ascat/$1_$2.pdf
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 6G -m 12G,"mkdir -p ascat/bychr/ && \
-												 mkdir -p ascat/bychr/$1_$2 && \
-												 $(RSCRIPT) modules/copy_number/ascat.R --type plot-chr --file_in $$< --file_out ascat/bychr/$1_$2")
+	$$(call RUN, -v $(ASCAT_ENV) -s 6G -m 12G,"mkdir -p ascat/bychr/ && \
+											   mkdir -p ascat/bychr/$1_$2 && \
+											   $(RSCRIPT) modules/copy_number/ascat.R --type plot-chr --file_in $$< --file_out ascat/bychr/$1_$2")
 		
 endef
 
