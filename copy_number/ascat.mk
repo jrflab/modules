e62319b67cb988ebecc7d6c82bd8eceaad6e6f44
David Brown <ndbrown6@gmail.com>
2019-05-15 20:47:26 -0400

    Update ascat.mk

--- copy_number/ascat.mk
+++ copy_number/ascat.mk
@@ -59,8 +59,8 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 
 define ascat-plot-chr
 ascat/bychr/$1_$2/timestamp : facets/cncf/$1_$2.Rdata ascat/ascat/$1_$2.pdf
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 6G -m 12G,"mkdir -p ascat && \
-												 mkdir -p ascat/$1_$2 && \
+	$$(call RUN,-c -v $(ASCAT_ENV) -s 6G -m 12G,"mkdir -p ascat/bychr/ && \
+												 mkdir -p ascat/bychr/$1_$2 && \
 												 $(RSCRIPT) modules/copy_number/ascat.R --type plot-chr --file_in $$< --file_out ascat/bychr/$1_$2/ && \
 												 touch ascat/bychr/$1_$2/timestamp")
 		
