c6e22bd6a81042c6a0a00490785226d9b29f8e78
David Brown <ndbrown6@gmail.com>
2019-05-24 18:28:05 -0400

    Update cnvkitfix.mk

--- test/copy_number/cnvkitfix.mk
+++ test/copy_number/cnvkitfix.mk
@@ -13,7 +13,7 @@ cnvkit/cnr/%.A.cnr : cnvkit/cnn/tumor/%.A.targetcoverage.cnn cnvkit/cnn/tumor/%.
 cnvkit/cnr/%.B.cnr : cnvkit/cnn/tumor/%.B.targetcoverage.cnn cnvkit/cnn/tumor/%.B.antitargetcoverage.cnn cnvkit/reference/on_target_pool_B.cnr
 	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) cnvkit/reference/on_target_pool_B.cnr -o cnvkit/cnr/$$(*).B.cnr")
 	
-cnvkit/cnr/%.C.cnr : cnvkit/cnn/tumor/%.B.targetcoverage.cnn cnvkit/cnn/tumor/%.B.antitargetcoverage.cnn cnvkit/reference/off_target_no_pool.cnr
+cnvkit/cnr/%.C.cnr : cnvkit/cnn/tumor/%.C.targetcoverage.cnn cnvkit/cnn/tumor/%.C.antitargetcoverage.cnn cnvkit/reference/off_target_no_pool.cnr
 	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) cnvkit/reference/off_target_no_pool.cnr -o cnvkit/cnr/$$(*).C.cnr")
 	
 endef
