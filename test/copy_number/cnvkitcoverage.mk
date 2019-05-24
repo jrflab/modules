ee824b9db9754db86a5e20be4b99246ecd61aa3f
David Brown <ndbrown6@gmail.com>
2019-05-24 18:05:48 -0400

    Update cnvkitcoverage.mk

--- test/copy_number/cnvkitcoverage.mk
+++ test/copy_number/cnvkitcoverage.mk
@@ -4,7 +4,7 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/cnvkit_coverage.$(NOW)
 PHONY += cnvkit cnvkit/cnn cnvkit/cnn/tumor cnvkit/cnn/normal
 
-cnvkit_coverage : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnn/tumor/$(sample).A.targetcoverage.cnn cnvkit/cnn/tumor/$(sample).B.targetcoverage.cnn cnvkit/cnn/tumor/$(sample).antitargetcoverage.cnn) $(foreach sample,$(NORMAL_SAMPLES),cnvkit/cnn/normal/$(sample).A.targetcoverage.cnn cnvkit/cnn/normal/$(sample).B.targetcoverage.cnn cnvkit/cnn/normal/$(sample).antitargetcoverage.cnn)
+cnvkit_coverage : $(foreach sample,$(TUMOR_SAMPLES),cnvkit/cnn/tumor/$(sample).A.targetcoverage.cnn cnvkit/cnn/tumor/$(sample).B.targetcoverage.cnn cnvkit/cnn/tumor/$(sample).C.targetcoverage.cnn) $(foreach sample,$(NORMAL_SAMPLES),cnvkit/cnn/normal/$(sample).A.targetcoverage.cnn cnvkit/cnn/normal/$(sample).B.targetcoverage.cnn cnvkit/cnn/normal/$(sample).C.targetcoverage.cnn)
 
 define cnvkit-tumor-cnn
 cnvkit/cnn/tumor/%.A.targetcoverage.cnn : bam/%.bam
@@ -15,9 +15,9 @@ cnvkit/cnn/tumor/%.B.targetcoverage.cnn : bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_B) -o cnvkit/cnn/tumor/$$(*).B.targetcoverage.cnn && \
 									 touch cnvkit/cnn/tumor/$$(*).B.antitargetcoverage.cnn")
 
-cnvkit/cnn/tumor/%.antitargetcoverage.cnn : bam/%.bam
-	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o cnvkit/cnn/tumor/$$(*).antitargetcoverage.cnn && \
-									 touch cnvkit/cnn/tumor/$$(*).targetcoverage.cnn")
+cnvkit/cnn/tumor/%.C.antitargetcoverage.cnn : bam/%.bam
+	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o cnvkit/cnn/tumor/$$(*).C.antitargetcoverage.cnn && \
+									 touch cnvkit/cnn/tumor/$$(*).C.targetcoverage.cnn")
 endef
  $(foreach sample,$(TUMOR_SAMPLES),\
 		$(eval $(call cnvkit-tumor-cnn,$(sample))))
@@ -31,9 +31,9 @@ cnvkit/cnn/normal/%.B.targetcoverage.cnn : bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_B) -o cnvkit/cnn/normal/$$(*).B.targetcoverage.cnn && \
 									 touch cnvkit/cnn/normal/$$(*).B.antitargetcoverage.cnn")
 
-cnvkit/cnn/normal/%.antitargetcoverage.cnn : bam/%.bam
-	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o cnvkit/cnn/normal/$$(*).antitargetcoverage.cnn && \
-									 touch cnvkit/cnn/normal/$$(*).targetcoverage.cnn")
+cnvkit/cnn/normal/%.C.antitargetcoverage.cnn : bam/%.bam
+	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o cnvkit/cnn/normal/$$(*).C.antitargetcoverage.cnn && \
+									 touch cnvkit/cnn/normal/$$(*).C.targetcoverage.cnn")
 endef
  $(foreach sample,$(NORMAL_SAMPLES),\
 		$(eval $(call cnvkit-normal-cnn,$(sample))))
