3251a5b5ee3a6cfbca9bd7e3810f848caa59b1f9
David Brown <ndbrown6@gmail.com>
2019-05-24 22:42:17 -0400

    msk-access

--- test/copy_number/cnvaccess.R
+++ test/copy_number/cnvaccess.R
@@ -19,9 +19,13 @@ opt = arguments$options
 file_a_pool = paste0("cnvaccess/cnr/", opt$sample_name, ".A.cnr")
 file_b_pool = paste0("cnvaccess/cnr/", opt$sample_name, ".B.cnr")
 file_c_pool = paste0("cnvaccess/cnr/", opt$sample_name, ".C.cnr")
-outfile_a = paste0("cnvaccess/log2/", opt$sample_name, ".A.pdf")
-outfile_b = paste0("cnvaccess/log2/", opt$sample_name, ".B.cnr")
-outfile_c = paste0("cnvaccess/log2/", opt$sample_name, ".C.cnr")
+
+outfile_a = paste0("cnvaccess/log2/", opt$sample_name, ".onA.pdf")
+outfile_b = paste0("cnvaccess/log2/", opt$sample_name, ".onB.pdf")
+outfile_c = paste0("cnvaccess/log2/", opt$sample_name, ".offAB.pdf")
+outfile_ab = paste0("cnvaccess/log2/", opt$sample_name, ".onAB.pdf")
+outfile_f = paste0("cnvaccess/log2/", opt$sample_name, ".pdf")
+
 if (as.numeric(opt$type)==1) {
 	data = read.table(file=file_a_pool, header=TRUE, sep="\t", stringsAsFactors=FALSE)
 	data[data[,"chromosome"]=="X", "chromosome"] = 23
@@ -52,5 +56,154 @@ if (as.numeric(opt$type)==1) {
 	axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
 	box(lwd=1.5)
 	dev.off()
+	
+	data = read.table(file=file_b_pool, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	data[data[,"chromosome"]=="X", "chromosome"] = 23
+	data[data[,"chromosome"]=="Y", "chromosome"] = 24
+	data[,"chromosome"] = as.numeric(data[,"chromosome"])
+	data = subset(data, data[,"chromosome"]<23)
+	pdf(file=outfile_b, width=10, height=4.2)
+	par(mar=c(5, 5, 4, 2)+.1)
+	plot(data[,"log2"], type="p", pch=19, cex=.25, col="grey80", axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
+	axis(2, at = NULL, cex.axis = 1.15, las = 1)
+	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
+	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
+	abline(v=1, col="goldenrod3")
+	abline(h=0, col="red")
+	for (j in 2:max(data[,"chromosome"])) {
+		v = min(which(data[,"chromosome"]==j))
+		abline(v=v, col="goldenrod3")
+	}
+	abline(v=max(nrow(data)), col="goldenrod3")
+	start = NULL
+	end = NULL
+	for (j in 1:max(data[,"chromosome"])) {
+		start[j] = min(which(data[,"chromosome"]==j))
+		end[j] = max(which(data[,"chromosome"]==j))
+	}
+	labels = 1:max(data[,"chromosome"])
+	labels[labels==23] = "X"
+	axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
+	box(lwd=1.5)
+	dev.off()
+	
+	data = read.table(file=file_c_pool, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	data[data[,"chromosome"]=="X", "chromosome"] = 23
+	data[data[,"chromosome"]=="Y", "chromosome"] = 24
+	data[,"chromosome"] = as.numeric(data[,"chromosome"])
+	data = subset(data, data[,"chromosome"]<23)
+	pdf(file=outfile_c, width=10, height=4.2)
+	par(mar=c(5, 5, 4, 2)+.1)
+	plot(data[,"log2"], type="p", pch=19, cex=.25, col="grey80", axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
+	axis(2, at = NULL, cex.axis = 1.15, las = 1)
+	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
+	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
+	abline(v=1, col="goldenrod3")
+	abline(h=0, col="red")
+	for (j in 2:max(data[,"chromosome"])) {
+		v = min(which(data[,"chromosome"]==j))
+		abline(v=v, col="goldenrod3")
+	}
+	abline(v=max(nrow(data)), col="goldenrod3")
+	start = NULL
+	end = NULL
+	for (j in 1:max(data[,"chromosome"])) {
+		start[j] = min(which(data[,"chromosome"]==j))
+		end[j] = max(which(data[,"chromosome"]==j))
+	}
+	labels = 1:max(data[,"chromosome"])
+	labels[labels==23] = "X"
+	axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
+	box(lwd=1.5)
+	dev.off()
+	
+	dataA = read.table(file=file_a_pool, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	dataA[dataA[,"chromosome"]=="X", "chromosome"] = 23
+	dataA[dataA[,"chromosome"]=="Y", "chromosome"] = 24
+	dataA[,"chromosome"] = as.numeric(dataA[,"chromosome"])
+	dataA = subset(dataA, dataA[,"chromosome"]<23)
+	dataB = read.table(file=file_b_pool, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	dataB[dataB[,"chromosome"]=="X", "chromosome"] = 23
+	dataB[dataB[,"chromosome"]=="Y", "chromosome"] = 24
+	dataB[,"chromosome"] = as.numeric(dataB[,"chromosome"])
+	dataB = subset(dataB, dataB[,"chromosome"]<23)
+	
+	data = rbind(dataA, dataB)
+	index = order(data[,2])
+	data = data[index,,drop=FALSE]
+	index = order(data[,1])
+	data = data[index,,drop=FALSE]
+	pdf(file=outfile_ab, width=10, height=4.2)
+	par(mar=c(5, 5, 4, 2)+.1)
+	plot(data[,"log2"], type="p", pch=19, cex=.25, col="grey80", axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
+	axis(2, at = NULL, cex.axis = 1.15, las = 1)
+	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
+	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
+	abline(v=1, col="goldenrod3")
+	abline(h=0, col="red")
+	for (j in 2:max(data[,"chromosome"])) {
+		v = min(which(data[,"chromosome"]==j))
+		abline(v=v, col="goldenrod3")
+	}
+	abline(v=max(nrow(data)), col="goldenrod3")
+	start = NULL
+	end = NULL
+	for (j in 1:max(data[,"chromosome"])) {
+		start[j] = min(which(data[,"chromosome"]==j))
+		end[j] = max(which(data[,"chromosome"]==j))
+	}
+	labels = 1:max(data[,"chromosome"])
+	labels[labels==23] = "X"
+	axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
+	box(lwd=1.5)
+	dev.off()
+	
+	dataA = read.table(file=file_a_pool, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	dataA[dataA[,"chromosome"]=="X", "chromosome"] = 23
+	dataA[dataA[,"chromosome"]=="Y", "chromosome"] = 24
+	dataA[,"chromosome"] = as.numeric(dataA[,"chromosome"])
+	dataA = subset(dataA, dataA[,"chromosome"]<23)
+	dataB = read.table(file=file_b_pool, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	dataB[dataB[,"chromosome"]=="X", "chromosome"] = 23
+	dataB[dataB[,"chromosome"]=="Y", "chromosome"] = 24
+	dataB[,"chromosome"] = as.numeric(dataB[,"chromosome"])
+	dataB = subset(dataB, dataB[,"chromosome"]<23)
+	dataC = read.table(file=file_c_pool, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+	dataC[dataC[,"chromosome"]=="X", "chromosome"] = 23
+	dataC[dataC[,"chromosome"]=="Y", "chromosome"] = 24
+	dataC[,"chromosome"] = as.numeric(dataC[,"chromosome"])
+	dataC = subset(dataC, dataC[,"chromosome"]<23)
+	
+	data = rbind(dataA, dataB, dataC)
+	index = order(data[,2])
+	data = data[index,,drop=FALSE]
+	index = order(data[,1])
+	data = data[index,,drop=FALSE]
+	pdf(file=outfile_f, width=10, height=4.2)
+	par(mar=c(5, 5, 4, 2)+.1)
+	plot(data[,"log2"], type="p", pch=19, cex=.25, col="grey80", axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
+	axis(2, at = NULL, cex.axis = 1.15, las = 1)
+	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
+	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
+	abline(v=1, col="goldenrod3")
+	abline(h=0, col="red")
+	for (j in 2:max(data[,"chromosome"])) {
+		v = min(which(data[,"chromosome"]==j))
+		abline(v=v, col="goldenrod3")
+	}
+	abline(v=max(nrow(data)), col="goldenrod3")
+	start = NULL
+	end = NULL
+	for (j in 1:max(data[,"chromosome"])) {
+		start[j] = min(which(data[,"chromosome"]==j))
+		end[j] = max(which(data[,"chromosome"]==j))
+	}
+	labels = 1:max(data[,"chromosome"])
+	labels[labels==23] = "X"
+	axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
+	box(lwd=1.5)
+	dev.off()
+	
+	
 }
 
--- test/copy_number/cnvaccessplot.mk
+++ test/copy_number/cnvaccessplot.mk
@@ -4,19 +4,17 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/cnvaccess_plot.$(NOW)
 PHONY += cnvkit cnvaccess/plot
 
-cnvaccess_plot : $(foreach sample,$(TUMOR_SAMPLES),cnvaccess/log2/$(sample).A.ontarget.pdf) # \
-#				cnvaccess/log2/$(sample).B.ontarget.pdf \
-#				cnvaccess/log2/$(sample).AB.ontarget.pdf \
-#				cnvaccess/log2/$(sample).offtarget.pdf \
-#				cnvaccess/log2/$(sample).pdf)
+cnvaccess_plot : $(foreach sample,$(TUMOR_SAMPLES),cnvaccess/log2/$(sample).onA.pdf \
+				cnvaccess/log2/$(sample).onB.pdf \
+				cnvaccess/log2/$(sample).onAB.pdf \
+				cnvaccess/log2/$(sample).offAB.pdf \
+				cnvaccess/log2/$(sample).pdf)
 
 define cnvaccess-plot
-cnvaccess/log2/%.A.ontarget.pdf : cnvaccess/cnr/%.A.cnr cnvaccess/cnr/%.B.cnr cnvaccess/cnr/%.C.cnr
+cnvaccess/log2/%.onA.pdf cnvaccess/log2/%.onB.pdf cnvaccess/log2/%.onAB.pdf cnvaccess/log2/%.offAB.pdf cnvaccess/log2/%.pdf : cnvaccess/cnr/%.A.cnr cnvaccess/cnr/%.B.cnr cnvaccess/cnr/%.C.cnr
 	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 10G -m 12G,"$(RSCRIPT) modules/test/copy_number/cnvaccess.R --type 1 --sample_name $$(*)")
 endef
  $(foreach sample,$(TUMOR_SAMPLES),\
 		$(eval $(call cnvaccess-plot,$(sample))))
 				
 .PHONY: $(PHONY)
-
-# cnvaccess/log2/%.B.ontarget.pdf cnvaccess/log2/%.AB.ontarget.pdf cnvaccess/log2/%.offtarget.pdf cnvaccess/log2/%.pdf
