565baa9811940e3f14fe23b44218d1052e209469
David Brown <ndbrown6@gmail.com>
2019-05-25 13:27:38 -0400

    cnv_access

--- test/copy_number/cnvaccessplot.mk
+++ test/copy_number/cnvaccessplot.mk
@@ -2,16 +2,12 @@ include modules/Makefile.inc
 include modules/genome_inc/b37.inc
 
 LOGDIR ?= log/cnvaccess_plot.$(NOW)
-PHONY += cnvkit cnvaccess/plot
+PHONY += cnvkit cnvaccess/plot cnvaccess/plot/log2
 
-cnvaccess_plot : $(foreach sample,$(TUMOR_SAMPLES),cnvaccess/log2/$(sample).onA.pdf \
-				cnvaccess/log2/$(sample).onB.pdf \
-				cnvaccess/log2/$(sample).onAB.pdf \
-				cnvaccess/log2/$(sample).offAB.pdf \
-				cnvaccess/log2/$(sample).pdf)
+cnvaccess_plot : $(foreach sample,$(TUMOR_SAMPLES),cnvaccess/plot/log2/$(sample).pdf)
 
 define cnvaccess-plot
-cnvaccess/log2/%.onA.pdf cnvaccess/log2/%.onB.pdf cnvaccess/log2/%.onAB.pdf cnvaccess/log2/%.offAB.pdf cnvaccess/log2/%.pdf : cnvaccess/cnr/%.A.cnr cnvaccess/cnr/%.B.cnr cnvaccess/cnr/%.C.cnr
+cnvaccess/plot/log2/%.pdf : cnvaccess/cnr/%.A.cnr cnvaccess/cnr/%.B.cnr cnvaccess/cnr/%.C.cnr
 	$$(call RUN,-c -v ~/share/usr/anaconda-envs/ascat -s 10G -m 12G,"$(RSCRIPT) modules/test/copy_number/cnvaccess.R --type 1 --sample_name $$(*)")
 endef
  $(foreach sample,$(TUMOR_SAMPLES),\
