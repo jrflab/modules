2548e9712b1b0a1b7b3f1f09e2bbc238a1e51986
David Brown <ndbrown6@gmail.com>
2019-05-25 13:33:54 -0400

    Update cnvaccess.R

--- test/copy_number/cnvaccess.R
+++ test/copy_number/cnvaccess.R
@@ -4,6 +4,7 @@ suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("copynumber"))
 suppressPackageStartupMessages(library("colorspace"))
 suppressPackageStartupMessages(library("ASCAT"))
+suppressPackageStartupMessages(library("GAP"))
 
 if (!interactive()) {
     options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
@@ -15,6 +16,35 @@ args_list = list(make_option("--type", default = NA, type = 'character', help = 
 parser = OptionParser(usage = "%prog", option_list = args_list)
 arguments = parse_args(parser, positional_arguments = T)
 opt = arguments$options
+
+'plot_log2_' <- function(x, title = "")
+{
+   	par(mar=c(5, 5, 4, 2)+.1)
+   	data("CytoBand")
+   	end = NULL
+   	for (i in 1:23) {
+   		end = c(end, max(CytoBand[CytoBand[,1]==i,"End"]))
+   	}
+   	end = cumsum(end)
+   	start = c(1, end[1:22]+1)
+   	CytoBand = cbind(start, end)
+   	index = NULL
+   	for (i in 1:23) {
+   		index = c(index, seq(from = CytoBand[i, "start"], to=CytoBand[i, "end"], length=sum(x$chromosome==i)))
+   	}
+	plot(index, x$log2, type="p", pch=".", cex=1.95, col="grey80", axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,5))
+  	axis(2, at = c(-4, -2, 0, 2, 4), labels = c(-4, -2, 0, 2, 4), cex.axis = 1, las = 1)
+	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
+	abline(v=1, col="goldenrod3", lty=3, lwd=.5)
+	abline(h=0, col="red", lty=1, lwd=1)
+	for (j in 1:23) {
+		abline(v=CytoBand[j,"end"], col="goldenrod3", lty=3, lwd=.5)
+	}
+	axis(1, at = .5*(CytoBand[,"start"]+CytoBand[,"end"]), labels=c(1:22, "X"), cex.axis = 0.85, las = 1)
+	rect(xleft=1-1e10, xright=CytoBand[23,"end"]+1e10, ybottom=4, ytop=6, col="lightgrey", border="black", lwd=1.5)
+	title(main = title, line=-1, cex.main=.75, font.main=1)
+    box(lwd=1.5)
+}
 
 if (as.numeric(opt$type)==1) {
 	
@@ -43,28 +73,7 @@ if (as.numeric(opt$type)==1) {
 	data = data[index,,drop=FALSE]
 	
 	pdf(file = paste0("cnvaccess/plot/log2/", opt$sample_name, ".pdf"), width=10, height=4.2)
-	par(mar=c(5, 5, 4, 2)+.1)
-	plot(data[,"log2"], type="p", pch=19, cex=.25, col="grey80", axes=FALSE, frame=TRUE, xlab="", ylab="", main="", ylim=c(-4,4))
-	axis(2, at = NULL, cex.axis = 1.15, las = 1)
-	mtext(side = 1, text = "Chromosome", line = 3, cex = 1.25)
-	mtext(side = 2, text = expression(Log[2]~"Ratio"), line = 3.15, cex = 1.25)
-	abline(v=1, col="goldenrod3")
-	abline(h=0, col="red")
-	for (j in 2:max(data[,"chromosome"])) {
-		v = min(which(data[,"chromosome"]==j))
-		abline(v=v, col="goldenrod3")
-	}
-	abline(v=max(nrow(data)), col="goldenrod3")
-	start = NULL
-	end = NULL
-	for (j in 1:max(data[,"chromosome"])) {
-		start[j] = min(which(data[,"chromosome"]==j))
-		end[j] = max(which(data[,"chromosome"]==j))
-	}
-	labels = 1:max(data[,"chromosome"])
-	labels[labels==23] = "X"
-	axis(1, at = .5*(start+end), labels=labels, cex.axis = 0.85, las = 1)
-	box(lwd=1.5)
+	plot_log2_(x=data, title=opt$sample_name)
 	dev.off()
 	
 }
