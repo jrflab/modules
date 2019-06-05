b65cc0d5f88086de77bf6df2375b11f92e9bfef6
Brown <brownd7@lski2464.mskcc.org>
2019-06-04 23:18:03 -0400

    Update medicc.mk

--- test/phylogeny/medicc.mk
+++ test/phylogeny/medicc.mk
@@ -3,7 +3,7 @@ include modules/Makefile.inc
 LOGDIR ?= log/medicc.$(NOW)
 PHONY += medicc medicc/mad medicc/aspcf medicc/mpcf medicc/medicc medicc/medicc/allele_specific  medicc/medicc/total_copy medicc/boot medicc/plots
 
-medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/aspcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/allele_specific/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/total_copy/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/allele_specific/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/medicc/total_copy/$(set)/tree_final.new) #$(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/init.timestamp) $(foreach set,$(SAMPLE_SETS),medicc/plots/$(set).pdf)
+medicc : $(foreach set,$(SAMPLE_SETS),medicc/mad/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/aspcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/mpcf/$(set).RData) $(foreach set,$(SAMPLE_SETS),medicc/medicc/allele_specific/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/total_copy/$(set)/desc.txt) $(foreach set,$(SAMPLE_SETS),medicc/medicc/allele_specific/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/medicc/total_copy/$(set)/tree_final.new) $(foreach set,$(SAMPLE_SETS),medicc/plots/$(set)_allele_specific.pdf) $(foreach set,$(SAMPLE_SETS),medicc/plots/$(set)_total_copy.pdf) #$(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)) $(foreach set,$(SAMPLE_SETS),medicc/boot/$(set)/init.timestamp)
 
 define combine-samples
 medicc/mad/%.RData : $(wildcard $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).Rdata))
