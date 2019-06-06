f9298eacab6bbb0d90308aef04d30ad4960f5900
Brown <brownd7@lski2464.mskcc.org>
2019-06-05 23:09:02 -0400

    Update plotmedicc.R

--- test/phylogeny/plotmedicc.R
+++ test/phylogeny/plotmedicc.R
@@ -21,18 +21,28 @@ parser <- OptionParser(usage = "%prog", option_list = args_list)
 arguments <- parse_args(parser, positional_arguments = T)
 opt <- arguments$options
 
-allele_specific_tree = read.tree(file=paste0("medicc/medicc/allele_specific/", opt$sample_set, "/tree_final.new"))
-allele_specific_tree = root(allele_specific_tree, outgroup="diploid")
+phylo_tree = read.tree(file=paste0("medicc/medicc/allele_specific/", opt$sample_set, "/tree_final.new"))
+tip_labels = phylo_tree$tip.label
+index = grep("pad00", tip_labels)
+if (length(index)!=0) {
+	phylo_tree = drop.tip(phy=phylo_tree, tip=tip_labels[index], trim.internal=TRUE, rooted=FALSE)
+}
+phylo_tree = root(phylo_tree, outgroup="diploid")
 
 pdf(file=paste0("medicc/plots/", opt$sample_set, "_allele_specific.pdf"), height=7, width=7)
-plotTree(tree=allele_specific_tree, color="#8CC63F", lwd=3, offset=1)
-edgelabels(text=paste0(allele_specific_tree$edge.length, " "), cex=.75)
+plotTree(tree=phylo_tree, color="#8CC63F", lwd=3, offset=1)
+edgelabels(text=paste0(phylo_tree$edge.length, " "), cex=.75)
 dev.off()
 
-total_copy_tree = read.tree(file=paste0("medicc/medicc/total_copy/", opt$sample_set, "/tree_final.new"))
-total_copy_tree = root(total_copy_tree, outgroup="diploid")
+phylo_tree = read.tree(file=paste0("medicc/medicc/total_copy/", opt$sample_set, "/tree_final.new"))
+tip_labels = phylo_tree$tip.label
+index = grep("pad00", tip_labels)
+if (length(index)!=0) {
+	phylo_tree = drop.tip(phy=phylo_tree, tip=tip_labels[index], trim.internal=TRUE, rooted=FALSE)
+}
+phylo_tree = root(phylo_tree, outgroup="diploid")
 
 pdf(file=paste0("medicc/plots/", opt$sample_set, "_total_copy.pdf"), height=7, width=7)
-plotTree(tree=total_copy_tree, color="#8CC63F", lwd=3, offset=1)
-edgelabels(text=paste0(total_copy_tree$edge.length, " "), cex=.75)
+plotTree(tree=phylo_tree, color="#8CC63F", lwd=3, offset=1)
+edgelabels(text=paste0(phylo_tree$edge.length, " "), cex=.75)
 dev.off()
