7797f5bfff738ff82ad6dff71108e7f5bea61e25
Brown <brownd7@lski2464.mskcc.org>
2019-06-05 20:52:07 -0400

    Update fetchimpact.mk

--- test/workflows/fetchimpact.mk
+++ test/workflows/fetchimpact.mk
@@ -7,7 +7,7 @@ fetch_impact : $(foreach sample,$(SAMPLES),unprocessed_bam/$(sample).bam)
 
 define fetch-impact
 unprocessed_bam/%.bam :
-	$$(call RUN,-c -s 4G -m 12G,"scp luna.mskcc.org:/ifs/dmp/dmpshare/share/irb12_245/$$(*).bam unprocessed_bam/$$(*).bam")
+	$$(call RUN,-c -s 4G -m 12G,"scp luna.mskcc.org:/ifs/dmpshare/share/irb12_245/$$(*).bam unprocessed_bam/$$(*).bam")
 	
 endef
  $(foreach sample,$(SAMPLES),\
