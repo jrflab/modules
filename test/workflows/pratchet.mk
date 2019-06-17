d45efacc5f543020800afbf47382231d978f2816
Brown <brownd7@lski2464.mskcc.org>
2019-06-16 21:51:14 -0400

    Update pratchet.mk

--- test/workflows/pratchet.mk
+++ test/workflows/pratchet.mk
@@ -3,12 +3,12 @@ include modules/Makefile.inc
 LOGDIR ?= log/pratchet.$(NOW)
 PHONY += pratchet
 
-pratchet : $(foreach set,$(SAMPLE_SETS),pratchet/$(set)/tree_final.RData)
+pratchet : $(foreach set,$(SAMPLE_SETS),pratchet/$(set)/tree_final.RData) $(foreach set,$(SAMPLE_SETS),pratchet/$(set)/tree_final.pdf)
 
 define parsimony-ratchet
 pratchet/%/tree_final.RData : sufam/%.tsv
 	$$(call RUN,-c -s 8G -m 12G -v $(PHANGORN_ENV),"mkdir -p pratchet && \
-								 					mkdir -p pratchet/% && \
+								 					mkdir -p pratchet/$$* && \
 								 					$(RSCRIPT) modules/test/phylogeny/pratchet.R --sample_set $$* --normal_samples '$(NORMAL_SAMPLES)'")
 
 pratchet/%/tree_final.pdf : pratchet/%/tree_final.RData
