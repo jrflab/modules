29390fb9bb8dc04751e6619d220223932cc62865
Brown <brownd7@lski2464.mskcc.org>
2019-06-17 20:52:22 -0400

    Update plotpyclone.mk

--- clonality/plotpyclone.mk
+++ clonality/plotpyclone.mk
@@ -7,7 +7,7 @@ PHONY += pyclone
 plot_pyclone : $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/report/pyclone.pdf)
 
 define plot-pyclone
-pyclone/%/report/ : pyclone/%/report/pyclone.tsv
+pyclone/%/report/pyclone.pdf : pyclone/%/report/pyclone.tsv
 	$$(call RUN,-s 4G -m 6G -w 7200,"$(RSCRIPT) modules/clonality/plotpyclone.R --sample_set $$(*) --normal_samples $(NORMAL_SAMPLES) --min_depth $(MIN_DEPTH)")
 
 endef
