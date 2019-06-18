08e52b0bfa3e3b162a1ae7c1bb60992863871b93
Brown <brownd7@lski2464.mskcc.org>
2019-06-17 20:03:41 -0400

    Update runpyclone.mk

--- clonality/runpyclone.mk
+++ clonality/runpyclone.mk
@@ -10,9 +10,10 @@ pyclone/%/trace/alpha.tsv.bz2 : pyclone/%/config.yaml
 	$$(call RUN,-s 4G -m 6G -w 7200,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
 							 		 PyClone run_analysis --config_file pyclone/$$*/config.yaml --seed 0")
 
-pyclone/%/pyclone.tsv : pyclone/%/trace/alpha.tsv.bz2
-	$$(call RUN,-s 4G -m 6G -w 7200,"source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
-							 		 PyClone build_table --config_file pyclone/$$*/config.yaml --out_file pyclone/$$*/pyclone.tsv --max_cluster 10 --table_type old_style --burnin 5000")
+pyclone/%/report/pyclone.tsv : pyclone/%/trace/alpha.tsv.bz2
+	$$(call RUN,-s 4G -m 6G -w 7200,"make -p pyclone/$$*/report && \
+									 source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate /home/${USER}/share/usr/anaconda-envs/PyClone-0.13.1 && \
+							 		 PyClone build_table --config_file pyclone/$$*/config.yaml --out_file pyclone/$$*/report/pyclone.tsv --max_cluster 10 --table_type old_style --burnin 5000")
 							 
 endef
 $(foreach set,$(SAMPLE_SETS),\
