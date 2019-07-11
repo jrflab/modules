60cfae4ca81fd973a9f97a58f76f2bee6e37fe3d
David Brown <ndbrown6@users.noreply.github.com>
2019-07-10 23:24:32 -0400

    Update clipumi.mk

--- test/bam_tools/clipumi.mk
+++ test/bam_tools/clipumi.mk
@@ -4,7 +4,8 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/clip_umi.$(NOW)
 PHONY += marianas
 
-clip_umi : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R1.fastq.gz $(SAMPLES),marianas/$(sample)/$(sample)_R2.fastq.gz)
+clip_umi : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R1.fastq.gz) \
+		   $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R2.fastq.gz)
 
 JAVA = /home/${USER}/share/usr/jdk1.8.0_74/bin/java
 PICARD = /home/${USER}/share/usr/picard/bin/picard.jar
