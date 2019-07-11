bf7dc630415e1f76b740b4a48aa0283d52b12360
David Brown <ndbrown6@users.noreply.github.com>
2019-07-11 19:50:15 -0400

    Update clipumi.mk

--- test/fastq_tools/clipumi.mk
+++ test/fastq_tools/clipumi.mk
@@ -15,7 +15,7 @@ define clip-umi-fastq
 marianas/$1/$1_R1.fastq.gz marianas/$1/$1_R2.fastq.gz : $3
 	$$(call RUN,-c -n 1 -s 8G -m 16G,"set -o pipefail && \
 									  mkdir -p marianas/$1 && \
-									  a=($(echo $3 | tr ',' \"\n\"))")
+									  a=($$(echo $3 | tr ',' \"\n\"))")
 endef
 $(foreach ss,$(SPLIT_SAMPLES),\
 	$(if $(fq.$(ss)),$(eval $(call clip-umi-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
