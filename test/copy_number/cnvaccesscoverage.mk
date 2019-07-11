00c83ac729ab070d6e82e55e56aaf887f0806b0b
David Brown <ndbrown6@users.noreply.github.com>
2019-07-10 22:47:15 -0400

    CNVkit

--- test/copy_number/cnvaccesscoverage.mk
+++ test/copy_number/cnvaccesscoverage.mk
@@ -21,15 +21,15 @@ cnvaccess/bam/%.bam : fgbio/%.regrouped.bam
 									   samtools index cnvaccess/bam/$$(*).bam && \
 									   cp cnvaccess/bam/$$(*).bam.bai cnvaccess/bam/$$(*).bai")
 
-cnvaccess/cnn/tumor/%.pool-A.targetcoverage.cnn : cnvaccess/bam/%.bam
+cnvaccess/cnn/tumor/%.pool-A.targetcoverage.cnn cnvaccess/cnn/tumor/%.pool-A.antitargetcoverage.cnn : cnvaccess/bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_A) -o cnvaccess/cnn/tumor/$$(*).pool-A.targetcoverage.cnn && \
 									 touch cnvaccess/cnn/tumor/$$(*).pool-A.antitargetcoverage.cnn")
 	
-cnvaccess/cnn/tumor/%.pool-B.targetcoverage.cnn : cnvaccess/bam/%.bam
+cnvaccess/cnn/tumor/%.pool-B.targetcoverage.cnn cnvaccess/cnn/tumor/%.pool-B.antitargetcoverage.cnn : cnvaccess/bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_B) -o cnvaccess/cnn/tumor/$$(*).pool-B.targetcoverage.cnn && \
 									 touch cnvaccess/cnn/tumor/$$(*).pool-B.antitargetcoverage.cnn")
 
-cnvaccess/cnn/tumor/%.no-pool.antitargetcoverage.cnn : cnvaccess/bam/%.bam
+cnvaccess/cnn/tumor/%.no-pool.antitargetcoverage.cnn cnvaccess/cnn/tumor/%.no-pool.targetcoverage.cnn : cnvaccess/bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o cnvaccess/cnn/tumor/$$(*).no-pool.antitargetcoverage.cnn && \
 									 touch cnvaccess/cnn/tumor/$$(*).no-pool.targetcoverage.cnn")
 endef
@@ -45,15 +45,15 @@ cnvaccess/bam/%.bam : fgbio/%.regrouped.bam
 									   samtools index cnvaccess/bam/$$(*).bam && \
 									   cp cnvaccess/bam/$$(*).bam.bai cnvaccess/bam/$$(*).bai")
 									   
-cnvaccess/cnn/normal/%.pool-A.targetcoverage.cnn : cnvaccess/bam/%.bam
+cnvaccess/cnn/normal/%.pool-A.targetcoverage.cnn cnvaccess/cnn/normal/%.pool-A.antitargetcoverage.cnn : cnvaccess/bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_A) -o cnvaccess/cnn/normal/$$(*).pool-A.targetcoverage.cnn && \
 									 touch cnvaccess/cnn/normal/$$(*).pool-A.antitargetcoverage.cnn")
 	
-cnvaccess/cnn/normal/%.pool-B.targetcoverage.cnn : cnvaccess/bam/%.bam
+cnvaccess/cnn/normal/%.pool-B.targetcoverage.cnn cnvaccess/cnn/normal/%.pool-B.antitargetcoverage.cnn : cnvaccess/bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE_B) -o cnvaccess/cnn/normal/$$(*).pool-B.targetcoverage.cnn && \
 									 touch cnvaccess/cnn/normal/$$(*).pool-B.antitargetcoverage.cnn")
 
-cnvaccess/cnn/normal/%.no-pool.antitargetcoverage.cnn : cnvaccess/bam/%.bam
+cnvaccess/cnn/normal/%.no-pool.antitargetcoverage.cnn cnvaccess/cnn/normal/%.no-pool.targetcoverage.cnn : cnvaccess/bam/%.bam
 	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o cnvaccess/cnn/normal/$$(*).no-pool.antitargetcoverage.cnn && \
 									 touch cnvaccess/cnn/normal/$$(*).no-pool.targetcoverage.cnn")
 endef
