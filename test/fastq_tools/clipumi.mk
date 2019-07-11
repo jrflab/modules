7f000124a60f3fe0ac64c5b54e349388db6e0fc2
David Brown <ndbrown6@users.noreply.github.com>
2019-07-11 19:45:45 -0400

    Update clipumi.mk

--- test/fastq_tools/clipumi.mk
+++ test/fastq_tools/clipumi.mk
@@ -19,7 +19,9 @@ marianas/$1/$1_R1.fastq.gz marianas/$1/$1_R2.fastq.gz : $3
 									  org.mskcc.marianas.umi.duplex.fastqprocessing.ProcessLoopUMIFastq \
 									  $$(^) \
 									  3 && \
-									  mv (echo $3 | sed s/.fastq.gz/_umi-clipped.fastq.gz/g) ")
+									  echo $1 && \
+									  echo $2 && \
+									  echo $3")
 endef
 $(foreach ss,$(SPLIT_SAMPLES),\
 	$(if $(fq.$(ss)),$(eval $(call clip-umi-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
