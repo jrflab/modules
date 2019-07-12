4b9b4699c2c06f62547b6f4b93e2771750178d37
David Brown <ndbrown6@users.noreply.github.com>
2019-07-11 21:53:07 -0400

    Update clipumi.mk

--- test/fastq_tools/clipumi.mk
+++ test/fastq_tools/clipumi.mk
@@ -13,26 +13,25 @@ MARIANAS = /home/${USER}/share/usr/marianas-1.8.1/Marianas-1.8.1.jar
 define copy-fastq
 marianas/$1/$1_R1.fastq.gz marianas/$1/$1_R2.fastq.gz : $3
 	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
-									  mkdir -p marianas/$1 && \
-									  $(RSCRIPT) modules/test/fastq_tools/copyfastq.R --sample_name $1 --fastq_files $3")
+								  mkdir -p marianas/$1 && \
+								  $(RSCRIPT) modules/test/fastq_tools/copyfastq.R --sample_name $1 --fastq_files $3")
 
 endef
 $(foreach ss,$(SPLIT_SAMPLES),\
 	$(if $(fq.$(ss)),$(eval $(call clip-umi-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
 	
 	
-define clip-umi
-marianas/%/%_R1_umi-clipped.fastq.gz marianas/%/%_R2_umi-clipped.fastq.gz : marianas/%/%_R1.fastq.gz marianas/%/%_R2.fastq.gz
-	$$(call RUN,-c -n 1 -s 8G -m 16G,"set -o pipefail && \
-									  $(JAVA) -Djava.io.tmpdir=$(TMPDIR) -server -Xms2G -Xmx8G -cp $(MARIANAS) \
-									  org.mskcc.marianas.umi.duplex.fastqprocessing.ProcessLoopUMIFastq \
-									  marianas/$$(*)/$$(*)_R1.fastq.gz marianas/$$(*)/$$(*)_R2.fastq.gz \
-									  3")
-
-endef
- $(foreach sample,$(SAMPLES),\
-		$(eval $(call clip-umi,$(sample))))
-
+#define clip-umi
+#marianas/%/%_R1_umi-clipped.fastq.gz marianas/%/%_R2_umi-clipped.fastq.gz : marianas/%/%_R1.fastq.gz marianas/%/%_R2.fastq.gz
+#	$$(call RUN,-c -n 1 -s 8G -m 16G,"set -o pipefail && \
+#									  $(JAVA) -Djava.io.tmpdir=$(TMPDIR) -server -Xms2G -Xmx8G -cp $(MARIANAS) \
+#									  org.mskcc.marianas.umi.duplex.fastqprocessing.ProcessLoopUMIFastq \
+#									  marianas/$$(*)/$$(*)_R1.fastq.gz marianas/$$(*)/$$(*)_R2.fastq.gz \
+#									  3")
+#
+#endef
+#$(foreach sample,$(SAMPLES),\
+#		$(eval $(call clip-umi,$(sample))))
 
 .DELETE_ON_ERROR:
 .SECONDARY:
