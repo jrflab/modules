c0f3caf161e53fdf47cf7236bab941bf54b96d03
David Brown <ndbrown6@users.noreply.github.com>
2019-07-11 22:53:50 -0400

    Update clipumi.mk

--- test/fastq_tools/clipumi.mk
+++ test/fastq_tools/clipumi.mk
@@ -24,9 +24,10 @@ $(foreach ss,$(SPLIT_SAMPLES),\
 
 define clip-umi
 marianas/$1/$1_R1_umi-clipped.fastq.gz marianas/$1/$1_R2_umi-clipped.fastq.gz : marianas/$1/$1_R1.fastq.gz marianas/$1/$1_R2.fastq.gz
-	$$(call RUN,-c -n 1 -s 8G -m 16G,"$(JAVA) -Djava.io.tmpdir=$(TMPDIR) -server -Xms2G -Xmx8G -cp $(MARIANAS) \
+	$$(call RUN,-c -n 1 -s 8G -m 16G,"cd marianas/$1/ && \
+									  $(JAVA) -Djava.io.tmpdir=$(TMPDIR) -server -Xms2G -Xmx8G -cp $(MARIANAS) \
 									  org.mskcc.marianas.umi.duplex.fastqprocessing.ProcessLoopUMIFastq \
-									  $$(<) $$(<<) \
+									  $1_R1.fastq.gz $1_R2.fastq.gz \
 									  3")
 
 endef
