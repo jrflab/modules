1d38166dcc37c39331d23d3aa3a77e4f88b4d614
David Brown <ndbrown6@users.noreply.github.com>
2019-07-11 21:38:48 -0400

    Update clipumi.mk

--- test/fastq_tools/clipumi.mk
+++ test/fastq_tools/clipumi.mk
@@ -4,23 +4,34 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/clip_umi.$(NOW)
 PHONY += marianas
 
-clip_umi : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R1.fastq.gz) \
-		   $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R2.fastq.gz)
+clip_umi : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R1_umi-clipped.fastq.gz) \
+		   $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R2_umi-clipped.fastq.gz)
 
 JAVA = /home/${USER}/share/usr/jdk1.8.0_74/bin/java
 PICARD = /home/${USER}/share/usr/picard/bin/picard.jar
 MARIANAS = /home/${USER}/share/usr/marianas/Marianas-1.8.1.jar
 
-define clip-umi-fastq
+define copy-fastq
 marianas/$1/$1_R1.fastq.gz marianas/$1/$1_R2.fastq.gz : $3
-	$$(call RUN,-c -n 1 -s 8G -m 16G,"set -o pipefail && \
+	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
 									  mkdir -p marianas/$1 && \
 									  $(RSCRIPT) modules/test/fastq_tools/copyfastq.R --sample_name $1 --fastq_files $3")
+
 endef
 $(foreach ss,$(SPLIT_SAMPLES),\
 	$(if $(fq.$(ss)),$(eval $(call clip-umi-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
+	
+	
+define clip-umi
+marianas/$1/$1_R1_umi-clipped.fastq.gz marianas/$1/$1_R2_umi-clipped.fastq.gz : marianas/$1/$1_R1.fastq.gz marianas/$1/$1_R2.fastq.gz
+	$$(call RUN,-c -n 1 -s 8G -m 16G,"set -o pipefail && \
+									  $(JAVA) -Djava.io.tmpdir=$(TMPDIR) -server -Xms8G -Xmx8G -cp Marianas-1.8.1.jar \
+									  org.mskcc.marianas.umi.duplex.fastqprocessing.ProcessLoopUMIFastq \
+									  marianas/$1/$1_R1_umi-clipped.fastq.gz marianas/$1/$1_R2_umi-clipped.fastq.gz \
+									  3")
+
+endef
 
 .DELETE_ON_ERROR:
 .SECONDARY:
 .PHONY: $(PHONY)
-
