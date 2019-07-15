46cd3a3e166dfb994e6ac2995ee0bc6d2ee9e4cd
David Brown <ndbrown6@users.noreply.github.com>
2019-07-15 12:30:31 -0400

    Update marianasaccess.mk

--- test/workflows/marianasaccess.mk
+++ test/workflows/marianasaccess.mk
@@ -4,10 +4,12 @@ LOGDIR ?= log/marianas_access.$(NOW)
 PHONY += bam marianas cnvaccess
 
 MSK_ACCESS_WORKFLOW += clip_umi
+MSK_ACCESS_WORKFLOW += align_fastq
 
 msk_access_workflow : $(MSK_ACCESS_WORKFLOW)
 
 include modules/test/fastq_tools/clipumi.mk
+include modules/test/bam_tools/alignfastq.mk
 
 .DELETE_ON_ERROR:
 .SECONDARY:
