5316fb967bbea9d70b7261555a53d94c761ba0b5
David Brown <ndbrown6@users.noreply.github.com>
2019-07-15 12:34:06 -0400

    Update alignfastq.mk

--- test/bam_tools/alignfastq.mk
+++ test/bam_tools/alignfastq.mk
@@ -14,7 +14,7 @@ align_fastq : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample).bam)
 define fastq-to-bam
 marianas/$1/$1.bam : marianas/$1/$1_R1_umi-clipped.fastq.gz marianas/$1/$1_R2_umi-clipped.fastq.gz
 	$$(call RUN,-c -n $(BWAMEM_THREADS) -s 1G -m $(BWAMEM_MEM_PER_THREAD),"set -o pipefail && \
-																		   $(BWA) mem -t $(BWAMEM_THREADS) $(BWA_ALN_OPTS) -R \"@RG\tID:$*\tLB:$${LBID}\tPL:${SEQ_PLATFORM}\tSM:$${LBID}\" $(BWAMEM_REF_FASTA) $$^ | $(SAMTOOLS) view -bhS - > $@")
+																		   $(BWA) mem -t $(BWAMEM_THREADS) $(BWA_ALN_OPTS) -R \"@RG\tID:$*\tLB:$${LBID}\tPL:${SEQ_PLATFORM}\tSM:$${LBID}\" $(BWAMEM_REF_FASTA) $$(^) | $(SAMTOOLS) view -bhS - > $$(@)")
 
 endef
 $(foreach sample,$(SAMPLES),\
