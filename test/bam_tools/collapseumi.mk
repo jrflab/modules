8693407787da74d54c13e176d6a672ac36611fa4
David Brown <ndbrown6@users.noreply.github.com>
2019-07-16 12:01:05 -0400

    Update collapseumi.mk

--- test/bam_tools/collapseumi.mk
+++ test/bam_tools/collapseumi.mk
@@ -9,14 +9,13 @@ umi_collapse : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)-pileup.t
 JAVA = /home/${USER}/share/usr/jdk1.8.0_74/bin/java
 MARIANAS = /home/${USER}/share/usr/marianas-1.8.1/Marianas-1.8.1.jar
 WALTZ = /home/${USER}/share/usr/marianas-1.8.1/Waltz-3.0.jar
-BED_FILE=/home/${USER}/share/reference/target_panels/MSK-ACCESS-v1_0-probe-AB.ontarget.bed
+BED_FILE=/home/${USER}/share/reference/target_panels/MSK-ACCESS-v1_0-probe-AB.waltz.bed
 
 define waltz-genotype
 marianas/$1/$1-pileup.txt : marianas/$1/$1.bam
 	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
 									  cd marianas/$1 && \
-									  cat $(BED_FILE) | awk '{print $1 \"\t\" $2 \"\t\" $3 \"\t\" $4 \"\t\" $4}' > MSK-ACCESS-v1_0-probe-AB.bed && \
-									  $(JAVA) -server -Xms2G -Xmx8G -cp $(WALTZ) org.mskcc.juber.waltz.Waltz PileupMetrics 20 $1.bam $(REF_FASTA) MSK-ACCESS-v1_0-probe-AB.bed")
+									  $(JAVA) -server -Xms2G -Xmx8G -cp $(WALTZ) org.mskcc.juber.waltz.Waltz PileupMetrics 20 $1.bam $(REF_FASTA) $(BED_FILE)")
 
 endef
 $(foreach sample,$(SAMPLES),\
