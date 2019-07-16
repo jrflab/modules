1a6fbfa412f7412c9c809005ffc6ea23af30841a
David Brown <ndbrown6@users.noreply.github.com>
2019-07-16 11:57:54 -0400

    Update collapseumi.mk

--- test/bam_tools/collapseumi.mk
+++ test/bam_tools/collapseumi.mk
@@ -12,7 +12,7 @@ WALTZ = /home/${USER}/share/usr/marianas-1.8.1/Waltz-3.0.jar
 BED_FILE=/home/${USER}/share/reference/target_panels/MSK-ACCESS-v1_0-probe-AB.ontarget.bed
 
 define waltz-genotype
-marianas/$1/$1.bam marianas/$1/ : marianas/$1/$1-pileup.txt
+marianas/$1/$1-pileup.txt : marianas/$1/$1.bam
 	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
 									  cd marianas/$1 && \
 									  cat $(BED_FILE) | awk '{print $1 \"\t\" $2 \"\t\" $3 \"\t\" $4 \"\t\" $4}' > MSK-ACCESS-v1_0-probe-AB.bed && \
