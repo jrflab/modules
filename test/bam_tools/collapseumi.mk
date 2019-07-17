cb7f298ac9fa2d56a1e67d6e382b2cc8df2be16e
David Brown <ndbrown6@users.noreply.github.com>
2019-07-16 23:43:02 -0400

    Update collapseumi.mk

--- test/bam_tools/collapseumi.mk
+++ test/bam_tools/collapseumi.mk
@@ -4,7 +4,7 @@ include modules/genome_inc/b37.inc
 LOGDIR ?= log/collapseumi.$(NOW)
 PHONY += marianas
 
-umi_collapse : $(foreach sample,$(SAMPLES),marianas/$(sample)/first-pass.mate-position-sorted.txt)
+umi_collapse : $(foreach sample,$(SAMPLES),marianas/$(sample)/second-pass-alt-alleles.txt marianas/$(sample)/second-pass-insertions.txt)
 
 JAVA = /home/${USER}/share/usr/jdk1.8.0_74/bin/java
 MARIANAS = /home/${USER}/share/usr/marianas-1.8.1/Marianas-1.8.1.jar
@@ -26,6 +26,13 @@ marianas/$1/first-pass.mate-position-sorted.txt : marianas/$1/$1-pileup.txt
 									  sort -S 6G -k 8 first-pass.txt | sort -S 6G -k 6 > first-pass.mate-position-sorted.txt && \
 									  cd ../..")
 
+
+marianas/$1/second-pass-alt-alleles.txt marianas/$1/second-pass-insertions.txt : marianas/$1/first-pass.mate-position-sorted.txt
+	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
+									  cd marianas/$1 && \
+									  $(JAVA) -server -Xms2G -Xmx8G -cp $(MARIANAS) org.mskcc.marianas.umi.duplex.DuplexUMIBamToCollapsedFastqSecondPass $1.bam $1-pileup.txt 1 20 0 1 90 $(REF_FASTA) first-pass.mate-position-sorted.txt && \
+									  cd ../..")
+									  									  
 endef
 $(foreach sample,$(SAMPLES),\
 	$(eval $(call genotype-and-collapse,$(sample))))
