6bed81ebac54d3e2eb8d16efe9a8831422c1b993
David Brown <ndbrown6@users.noreply.github.com>
2019-07-16 20:24:38 -0400

    Update collapseumi.mk

--- test/bam_tools/collapseumi.mk
+++ test/bam_tools/collapseumi.mk
@@ -11,6 +11,8 @@ MARIANAS = /home/${USER}/share/usr/marianas-1.8.1/Marianas-1.8.1.jar
 WALTZ = /home/${USER}/share/usr/marianas-1.8.1/Waltz-3.0.jar
 BED_FILE=/home/${USER}/share/reference/target_panels/MSK-ACCESS-v1_0-probe-AB.waltz.bed
 
+comma = ,
+
 define genotype-and-collapse
 marianas/$1/$1-pileup.txt : marianas/$1/$1.bam
 	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
@@ -22,7 +24,7 @@ marianas/$1/first-pass.mate-position-sorted.txt : marianas/$1/$1-pileup.txt
 	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
 									  cd marianas/$1 && \
 									  $(JAVA) -server -Xms2G -Xmx8G -cp $(MARIANAS) org.mskcc.marianas.umi.duplex.DuplexUMIBamToCollapsedFastqFirstPass $1.bam $1-pileup.txt 1 20 0 1 90 $(REF_FASTA) && \
-									  sort -S 8G -k `6,6n' -k \"8,8n\" first-pass.txt > first-pass.mate-position-sorted.txt && \
+									  sort -S 8G -k 6$comma6n -k 8$comma8n first-pass.txt > first-pass.mate-position-sorted.txt && \
 									  cd ../..")
 
 endef
