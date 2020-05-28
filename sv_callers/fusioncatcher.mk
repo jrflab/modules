92010f0c1d959a7ce3514613b18282d77efe6356
David Brown <ndbrown6@users.noreply.github.com>
2020-05-28 17:50:49 -0400

    Update fusioncatcher.mk

--- sv_callers/fusioncatcher.mk
+++ sv_callers/fusioncatcher.mk
@@ -6,7 +6,7 @@ LOGDIR = log/fusioncatcher.$(NOW)
 ##### MAKE INCLUDES #####
 include modules/Makefile.inc
 
-FUSIONCATCHER = $(HOME)/share/usr/fusioncatcher/bin/fusioncatcher
+FUSIONCATCHER = $(HOME)/share/usr/fusioncatcher/fusioncatcher_v0.99.2/fusioncatcher
 FUSIONCATCHER_OPTS = -d $(HOME)/share/usr/fusioncatcher/data/current --extract-buffer-size=35000000000
 
 .DELETE_ON_ERROR:
