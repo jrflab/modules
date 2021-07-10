150e19e5cb446745d1a89c413eedf7defddc37b7
David Brown <ndbrown6@gmail.com>
2021-07-10 17:32:59 -0400

    Update splitRG.mk

--- bam_tools/splitRG.mk
+++ bam_tools/splitRG.mk
@@ -6,7 +6,7 @@ split : $(foreach sample,$(SAMPLES),rg/EEC128/$(sample).bam)
 
 define split-rg
 rg/EEC128/$1.bam : bam/EEC128.bam
-	$$(call RUN,-n 1 -s 4G -m 6G,"set -o pipefail && \
+	$$(call RUN,-n 1 -s 4G -m 8G,"set -o pipefail && \
 				      mkdir -p rg/EEC128 && \
 				      $$(SAMTOOLS) view -b -r $1 bam/EEC128.bam > rg/EEC128/$1.bam")
 
