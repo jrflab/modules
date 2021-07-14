bb5738acb2c42eabbdcaabc0f36c9ed3e7618393
David Brown <ndbrown6@gmail.com>
2021-07-14 13:23:56 -0400

    Update getBaseCount.mk

--- variant_callers/getBaseCount.mk
+++ variant_callers/getBaseCount.mk
@@ -8,15 +8,15 @@ GBC_EXE = $(HOME)/share/data/common/eec_sc_split/etc/GetBaseCounts/GetBaseCounts
 MAPQ := 10
 BAQ := 15
 
-getbasecount : $(foreach sample,$(SAMPLES),gbc/EEC128/$(sample).tsv)
+getbasecount : $(foreach sample,$(SAMPLES),gbc/EEC87/$(sample).tsv)
 
 define get-basecount
-gbc/EEC128/$1.txt : bam/EEC128/$1.bam
+gbc/EEC87/$1.txt : bam/EEC87/$1.bam
 	$$(call RUN,-n 6 -s 1G -m 2G -v $(GBC_ENV),"set -o pipefail && \
-				      		    mkdir -p gbc/EEC128 && \
+				      		    mkdir -p gbc/EEC87 && \
 						    $(GBC_EXE) --fasta ~/share/reference/ucsc_gatk_bundle_2.8/ucsc.hg19.fasta \
 						    --bam $$(<) \
-						    --vcf etc/vcf/EEC128.vcf \
+						    --vcf etc/vcf/EEC87.vcf \
 						    --output $$(@) \
 						    --maq $(MAPQ) \
 						    --baq $(BAQ) \
@@ -25,7 +25,7 @@ gbc/EEC128/$1.txt : bam/EEC128/$1.bam
 						    --filter_qc_failed 1 \
 						    --thread 6")
 						    
-gbc/EEC128/$1.tsv : gbc/EEC128/$1.txt
+gbc/EEC87/$1.tsv : gbc/EEC87/$1.txt
 	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
 				      $(RSCRIPT) modules/variant_callers/getBaseCount.R --file_name $$(<)")
 
