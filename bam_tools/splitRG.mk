696fee32fc7526f7c010de3cc8a699b268b97985
David Brown <ndbrown6@gmail.com>
2021-08-07 20:06:41 -0400

    Update splitRG.mk

--- bam_tools/splitRG.mk
+++ bam_tools/splitRG.mk
@@ -2,16 +2,16 @@ include modules/Makefile.inc
 
 LOGDIR = log/split_rg.$(NOW)
 
-split : $(foreach sample,$(SAMPLES),bam/ISHI-HEC6/$(sample).bam) \
-	$(foreach sample,$(SAMPLES),bam/ISHI-HEC6/$(sample).bam.bai)
+split : $(foreach sample,$(SAMPLES),bam/EEC91/$(sample).bam) \
+	$(foreach sample,$(SAMPLES),bam/EEC91/$(sample).bam.bai)
 
 define split-rg
-bam/ISHI-HEC6/$1.bam : etc/bam/ISHI-HEC6-2.bam
+bam/EEC91/$1.bam : etc/bam/EEC91-1.bam
 	$$(call RUN,-n 1 -s 4G -m 8G,"set -o pipefail && \
-				      mkdir -p bam/ISHI-HEC6 && \
+				      mkdir -p bam/EEC91 && \
 				      $$(SAMTOOLS) view -b -r $1 $$(<) > $$(@)")
 
-bam/ISHI-HEC6/$1.bam.bai : bam/ISHI-HEC6/$1.bam
+bam/EEC91/$1.bam.bai : bam/EEC91/$1.bam
 	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
 				      $$(SAMTOOLS) index $$(<)")
 
