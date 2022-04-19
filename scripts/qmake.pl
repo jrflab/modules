04de094b908226e554f1ba35361da0b3450b0054
David Brown <ndbrown6@gmail.com>
2022-04-19 18:12:04 -0400

    Update qmake.pl

--- scripts/qmake.pl
+++ scripts/qmake.pl
@@ -10,7 +10,7 @@ my $err_slack = "pipeline_error";
 my $fin_slack = "pipeline_finished";
 
 my %slack_map = (
-    brownd7 => "W01BT68MSSD",
+    brownd7 => "U01ERFWQXCY",
     selenicp => "U22A5U23X",
     dacruzpa => "U6PAUB3C6",
     parejaf => "UBF6MRSV8",
