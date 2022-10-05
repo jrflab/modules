aca16181323f4f86c90d13b8df0b67dff74d7716
David Brown <ndbrown6@gmail.com>
2022-10-05 13:17:55 -0400

    Update annotateSummaryVcf.mk

--- vcf_tools/annotateSummaryVcf.mk
+++ vcf_tools/annotateSummaryVcf.mk
@@ -6,22 +6,19 @@ LOGDIR ?= log/annotate_smry_maf.$(NOW)
 annotate_smry_maf : vcf2maf/mutation_summary.vcf \
 		    vcf2maf/mutation_summary.maf
 		   
-VCF2MAF_ENV = $(HOME)/share/usr/env/vcf2maf-1.6.17
-VCF2MAF = vcf2maf.pl
-		   
 vcf2maf/mutation_summary.vcf : summary/tsv/mutation_summary.tsv
 	$(call RUN, -c -n 1 -s 8G -m 12G,"set -o pipefail && \
 					  $(RSCRIPT) $(SCRIPTS_DIR)/annotateSummaryVcf.R --option 1 --input $(<) --output $(@)")
 							
 vcf2maf/mutation_summary.maf : vcf2maf/mutation_summary.vcf
-	$(call RUN, -c -n 12 -s 2G -m 3G -v $(VCF2MAF_ENV) -w 72:00:00,"set -o pipefail && \
+	$(call RUN, -c -n 12 -s 2G -m 3G -v $(VEP_ENV) -w 72:00:00,"set -o pipefail && \
 									$(VCF2MAF) \
 									--input-vcf $(<) \
 									--output-maf $(@) \
 									--tmp-dir $(TMPDIR) \
 									--tumor-id NA \
 									--normal-id NA \
-									--vep-path $(VCF2MAF_ENV)/bin \
+									--vep-path $(VEP_ENV)/bin \
 									--vep-data $(HOME)/share/lib/resource_files/VEP/GRCh37/ \
 									--vep-forks 12 \
 									--ref-fasta $(HOME)/share/lib/resource_files/VEP/GRCh37/homo_sapiens/99_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz \
