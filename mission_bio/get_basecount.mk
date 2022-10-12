d112444d5e8f46d9fb736677f749514227f2ceb5
David Brown <ndbrown6@gmail.com>
2022-10-12 12:20:05 -0400

    Update get_basecount.mk

--- mission_bio/get_basecount.mk
+++ mission_bio/get_basecount.mk
@@ -2,24 +2,30 @@ include modules/Makefile.inc
 
 LOGDIR ?= log/get_basecount.$(NOW)
 
-MAPQ := 15
-BAQ := 20
+MAPQ := 0
+BAQ := 0
+COV := 0
 
-getbasecount : $(foreach sample,$(SAMPLES),gbc/$(sample).txt)
+getbasecount : $(foreach sample,$(SAMPLES),gbc/$(sample).txt.gz)
 
 define get-basecount
-gbc/$1.txt : bam/$1.bam vcf/$1.vcf
+gbc/$1.txt.gz : bam/$1.bam vcf/$1.vcf
 	$$(call RUN,-n 6 -s 3G -m 6G,"set -o pipefail && \
 				      $(GBC) --fasta $(REF_FASTA) \
 				      --bam $$(<) \
 				      --vcf $$(<<) \
 				      --output $$(@) \
+				      --thread 6 \
+				      --sort_output \
+				      --compress_output \
 				      --maq $(MAPQ) \
 				      --baq $(BAQ) \
-				      --filter_duplicate 1 \
-				      --filter_improper_pair 1 \
+				      --cov $(COV) \
+				      --filter_duplicate 0 \
+				      --filter_improper_pair 0 \
 				      --filter_qc_failed 1 \
-				      --thread 6")
+				      --filter_indel 0 \
+				      --filter_non_primary 1")
 						    
 endef
 $(foreach sample,$(SAMPLES),\
