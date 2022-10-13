76df39eddf51da4612e9ec02cfa2649c3b474f25
David Brown <ndbrown6@gmail.com>
2022-10-13 14:20:11 -0400

    Update split_rg.mk

--- bam_tools/split_rg.mk
+++ bam_tools/split_rg.mk
@@ -2,22 +2,33 @@ include modules/Makefile.inc
 
 LOGDIR = log/split_rg.$(NOW)
 
-splitrg : $(foreach sample,$(SAMPLES),split_rg/$(sample).bam) \
-	  $(foreach sample,$(SAMPLES),split_rg/$(sample).bam.bai)
+NUM_BARCODES = 5
+BARCODE_NUM = $(shell seq 1 $(NUM_BARCODES))
+
+splitrg :  $(foreach sample,$(SAMPLES), \
+		  	$(foreach n,$(BARCODE_NUM),split_rg/$(sample)/$(BARCODE)$(n).bam)) \
+	   $(foreach sample,$(SAMPLES), \
+		  	$(foreach n,$(BARCODE_NUM),split_rg/$(sample)/$(BARCODE)$(n).bam.bai)) \
+	   $(foreach sample,$(SAMPLES), \
+		  	$(foreach n,$(BARCODE_NUM),split_rg/$(sample)/$(BARCODE)$(n).bai)
 
 define split-rg
-split_rg/$1.bam : bam/WBC-control-A.tube1.cells.bam
+split_rg/$1/$2.bam : bam/$1.bam
 	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
 				      $$(SAMTOOLS) view -b -r $1 $$(<) > $$(@)")
 
-split_rg/$1.bam.bai : split_rg/$1.bam
+split_rg/$1/$2.bam.bai : split_rg/$1/$2.bam
 	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
-				      $$(SAMTOOLS) index $$(<) && \
-				      cp $$(@) split_rg/$1.bai")
+				      $$(SAMTOOLS) index $$(<)")
+
+split_rg/$1/$2.bai : split_rg/$1/$2.bam.bai
+	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
+				      $$(CP) $$(<) $$(@)")
 
 endef
-$(foreach sample,$(SAMPLES),\
-		$(eval $(call split-rg,$(sample))))
+$(foreach sample,$(SAMPLES), \
+	$(foreach n,$(BARCODE_NUM), \
+		$(eval $(call split-rg,$(sample),$(n)))))
 
 ..DUMMY := $(shell mkdir -p version; \
 	     $(SAMTOOLS) --version > version/split_rg.txt;)
