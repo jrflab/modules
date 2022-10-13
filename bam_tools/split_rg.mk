f43d2481c1db07f0e047cd30a294b073ef8567ec
David Brown <ndbrown6@gmail.com>
2022-10-13 14:35:55 -0400

    Update split_rg.mk

--- bam_tools/split_rg.mk
+++ bam_tools/split_rg.mk
@@ -5,23 +5,10 @@ LOGDIR = log/split_rg.$(NOW)
 splitrg :  $(foreach sample,$(SAMPLES), \
 		  	$(foreach barcode,$(BARCODES),split_rg/$(sample)/$(barcode).bam))
 
-#$(foreach sample,$(SAMPLES), \
-#		  	$(foreach n,$(BARCODE_NUM),split_rg/$(sample)/$(BARCODES).$(n).bam.bai)) \
-#	   $(foreach sample,$(SAMPLES), \
-#		  	$(foreach n,$(BARCODE_NUM),split_rg/$(sample)/$(BARCODES).$(n).bai))
-
 define split-rg
 split_rg/$1/$2.bam : bam/$1.bam
 	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
 				      $$(SAMTOOLS) view -b -r $2 $$(<) > $$(@)")
-
-#split_rg/$1/$(BARCODES).$2.bam.bai : split_rg/$1/$(BARCODES).$2.bam
-#	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
-#				      $$(SAMTOOLS) index $$(<)")
-
-#split_rg/$1/$(BARCODES).$2.bai : split_rg/$1/$(BARCODES).$2.bam.bai
-#	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
-#				      $$(CP) $$(<) $$(@)")
 
 endef
 $(foreach sample,$(SAMPLES), \
