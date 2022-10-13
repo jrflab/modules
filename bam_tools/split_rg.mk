d6449822706d84870124478489b04ab1c782251b
David Brown <ndbrown6@gmail.com>
2022-10-13 14:30:17 -0400

    Update split_rg.mk

--- bam_tools/split_rg.mk
+++ bam_tools/split_rg.mk
@@ -2,11 +2,8 @@ include modules/Makefile.inc
 
 LOGDIR = log/split_rg.$(NOW)
 
-NUM_BARCODES = 5
-BARCODE_NUM = $(shell seq 1 $(NUM_BARCODES))
-
 splitrg :  $(foreach sample,$(SAMPLES), \
-		  	$(foreach n,$(BARCODE_NUM),split_rg/$(sample)/$(BARCODES).$(n).bam))
+		  	$(foreach barcode,$(BARCODES),split_rg/$(sample)/$(barcode).bam))
 
 #$(foreach sample,$(SAMPLES), \
 #		  	$(foreach n,$(BARCODE_NUM),split_rg/$(sample)/$(BARCODES).$(n).bam.bai)) \
@@ -14,9 +11,9 @@ splitrg :  $(foreach sample,$(SAMPLES), \
 #		  	$(foreach n,$(BARCODE_NUM),split_rg/$(sample)/$(BARCODES).$(n).bai))
 
 define split-rg
-split_rg/$1/$(BARCODES).$2.bam : bam/$1.bam
+split_rg/$1/$2.bam : bam/$1.bam
 	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
-				      $$(SAMTOOLS) view -b -r $${BARCODES}.$2 $$(<) > $$(@)")
+				      $$(SAMTOOLS) view -b -r $2 $$(<) > $$(@)")
 
 #split_rg/$1/$(BARCODES).$2.bam.bai : split_rg/$1/$(BARCODES).$2.bam
 #	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
@@ -28,8 +25,8 @@ split_rg/$1/$(BARCODES).$2.bam : bam/$1.bam
 
 endef
 $(foreach sample,$(SAMPLES), \
-	$(foreach n,$(BARCODE_NUM), \
-		$(eval $(call split-rg,$(sample),$(n)))))
+	$(foreach barcode,$(BARCODES), \
+		$(eval $(call split-rg,$(sample),$(barcode)))))
 
 ..DUMMY := $(shell mkdir -p version; \
 	     $(SAMTOOLS) --version > version/split_rg.txt;)
