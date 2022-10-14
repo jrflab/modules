bfbe300236f043023ddf6918755af28eca58fdd8
David Brown <ndbrown6@gmail.com>
2022-10-14 11:02:32 -0400

    Update svabaTN.mk

--- sv_callers/svabaTN.mk
+++ sv_callers/svabaTN.mk
@@ -15,20 +15,20 @@ svaba : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).svaba_sv.vcf \
 				       vcf/$(pair).svaba_candidate_sv.vcf)
 
 define svaba-tumor-normal
-#svaba/$1_$2.svaba.somatic.indel.vcf : bam/$1.bam bam/$2.bam
-#	$$(call RUN,-c -n $(SVABA_CORES) -s 4G -m $(SVABA_MEM_CORE) -v $(SVABA_ENV) -w 72:00:00,"set -o pipefail && \
-#												 mkdir -p svaba && \
-#										 		 cd svaba && \
-#												 $$(SVABA) run \
-#												 -t ../bam/$1.bam \
-#												 -n ../bam/$2.bam \
-#												 -p $$(SVABA_CORES) \
-#												 -D $$(SVABA_DBSNP) \
-#												 -L 100000 \
-#												 -x 25000 \
-#												 -k $$(SVABA_BLACKLIST) \
-#												 -a $1_$2 \
-#												 -G $$(SVABA_REF)")
+svaba/$1_$2.svaba.somatic.indel.vcf : bam/$1.bam bam/$2.bam
+	$$(call RUN,-c -n $(SVABA_CORES) -s 4G -m $(SVABA_MEM_CORE) -v $(SVABA_ENV) -w 72:00:00,"set -o pipefail && \
+												 mkdir -p svaba && \
+										 		 cd svaba && \
+												 $$(SVABA) run \
+												 -t ../bam/$1.bam \
+												 -n ../bam/$2.bam \
+												 -p $$(SVABA_CORES) \
+												 -D $$(SVABA_DBSNP) \
+												 -L 100000 \
+												 -x 25000 \
+												 -k $$(SVABA_BLACKLIST) \
+												 -a $1_$2 \
+												 -G $$(SVABA_REF)")
 
 svaba/$1_$2.svaba.somatic.sv.vcf : svaba/$1_$2.svaba.somatic.indel.vcf
 
