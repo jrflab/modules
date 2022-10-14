cd5038babfab695cdb5bda4235ef5dffd3f7de1f
David Brown <ndbrown6@users.noreply.github.com>
2022-10-13 23:41:14 -0400

    Update mantaTN.mk

--- sv_callers/mantaTN.mk
+++ sv_callers/mantaTN.mk
@@ -1,21 +1,19 @@
-# run manta on tumour-normal matched pairs
-
 include modules/Makefile.inc
 include modules/sv_callers/manta.inc
 
-LOGDIR ?= log/manta.$(NOW)
-PHONY += manta manta_vcfs
+LOGDIR ?= log/mantaTN.$(NOW)
 
-manta : manta_vcfs 
-
-manta_vcfs : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).manta_sv.eff.vcf vcf/$(pair).manta_indels.eff.vcf vcf/$(pair).manta_candidate_sv.eff.vcf)
+manta : $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).manta_sv.eff.vcf \
+				       vcf/$(pair).manta_indels.eff.vcf \
+				       vcf/$(pair).manta_candidate_sv.eff.vcf)
 
 define manta-tumor-normal
 manta/$1_$2/runWorkflow.py : bam/$1.bam bam/$2.bam bam/$1.bam.bai bam/$2.bam.bai
 	$$(INIT) $$(CONFIG_MANTA) $$(CONFIG_MANTA_OPTS) --tumorBam $$< --normalBam $$(<<) --runDir $$(@D) 
 
 manta/$1_$2.manta_timestamp : manta/$1_$2/runWorkflow.py
-	$$(call RUN,-n 8 -s 2G -m 2G,"python $$< -m local -j 8 && touch $$@")
+	$$(call RUN,-n 8 -s 2G -m 4G,"set -o pipefail && \
+				      python $$< -m local -j 8 && touch $$@")
 
 manta/$1_$2/results/variants/somaticSV.vcf.gz : manta/$1_$2.manta_timestamp
 
