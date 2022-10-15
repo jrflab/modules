c82ef7962e12fb33187ec402db7075f4ac6d85bc
David Brown <ndbrown6@gmail.com>
2022-10-15 17:53:13 -0400

    Update immunedeconv.mk

--- rnaseq/immunedeconv.mk
+++ rnaseq/immunedeconv.mk
@@ -8,15 +8,15 @@ immunedeconv : immunedeconv/quantiseq.txt \
 
 immunedeconv/quantiseq.txt : kallisto/tpm_bygene.txt
 	$(call RUN, -c -n 1 -s 8G -m 16G -v $(IMMUNE_ENV),"set -o pipefail && \
-							   $(RSCRIPT) $(SCRIPTS_DIR)/immunedeconv.R --option 1 --input_file $$(<) --output_file $$(@)")
+							   $(RSCRIPT) $(SCRIPTS_DIR)/immunedeconv.R --option 1 --input_file $(<) --output_file $(@)")
 
 immunedeconv/mcpcounter.txt : kallisto/tpm_bygene.txt
 	$(call RUN, -c -n 1 -s 8G -m 16G -v $(IMMUNE_ENV),"set -o pipefail && \
-							   $(RSCRIPT) $(SCRIPTS_DIR)/immunedeconv.R --option 2 --input_file $$(<) --output_file $$(@)")
+							   $(RSCRIPT) $(SCRIPTS_DIR)/immunedeconv.R --option 2 --input_file $(<) --output_file $(@)")
 
 immunedeconv/cibersort.txt : kallisto/tpm_bygene.txt
 	$(call RUN, -c -n 1 -s 8G -m 16G -v $(IMMUNE_ENV),"set -o pipefail && \
-							   $(RSCRIPT) $(SCRIPTS_DIR)/immunedeconv.R --option 3 --input_file $$(<) --output_file $$(@)")
+							   $(RSCRIPT) $(SCRIPTS_DIR)/immunedeconv.R --option 3 --input_file $(<) --output_file $(@)")
 
 ..DUMMY := $(shell mkdir -p version; \
 	     ~/share/usr/env/r-immunedeconv-2.1.0/bin/R --version >> version/immunedeconv.txt;)
