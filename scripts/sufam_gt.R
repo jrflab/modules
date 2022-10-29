0fc9b6aa708290094d0fc2b6e10271eaeffae643
David Brown <ndbrown6@gmail.com>
2022-10-29 16:47:41 -0400

    Update sufam_gt.R

--- scripts/sufam_gt.R
+++ scripts/sufam_gt.R
@@ -49,16 +49,3 @@ if (as.numeric(opt$option)==1) {
 	cat("##fileformat=VCFv4.2\n", file = as.character(opt$output_file), append=FALSE)
 	readr::write_tsv(x = smry, path = as.character(opt$output_file), append = TRUE, col_names = TRUE)
 }
-
-#all_vars = read.csv(file="summary/tsv/mutation_summary.tsv", header=TRUE, sep="\t", stringsAsFactors=FALSE)
-#CHROM = all_vars[,"CHROM"]
-#POS = all_vars[,"POS"]
-#ID = all_vars[,"ID"]
-#REF = all_vars[,"REF"]
-#ALT = all_vars[,"ALT"]
-#QUAL = FILTER = rep(".", nrow(all_vars))
-#INFO = paste0(all_vars[,"SYMBOL"], all_vars[,"HGVSp_Short"])
-#vcf = data.frame(CHROM, POS, ID, REF, ALT, QUAL, INFO)
-#
-#cat("#", file="sufam/pdx.vcf", append=FALSE)
-#write.table(vcf, file="sufam/pdx.vcf", col.names=TRUE, row.names=FALSE, sep="\t", quote=FALSE, append=TRUE)
