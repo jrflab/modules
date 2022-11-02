48ccbc47e53ab630e614302a650eb0d14bfa30e8
David Brown <ndbrown6@gmail.com>
2022-11-02 12:13:32 -0400

    Update pyclone.mk

--- ploidy/pyclone.mk
+++ ploidy/pyclone.mk
@@ -9,7 +9,8 @@ pyclone : $(foreach sample,$(TUMOR_SAMPLES),pyclone/$(sample).vcf) \
 	  $(foreach sample,$(TUMOR_SAMPLES),pyclone/$(sample).txt) \
 	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set).tsv) \
 	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set).hd5) \
-	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set).txt)
+	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set).txt) \
+	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set).pdf)
 
 
 define r-sufam
@@ -59,10 +60,18 @@ pyclone/$1.hd5 : pyclone/$1.tsv
 									 --num-restarts 100")
 									 
 pyclone/$1.txt : pyclone/$1.hd5
-	$$(call RUN,-c -n 1 -s 12G -m 24G -v $(PYCLONE_ENV),"set -o pipefail && \
-							     pyclone-vi write-results-file \
-							     --in-file $$(<) \
-							     --out-file $$(@)")
+	$$(call RUN,-c -n 1 -s 8 -m 12G -v $(PYCLONE_ENV),"set -o pipefail && \
+							   pyclone-vi write-results-file \
+							   --in-file $$(<) \
+							   --out-file $$(@)")
+							     
+pyclone/$1.pdf : pyclone/$1.txt
+	$$(call RUN,-c -n 1 -s 8G -m 12G -v $(PYCLONE_ENV),"set -o pipefail && \
+							   $(RSCRIPT) $(SCRIPTS_DIR)/pyclone.R \
+							   --option 2 \
+							   --sample_set '$(tumors.$1)' \
+							   --input_file $$(<) \
+							   --output_file $$(@)")
 
 endef
 $(foreach set,$(SAMPLE_SETS),\
