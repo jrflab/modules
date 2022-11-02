3171eba86ac6933445b852c48f8207a11f75a61e
David Brown <ndbrown6@gmail.com>
2022-11-02 15:44:19 -0400

    Update pyclone.mk

--- ploidy/pyclone.mk
+++ ploidy/pyclone.mk
@@ -11,8 +11,8 @@ pyclone : $(foreach sample,$(TUMOR_SAMPLES),pyclone/$(sample)/$(sample).vcf) \
 	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/$(set).tsv) \
 	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/$(set).hd5) \
 	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/$(set).txt)
-#	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/$(set)_CCF_PSP.pdf) \
-#	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/$(set)_CCF_HM.pdf)
+#	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/$(set)__PS__.pdf) \
+#	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set)/$(set)__HM__.pdf)
 
 
 define r-sufam
@@ -79,7 +79,7 @@ pyclone/$1/$1.txt : pyclone/$1/$1.hd5
 							   --in-file $$(<) \
 							   --out-file $$(@)")
 							     
-pyclone/$1/$1_CCF_PSP.pdf : pyclone/$1/$1.txt
+pyclone/$1/$1__CCF__.pdf : pyclone/$1/$1.txt
 	$$(call RUN,-c -n 1 -s 8G -m 12G -v $(PYCLONE_ENV),"set -o pipefail && \
 							   $(RSCRIPT) $(SCRIPTS_DIR)/pyclone.R \
 							   --option 2 \
