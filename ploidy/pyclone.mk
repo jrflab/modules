94d27c550a7b244516729388bc09d3fe44255ab1
David Brown <ndbrown6@gmail.com>
2022-11-01 22:27:30 -0400

    Update pyclone.mk

--- ploidy/pyclone.mk
+++ ploidy/pyclone.mk
@@ -7,12 +7,11 @@ SUFAM_OPTS = --mpileup-parameters='-A -q 15 -Q 15 -d 50000'
 
 pyclone : $(foreach sample,$(TUMOR_SAMPLES),pyclone/$(sample).vcf) \
 	  $(foreach sample,$(TUMOR_SAMPLES),pyclone/$(sample).txt) \
-	  $(foreach sample,$(TUMOR_SAMPLES),pyclone/$(sample).maf) \
 	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set).tsv) \
 	  $(foreach set,$(SAMPLE_SETS),pyclone/$(set).hd5)
 
 
-define run-sufam
+define r-sufam
 pyclone/$1.vcf : summary/tsv/all.tsv
 	$$(call RUN,-c -n 1 -s 4G -m 8G,"set -o pipefail && \
 					 $(RSCRIPT) $(SCRIPTS_DIR)/sufam_gt.R \
@@ -32,24 +31,11 @@ pyclone/$1.txt : pyclone/$1.vcf bam/$1.bam
 							 $$(<<) \
 							 > $$(@)")
 
-pyclone/$1.maf : pyclone/$1.vcf
-	$$(call RUN,-c -n 12 -s 1G -m 2G -v $(VEP_ENV),"set -o pipefail && \
-							$$(VCF2MAF) \
-							--input-vcf $$< \
-							--tumor-id $1 \
-							--filter-vcf $$(EXAC_NONTCGA) \
-							--ref-fasta $$(REF_FASTA) \
-							--vep-path $$(VEP_PATH) \
-							--vep-data $$(VEP_DATA) \
-							--tmp-dir `mktemp -d` \
-							--output-maf $$(@)")
-
-
 endef
 $(foreach sample,$(TUMOR_SAMPLES),\
-		$(eval $(call run-sufam,$(sample))))
+		$(eval $(call r-sufam,$(sample))))
 		
-define run-pyclone
+define r-pyclone
 pyclone/$1.tsv : $(foreach sample,$(TUMOR_SAMPLES),pyclone/$(sample).txt)
 	$$(call RUN,-c -n 1 -s 4G -m 8G -v $(PYCLONE_ENV),"set -o pipefail && \
 							   $(RSCRIPT) $(SCRIPTS_DIR)/pyclone.R \
@@ -66,13 +52,13 @@ pyclone/$1.hd5 : pyclone/$1.tsv
 									 --num-clusters 10 \
 									 --density beta-binomial \
 									 --num-grid-points 100 \
-									 --max-iters 10000 \
+									 --max-iters 1000000 \
 									 --mix-weight-prior 1 \
 									 --precision 200")
 
 endef
 $(foreach set,$(SAMPLE_SETS),\
-		$(eval $(call run-pyclone,$(set))))
+		$(eval $(call r-pyclone,$(set))))
 		
 ..DUMMY := $(shell mkdir -p version; \
 	     R --version > version/pyclone.txt)
