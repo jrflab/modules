5924ace289b30e6ffd52f08fcedd8fbf20941b6a
David Brown <ndbrown6@gmail.com>
2022-11-02 12:10:54 -0400

    Update pyclone.R

--- scripts/pyclone.R
+++ scripts/pyclone.R
@@ -5,6 +5,8 @@ suppressPackageStartupMessages(library("readr"))
 suppressPackageStartupMessages(library("dplyr"))
 suppressPackageStartupMessages(library("magrittr"))
 suppressPackageStartupMessages(library("fuzzyjoin"))
+suppressPackageStartupMessages(library("ggplot2"))
+suppressPackageStartupMessages(library("reshape2"))
 
 if (!interactive()) {
     options(warn = -1, error = quote({ traceback(); q('no', status = 1) }))
@@ -19,7 +21,7 @@ parser = OptionParser(usage = "%prog", option_list = optList)
 arguments = parse_args(parser, positional_arguments = T)
 opt = arguments$options
 
-if (as.numeric(opt$option)==1) {
+if (as.numeric(opt$option) == 1) {
 	sample_set = unlist(strsplit(x = as.character(opt$sample_set), split = "_"))
 	normal_sample = as.character(opt$normal_sample)
 	sample_set = setdiff(sample_set, normal_sample)
@@ -79,4 +81,55 @@ if (as.numeric(opt$option)==1) {
 		  dplyr::filter(!is.na(minor_cn))
 	
 	readr::write_tsv(x = pyclone, file = opt$output_file, append = FALSE, col_names = TRUE)
+	
+} else if (as.numeric(opt$option) == 2) {
+	sample_set = unlist(strsplit(x = as.character(opt$sample_set), split = " "))
+	pyclone = readr::read_tsv(file = as.character(opt$input_file), col_names = TRUE, col_types = cols(.default = col_character())) %>%
+		  readr::type_convert()
+	
+	pyclone_ft = list()
+	index = 1
+	for (i in 1:(length(sample_set)-1)) {
+		for (j in 2:length(sample_set)) {
+			pyclone_ft[[index]] = pyclone %>%
+					      dplyr::filter(sample_id == sample_set[i]) %>%
+					      dplyr::rename(sample_id_x = sample_id,
+							    cellular_prevalence_x = cellular_prevalence,
+							    cellular_prevalence_std_x = cellular_prevalence_std) %>%
+					      dplyr::full_join(pyclone %>%
+							       dplyr::filter(sample_id == sample_set[j]) %>%
+							       dplyr::rename(sample_id_y = sample_id,
+									     cellular_prevalence_y = cellular_prevalence,
+									     cellular_prevalence_std_y = cellular_prevalence_std)) %>%
+					      readr::type_convert()
+			index = index + 1
+		}
+	}
+	pyclone_ft = do.call(bind_rows, pyclone_ft) %>%
+		     dplyr::filter(cellular_prevalence_x > 0 & cellular_prevalence_y > 0)
+	smry_c = pyclone_ft %>%
+		 dplyr::group_by(mutation_id) %>%
+		 dplyr::summarize(cluster_id = unique(cluster_id)) %>%
+		 dplyr::ungroup() %>%
+		 dplyr::group_by(cluster_id) %>%
+		 dplyr::summarize(n = n())
+	pyclone_ft = pyclone_ft %>%
+		     dplyr::left_join(smry_c, by = "cluster_id")
+	
+	plot_ = pyclone_ft %>%
+		ggplot(aes(x = 100*cellular_prevalence_x, y = 100*cellular_prevalence_y, color = factor(cluster_id), size = n)) +
+		geom_point(stat = "identity", alpha = .75, shape = 21) +
+		scale_color_brewer(type = "qual", palette = 6) +
+		xlab("\n\nCCF (%)\n") +
+		ylab("\nCCF (%)\n\n") +
+		guides(color = guide_legend(title = "Cluster"),
+		       size = guide_legend(title = "N")) +
+		facet_wrap(sample_id_x~sample_id_y)
+	pdf(file = as.character(opt$output_file), width = 21, height = 21)
+	print(plot_)
+	dev.off()
+		
+		
+	
+	
 }
