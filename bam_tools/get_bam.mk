f9015d2a6d365c5a591e252dd9496a120aeb205e
David Brown <ndbrown6@gmail.com>
2022-11-03 22:15:29 -0400

    Update get_bam.mk

--- bam_tools/get_bam.mk
+++ bam_tools/get_bam.mk
@@ -7,7 +7,9 @@ get_bam : $(foreach sample,$(SAMPLES),bam/$(sample).bam)
 define get-bam
 bam/$1.bam :
 	$$(call RUN,-c -n 1 -s 2G -m 4G, "set -o pipefail && \
-					  scp $(USER)@selene.mskcc.org:/res/dmpcollab/dmpshare/share/irb12_245/$(cut -c 1-1 $1)/$(cut -c 2-2 $1)/$1.bam \
+					  I=$(cut -c 1-1 $1) && \
+					  J=$(cut -c 2-2 $1) && \
+					  scp $(USER)@selene.mskcc.org:/res/dmpcollab/dmpshare/share/irb12_245/$$(I)/$$(J)/$1.bam \
 					  bam/")
 
 endef
