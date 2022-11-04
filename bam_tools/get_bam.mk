d08a1dfd44f6493d6db150e02ba063219faaa8c7
David Brown <ndbrown6@gmail.com>
2022-11-03 22:29:58 -0400

    Update get_bam.mk

--- bam_tools/get_bam.mk
+++ bam_tools/get_bam.mk
@@ -9,7 +9,7 @@ bam/$1.bam :
 	$$(call RUN,-c -n 1 -s 2G -m 4G, "set -o pipefail && \
 					  I=`echo "$1" | cut -c 1-1`; \
 					  J=`echo "$1" | cut -c 2-2`; \
-					  scp $(USER)@selene.mskcc.org:/res/dmpcollab/dmpshare/share/irb12_245/\"$I\"/\"$J\"/$1.bam \
+					  scp $(USER)@selene.mskcc.org:/res/dmpcollab/dmpshare/share/irb12_245/\"$${I}\"/\"$${J}\"/$1.bam \
 					  bam/")
 
 endef
