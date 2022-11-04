1acadbb87e6eb3996b9c1e2119cf694766799ab1
David Brown <ndbrown6@gmail.com>
2022-11-03 22:39:06 -0400

    Update get_bam.mk

--- bam_tools/get_bam.mk
+++ bam_tools/get_bam.mk
@@ -2,13 +2,23 @@ include modules/Makefile.inc
 
 LOGDIR = log/get_bam.$(NOW)
 
-get_bam : $(foreach sample,$(SAMPLES),bam/$(sample).bam)
+get_bam : $(foreach sample,$(SAMPLES),bam/$(sample).bam) \
+	  $(foreach sample,$(SAMPLES),bam/$(sample).bam.bai)
 
 define get-bam
 bam/$1.bam :
 	$$(call RUN,-c -n 1 -s 2G -m 4G, "set -o pipefail && \
 					  scp $(USER)@selene.mskcc.org:/res/dmpcollab/dmpshare/share/irb12_245/`echo $1 | cut -c 1-1`/`echo $1 | cut -c 2-2`/$1.bam \
 					  bam/")
+					  
+bam/$1.bam.bai : bam/$1.bam
+	$$(call RUN,-c -n 1 -s 2G -m 4G, "set -o pipefail && \
+					  $(SAMTOOLS) index $$(<)")
+					  
+bam/$1.bai : bam/$1.bam bam/$1/bam.bai
+	$$(call RUN,-c -n 1 -s 2G -m 4G, "set -o pipefail && \
+					  cp $$(<<) $$(@)")
+
 
 endef
  $(foreach sample,$(SAMPLES),\
