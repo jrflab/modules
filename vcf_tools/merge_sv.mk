900389978333ee453d5cdcf83007bc43c77f6868
David Brown <ndbrown6@gmail.com>
2022-11-05 15:21:37 -0400

    Update merge_sv.mk

--- vcf_tools/merge_sv.mk
+++ vcf_tools/merge_sv.mk
@@ -10,22 +10,17 @@ STRAND = 1
 MIN_SIZE = 30
 
 merge_sv :  $(foreach pair,$(SAMPLE_PAIRS),merge_sv/$(pair)/samples.txt)
-#	    $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).merged_sv.vcf)
+	    $(foreach pair,$(SAMPLE_PAIRS),merge_sv/$(pair)/$(pair).merged_sv.vcf)
 	   
 define merge-sv
 merge_sv/$1_$2/samples.txt : $(foreach caller,$(SV_CALLERS),vcf/$1_$2.$(caller)_sv.vcf)
 	mkdir -p merge_sv/$1_$2 && \
 	$(foreach caller,$(SV_CALLERS),echo vcf/$1_$2.$(caller)_sv.vcf >> $$(@);)
 
-#merge_sv/$1_$2/samples.txt : $(foreach caller,$(SV_CALLERS),vcf/$1_$2.$(caller)_sv.vcf)
-#	mkdir -p merge_sv/$1_$2
-#	echo vcf/$1_$2.svaba_sv.vcf > $$(@)
-#	echo vcf/$1_$2.manta_sv.vcf >> $$(@)
-#	
-#vcf/$1_$2.merged_sv.vcf : merge_sv/$1_$2/sample_list_sv.txt
-#	$$(call RUN,-c -n 1 -s 4G -m 8G -v $(SURVIVOR_ENV),"set -o pipefail && \
-#							    SURVIVOR merge $$(<) \
-#							    $(MAX_DIST) $(NUM_CALLERS) $(TYPE) $(STRAND) 0 $(MIN_SIZE) $$(@)")
+merge_sv/$1_$2/$1_$2.merged_sv.vcf : merge_sv/$1_$2/samples.txt
+	$$(call RUN,-c -n 1 -s 4G -m 8G -v $(SURVIVOR_ENV),"set -o pipefail && \
+							    SURVIVOR merge $$(<) \
+							    $(MAX_DIST) $(NUM_CALLERS) $(TYPE) $(STRAND) 0 $(MIN_SIZE) $$(@)")
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
