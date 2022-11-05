3730bcdd57a14a32bb59651471b284ac6d6a2859
David Brown <ndbrown6@gmail.com>
2022-11-05 14:47:47 -0400

    Update gridss_tumor_normal.mk

--- sv_callers/gridss_tumor_normal.mk
+++ sv_callers/gridss_tumor_normal.mk
@@ -11,7 +11,8 @@ GRIDSS_FILTER ?= gridss_somatic_filter
 GRIDSS_PON_DIR ?= $(HOME)/share/lib/resource_files/gridss/pon/
 
 gridss : $(foreach pair,$(SAMPLE_PAIRS),gridss/$(pair)/$(pair).gridss_sv.vcf) \
-	 $(foreach pair,$(SAMPLE_PAIRS),gridss/$(pair)/$(pair).gridss_sv_ft.vcf)
+	 $(foreach pair,$(SAMPLE_PAIRS),gridss/$(pair)/$(pair).gridss_sv_ft.vcf.bgz) \
+	 $(foreach pair,$(SAMPLE_PAIRS),vcf/$(pair).gridss_sv.vcf)
 
 define gridss-tumor-normal
 gridss/$1_$2/$1_$2.gridss_sv.vcf : bam/$1.bam bam/$2.bam
@@ -26,27 +27,19 @@ gridss/$1_$2/$1_$2.gridss_sv.vcf : bam/$1.bam bam/$2.bam
 												    ../../bam/$2.bam \
 												    ../../bam/$1.bam")
 												    
-gridss/$1_$2/$1_$2.gridss_sv_ft.vcf : gridss/$1_$2/$1_$2.gridss_sv.vcf
-	$$(call RUN,-c -n $(GRIDSS_CORES) -s 4G -m $(GRIDSS_MEM_CORE) -v $(GRIDSS_ENV),"set -o pipefail && \
-											cd gridss/$1_$2 && \
-											$$(GRIDSS_FILTER) \
-											--pondir $$(GRIDSS_PON_DIR) \
-											--ref $$(GRIDSS_REF) \
-											--input $1_$2.gridss_sv.vcf \
-											--output $1_$2.gridss_sv_ft.vcf.gz \
-											--fulloutput $1_$2.gridss_sv_high_and_low_confidence_somatic.vcf.gz \
-											--scriptdir '$(GRIDSS_ENV)/bin' \
-											-n 1 \
-											-t 2")
-												    
+gridss/$1_$2/$1_$2.gridss_sv_ft.vcf.bgz : gridss/$1_$2/$1_$2.gridss_sv.vcf
+	$$(call RUN,-c -n 1 -s 12G -m 18G -v $(GRIDSS_ENV),"set -o pipefail && \
+							    cd gridss/$1_$2 && \
+							    $$(GRIDSS_FILTER) \
+							    --pondir $$(GRIDSS_PON_DIR) \
+							    --input $1_$2.gridss_sv.vcf \
+							    --output $1_$2.gridss_sv_ft.vcf.bgz \
+							    --fulloutput $1_$2.gridss_sv_high_and_low_confidence_somatic.vcf.bgz \
+							    -n 1 \
+							    -t 2")
 
-
-#svaba/$1_$2.svaba.somatic.sv.vcf : svaba/$1_$2.svaba.somatic.indel.vcf
-
-#svaba/$1_$2.svaba.unfiltered.somatic.sv.vcf : svaba/$1_$2.svaba.somatic.indel.vcf
-
-#vcf/$1_$2.svaba_sv.vcf : svaba/$1_$2.svaba.somatic.sv.vcf
-#	$$(INIT) cat $$< > $$@
+vcf/$1_$2.gridss_sv.vcf : gridss/$1_$2/$1_$2.gridss_sv_ft.vcf.bgz
+	$$(INIT) zcat $$(<) > $$(@)
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
