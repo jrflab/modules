f7e03297a4f2f605885b5d4f1766d630930955e0
David Brown <ndbrown6@gmail.com>
2022-11-04 23:26:25 -0400

    Update pyclone_13.R

--- scripts/pyclone_13.R
+++ scripts/pyclone_13.R
@@ -233,7 +233,7 @@ if (as.numeric(opt$option) == 1) {
 	
 	smry_cl = pyclone %>%
 		  dplyr::group_by(cluster_id) %>%
-		  dplyr::summarize(mean = sum(cellular_prevalence)) %>%
+		  dplyr::summarize(mean = mean(cellular_prevalence)) %>%
 		  dplyr::ungroup() %>%
 		  dplyr::arrange(desc(mean)) %>%
 		  dplyr::mutate(cluster_id_ordered = nrow(.):1)
@@ -257,7 +257,7 @@ if (as.numeric(opt$option) == 1) {
 		  col.dendrogram = FALSE,
 		  smooth.heat = FALSE,
 		  scale = FALSE,
-		  heat.pal = c(rep("#d9d9d9", 10), "#9ecae1", "#4292c6", "#2171b5", "#08519c", "#08306b"),
+		  heat.pal = c(rep("#d9d9d9", 6), "#9ecae1", "#4292c6", "#2171b5", "#08519c", "#08306b"),
 		  legend = FALSE,
 		  grid.hline = FALSE,
 		  grid.vline = TRUE,
