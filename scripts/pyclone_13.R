e31ee912ae1727d47ed2e8ecda972a83ece79db3
David Brown <ndbrown6@gmail.com>
2022-11-04 21:15:26 -0400

    Update pyclone_13.R

--- scripts/pyclone_13.R
+++ scripts/pyclone_13.R
@@ -106,7 +106,7 @@ if (as.numeric(opt$option) == 1) {
 			      readr::type_convert() %>%
 			      .[["X1"]]
 	}
-	cat(paste0("num_iters: ", as.numeric(opt$num_iter), "\n\n", file = as.character(opt$output_file), append = FALSE)
+	cat(paste0("num_iters: ", as.numeric(opt$num_iter), "\n\n"), file = as.character(opt$output_file), append = FALSE)
 	cat("base_measure_params:\n", file = as.character(opt$output_file), append = TRUE)
 	cat("  alpha: 1\n", file = as.character(opt$output_file), append = TRUE)
 	cat("  beta: 1\n", file = as.character(opt$output_file), append = TRUE)
