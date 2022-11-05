fc4da0af613e35b34bdf4c7d9d9d0fd2b6cbcd1e
David Brown <ndbrown6@gmail.com>
2022-11-05 15:18:29 -0400

    Update merge_sv.mk

--- vcf_tools/merge_sv.mk
+++ vcf_tools/merge_sv.mk
@@ -15,9 +15,7 @@ merge_sv :  $(foreach pair,$(SAMPLE_PAIRS),merge_sv/$(pair)/samples.txt)
 define merge-sv
 merge_sv/$1_$2/samples.txt : $(foreach caller,$(SV_CALLERS),vcf/$1_$2.$(caller)_sv.vcf)
 	mkdir -p merge_sv/$1_$2 && \
-	for caller in $$(SV_CALLERS); do \
-		echo vcf/$1_$2.$(caller)_sv.vcf > $$(@); \
-	done
+	$(foreach caller,$(SV_CALLERS),echo vcf/$1_$2.$(caller)_sv.vcf > $$(@);)
 
 #merge_sv/$1_$2/samples.txt : $(foreach caller,$(SV_CALLERS),vcf/$1_$2.$(caller)_sv.vcf)
 #	mkdir -p merge_sv/$1_$2
