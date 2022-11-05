8b8ef1c1e4a094f02b1fe7bb27f2d8eee9856419
David Brown <ndbrown6@gmail.com>
2022-11-05 17:58:49 -0400

    Update annotate_sv.mk

--- vcf_tools/annotate_sv.mk
+++ vcf_tools/annotate_sv.mk
@@ -7,6 +7,8 @@ ANNOTATE_SV ?= $(HOME)/share/usr/env/annot_sv-3.1.3/opt/AnnotSV/bin/AnnotSV
 
 annotate_sv :  $(foreach pair,$(SAMPLE_PAIRS), \
 			$(foreach caller,$(SV_CALLERS),annotate_sv/$(pair)/$(pair).$(caller)_sv.tsv))
+	       $(foreach pair,$(SAMPLE_PAIRS), \
+			$(foreach caller,$(SV_CALLERS),annotate_sv/$(pair)/$(pair).$(caller)_sv.maf))
 			
 define annotate-sv
 annotate_sv/$1/$2/$1.$2_sv.tsv : vcf/$1.$2_sv.vcf
@@ -19,6 +21,22 @@ annotate_sv/$1/$2/$1.$2_sv.tsv : vcf/$1.$2_sv.vcf
 							       
 annotate_sv/$1/$1.$2_sv.tsv : annotate_sv/$1/$2/$1.$2_sv.tsv
 	$$(INIT) cat $$(<) > $$(@)
+	
+annotate_sv/$1/$1.$2_sv.maf : vcf/$1.$2_sv.vcf
+	$$(call RUN,-c -n 12 -s 1G -m 2G -v $(VEP_ENV),"set -o pipefail && \
+							$$(VCF2MAF) \
+							--input-vcf $$(<) \
+							--tumor-id $1 \
+							--filter-vcf $$(EXAC_NONTCGA) \
+							--ref-fasta $$(REF_FASTA) \
+							--vep-path $$(VEP_PATH) \
+							--vep-data $$(VEP_DATA) \
+							--tmp-dir `mktemp -d` \
+							--output-maf $$(@)")
+
+endef
+$(foreach sample,$(TUMOR_SAMPLES),\
+		$(eval $(call r-sufam,$(sample))))
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
