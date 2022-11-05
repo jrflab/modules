d7b99f4d2a28ff7bdc2fcdbc37b71f8d0e697404
David Brown <ndbrown6@gmail.com>
2022-11-05 14:28:55 -0400

    Update gridss_tumor_normal.mk

--- sv_callers/gridss_tumor_normal.mk
+++ sv_callers/gridss_tumor_normal.mk
@@ -30,12 +30,12 @@ gridss/$1_$2/$1_$2.gridss_sv_ft.vcf : gridss/$1_$2/$1_$2.gridss_sv.vcf
 	$$(call RUN,-c -n $(GRIDSS_CORES) -s 4G -m $(GRIDSS_MEM_CORE) -v $(GRIDSS_ENV),"set -o pipefail && \
 											cd gridss/$1_$2 && \
 											$$(GRIDSS_FILTER) \
-											--pondir  $$(GRIDSS_PON_DIR) \
+											--pondir $$(GRIDSS_PON_DIR) \
 											--ref $$(GRIDSS_REF) \
-											--input $1_$2.gridss.sv.vcf \
+											--input $1_$2.gridss_sv.vcf \
 											--output $1_$2.gridss_sv_ft.vcf.gz \
 											--fulloutput $1_$2.gridss_sv_high_and_low_confidence_somatic.vcf.gz \
-											--scriptdir $$(GRIDSS_ENV)/bin \
+											--scriptdir '$$(GRIDSS_ENV)/bin' \
 											-n 1 \
 											-t 2")
 												    
