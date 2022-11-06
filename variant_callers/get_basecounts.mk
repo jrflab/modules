84bc191ccac122d551d9bf40f50e62e68a83d5b0
David Brown <ndbrown6@gmail.com>
2022-11-06 17:16:20 -0500

    ++

--- variant_callers/get_basecounts.mk
+++ variant_callers/get_basecounts.mk
@@ -9,32 +9,31 @@ COV := 0
 getbasecount : $(foreach sample,$(SAMPLES),gbc/$(sample).txt.gz)
 
 define get-basecount
-gbc/$1.txt.gz : bam/$1.bam
-	$$(call RUN,-n 6 -s 3G -m 6G -v $(GBC_ENV),"set -o pipefail && \
-				      		    mkdir -p gbc/MFE296 && \
-						    $(GBC_EXE) --fasta ~/share/reference/ucsc_gatk_bundle_2.8/ucsc.hg19.fasta \
-						    --bam $$(<) \
-						    --vcf etc/vcf/MFE296.vcf \
-						    --output $$(@) \
-						    --maq $(MAPQ) \
-						    --baq $(BAQ) \
-						    --filter_duplicate 0 \
-						    --filter_improper_pair 0 \
-						    --filter_qc_failed 1 \
-						    --thread 6")
+gbc/$1.txt.gz : bam/$1.bam vcf/dataSilentNoPoleNotTertPromot.vcf
+	$$(call RUN,-n 6 -s 3G -m 6G,"set -o pipefail && \
+				      $(GBC) --fasta $(REF_FASTA) \
+				      --bam $$(<) \
+				      --vcf $$(<<) \
+				      --output $$(@) \
+				      --thread 6 \
+				      --maq $(MAPQ) \
+				      -sort_output \
+				      --compress_output \
+				      --maq $(MAPQ) \
+				      --baq $(BAQ) \
+				      --cov $(COV) \
+				      --filter_duplicate 0 \
+				      --filter_improper_pair 0 \
+				      --filter_qc_failed 1 \
+				      --filter_indel 0 \
+				      --filter_non_primary 1")
 						    
-gbc/MFE296/$1.tsv : gbc/MFE296/$1.txt
-	$$(call RUN,-n 1 -s 12G -m 18G,"set -o pipefail && \
-					$(RSCRIPT) modules/variant_callers/getBaseCount.R --file_name $$(<) && \
-					rm $$(<)")
-
-
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call get-basecount,$(sample))))
 
 ..DUMMY := $(shell mkdir -p version; \
-	     /lila/home/brownd7/share/data/common/eec_sc_split/etc/GetBaseCounts/GetBaseCounts &> version/get_basecount.txt;)
+	     ${GBC} &> version/get_basecount.txt;)
 .SECONDARY:
 .DELETE_ON_ERROR:
 .PHONY: getbasecount
