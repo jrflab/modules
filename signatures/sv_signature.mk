582e21dd38fcb7d6998d1fdaf1eb40b8c3c478ee
David Brown <ndbrown6@gmail.com>
2022-11-13 15:13:00 -0500

    Update sv_signature.mk

--- signatures/sv_signature.mk
+++ signatures/sv_signature.mk
@@ -5,7 +5,8 @@ LOGDIR ?= log/sv_signature.$(NOW)
 MIN_SIZE = 1
 MAX_SIZE = 10000000000000000
 
-signature_sv :  $(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/$(pair).manta.bed)
+signature_sv :  $(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/$(pair).manta.bed) \
+		$(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/$(pair).manta.bedpe)
 	   
 define signature-sv
 sv_signature/$1_$2/$1_$2.manta.bed : vcf/$1_$2.manta_sv.vcf
@@ -15,6 +16,12 @@ sv_signature/$1_$2/$1_$2.manta.bed : vcf/$1_$2.manta_sv.vcf
 							    $(MIN_SIZE) \
 							    $(MAX_SIZE) \
 							    $$(@)")
+							    
+sv_signature/$1_$2/$1_$2.manta.bedpe : sv_signature/$1_$2/$1_$2.manta.bed
+	$$(call RUN,-c -n 1 -s 4G -m 8G,"set -o pipefail && \
+					 echo 'chrom1\tstart1\tend1\tchrom2\tstart2\tend2\tsv_id\tpe_support\tstrand1\tstrand2\tsvclass\n' > \
+					 $$(@) && \
+					 cat $$(<) >> $$(@)")
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
