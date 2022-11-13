3a7f7ea08bee6dd48bee04604d17c47dcf9b8421
David Brown <ndbrown6@gmail.com>
2022-11-13 15:05:15 -0500

    Update sv_signature.mk

--- signatures/sv_signature.mk
+++ signatures/sv_signature.mk
@@ -5,7 +5,7 @@ LOGDIR ?= log/sv_signature.$(NOW)
 MIN_SIZE = 1
 MAX_SIZE = 10000000000000000
 
-signature_sv :  $(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/samples.bed)
+signature_sv :  $(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/$(pair).manta.bed)
 	   
 define signature-sv
 signature_sv/$1_$2/$1_$2.manta.bed : vcf/$1_$2.manta_sv.vcf
