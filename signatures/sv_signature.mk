3f3b525a6ff1d81b59a9eb19a82b1626148f6b21
David Brown <ndbrown6@gmail.com>
2022-11-13 15:21:07 -0500

    Update sv_signature.mk

--- signatures/sv_signature.mk
+++ signatures/sv_signature.mk
@@ -5,11 +5,11 @@ LOGDIR ?= log/sv_signature.$(NOW)
 MIN_SIZE = 1
 MAX_SIZE = 10000000000000000
 
-signature_sv :  $(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/$(pair).manta.bed) \
-		$(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/$(pair).manta.bedpe)
+signature_sv :  $(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/$(pair).merged.bed) \
+		$(foreach pair,$(SAMPLE_PAIRS),sv_signature/$(pair)/$(pair).merged.bedpe)
 	   
 define signature-sv
-sv_signature/$1_$2/$1_$2.manta.bed : vcf/$1_$2.manta_sv.vcf
+sv_signature/$1_$2/$1_$2.merged.bed : vcf/$1_$2.merged_sv.vcf
 	$$(call RUN,-c -n 1 -s 4G -m 8G -v $(SURVIVOR_ENV),"set -o pipefail && \
 							    SURVIVOR vcftobed \
 							    $$(<) \
@@ -17,7 +17,7 @@ sv_signature/$1_$2/$1_$2.manta.bed : vcf/$1_$2.manta_sv.vcf
 							    $(MAX_SIZE) \
 							    $$(@)")
 							    
-sv_signature/$1_$2/$1_$2.manta.bedpe : sv_signature/$1_$2/$1_$2.manta.bed
+sv_signature/$1_$2/$1_$2.merged.bedpe : sv_signature/$1_$2/$1_$2.merged.bed
 	$$(call RUN,-c -n 1 -s 4G -m 8G,"set -o pipefail && \
 					 echo \"chrom1	start1	end1	chrom2	start2	end2	sv_id	pe_support	strand1	strand2	svclass\" > \
 					 $$(@) && \
