2fca098ceb6da23c8b4c215d06581c377b49ccde
David Brown <ndbrown6@gmail.com>
2022-12-10 17:02:08 -0500

    ++

--- copy_number/medicc2.mk
+++ copy_number/medicc2.mk
@@ -10,7 +10,7 @@ medicc : $(foreach sample,$(TUMOR_SAMPLES),medicc2/$(sample)/$(sample).txt) \
 define collect-copy-number
 medicc2/$1/$1.txt : facets/cncf/$1_$2.Rdata
 	$$(call RUN,-c -n 1 -s 1G -m 2G -v $(MEDICC_ENV),"set -o pipefail && \
-							  $(RSCRIPT) modules/copy_number/medicc2.R \
+							  $(RSCRIPT) $(SCRIPTS_DIR)/medicc2.R \
 							  --option 1 \
 							  --tumor_sample_name $1 \
 							  --normal_sample_name $2 \
@@ -25,7 +25,7 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 define aggregate-copy-number
 medicc2/$1/$1.txt : $(foreach sample,$(TUMOR_SAMPLES),medicc2/$(sample)/$(sample).txt)
 	$$(call RUN,-c -n 1 -s 2G -m 4G -v $(MEDICC_ENV),"set -o pipefail && \
-							  $(RSCRIPT) modules/copy_number/medicc2.R \
+							  $(RSCRIPT) $(SCRIPTS_DIR)/medicc2.R \
 							  --option 2 \
 							  --tumor_sample_name '$(tumors.$1)' \
 							  --normal_sample_name '$(normal.$1)' \
@@ -33,7 +33,7 @@ medicc2/$1/$1.txt : $(foreach sample,$(TUMOR_SAMPLES),medicc2/$(sample)/$(sample
 
 medicc2/$1/$1.tsv : medicc2/$1/$1.txt
 	$$(call RUN,-c -n 1 -s 2G -m 4G -v $(MEDICC_ENV),"set -o pipefail && \
-							  $(RSCRIPT) modules/copy_number/medicc2.R \
+							  $(RSCRIPT) $(SCRIPTS_DIR)/medicc2.R \
 							  --option 3 \
 							  --tumor_sample_name '$(tumors.$1)' \
 							  --normal_sample_name '$(normal.$1)' \
@@ -54,7 +54,7 @@ medicc2/$1/$1_summary.tsv : medicc2/$1/$1.tsv
 							  --input-type tsv \
 							  --normal-name diploid \
 							  --total-copy-numbers \
-							  --input-allele-columns 'cn_t' \
+							  --input-allele-columns 'nAB' \
 							  --plot both \
 							  --maxcn 8 \
 							  --n-cores 4")
