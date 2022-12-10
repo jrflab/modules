6c70622c401e4c8eb45718256b890a07158bfad3
David Brown <ndbrown6@gmail.com>
2022-12-09 20:18:01 -0500

    Update medicc2.mk

--- copy_number/medicc2.mk
+++ copy_number/medicc2.mk
@@ -3,7 +3,8 @@ include modules/Makefile.inc
 LOGDIR ?= log/medicc2.$(NOW)
 
 medicc : $(foreach sample,$(TUMOR_SAMPLES),medicc2/$(sample)/$(sample).txt) \
-	 $(foreach set,$(SAMPLE_SETS),medicc2/$(set)/$(set).txt)
+	 $(foreach set,$(SAMPLE_SETS),medicc2/$(set)/$(set).txt) \
+	 $(foreach set,$(SAMPLE_SETS),medicc2/$(set)/$(set).tsv)
 
 define collect-copy-number
 medicc2/$1/$1.txt : facets/cncf/$1_$2.Rdata
@@ -29,6 +30,14 @@ medicc2/$1/$1.txt : $(foreach sample,$(TUMOR_SAMPLES),medicc2/$(sample)/$(sample
 							  --normal_sample_name '$(normal.$1)' \
 							  --file_out $$(@)")
 
+medicc2/$1/$1.tsv : medicc2/$1/$1.txt $(foreach sample,$(TUMOR_SAMPLES),facets/$(sample)/$(sample).txt)
+	$$(call RUN,-c -n 1 -s 2G -m 4G -v $(MEDICC_ENV),"set -o pipefail && \
+							  $(RSCRIPT) modules/copy_number/medicc2.R \
+							  --option 3 \
+							  --tumor_sample_name '$(tumors.$1)' \
+							  --normal_sample_name '$(normal.$1)' \
+							  --file_in $$(<) \
+							  --file_out $$(@)")
 
 endef
 $(foreach set,$(SAMPLE_SETS),\
