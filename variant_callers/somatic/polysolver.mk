302cb8d6953cfef9ced7b11f1f4b04167f4b88b3
David Brown <ndbrown6@gmail.com>
2023-05-19 18:30:31 -0400

    ++

--- variant_callers/somatic/polysolver.mk
+++ variant_callers/somatic/polysolver.mk
@@ -1,44 +1,64 @@
 include modules/Makefile.inc
 
 LOGDIR ?= log/hla_polysolver.$(NOW)
-PHONY += hla_polysolver hla_polysolver/summary
 
-hla_polysolver : $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/$(pair).taskcomplete) hla_polysolver/summary/genotype_summary.txt
+
+hla_polysolver : $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/winners.hla.txt) \
+		 $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/hla.intervals) \
+		 $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/$(pair).mutect.unfiltered.annotated) \
+		 $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/$(pair).strelka_indels.unfiltered.annotated) \
+		 hla_polysolver/summary/genotype_summary.txt
 
 define hla-polysolver
 hla_polysolver/$1_$2/winners.hla.txt : bam/$1.bam bam/$2.bam
-	$$(call RUN,-n 8 -s 2G -m 4G, "source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate \
-								   /home/${USER}/share/usr/anaconda-envs/hla-polysolver && \
-								   export CONDA_PREFIX=/home/${USER}/share/usr/anaconda-envs/hla-polysolver && \
-								   export PERL5LIB=/home/${USER}/share/usr/anaconda-envs/hla-polysolver/lib/perl5/5.22.0 && \
-								   if [ ! -d hla_polysolver/$1_$2 ]; then mkdir hla_polysolver/$1_$2; fi && \
-								   shell_call_hla_type bam/$2.bam Unknown 1 hg19 STDFQ 0 hla_polysolver/$1_$2")
+	$$(call RUN,-c -n 8 -s 2G -m 4G -v $(POLYSOLVER_ENV) -w 24:00:00, "set -o pipefail && \
+									   export CONDA_PREFIX=$$(POLYSOLVER_ENV) && \
+									   export PERL5LIB=$$(POLYSOLVER_ENV)/lib/perl5/5.22.0 && \
+									   shell_call_hla_type \
+									   -bam $$(<<) \
+									   -race Unknown \
+									   -includeFreq 1 \
+									   -build hg19 \
+									   -format STDFQ \
+									   -insertCalc 1 \
+									   -outDir hla_polysolver/$1_$2")
 								 	  
-hla_polysolver/$1_$2/hla.intervals : hla_polysolver/$1_$2/winners.hla.txt
-	$$(call RUN,-n 8 -s 2G -m 4G, "source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate \
-								   /home/${USER}/share/usr/anaconda-envs/hla-polysolver && \
-								   export CONDA_PREFIX=/home/${USER}/share/usr/anaconda-envs/hla-polysolver && \
-								   export PERL5LIB=/home/${USER}/share/usr/anaconda-envs/hla-polysolver/lib/perl5/5.22.0 && \
-								   shell_call_hla_mutations_from_type bam/$2.bam bam/$1.bam hla_polysolver/$1_$2/winners.hla.txt hg19 STDFQ hla_polysolver/$1_$2")
+hla_polysolver/$1_$2/hla.intervals : bam/$1.bam bam/$2.bam hla_polysolver/$1_$2/winners.hla.txt
+	$$(call RUN,-c -n 8 -s 2G -m 4G -v $(POLYSOLVER_ENV) -w 24:00:00, "set -o pipefail && \
+									   export CONDA_PREFIX=$$(POLYSOLVER_ENV) && \
+									   export PERL5LIB=$$(POLYSOLVER_ENV)/lib/perl5/5.22.0 && \
+									   shell_call_hla_mutations_from_type \
+									   -normal_bam_hla $$(<<) \
+									   -tumor_bam_hla $$(<) \
+									   -hla $$(<<<) \
+									   -build hg19 \
+									   -format STDFQ \
+									   -outDir hla_polysolver/$1_$2")
 								 	 
-hla_polysolver/$1_$2/$1_$2.mutect.unfiltered.annotated hla_polysolver/$1_$2/$1_$2.strelka_indels.unfiltered.annotated : hla_polysolver/$1_$2/hla.intervals
-	$$(call RUN,-n 8 -s 2G -m 4G, "source /home/${USER}/share/usr/anaconda-envs/jrflab-modules-0.1.5/bin/activate \
-								   /home/${USER}/share/usr/anaconda-envs/hla-polysolver && \
-								   export CONDA_PREFIX=/home/${USER}/share/usr/anaconda-envs/hla-polysolver && \
-								   export PERL5LIB=/home/${USER}/share/usr/anaconda-envs/hla-polysolver/lib/perl5/5.22.0 && \
-								   shell_annotate_hla_mutations $1_$2 hla_polysolver/$1_$2")
+hla_polysolver/$1_$2/$1_$2.mutect.unfiltered.annotated : hla_polysolver/$1_$2/hla.intervals
+	$$(call RUN,-c -n 8 -s 2G -m 4G -v $(POLYSOLVER_ENV) -w 24:00:00, "set -o pipefail && \
+									   export CONDA_PREFIX=$$(POLYSOLVER_ENV) && \
+									   export PERL5LIB=$$(POLYSOLVER_ENV)/lib/perl5/5.22.0 && \
+									   shell_annotate_hla_mutations \
+									   -indiv $1_$2 \
+									   -dir hla_polysolver/$1_$2")
 
-hla_polysolver/$1_$2/$1_$2.taskcomplete : hla_polysolver/$1_$2/$1_$2.mutect.unfiltered.annotated hla_polysolver/$1_$2/$1_$2.strelka_indels.unfiltered.annotated
-	$$(call RUN,-n 1 -s 1G -m 1G,"touch hla_polysolver/$1_$2/$1_$2.taskcomplete")
+hla_polysolver/$1_$2/$1_$2.strelka_indels.unfiltered.annotated : hla_polysolver/$1_$2/$1_$2.mutect.unfiltered.annotated
+	
 
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
 		$(eval $(call hla-polysolver,$(tumor.$(pair)),$(normal.$(pair)))))
 		
-hla_polysolver/summary/genotype_summary.txt : $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/$(pair).taskcomplete)
-	$(call RUN,-c -s 12G -m 24G,"mkdir -p hla_polysolver/summary && \
-							 	 $(RSCRIPT) modules/variant_callers/somatic/hla_summary.R --sample_names '$(SAMPLE_PAIRS)'")
+hla_polysolver/summary/genotype_summary.txt : $(foreach pair,$(SAMPLE_PAIRS),$(pair).mutect.unfiltered.annotated) $(foreach pair,$(SAMPLE_PAIRS),hla_polysolver/$(pair)/$(pair).strelka_indels.unfiltered.annotated)
+	$(call RUN,-c -s 12G -m 24G,"set -o pipefail && \
+				     $(RSCRIPT) modules/variant_callers/somatic/hla_summary.R --sample_names '$(SAMPLE_PAIRS)'")
 
-.DELETE_ON_ERROR:
+
+..DUMMY := $(shell mkdir -p version; \
+	     $(POLYSOLVER_ENV)/shell_call_hla_type --help &> version/hla_polysolver.txt; \
+	     $(POLYSOLVER_ENV)/shell_call_hla_mutations_from_type --help &> version/hla_polysolver.txt; \
+	     $(POLYSOLVER_ENV)/shell_annotate_hla_mutations --help &> version/hla_polysolver.txt)
 .SECONDARY:
-.PHONY: $(PHONY)
+.DELETE_ON_ERROR:
+.PHONY: hla_polysolver
