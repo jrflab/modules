d3d7ec42d71526ae33623970be979633544a658a
David Brown <ndbrown6@gmail.com>
2023-10-19 19:21:47 -0400

    Update put_bam_data_mirror.mk

--- bam_tools/put_bam_data_mirror.mk
+++ bam_tools/put_bam_data_mirror.mk
@@ -11,7 +11,7 @@ bam/$1.taskcomplete : bam/$1.bam
 	$$(call RUN,-c -n 1 -s 2G -m 4G, "set -o pipefail && \
 					  rsync -aP -e ssh bam/$1.bam $(USER)@swan.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bam && \
 					  rsync -aP -e ssh bam/$1.bam.bai $(USER)@swan.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bam.bai && \
-					  rsync -aP -e ssh bam/$1.bai $(USER)@swan.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bam.bai && \
+					  rsync -aP -e ssh bam/$1.bam.bai $(USER)@swan.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bai && \
 					  echo 'finished!' > $$(@)")
 					  
 endef
