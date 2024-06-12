5ae0e5724ccd1a48b606b20440329eb073b5f218
David Brown <ndbrown6@gmail.com>
2024-06-12 10:10:05 -0400

    Update get_bam_irb_mirror.mk

--- bam_tools/get_bam_irb_mirror.mk
+++ bam_tools/get_bam_irb_mirror.mk
@@ -9,7 +9,7 @@ get_bam : $(foreach sample,$(SAMPLES),bam/$(sample).bam) \
 define get-bam
 bam/$1.bam :
 	$$(call RUN,-c -n 1 -s 2G -m 4G, "set -o pipefail && \
-					  scp $(USER)@selene.mskcc.org:/res/dmpcollab/dmpshare/share/irb12_245/`echo $1 | cut -c 1-1`/`echo $1 | cut -c 2-2`/$1.bam \
+					  scp $(USER)@juno-xfer01.mskcc.org:/juno/dmp/share/irb12_245/`echo $1 | cut -c 1-1`/`echo $1 | cut -c 2-2`/$1.bam \
 					  bam/")
 					  
 bam/$1.bam.bai : bam/$1.bam
