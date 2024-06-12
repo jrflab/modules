471c00c4062a8d6b573c8f00a505f522944f3788
David Brown <ndbrown6@gmail.com>
2024-06-12 10:11:12 -0400

    Update put_bam_data_mirror.mk

--- bam_tools/put_bam_data_mirror.mk
+++ bam_tools/put_bam_data_mirror.mk
@@ -9,9 +9,9 @@ PROJECT_NAME = $(shell basename $(PWD))
 define put-bam
 bam/$1.taskcomplete : bam/$1.bam
 	$$(call RUN,-c -n 1 -s 2G -m 4G, "set -o pipefail && \
-					  rsync -aP -e ssh bam/$1.bam $(USER)@swan.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bam && \
-					  rsync -aP -e ssh bam/$1.bam.bai $(USER)@swan.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bam.bai && \
-					  rsync -aP -e ssh bam/$1.bam.bai $(USER)@swan.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bai && \
+					  rsync -aP -e ssh bam/$1.bam $(USER)@lilac-xfer01.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bam && \
+					  rsync -aP -e ssh bam/$1.bam.bai $(USER)@lilac-xfer01.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bam.bai && \
+					  rsync -aP -e ssh bam/$1.bam.bai $(USER)@lilac-xfer01.mskcc.org:/oscar/warm/reis-filho/by_user/$(USER)/$(PROJECT_NAME)/$1.bai && \
 					  echo 'finished!' > $$(@)")
 					  
 endef
